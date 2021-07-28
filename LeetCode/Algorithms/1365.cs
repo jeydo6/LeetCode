191deed32913404bfb2fb19e41dffe534d5b6258
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-28 18:38:04 +0300

    1365

---
+++ LeetCode/Algorithms/1365.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1365
+	{
+		public static int[] SmallerNumbersThanCurrent(int[] numbers)
+		{
+			var result = new int[numbers.Length];
+			for (var i = 0; i < numbers.Length; i++)
+			{
+				for (var j = 0; j < numbers.Length; j++)
+				{
+					if (numbers[i] > numbers[j])
+					{
+						result[i]++;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
