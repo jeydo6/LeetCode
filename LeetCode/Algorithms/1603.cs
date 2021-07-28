4299cd65ca6e4ecc17f4493bc162b0e84449ab08
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-28 15:50:16 +0300

    1603

---
+++ LeetCode/Algorithms/1603.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1603
+	{
+		public class ParkingSystem
+		{
+			private readonly int[] _slots;
+
+			public ParkingSystem(int big, int medium, int small)
+			{
+				_slots = new int[3] { big, medium, small };
+			}
+
+			public bool AddCar(int carType)
+			{
+				if (_slots[carType - 1] > 0)
+				{
+					_slots[carType - 1]--;
+
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}
+}
