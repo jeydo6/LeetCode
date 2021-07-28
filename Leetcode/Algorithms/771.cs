f83bc04222db2ebd4b74afcbf5a64b7d9adc0cf1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-28 15:40:08 +0300

    771

--- Leetcode/Algorithms/771.cs
+++ Leetcode/Algorithms/771.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _771
-    {
-        public Int32 NumJewelsInStones(String J, String S)
-        {
-            Int32 result = 0;
-
-            foreach (Char s in S)
-            {
-                if (J.Contains(s))
-                {
-                    result++;
-                }
-            }
-
-            return result;
-        }
-    }
+	public class _771
+	{
+		public static int NumJewelsInStones(string jewels, string stones)
+		{
+			int result = 0;
+			foreach (var s in stones)
+			{
+				if (jewels.Contains(s))
+				{
+					result++;
+				}
+			}
+			return result;
+		}
+	}
 }
