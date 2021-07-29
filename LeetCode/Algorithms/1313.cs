0a45a822c7693c58f2ec9f6819b09a71467daea5
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-29 15:29:13 +0300

    1313

---
+++ LeetCode/Algorithms/1313.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _1313
+	{
+		public int[] DecompressRLElist(int[] numbers)
+		{
+			var result = new List<int>();
+			for (var i = 0; i < numbers.Length / 2; i++)
+			{
+				for (var j = 0; j < numbers[2 * i]; j++)
+				{
+					result.Add(numbers[2 * i + 1]);
+				}
+			}
+			return result.ToArray();
+		}
+	}
+}
