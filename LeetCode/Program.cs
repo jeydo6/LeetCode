8cdd543d8672133343fbd76a9012a8727254868f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-30 12:01:46 +0300

    Program

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -8,7 +8,7 @@ namespace LeetCode
 		static void Main()
 		{
 			var json = JsonConvert.SerializeObject(
-				_1389.CreateTargetArray(new int[] { 4, 2, 1, 1 }, new int[] { 0, 0, 2, 0 })
+				""
 			);
 			Console.WriteLine(json);
 		}
