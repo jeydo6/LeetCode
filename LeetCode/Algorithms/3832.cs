db044a377d0cb5e81fe7bd467062a6c80bb5074c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-30 10:53:44 +0300

    677, 3832

---
+++ LeetCode/Algorithms/3832.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Algorithms
+{
+	class _3832
+	{
+		public static string GetResult()
+		{
+			var mapSum = new MapSum();
+			mapSum.Insert("apple", 3);
+			mapSum.Sum("ap");
+			mapSum.Insert("app", 2);
+			mapSum.Sum("ap");
+
+			return "";
+		}
+
+		public class MapSum
+		{
+			private readonly IDictionary<string, int> _map;
+
+			public MapSum()
+			{
+				_map = new Dictionary<string, int>();
+			}
+
+			public void Insert(string key, int value)
+			{
+				_map[key] = value;
+			}
+
+			public int Sum(string prefix)
+			{
+				return _map
+					.Where(kvp => kvp.Key.StartsWith(prefix))
+					.Select(kvp => kvp.Value)
+					.Sum();
+			}
+		}
+	}
+}
