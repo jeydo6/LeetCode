dca5cc1c13451e7f9874956aff34185c96c318e3
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-01 01:00:28 +0300

    1791

---
+++ LeetCode/Algorithms/1791.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _1791
+	{
+		//public static int FindCenter(int[][] edges)
+		//{
+		//	var hashSet = new HashSet<int>();
+		//	foreach (var edge in edges)
+		//	{
+		//		foreach (var node in edge)
+		//		{
+		//			if (hashSet.Contains(node))
+		//			{
+		//				return node;
+		//			}
+		//			else
+		//			{
+		//				hashSet.Add(node);
+		//			}
+		//		}
+		//	}
+		//	return 0;
+		//}
+
+		public static int FindCenter(int[][] edges)
+		{
+			return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ?
+				edges[0][0] :
+				edges[0][1];
+		}
+	}
+}
