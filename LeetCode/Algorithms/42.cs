fb98514a24a11f779ea1abd35654c4bf73c734ce
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-31 15:56:50 +0300

    42

---
+++ LeetCode/Algorithms/42.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _42
+	{
+		public static int Trap(int[] height)
+		{
+			if (height == null || height.Length == 0)
+			{
+				return 0;
+			}
+
+			var length = height.Length;
+
+			var leftMax = new int[length];
+			leftMax[0] = height[0];
+			for (var i = 1; i < length; i++)
+			{
+				leftMax[i] = Math.Max(height[i], leftMax[i - 1]);
+			}
+
+			var rightMax = new int[length];
+			rightMax[length - 1] = height[length - 1];
+			for (var i = length - 2; i >= 0; i--)
+			{
+				rightMax[i] = Math.Max(height[i], rightMax[i + 1]);
+			}
+
+			var result = 0;
+			for (var i = 1; i < length - 1; i++)
+			{
+				result += Math.Min(leftMax[i], rightMax[i]) - height[i];
+			}
+			return result;
+		}
+	}
+}
