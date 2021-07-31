ba1685f37d2347cfbbac7c9d8924a2abfc6b80d2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-31 23:59:50 +0300

    1773

---
+++ LeetCode/Algorithms/1773.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _1773
+	{
+		private static readonly IDictionary<string, int> _map = new Dictionary<string, int>
+		{
+			["type"] = 0,
+			["color"] = 1,
+			["name"] = 2,
+		};
+
+		public static int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+		{
+			var result = 0;
+			foreach (var item in items)
+			{
+				if (item[_map[ruleKey]] == ruleValue)
+				{
+					result++;
+				}
+			}
+			return result;
+		}
+	}
+}
