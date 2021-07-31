657d88b7e5cab3af452ebfeda9dd8488ca70e1ef
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-01 00:45:09 +0300

    1221

---
+++ LeetCode/Algorithms/1221.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1221
+	{
+		public static int BalancedStringSplit(string s)
+		{
+			var result = 0;
+
+			var depth = 0;
+			foreach (var ch in s)
+			{
+				depth += ch switch
+				{
+					'L' => 1,
+					'R' => -1,
+					_ => 0
+				};
+
+				if (depth == 0)
+				{
+					result++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
