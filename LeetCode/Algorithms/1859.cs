6ad54fab88ccae2a95d04402e216178b40e277b2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-01 22:36:23 +0300

    1859

---
+++ LeetCode/Algorithms/1859.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1859
+	{
+		public static string SortSentence(string s)
+		{
+			var words = s.Split(" ");
+			var result = new string[words.Length];
+			foreach (var word in words)
+			{
+				result[word[^1] - '1'] = word[..^1];
+			}
+			return string.Join(" ", result);
+		}
+	}
+}
