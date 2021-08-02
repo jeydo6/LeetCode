c891568915903ce64cb1e9067faacb8a50e5c935
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 15:50:43 +0300

    1816

---
+++ LeetCode/Algorithms/1816.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	class _1816
+	{
+		public static string TruncateSentence(string s, int k)
+		{
+			var words = s.Split(' ');
+
+			var sb = new StringBuilder();
+			for (var i = 0; i < Math.Min(words.Length, k); i++)
+			{
+				sb.Append(words[i]);
+				sb.Append(' ');
+			}
+			return sb.ToString().TrimEnd();
+		}
+	}
+}
