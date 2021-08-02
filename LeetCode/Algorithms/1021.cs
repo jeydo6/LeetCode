1e9f6e319c23b7e191708fd16c81c1fca068c2d7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 16:38:25 +0300

    1021

--- LeetCode/Algorithms/1021.cs
+++ LeetCode/Algorithms/1021.cs
@@ -1,67 +1,93 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System;
 
 public class _1021
 {
-    // public String RemoveOuterParentheses(String S)
-    // {
-    //     String result = "";
+	// public String RemoveOuterParentheses(String S)
+	// {
+	//     String result = "";
 
-    //     Int32 pCount = 0;
-    //     String primitive = "";
-    //     for (Int32 i = 0; i < S.Length; i++)
-    //     {
-    //         primitive += S[i];
+	//     Int32 pCount = 0;
+	//     String primitive = "";
+	//     for (Int32 i = 0; i < S.Length; i++)
+	//     {
+	//         primitive += S[i];
 
-    //         if (S[i] == '(')
-    //         {
-    //             pCount++;
-    //         }
-    //         else if (S[i] == ')')
-    //         {
-    //             pCount--;
-    //         }
+	//         if (S[i] == '(')
+	//         {
+	//             pCount++;
+	//         }
+	//         else if (S[i] == ')')
+	//         {
+	//             pCount--;
+	//         }
 
-    //         if (pCount == 0 && primitive.Length > 0)
-    //         {
-    //             result += primitive.Substring(1, primitive.Length - 2);
-    //             primitive = "";
-    //         }
-    //     }
+	//         if (pCount == 0 && primitive.Length > 0)
+	//         {
+	//             result += primitive.Substring(1, primitive.Length - 2);
+	//             primitive = "";
+	//         }
+	//     }
 
-    //     return result;
-    // }
+	//     return result;
+	// }
 
-    public String RemoveOuterParentheses(String S)
-    {
-        Stack<Char> stack = new Stack<Char>();
-        StringBuilder result = new StringBuilder();
+	//  public String RemoveOuterParentheses(String S)
+	//  {
+	//      Stack<Char> stack = new Stack<Char>();
+	//      StringBuilder result = new StringBuilder();
 
-		if (String.IsNullOrEmpty(S))
-            return "";
-        
-        foreach(Char c in S)
-        {
-            if (c == '(')    
-            {   
-                if (stack.Count() != 0)
-                {
-                    result.Append(c);
-                }
-                stack.Push(c);
-            }
-            else if (c == ')')
-            {
-                if (stack.Count() != 1)
-                {
-                    result.Append(c);
-                }
-                stack.Pop();
-            }
-        }
-        
-        return result.ToString();
-    }
+	//if (String.IsNullOrEmpty(S))
+	//          return "";
+
+	//      foreach(Char c in S)
+	//      {
+	//          if (c == '(')    
+	//          {   
+	//              if (stack.Count() != 0)
+	//              {
+	//                  result.Append(c);
+	//              }
+	//              stack.Push(c);
+	//          }
+	//          else if (c == ')')
+	//          {
+	//              if (stack.Count() != 1)
+	//              {
+	//                  result.Append(c);
+	//              }
+	//              stack.Pop();
+	//          }
+	//      }
+
+	//      return result.ToString();
+	//  }
+	public static string RemoveOuterParentheses(string s)
+	{
+		var sb = new StringBuilder();
+
+		var stack = new Stack<char>();
+		foreach (var ch in s)
+		{
+			if (ch == '(')
+			{
+				if (stack.Count != 0)
+				{
+					sb.Append(ch);
+				}
+				stack.Push(ch);
+			}
+			else if (ch == ')')
+			{
+				if (stack.Count != 1)
+				{
+					sb.Append(ch);
+				}
+				stack.Pop();
+			}
+		}
+
+		return sb.ToString();
+	}
 }
