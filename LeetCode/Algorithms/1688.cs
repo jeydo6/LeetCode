f916d1500954718298964c688b84d4476ce64091
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 11:31:07 +0300

    1688

---
+++ LeetCode/Algorithms/1688.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1688
+	{
+		public static int NumberOfMatches(int n)
+		{
+			var result = 0;
+			while (n > 1)
+			{
+				if (n % 2 == 0)
+				{
+					result += n / 2;
+					n /= 2;
+				}
+				else
+				{
+					result += (n - 1) / 2;
+					n = (n - 1) / 2 + 1;
+				}
+			}
+			return result;
+		}
+	}
+}
