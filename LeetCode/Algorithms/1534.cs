32dd5c993d095a74142a2531b147dcfc9d7a58e6
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 15:59:17 +0300

    1534

---
+++ LeetCode/Algorithms/1534.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1534
+	{
+		public static int CountGoodTriplets(int[] arr, int a, int b, int c)
+		{
+			var result = 0;
+			for (var i = 0; i < arr.Length; i++)
+			{
+				for (var j = i + 1; j < arr.Length; j++)
+				{
+					for (var k = j + 1; k < arr.Length; k++)
+					{
+						if (
+							Math.Abs(arr[i] - arr[j]) <= a &&
+							Math.Abs(arr[j] - arr[k]) <= b &&
+							Math.Abs(arr[i] - arr[k]) <= c
+						)
+						{
+							result++;
+						}
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
