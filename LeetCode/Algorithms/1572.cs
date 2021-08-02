eca0b38322d7ef31320545f41478ada8825e7b08
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 23:29:16 +0300

    1572

---
+++ LeetCode/Algorithms/1572.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1572
+	{
+		public static int DiagonalSum(int[][] matrix)
+		{
+			var result = 0;
+
+			var length = matrix.Length;
+			for (var i = 0; i < length / 2; i++)
+			{
+				result += matrix[i][i] + matrix[i][^(i + 1)] + matrix[^(i + 1)][i] + matrix[^(i + 1)][^(i + 1)];
+			}
+
+			if (length % 2 == 1)
+			{
+				result += matrix[length / 2][length / 2];
+			}
+
+			return result;
+		}
+	}
+}
