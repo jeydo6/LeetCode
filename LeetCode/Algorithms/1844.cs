df65489f10c2b29e357b27ed7cea994594c43028
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 16:07:19 +0300

    1844

---
+++ LeetCode/Algorithms/1844.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1844
+	{
+		public static string ReplaceDigits(string s)
+		{
+			var result = new char[s.Length];
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (i % 2 == 0)
+				{
+					result[i] = s[i];
+				}
+				else
+				{
+					result[i] = (char)(s[i - 1] + s[i] - '0');
+				}
+			}
+			return string.Concat(result);
+		}
+	}
+}
