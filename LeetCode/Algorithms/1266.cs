2d954ee95796c92ff57635cd0123b4575675cb19
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 17:06:05 +0300

    1266

---
+++ LeetCode/Algorithms/1266.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1266
+	{
+		public static int MinTimeToVisitAllPoints(int[][] points)
+		{
+			var result = 0;
+			for (var i = 1; i < points.Length; i++)
+			{
+				var x = Math.Abs(points[i][0] - points[i - 1][0]);
+				var y = Math.Abs(points[i][1] - points[i - 1][1]);
+
+				result += Math.Min(x, y) + Math.Abs(x - y);
+			}
+			return result;
+		}
+	}
+}
