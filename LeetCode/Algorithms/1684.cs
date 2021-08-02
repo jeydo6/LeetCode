1f94c7acb25d0f0e7e6b2b970ff704168e56da96
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 13:48:12 +0300

    1684

---
+++ LeetCode/Algorithms/1684.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1684
+	{
+		public static int CountConsistentStrings(string allowed, string[] words)
+		{
+			var result = words.Length;
+			foreach (var word in words)
+			{
+				foreach (var ch in word)
+				{
+					if (!allowed.Contains(ch))
+					{
+						result--;
+						break;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
