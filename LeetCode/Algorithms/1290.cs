7f391ad67050bfd1726653f111a2c143e8bf701d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 14:39:25 +0300

    1290

---
+++ LeetCode/Algorithms/1290.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1290
+	{
+		public static int GetResult()
+		{
+			var listNode = new ListNode(1, new ListNode(0, new ListNode(1)));
+			return GetDecimalValue(listNode);
+		}
+
+		public static int GetDecimalValue(ListNode head)
+		{
+			var result = head.val;
+			while (head.next != null)
+			{
+				result = (result << 1) | head.next.val;
+				head = head.next;
+			}
+			return result;
+		}
+
+		public class ListNode
+		{
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+
+			public int val;
+			public ListNode next;
+		}
+	}
+}
