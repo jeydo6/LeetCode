7d424624cb4434bd80afe7ba0afb1a4214f6962a
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-02 13:12:28 +0300

    1662

---
+++ LeetCode/Algorithms/1662.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1662
+	{
+		public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+		{
+			return string.Concat(word1) == string.Concat(word2);
+		}
+	}
+}
