3c9765abf20da1f00f82088a60bbf0fa09742543
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-04 00:34:11 +0300

    1863

---
+++ LeetCode/Algorithms/1863.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _1863
+	{
+		public static int SubsetXORSum(int[] numbers)
+		{
+			var result = 0;
+			foreach (var number in numbers)
+			{
+				result |= number;
+			}
+			return result * (1 << numbers.Length - 1);
+		}
+	}
+}
