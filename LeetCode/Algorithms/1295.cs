37c8997a4c02c048a47604fb5516b5fdbed3ba9f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-04 00:40:25 +0300

    1295

---
+++ LeetCode/Algorithms/1295.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1295
+	{
+		public static int FindNumbers(int[] numbers)
+		{
+			var result = 0;
+			foreach (var number in numbers)
+			{
+				var length = 0;
+
+				var temp = number;
+				while (temp > 0)
+				{
+					length++;
+					temp /= 10;
+				}
+
+				if (length % 2 == 0)
+				{
+					result++;
+				}
+			}
+			return result;
+		}
+	}
+}
