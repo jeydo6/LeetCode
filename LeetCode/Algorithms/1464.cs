b279020995b4114d7428f7dd23730a55bf2c5f00
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-06 21:02:40 +0300

    1464

---
+++ LeetCode/Algorithms/1464.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1464
+	{
+		public static int MaxProduct(int[] numbers)
+		{
+			Array.Sort(numbers);
+			return (numbers[^1] - 1) * (numbers[^2] - 1);
+		}
+	}
+}
