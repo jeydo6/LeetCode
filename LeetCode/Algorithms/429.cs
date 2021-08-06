fa5022ee3497d312a18a298026d0c620069cb7db
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-06 11:00:36 +0300

    429

---
+++ LeetCode/Algorithms/429.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _429
+	{
+		public class Node
+		{
+			public int val;
+			public IList<Node> children;
+
+			public Node()
+			{
+
+			}
+
+			public Node(int val)
+			{
+				this.val = val;
+			}
+
+			public Node(int val, IList<Node> children)
+			{
+				this.val = val;
+				this.children = children;
+			}
+		}
+
+		public static IList<IList<int>> LevelOrder(Node root)
+		{
+			var result = new List<IList<int>>();
+
+			if (root == null)
+			{
+				return result;
+			}
+
+			var queue = new Queue<Node>();
+			queue.Enqueue(root);
+			while (queue.Count > 0)
+			{
+				var level = new List<int>();
+				var levelCount = queue.Count;
+				for (var i = 0; i < levelCount; i++)
+				{
+					var node = queue.Dequeue();
+					level.Add(node.val);
+					foreach (var child in node.children)
+					{
+						queue.Enqueue(child);
+					}
+				}
+				result.Add(level);
+			}
+
+			return result;
+		}
+	}
+}
