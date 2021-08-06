81e6b818a7b339dc33e57d9c3be3baf8bbf03f74
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-06 21:40:30 +0300

    1941

---
+++ LeetCode/Algorithms/1941.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1941
+	{
+		public static bool AreOccurrencesEqual(string s)
+		{
+			var dict = new int[26];
+			foreach (var ch in s)
+			{
+				dict[ch - 'a']++;
+			}
+
+			var temp = dict[s[0] - 'a'];
+			foreach (var value in dict)
+			{
+				if (value != 0 && value != temp)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
