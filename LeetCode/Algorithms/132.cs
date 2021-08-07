9ae73c8f1b63c290f2acfeb17dbee6403f3be142
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-07 23:00:13 +0300

    132

---
+++ LeetCode/Algorithms/132.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _132
+	{
+		public static int MinCut(string s)
+		{
+			var cut = new int[s.Length];
+			var palindrom = new bool[s.Length][];
+			for (var i = 0; i < s.Length; i++)
+			{
+				var min = i;
+				palindrom[i] = new bool[s.Length];
+				for (var j = 0; j <= i; j++)
+				{
+					if (s[j] == s[i] && (j + 1 > i - 1 || palindrom[j + 1][i - 1]))
+					{
+						palindrom[j][i] = true;
+						min = j == 0 ? 0 : Math.Min(min, cut[j - 1] + 1);
+					}
+				}
+				cut[i] = min;
+			}
+			return cut[^1];
+		}
+	}
+}
