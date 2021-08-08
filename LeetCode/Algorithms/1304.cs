e5f77a36deb5e6f7c4555039277835ccd11b5bd2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-09 00:13:33 +0300

    1304

---
+++ LeetCode/Algorithms/1304.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	class _1304
+	{
+		public static int[] SumZero(int n)
+		{
+			var result = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				result[i] = i * 2 - n + 1;
+			}
+			return result;
+		}
+	}
+}
