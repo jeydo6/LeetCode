4cf57d3b865a851887d1fa7f5d0ba1a92c469d3e
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-09 22:41:26 +0300

    897

--- LeetCode/Algorithms/897.cs
+++ LeetCode/Algorithms/897.cs
@@ -1,88 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _897
-    {
-        public class TreeNode
-        {
-            public TreeNode(Int32 x)
-            {
-                val = x;
-            }
+	public class _897
+	{
+		public class TreeNode
+		{
+			public TreeNode(int x)
+			{
+				val = x;
+			}
 
-            public Int32 val;
+			public int val;
 
-            public TreeNode left;
+			public TreeNode left;
 
-            public TreeNode right;
+			public TreeNode right;
 
-        }
+		}
 
-        TreeNode current;
+		public static TreeNode IncreasingBST(TreeNode root, TreeNode tail = null)
+		{
+			if (root == null)
+			{
+				return tail;
+			}
 
-        public TreeNode IncreasingBST(TreeNode root)
-        {
-            ICollection<Int32> values = new List<Int32>();
+			var result = IncreasingBST(root.left, root);
+			root.left = null;
+			root.right = IncreasingBST(root.right, tail);
 
-            TreeNode result = new TreeNode(0);
-            current = result;
-
-            Inorder(root);
-
-            return result.right;
-        }
-
-        public void Inorder(TreeNode node)
-        {
-            if (node == null)
-            {
-                return;
-            }
-
-            Inorder(node.left);
-
-            node.left = null;
-            current.right = node;
-            current = node;
-
-            Inorder(node.right);
-        }
-
-        //public TreeNode IncreasingBST(TreeNode root)
-        //{
-        //    ICollection<Int32> values = new List<Int32>();
-
-        //    Inorder(root, values);
-
-        //    TreeNode result = new TreeNode(0);
-
-        //    TreeNode temp = result;
-        //    foreach (Int32 value in values)
-        //    {
-        //        temp.right = new TreeNode(value);
-        //        temp = temp.right;
-        //    }
-
-        //    return result.right;
-        //}
-
-        //public void Inorder(TreeNode node, ICollection<Int32> values)
-        //{
-        //    if (node == null)
-        //    {
-        //        return;
-        //    }
-
-        //    Inorder(node.left, values);
-
-        //    values.Add(node.val);
-
-        //    Inorder(node.right, values);
-        //}
-    }
+			return result;
+		}
+	}
 }
