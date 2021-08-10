57a637c6acf6466da774eb34350ae07ac8887cb5
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-10 10:59:16 +0300

    926

---
+++ LeetCode/Algorithms/926.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _926
+	{
+		public static int MinFlipsMonoIncr(string s)
+		{
+			int[] p = new int[s.Length + 1];
+			for (var i = 0; i < s.Length; i++)
+			{
+				p[i + 1] = p[i] + (s[i] == '1' ? 1 : 0);
+			}
+
+			var result = int.MaxValue;
+			for (var i = 0; i < p.Length; i++)
+			{
+				result = Math.Min(result, p[i] + p.Length - i - 1 - (p[^1] - p[i]));
+			}
+			return result;
+		}
+
+		//public static int MinFlipsMonoIncr(string s)
+		//{
+		//	var flip = 0;
+		//	var count = 0;
+		//	foreach (var ch in s)
+		//	{
+		//		if (ch == '0')
+		//		{
+		//			if (count > 0)
+		//			{
+		//				flip++;
+		//			}
+		//		}
+		//		else
+		//		{
+		//			count++;
+		//		}
+		//		flip = Math.Min(flip, count);
+		//	}
+		//	return flip;
+		//}
+	}
+}
