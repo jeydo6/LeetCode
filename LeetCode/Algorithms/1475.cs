262cd56207add2b6bebbc348a8b8cd8cfbbc46fb
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-11 00:55:21 +0300

    1475

---
+++ LeetCode/Algorithms/1475.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	class _1475
+	{
+		public static int[] FinalPrices(int[] prices)
+		{
+			var stack = new Stack<int>();
+			for (var i = 0; i < prices.Length; i++)
+			{
+				while (stack.Count > 0 && prices[stack.Peek()] >= prices[i])
+				{
+					prices[stack.Pop()] -= prices[i];
+				}
+				stack.Push(i);
+			}
+			return prices;
+		}
+	}
+}
