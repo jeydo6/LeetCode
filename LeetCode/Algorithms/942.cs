2c94e932697715dcf0ddd8144e0454d5a4ac2774
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-12 22:38:34 +0300

    942

--- LeetCode/Algorithms/942.cs
+++ LeetCode/Algorithms/942.cs
@@ -1,32 +1,25 @@
-using System;
-
-public class _942
+namespace Leetcode.Algorithms
 {
-    public Int32[] DiStringMatch(String S)
-    {
-        S = S.ToLower();
-        Int32 length = S.Length;
-        Int32 min = 0;
-        Int32 max = length;
-
-        Int32[] resultArray = new Int32[length + 1];
-
-        for (Int32 i = 0; i < length; i++)
-        {
-            Char c = S[i];
-            if (c == 'i')
-            {
-                resultArray[i] = min++;
-            }
-
-            if (c == 'd')
-            {
-                resultArray[i] = max--;
-            }
-        }
-
-        resultArray[length] = min;
-
-        return resultArray;
-    }
+	public class _942
+	{
+		public static int[] DiStringMatch(string s)
+		{
+			var min = 0;
+			var max = s.Length;
+			var result = new int[s.Length + 1];
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (s[i] == 'I')
+				{
+					result[i] = min++;
+				}
+				else
+				{
+					result[i] = max--;
+				}
+			}
+			result[s.Length] = min;
+			return result;
+		}
+	}
 }
