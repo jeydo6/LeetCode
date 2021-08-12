97599e9dc94ceb4be11daaac4f1aa6510df5a067
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-12 23:09:01 +0300

    1299

---
+++ LeetCode/Algorithms/1299.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1299
+	{
+		public static int[] ReplaceElements(int[] array)
+		{
+			var end = array.Length - 1;
+			var max = array[^1];
+
+			for (var i = array.Length - 2; i >= 0; i--)
+			{
+				array[end] = array[i];
+				array[i] = max;
+
+				if (array[end] > max)
+				{
+					max = array[end];
+				}
+			}
+
+			array[^1] = -1;
+			return array;
+		}
+	}
+}
