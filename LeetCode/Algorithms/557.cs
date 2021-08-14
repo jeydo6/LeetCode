da55a5c22035f58f23d3cf65b0b763cb8ac47e69
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-14 23:24:49 +0300

    557

--- LeetCode/Algorithms/557.cs
+++ LeetCode/Algorithms/557.cs
@@ -1,42 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Leetcode.Algorithms
 {
-    public class _557
-    {
-        public String ReverseWords(String s)
-        {
-            StringBuilder result = new StringBuilder(s.Length + 1);
-            StringBuilder w = new StringBuilder();
+	public class _557
+	{
+		//public String ReverseWords(String s)
+		//{
+		//	StringBuilder result = new StringBuilder(s.Length + 1);
+		//	StringBuilder w = new StringBuilder();
 
-            foreach (Char c in s)
-            {
-                if (c != ' ')
-                {
-                    w.Insert(0, c);
-                }
-                else
-                {
-                    result.Append(w);
-                    result.Append(" ");
+		//	foreach (Char c in s)
+		//	{
+		//		if (c != ' ')
+		//		{
+		//			w.Insert(0, c);
+		//		}
+		//		else
+		//		{
+		//			result.Append(w);
+		//			result.Append(" ");
 
-                    w.Clear();
-                }
-            }
-            
-            if (w.Length > 0)
-            {
-                result.Append(w);
-                result.Append(" ");
-            }
+		//			w.Clear();
+		//		}
+		//	}
 
-            return result
-                .ToString()
-                .TrimEnd();
-        }
-    }
+		//	if (w.Length > 0)
+		//	{
+		//		result.Append(w);
+		//		result.Append(" ");
+		//	}
+
+		//	return result
+		//		.ToString()
+		//		.TrimEnd();
+		//}
+
+		public static string ReverseWords(string s)
+		{
+			var result = new StringBuilder(s.Length);
+			var words = s.Split(' ');
+			for (var i = 0; i < words.Length; i++)
+			{
+				for (var j = 0; j < words[i].Length; j++)
+				{
+					result.Append(words[i][^(j + 1)]);
+				}
+				result.Append(' ');
+			}
+			return result
+				.ToString()
+				.TrimEnd();
+		}
+	}
 }
