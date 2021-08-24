8b04fea5b0145cc456243cb157c8e6d6f27227b5
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-24 11:14:20 +0300

    537

---
+++ LeetCode/Algorithms/537.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _537
+	{
+		public static string ComplexNumberMultiply(string num1, string num2)
+		{
+			var coefs1 = num1
+				.Replace("i", "")
+				.Split("+");
+			var coefs2 = num2
+				.Replace("i", "")
+				.Split("+");
+
+			var a = int.Parse(coefs1[0]);
+			var b = int.Parse(coefs1[1]);
+			var c = int.Parse(coefs2[0]);
+			var d = int.Parse(coefs2[1]);
+
+			return $"{a * c - b * d}+{a * d + b * c}i";
+		}
+	}
+}
