34899768f77c5d0f43008b975df4b0f81b9cfc4e
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-25 10:33:45 +0300

    633

---
+++ LeetCode/Algorithms/633.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _633
+	{
+		public static bool JudgeSquareSum(int c)
+		{
+			var a = 0;
+			var b = (int)Math.Sqrt(c);
+			while (a <= b)
+			{
+				var t = a * a + b * b;
+				if (t < c)
+				{
+					a++;
+				}
+				else if (t > c)
+				{
+					b--;
+				}
+				else
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
