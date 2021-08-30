2238be69a93de2841820a3d2ab7fe833959f98af
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-30 21:01:53 +0300

    1710

---
+++ LeetCode/Algorithms/1710.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1710
+	{
+		public static int MaximumUnits(int[][] boxTypes, int truckSize)
+		{
+			Array.Sort(boxTypes, (a, b) => b[1] - a[1]);
+			
+			var result = 0;
+			foreach (var item in boxTypes)
+			{
+				if (item[0] < truckSize)
+				{
+					result += item[0] * item[1];
+					truckSize -= item[0];
+				}
+				else
+				{
+					result += truckSize * item[1];
+					return result;
+				}
+			}
+			return result;
+		}
+	}
+}
