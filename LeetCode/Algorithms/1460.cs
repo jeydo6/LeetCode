04acf80d10721b358120b50ee55c5f452e067052
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-30 19:52:29 +0300

    1460

---
+++ LeetCode/Algorithms/1460.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1460
+	{
+		public static bool CanBeEqual(int[] target, int[] arr)
+		{
+			Array.Sort(target);
+			Array.Sort(arr);
+
+			for (var i = 0; i < target.Length; i++)
+			{
+				if (target[i] != arr[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
