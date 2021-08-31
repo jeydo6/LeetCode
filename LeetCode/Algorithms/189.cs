2e6db46ab690641cb59a7f3389f53bc0696a1faf
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-31 10:40:13 +0300

    189

---
+++ LeetCode/Algorithms/189.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _189
+	{
+		public static void Rotate(int[] nums, int k)
+		{
+			k %= nums.Length;
+			Reverse(nums, 0, nums.Length - 1);
+			Reverse(nums, 0, k - 1);
+			Reverse(nums, k, nums.Length - 1);
+		}
+
+		private static void Reverse(int[] nums, int start, int end)
+		{
+			while (start < end)
+			{
+				var temp = nums[start];
+				nums[start] = nums[end];
+				nums[end] = temp;
+
+				start++;
+				end--;
+			}
+		}
+	}
+}
