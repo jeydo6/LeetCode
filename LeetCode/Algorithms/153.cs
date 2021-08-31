c5c78706f72f507e7be8cafb1216825c0b6a2ad1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-31 10:23:23 +0300

    153

---
+++ LeetCode/Algorithms/153.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _153
+	{
+		public static int FindMin(int[] nums)
+		{
+			var lo = 0;
+			var hi = nums.Length - 1;
+			while (lo < hi)
+			{
+				if (nums[lo] < nums[hi])
+				{
+					return nums[lo];
+				}
+
+				var mid = (lo + hi) / 2;
+
+				if (nums[mid] >= nums[lo])
+				{
+					lo = mid + 1;
+				}
+				else
+				{
+					hi = mid;
+				}
+			}
+			return nums[lo];
+		}
+	}
+}
