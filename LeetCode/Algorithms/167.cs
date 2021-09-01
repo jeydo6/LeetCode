9a03b87c676640ced719f615d0a86ba12cbf0829
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-01 12:21:16 +0300

    167

---
+++ LeetCode/Algorithms/167.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _167
+	{
+		public static int[] TwoSum(int[] nums, int target)
+		{
+			var lo = 0;
+			var hi = nums.Length - 1;
+			while (lo < hi)
+			{
+				var sum = nums[lo] + nums[hi];
+				if (sum < target)
+				{
+					lo++;
+				}
+				else if (sum > target)
+				{
+					hi--;
+				}
+				else
+				{
+					return new int[2] { lo + 1, hi + 1 };
+				}
+			}
+			return null;
+		}
+	}
+}
