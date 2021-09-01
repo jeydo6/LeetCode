d2e49f168237f39fb6b279c28a2fdf97497e07d0
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-01 12:13:06 +0300

    283

---
+++ LeetCode/Algorithms/283.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _283
+	{
+		public static void MoveZeroes(int[] nums)
+		{
+			var j = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] != 0)
+				{
+					var temp = nums[i];
+					nums[i] = nums[j];
+					nums[j] = temp;
+
+					j++;
+				}
+			}
+		}
+	}
+}
