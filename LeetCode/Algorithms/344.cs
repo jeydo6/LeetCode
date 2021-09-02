7f84358e5d779a9888c6786ba802dcbc8ef6e18f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-02 21:12:43 +0300

    344

--- LeetCode/Algorithms/344.cs
+++ LeetCode/Algorithms/344.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _344
-    {
-        public void ReverseString(Char[] s)
-        {
-            Int32 length = s.Length / 2;
-            for (Int32 i = 0; i < length; i++)
-            {
-                Char temp = s[i];
-                s[i] = s[s.Length - 1 - i];
-                s[s.Length - 1 - i] = temp;
-            }
-        }
-    }
+	public class _344
+	{
+		public void ReverseString(char[] s)
+		{
+			var length = s.Length / 2;
+			for (var i = 0; i < length; i++)
+			{
+				var temp = s[i];
+				s[i] = s[^(i + 1)];
+				s[^(i + 1)] = temp;
+			}
+		}
+	}
 }
