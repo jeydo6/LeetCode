2161cd75f0ceb6be607ea6e900bfb592aae0ba02
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-04 01:38:36 +0300

    19

---
+++ LeetCode/Algorithms/19.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _19
+	{
+		public class ListNode
+		{
+			public ListNode(int x)
+			{
+				val = x;
+			}
+
+			public int val;
+
+			public ListNode next;
+		}
+
+		public static ListNode RemoveNthFromEnd(ListNode head, int n)
+		{
+			var start = new ListNode(0)
+			{
+				next = head
+			};
+			var slow = start;
+			var fast = start;
+
+			for (var i = 0; i < n + 1; i++)
+			{
+				fast = fast.next;
+			}
+
+			while (fast != null)
+			{
+				slow = slow.next;
+				fast = fast.next;
+			}
+
+			slow.next = slow.next.next;
+			return start.next;
+		}
+	}
+}
