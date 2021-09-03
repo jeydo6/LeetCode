4ebcfd17720cd78d33933dade87c2aa0220b2708
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-04 01:24:51 +0300

    587

---
+++ LeetCode/Algorithms/587.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Algorithms
+{
+	// HARD
+	class _587
+	{
+		public static int[][] OuterTrees(int[][] trees)
+		{
+			Array.Sort(trees, (p, q) => q[0] - p[0] == 0 ? q[1] - p[1] : q[0] - p[0]);
+
+			var stack = new Stack<int[]>();
+			for (var i = 0; i < trees.Length; i++)
+			{
+				while (stack.Count >= 2 && Orientation(stack.ElementAt(1), stack.Peek(), trees[i]) > 0)
+				{
+					stack.Pop();
+				}
+				stack.Push(trees[i]);
+			}
+
+			stack.Pop();
+
+			for (var i = trees.Length - 1; i >= 0; i--)
+			{
+				while (stack.Count >= 2 && Orientation(stack.ElementAt(1), stack.Peek(), trees[i]) > 0)
+				{
+					stack.Pop();
+				}
+				stack.Push(trees[i]);
+			}
+
+			var hashSet = new HashSet<int[]>(stack);
+			return hashSet.ToArray();
+		}
+
+		private static int Orientation(int[] p, int[] q, int[] r)
+		{
+			return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
+		}
+	}
+}
