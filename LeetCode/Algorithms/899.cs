383258b046798a13cc34e04c7189b301f214c306
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-05 12:06:52 +0300

    899

---
+++ LeetCode/Algorithms/899.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	// HARD
+	class _899
+	{
+		public static string OrderlyQueue(string s, int k)
+		{
+			if (k > 1)
+			{
+				var chars = s.ToCharArray();
+				Array.Sort(chars);
+				return new string(chars);
+			}
+			else
+			{
+				var result = s;
+				for (var i = 1; i < s.Length; i++)
+				{
+					var temp = s[i..] + s[..i];
+					if (string.Compare(result, temp) > 0)
+					{
+						result = temp;
+					}
+				}
+				return result;
+			}
+		}
+	}
+}
