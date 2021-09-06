af90d0ca40329557678e2d2dddcf6cb1e169de3a
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-06 10:57:28 +0300

    1629

---
+++ LeetCode/Algorithms/1629.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1629
+	{
+		public static char SlowestKey(int[] releaseTimes, string keysPressed)
+		{
+			var keyPressed = keysPressed[0];
+
+			var longestDuration = releaseTimes[0];
+			for (var i = 1; i < releaseTimes.Length; i++)
+			{
+				var duration = releaseTimes[i] - releaseTimes[i - 1];
+
+				if (duration > longestDuration || (duration == longestDuration && keysPressed[i] > keyPressed))
+				{
+					longestDuration = duration;
+					keyPressed = keysPressed[i];
+				}
+			}
+
+			return keyPressed;
+		}
+	}
+}
