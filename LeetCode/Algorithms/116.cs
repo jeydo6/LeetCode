83735190362fc6917f7e61edbbe63e6c180100fe
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-06 11:28:02 +0300

    116

---
+++ LeetCode/Algorithms/116.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _116
+	{
+		public class Node
+		{
+			public int val;
+			public Node left;
+			public Node right;
+			public Node next;
+
+			public Node() { }
+
+			public Node(int _val)
+			{
+				val = _val;
+			}
+
+			public Node(int _val, Node _left, Node _right, Node _next)
+			{
+				val = _val;
+				left = _left;
+				right = _right;
+				next = _next;
+			}
+		}
+
+		public static Node Connect(Node root)
+		{
+			if (root == null)
+			{
+				return root;
+			}
+
+			var prev = root;
+			while (prev.left != null)
+			{
+				var current = prev;
+				while (current != null)
+				{
+					current.left.next = current.right;
+					if (current.next != null)
+					{
+						current.right.next = current.next.left;
+					}
+					current = current.next;
+				}
+				prev = prev.left;
+			}
+
+			return root;
+		}
+	}
+}
