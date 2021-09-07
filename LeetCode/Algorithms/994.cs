86b6cb675348a08e388318ed478fb16060dda843
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-07 14:14:56 +0300

    994

---
+++ LeetCode/Algorithms/994.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	public class _994
+	{
+		public static int OrangesRotting(int[][] grid)
+		{
+			if (grid.Length == 0)
+			{
+				return 0;
+			}
+
+			var fresh = 0;
+			var rotted = new Queue<int[]>();
+			for (var i = 0; i < grid.Length; i++)
+			{
+				for (var j = 0; j < grid[0].Length; j++)
+				{
+					if (grid[i][j] == 1)
+					{
+						fresh++;
+					}
+					else if (grid[i][j] == 2)
+					{
+						rotted.Enqueue(new int[] { i, j });
+					}
+				}
+			}
+
+			if (fresh == 0)
+			{
+				return 0;
+			}
+
+			var result = 0;
+			var directions = new int[][]
+			{
+				new int[] { 1, 0 },
+				new int[] { -1, 0 },
+				new int[] { 0, 1 },
+				new int[] { 0, -1 },
+			};
+			while (rotted.Count > 0)
+			{
+				result++;
+				var count = rotted.Count;
+				for (var i = 0; i < count; i++)
+				{
+					var item = rotted.Dequeue();
+					foreach (var direction in directions)
+					{
+						var x = item[0] + direction[0];
+						var y = item[1] + direction[1];
+						if (
+							x < 0 || x >= grid.Length ||
+							y < 0 || y >= grid[0].Length ||
+							grid[x][y] == 0 || grid[x][y] == 2
+						)
+						{
+							continue;
+						}
+
+						grid[x][y] = 2;
+						rotted.Enqueue(new int[] { x, y });
+						fresh--;
+					}
+				}
+			}
+
+			if (fresh == 0)
+			{
+				return result - 1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+	}
+}
