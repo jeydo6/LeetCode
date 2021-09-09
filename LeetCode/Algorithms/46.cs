b6ca4f15343a4acc3ea72b473344ffbf1586abb8
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-09 21:58:12 +0300

    46

---
+++ LeetCode/Algorithms/46.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _46
+	{
+		public static IList<IList<int>> Permute(int[] nums, List<int> current = null)
+		{
+			if (current == null)
+			{
+				current = new List<int>();
+			}
+
+			var result = new List<IList<int>>();
+			if (current.Count == nums.Length)
+			{
+				result.Add(new List<int>(current));
+			}
+			else
+			{
+				for (var i = 0; i < nums.Length; i++)
+				{
+					if (current.Contains(nums[i]))
+					{
+						continue;
+					}
+
+					current.Add(nums[i]);
+					var temp = Permute(nums, current);
+					foreach (var combination in temp)
+					{
+						result.Add(combination);
+					}
+					current.RemoveAt(current.Count - 1);
+				}
+			}
+			return result;
+		}
+	}
+}
