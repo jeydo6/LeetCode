eba0d2432ce595c9c42445a2d7db2f1925787efc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-11 23:01:23 +0300

    224

---
+++ LeetCode/Algorithms/224.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Algorithms
+{
+	// HARD
+	class _224
+	{
+		public static int Calculate(string s)
+		{
+			var result = 0;
+			var number = 0;
+			var sign = 1;
+			var stack = new Stack<int>();
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (char.IsDigit(s[i]))
+				{
+					number = 10 * number + s[i] - '0';
+				}
+				else if (s[i] == '+')
+				{
+					result += sign * number;
+					number = 0;
+					sign = 1;
+				}
+				else if (s[i] == '-')
+				{
+					result += sign * number;
+					number = 0;
+					sign = -1;
+				}
+				else if (s[i] == '(')
+				{
+					stack.Push(result);
+					stack.Push(sign);
+
+					sign = 1;
+					result = 0;
+				}
+				else if (s[i] == ')')
+				{
+					result += sign * number;
+					number = 0;
+
+					result *= stack.Pop();
+					result += stack.Pop();
+				}
+			}
+			if (number != 0)
+			{
+				result += sign * number;
+			}
+			return result;
+		}
+	}
+}
