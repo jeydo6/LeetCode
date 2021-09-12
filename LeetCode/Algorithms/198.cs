2db52dd9c094508a57fef0e1f9c05e536e4d1b37
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-12 20:42:12 +0300

    198

---
+++ LeetCode/Algorithms/198.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _198
+	{
+		public static int Rob(int[] nums)
+		{
+			if (nums.Length == 0)
+			{
+				return 0;
+			}
+			var prev1 = 0;
+			var prev2 = 0;
+			foreach (var num in nums)
+			{
+				var temp = prev1;
+				prev1 = Math.Max(prev2 + num, prev1);
+				prev2 = temp;
+			}
+			return prev1;
+		}
+	}
+}
