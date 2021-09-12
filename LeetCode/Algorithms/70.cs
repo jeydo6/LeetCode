de3a9bcb850246ce4f887fc28f6b6075f78b357c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-12 20:36:57 +0300

    70

---
+++ LeetCode/Algorithms/70.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _70
+	{
+		public static int ClimbStairs(int n)
+		{
+			if (n == 1)
+			{
+				return 1;
+			}
+			if (n == 2)
+			{
+				return 2;
+			}
+
+			var dp = new int[n + 1];
+			dp[0] = 0;
+			dp[1] = 1;
+			dp[2] = 2;
+			for (var i = 3; i < n + 1; i++)
+			{
+				dp[i] = dp[i - 1] + dp[i - 2];
+			}
+			return dp[n];
+		}
+	}
+}
