e85ec08cf44b718d206ca12a8b37b8b33b5f1af9
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-12 20:51:15 +0300

    120

---
+++ LeetCode/Algorithms/120.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _120
+	{
+		public static int MinimumTotal(IList<IList<int>> triangle)
+		{
+			if (triangle.Count == 0)
+			{
+				return 0;
+			}
+
+			var list = new List<int>(triangle[^1]);
+			for (var layer = triangle.Count - 2; layer >= 0; layer--)
+			{
+				for (var i = 0; i <= layer; i++)
+				{
+					list[i] = Math.Min(list[i], list[i + 1]) + triangle[layer][i];
+				}
+			}
+			return list[0];
+		}
+	}
+}
