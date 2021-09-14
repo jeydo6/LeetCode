e3b73cc06061a412286e5c4eae81279cf2d7bd2a
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-14 10:37:34 +0300

    917

---
+++ LeetCode/Algorithms/917.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	class _917
+	{
+		public static string ReverseOnlyLetters(string s)
+		{
+			var sb = new StringBuilder(s);
+
+			var i = 0;
+			var j = s.Length - 1;
+			while (i < j)
+			{
+				if (!char.IsLetter(s[i]))
+				{
+					i++;
+				}
+				else if (!char.IsLetter(s[j]))
+				{
+					j--;
+				}
+				else
+				{
+					sb[i] = s[j];
+					sb[j] = s[i];
+
+					i++;
+					j--;
+				}
+			}
+
+			return sb.ToString();
+		}
+	}
+}
