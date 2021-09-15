a2fffa0f59154322ac39c16773087d4eedbd8e3d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-15 12:58:33 +0300

    978

---
+++ LeetCode/Algorithms/978.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _978
+	{
+		public static int MaxTurbulenceSize(int[] arr)
+		{
+			var result = 1;
+			
+			var inc = 1;
+			var dec = 1;
+			for (var i = 1; i < arr.Length; i++)
+			{
+				if (arr[i] < arr[i - 1])
+				{
+					dec = inc + 1;
+					inc = 1;
+				}
+				else if (arr[i] > arr[i - 1])
+				{
+					inc = dec + 1;
+					dec = 1;
+				}
+				else
+				{
+					inc = 1;
+					dec = 1;
+				}
+				result = Math.Max(result, Math.Max(dec, inc));
+			}
+
+			return result;
+		}
+	}
+}
