8663358fcc2993b0deffcf498dde6351d57b610c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-19 01:15:38 +0300

    282

---
+++ LeetCode/Algorithms/282.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	internal class _282
+	{
+		public static IList<string> AddOperators(string num, int target, string path = "", int position = 0, long evaluation = 0L, long multiplied = 0L)
+		{
+			var result = new List<string>();
+
+			if (string.IsNullOrEmpty(num))
+			{
+				return result;
+			}
+
+			if (position == num.Length)
+			{
+				if (target == evaluation)
+				{
+					result.Add(path);
+				}
+				return result;
+			}
+
+			for (var i = position; i < num.Length; i++)
+			{
+				if (i != position && num[position] == '0')
+				{
+					break;
+				}
+
+				var current = long.Parse(num[position..(i + 1)]);
+				if (position == 0)
+				{
+					result.AddRange(
+						AddOperators(num, target, path + current, i + 1, current, current)
+					);
+				}
+				else
+				{
+					result.AddRange(
+						AddOperators(num, target, path + "+" + current, i + 1, evaluation + current, current)
+					);
+					result.AddRange(
+						AddOperators(num, target, path + "-" + current, i + 1, evaluation - current, -current)
+					);
+					result.AddRange(
+						AddOperators(num, target, path + "*" + current, i + 1, evaluation - multiplied + multiplied * current, multiplied * current)
+					);
+				}
+			}
+
+			return result;
+		}
+	}
+}
