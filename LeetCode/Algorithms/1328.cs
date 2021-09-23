7b7f49120c5a3db4a3a84b3bea59a3ecf2498898
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-23 23:33:01 +0300

    1328

---
+++ LeetCode/Algorithms/1328.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1328
+	{
+		public static string BreakPalindrome(string palindrome)
+		{
+			if (palindrome.Length == 1)
+			{
+				return "";
+			}
+
+			var index = -1;
+			var chars = palindrome.ToCharArray();
+			for (var i = 0; i < palindrome.Length / 2; i++)
+			{
+				if (chars[i] != 'a')
+				{
+					index = i;
+					chars[i] = 'a';
+
+					break;
+				}
+			}
+
+			if (index == -1)
+			{
+				chars[palindrome.Length - 1] = 'b';
+			}
+			return new string(chars);
+		}
+	}
+}
