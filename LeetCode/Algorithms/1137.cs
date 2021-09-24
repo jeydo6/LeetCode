eaa9344d426b0783f5bf84914ea1e7fee92ea429
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-09-24 10:21:52 +0300

    1137

---
+++ LeetCode/Algorithms/1137.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms
+{
+	internal class _1137
+	{
+		public static int Tribonacci(int n)
+		{
+			var arr = new int[] { 0, 1, 1 };
+			for (var i = 3; i <= n; ++i)
+			{
+				arr[i % 3] = arr[0] + arr[1] + arr[2];
+			}
+			return arr[n % 3];
+		}
+	}
+}
