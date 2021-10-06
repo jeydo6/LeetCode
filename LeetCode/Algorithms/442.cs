745c5c12a7469fd0ae05e8a9a0accb431f6ac411
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-06 10:06:16 +0300

    442

---
+++ LeetCode/Algorithms/442.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _442
+	{
+		public static IList<int> FindDuplicates(int[] nums)
+		{
+			var result = new List<int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var index = Math.Abs(nums[i]) - 1;
+				if (nums[index] < 0)
+				{
+					result.Add(Math.Abs(index + 1));
+				}
+				nums[index] = -nums[index];
+			}
+			return result;
+		}
+	}
+}
