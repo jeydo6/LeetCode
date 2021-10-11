94477dfb7bc411cc3b1d4b621061ea40adb61802
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-11 10:24:35 +0300

    543

---
+++ LeetCode/Algorithms/543.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _543
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		private static int _diameter = 0;
+
+		public static int DiameterOfBinaryTree(TreeNode root)
+		{
+			MaxHeight(root);
+
+			return _diameter;
+		}
+
+		private static int MaxHeight(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			var left = MaxHeight(root.left);
+			var right = MaxHeight(root.right);
+			_diameter = Math.Max(_diameter, left + right);
+
+			return Math.Max(left, right) + 1;
+		}
+	}
+}
