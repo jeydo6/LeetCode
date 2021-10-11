d69aae3c66bc988395ac00477bf032fe7891954b
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-11 11:04:51 +0300

    208

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,9 +3,9 @@ using System;
 
 namespace LeetCode
 {
-	class Program
+	internal static class Program
 	{
-		static void Main()
+		private static void Main()
 		{
 			var json = JsonConvert.SerializeObject(
 				""
