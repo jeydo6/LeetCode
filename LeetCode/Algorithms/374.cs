9487bd7cc91ba7924c279344e13c6c5014e2c995
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-12 19:10:58 +0300

    374

---
+++ LeetCode/Algorithms/374.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms
+{
+
+	// EASY
+	internal class _374
+	{
+		public static int GuessNumber(int n)
+		{
+			var l = 1;
+			var r = n;
+			while (l <= r)
+			{
+				int m = l + (r - l) / 2;
+				if (Guess(m) == 0)
+				{
+					return m;
+				}
+
+				if (Guess(m) < 0)
+				{
+					r = m - 1;
+				}
+				else
+				{
+					l = m + 1;
+				}
+			}
+			return 0;
+		}
+
+		private static int Guess(int n)
+		{
+			return n;
+		}
+	}
+}
