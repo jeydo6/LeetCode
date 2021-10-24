7c47857018b5e6297ba3949aaed12e7f079b2d81
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-24 22:05:36 +0300

    222

---
+++ LeetCode/Algorithms/222.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	public class _222
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static int CountNodes(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;	
+			}
+
+			TreeNode node;
+			
+			var left = 0;
+			node = root;
+			while (node.left != null)
+			{
+				left++;
+				node = node.left;
+			}
+			
+			var right = 0;
+			node = root;
+			while (node.right != null)
+			{
+				right++;
+				node = node.right;
+			}
+
+			if (left == right)
+			{
+				return 2 * ((int)Math.Pow(2, left) - 1) + 1;
+			}
+
+			return CountNodes(root.left) + CountNodes(root.right) + 1;
+		}
+	}
+}
