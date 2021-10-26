ad452fde518f4728f2db402f4bf6727d5e35eecb
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-26 12:35:23 +0300

    226

---
+++ LeetCode/Algorithms/226.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Algorithms
+{
+	internal class _226
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+		
+		public static TreeNode InvertTree(TreeNode root)
+		{
+			if (root == null)
+			{
+				return null;
+			}
+
+			(root.left, root.right) = (root.right, root.left);
+
+			InvertTree(root.left);
+			InvertTree(root.right);
+
+			return root;
+		}
+	}
+}
