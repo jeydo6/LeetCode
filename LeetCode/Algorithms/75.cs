7fb1b0bbe236dc21a76d4f94537392f116aaca34
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-28 01:49:00 +0300

    75

---
+++ LeetCode/Algorithms/75.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Algorithms
+{
+	internal class _75
+	{
+		public static void SortColors(int[] nums)
+		{
+			var zero = 0;
+			var one = 0;
+			var two = 0;
+
+			var i = 0;
+			while (i < nums.Length)
+			{
+				switch (nums[i])
+				{
+					case 0:
+						zero++;
+						break;
+					case 1:
+						one++;
+						break;
+					default:
+						two++;
+						break;
+				}
+
+				i++;
+			}
+
+			i = 0;
+			while (zero > 0)
+			{
+				nums[i++] = 0;
+				zero--;
+			}
+
+			while (one > 0)
+			{
+				nums[i++] = 1;
+				one--;
+			}
+
+			while (two > 0)
+			{
+				nums[i++] = 2;
+				two--;
+			}
+		}
+	}
+}
