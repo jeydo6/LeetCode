21867b865d416d09ad6af43938d79c6641cb6399
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-10-31 12:44:51 +0300

    430

---
+++ LeetCode/Algorithms/430.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Algorithms
+{
+	public class _430
+	{
+		public class Node
+		{
+			public int val;
+			public Node prev;
+			public Node next;
+			public Node child;
+		}
+
+		public static Node Flatten(Node head)
+		{
+			if (head == null)
+			{
+				return null;
+			}
+
+			var node = head;
+			while (node != null)
+			{
+				if (node.child == null)
+				{
+					node = node.next;
+					continue;
+				}
+
+				var temp = node.child;
+				while (temp.next != null)
+				{
+					temp = temp.next;
+				}
+
+				temp.next = node.next;
+				if (node.next != null)
+				{
+					node.next.prev = temp;
+				}
+
+				node.next = node.child;
+				node.child.prev = node;
+				node.child = null;
+			}
+
+			return head;
+		}
+	}
+}
