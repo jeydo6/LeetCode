da1a1e53dabb1b629bf49897320cf7343cf20fb9
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-11-05 17:06:26 +0300

    441

---
+++ LeetCode/Algorithms/441.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _441
+	{
+		public static int ArrangeCoins(int n)
+		{
+			return (int)Math.Floor(Math.Sqrt(2.0 * n + 0.25) - 0.5);
+		}
+	}
+}
