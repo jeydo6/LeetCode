dd74bdf5725851e4c73c1bd9b278a9330cc5b484
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-11-12 15:44:10 +0300

    203

---
+++ LeetCode/Algorithms/203.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _203
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode RemoveElements(ListNode head, int val)
+		{
+			if (head == null)
+			{
+				return null;
+			}
+
+			head.next = RemoveElements(head.next, val);
+
+			return head.val == val ? head.next : head;
+		}
+	}
+}
