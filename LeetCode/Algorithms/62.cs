78b24c5f28e8f4665d1c39c886bdaf69f5756df1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-11-17 10:11:01 +0300

    62

---
+++ LeetCode/Algorithms/62.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _62
+	{
+		public static int UniquePaths(int n, int m)
+		{
+			var dp = new int[n][];
+
+			for (var i = 0; i < n; i++)
+			{
+				dp[i] = new int[m];
+				for (var j = 0; j < m; j++)
+				{
+					dp[i][j] = 1;
+				}
+			}
+
+			for (var i = 1; i < n; i++)
+			{
+				for (var j = 1; j < m; j++)
+				{
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				}
+			}
+
+			return dp[n - 1][m - 1];
+		}
+	}
+}
