73e0dcc8e9f656207db92cab51c285672bea5899
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-11-18 16:58:26 +0300

    448

---
+++ LeetCode/Algorithms/448.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _448
+	{
+		public static IList<int> FindDisappearedNumbers(int[] nums)
+		{
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var m = Math.Abs(nums[i]) - 1;
+				nums[m] = nums[m] > 0 ? -nums[m] : nums[m];
+			}
+
+			var result = new List<int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] > 0)
+				{
+					result.Add(i + 1);
+				}
+			}
+			return result;
+		}
+	}
+}
