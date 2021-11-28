14351a6f7294974bb1401329482208be44e1443c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-11-28 18:15:55 +0300

    797

---
+++ LeetCode/Algorithms/797.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _797
+	{
+		public static IList<IList<int>> AllPathsSourceTarget(int[][] graph, int node = 0, IList<IList<int>> result = null, IList<int> path = null)
+		{
+			if (result == null)
+			{
+				result = new List<IList<int>>();
+			}
+
+			if (path == null)
+			{
+				path = new List<int>
+				{
+					0
+				};
+			}
+
+			if (node == graph.Length - 1)
+			{
+				result.Add(new List<int>(path));
+				return result;
+			}
+
+			for (var i = 0; i < graph[node].Length; i++)
+			{
+				path.Add(graph[node][i]);
+				AllPathsSourceTarget(graph, graph[node][i], result, path);
+				path.RemoveAt(path.Count - 1);
+			}
+
+			return result;
+		}
+	}
+}
