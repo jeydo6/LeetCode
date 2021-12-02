f677b84de5cdcda7be80d27aceb7c4c3ee9ff972
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-02 08:40:48 +0300

    328

---
+++ LeetCode/Algorithms/328.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _328
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode OddEvenList(ListNode head)
+		{
+			if (head != null)
+			{
+				var odd = head;
+				var even = head.next;
+				var temp = even;
+				while (even != null && even.next != null)
+				{
+					odd.next = odd.next.next;
+					even.next = even.next.next;
+					odd = odd.next;
+					even = even.next;
+				}
+				odd.next = temp;
+			}
+			return head;
+		}
+	}
+}
