b4ff67246a37704b8714c1a591e11ddf7809f6a4
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-03 13:15:47 +0300

    152

---
+++ LeetCode/Algorithms/152.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _152
+	{
+		public static int MaxProduct(int[] nums)
+		{
+			var result = nums[0];
+			var iMax = result;
+			var iMin = result;
+			for (var i = 1; i < nums.Length; i++)
+			{
+				if (nums[i] < 0)
+				{
+					var temp = iMax;
+					iMax = iMin;
+					iMin = temp;
+				}
+
+				iMax = Math.Max(nums[i], iMax * nums[i]);
+				iMin = Math.Min(nums[i], iMin * nums[i]);
+
+				result = Math.Max(result, iMax);
+			}
+			return result;
+		}
+	}
+}
