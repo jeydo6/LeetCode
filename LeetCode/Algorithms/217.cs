d32127cb7c0ab3161ed858bbbb00cb3096504887
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-06 01:08:21 +0300

    217

---
+++ LeetCode/Algorithms/217.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _217
+	{
+		public static bool ContainsDuplicate(int[] nums)
+		{
+			var hashSet = new HashSet<int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (hashSet.Contains(nums[i]))
+				{
+					return true;
+				}
+				hashSet.Add(nums[i]);
+			}
+			return false;
+		}
+	}
+}
