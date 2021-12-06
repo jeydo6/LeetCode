03dfe29b6821eb5a5dcd0c199024391a00e10dfb
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-06 10:07:33 +0300

    1217

--- LeetCode/Algorithms/1217.cs
+++ LeetCode/Algorithms/1217.cs
@@ -1,31 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Leetcode.Algorithms
 {
-    public class _1217
-    {
-        public Int32 MinCostToMoveChips(Int32[] chips)
-        {
-            Int32 odd = 0;
-            Int32 even = 0;
+	public class _1217
+	{
+		public static int MinCostToMoveChips(int[] chips)
+		{
+			var odd = 0;
+			var even = 0;
 
-            foreach (Int32 chip in chips)
-            {
-                if (chip % 2 == 0)
-                {
-                    even++;
-                }
-                else
-                {
-                    odd++;
-                }
-            }
+			for (var i = 0; i < chips.Length; i++)
+			{
+				if (chips[i] % 2 == 0)
+				{
+					even++;
+				}
+				else
+				{
+					odd++;
+				}
+			}
 
-            return Math.Min(odd, even);
-        }
-    }
+			return Math.Min(odd, even);
+		}
+	}
 }
