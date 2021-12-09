810821abac14862fd080e2c637af9a4a66f34854
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-09 10:31:52 +0300

    566

---
+++ LeetCode/Algorithms/566.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _566
+	{
+		public static int[][] MatrixReshape(int[][] matrix, int r, int c)
+		{
+			var n = matrix.Length;
+			var m = matrix[0].Length;
+
+			if (r * c != n * m)
+			{
+				return matrix;
+			}
+
+			var result = new int[r][];
+			for (var i = 0; i < r; i++)
+			{
+				result[i] = new int[c];
+			}
+
+			for (var i = 0; i < r * c; i++)
+			{
+				result[i / c][i % c] = matrix[i / m][i % m];
+			}
+
+			return result;
+		}
+	}
+}
