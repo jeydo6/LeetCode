e7b2eeb099981c32ec1cda754558f6b0b7f95815
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-14 11:09:25 +0300

    383

---
+++ LeetCode/Algorithms/383.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _383
+	{
+		public static bool CanConstruct(string ransomNote, string magazine)
+		{
+			var arr = new int[26];
+			for (var i = 0; i < magazine.Length; i++)
+			{
+				arr[magazine[i] - 'a']++;
+			}
+			for (var i = 0; i < ransomNote.Length; i++)
+			{
+				if (--arr[ransomNote[i] - 'a'] < 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
