c509fc828a4464a9dd67c98820dfa76d6af06f14
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-14 14:25:40 +0300

    20

---
+++ LeetCode/Algorithms/20.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _20
+	{
+		public static bool IsValid(string s)
+		{
+			var stack = new Stack<char>();
+			for (var i = 0; i < s.Length; i++)
+			{
+				var ch = s[i];
+				if (ch == '(')
+				{
+					stack.Push(')');
+				}
+				else if (ch == '{')
+				{
+					stack.Push('}');
+				}
+				else if (ch == '[')
+				{
+					stack.Push(']');
+				}
+				else if (stack.Count == 0 || stack.Pop() != ch)
+				{
+					return false;
+				}
+			}
+			return stack.Count == 0;
+		}
+	}
+}
