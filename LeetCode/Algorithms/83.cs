9afe488f7b2bbf0e1119ce77e24f607e0e827667
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-14 11:44:20 +0300

    83

---
+++ LeetCode/Algorithms/83.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _83
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode DeleteDuplicates(ListNode head)
+		{
+			if (head == null || head.next == null)
+			{
+				return head;
+			}
+			head.next = DeleteDuplicates(head.next);
+			return head.val == head.next.val ? head.next : head;
+		}
+	}
+}
