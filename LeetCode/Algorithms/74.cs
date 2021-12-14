e8c50b04ec6033936b1524d261d1c4f0aa0364b0
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-14 11:02:34 +0300

    74

---
+++ LeetCode/Algorithms/74.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _74
+	{
+		public static bool SearchMatrix(int[][] matrix, int target)
+		{
+			var n = matrix.Length;
+			var m = matrix[0].Length;
+			var l = 0;
+			var r = m * n - 1;
+			while (l != r)
+			{
+				var mid = (l + r - 1) >> 1;
+				if (matrix[mid / m][mid % m] < target)
+				{
+					l = mid + 1;
+				}
+				else
+				{
+					r = mid;
+				}
+			}
+			return matrix[r / m][r % m] == target;
+		}
+	}
+}
