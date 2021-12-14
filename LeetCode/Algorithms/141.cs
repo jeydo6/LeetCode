b62f3d597c41e51dfd937c4b14ab00a35e3c1973
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-14 11:29:26 +0300

    141

---
+++ LeetCode/Algorithms/141.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _141
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x)
+			{
+				val = x;
+				next = null;
+			}
+		}
+
+		public static bool HasCycle(ListNode head)
+		{
+			if (head == null)
+			{
+				return false;
+			}
+			var walker = head;
+			var runner = head;
+			while (runner.next != null && runner.next.next != null)
+			{
+				walker = walker.next;
+				runner = runner.next.next;
+				if (walker == runner)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
