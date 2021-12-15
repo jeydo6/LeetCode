fd7b7aaea722e15899dcb38dab360dd35ba2b154
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-15 12:26:15 +0300

    145

---
+++ LeetCode/Algorithms/145.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _145
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static IList<int> PostorderTraversal(TreeNode node)
+		{
+			var list = new List<int>();
+			var stack = new Stack<TreeNode>();
+			while (node != null || stack.Count > 0)
+			{
+				if (node != null)
+				{
+					list.Insert(0, node.val);
+					stack.Push(node.left);
+					node = node.right;
+				}
+				else
+				{
+					node = stack.Pop();
+				}
+			}
+			return list;
+		}
+	}
+}
