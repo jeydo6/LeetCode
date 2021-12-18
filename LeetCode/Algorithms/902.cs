c978bdc9498b7d62c255bee28891df979ca5593d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-18 21:07:24 +0300

    902

---
+++ LeetCode/Algorithms/902.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _902
+	{
+		public static int AtMostNGivenDigitSet(string[] digits, int n)
+		{
+			var nStr = n.ToString();
+			var result = 0;
+			for (var i = 1; i < nStr.Length; i++)
+			{
+				result += (int)Math.Pow(digits.Length, i);
+			}
+			for (var i = 0; i < nStr.Length; i++)
+			{
+				var same = false;
+				for (var j = 0; j < digits.Length; j++)
+				{
+					if (digits[j][0] < nStr[i])
+					{
+						result += (int)Math.Pow(digits.Length, nStr.Length - 1 - i);
+					}
+					else if (digits[j][0] == nStr[i])
+					{
+						same = true;
+					}
+				}
+				if (!same)
+				{
+					return result;
+				}
+			}
+			return result + 1;
+		}
+	}
+}
