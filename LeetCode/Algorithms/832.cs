884b61c6c6977b09171d04fb0e4cb34fa89ee5a1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-21 11:58:03 +0300

    832

--- LeetCode/Algorithms/832.cs
+++ LeetCode/Algorithms/832.cs
@@ -1,24 +1,8 @@
 namespace LeetCode.Algorithms
 {
-	public class _832
+	// EASY
+	internal class _832
 	{
-		//public Int32[][] FlipAndInvertImage(Int32[][] A)
-		//{
-		//    foreach (Int32[] row in A)
-		//    {
-		//        Array.Reverse(row);
-		//    }
-
-		//    foreach (Int32[] row in A)
-		//    {
-		//        for (Int32 i = 0; i < row.Length; i++)
-		//        {
-		//            row[i] = (row[i] + 1) % 2;
-		//        }
-		//    }
-		//    return A;
-		//}
-
 		public static int[][] FlipAndInvertImage(int[][] image)
 		{
 			var length = image.Length;
