4ececab218c9a6fb8d710446f8a4a9981a2db4eb
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-21 11:58:17 +0300

    852

--- LeetCode/Algorithms/852.cs
+++ LeetCode/Algorithms/852.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _852
-    {
-        public Int32 PeakIndexInMountainArray(Int32[] A)
-        {
-            Int32 index = 0;
-            while (A[index] < A[index + 1] && index < A.Length - 1)
-            {
-                index++;
-            }
-            return index;
-        }
-    }
+	// EASY
+	internal class _852
+	{
+		public static int PeakIndexInMountainArray(int[] arr)
+		{
+			var index = 0;
+			while (arr[index] < arr[index + 1] && index < arr.Length - 1)
+			{
+				index++;
+			}
+			return index;
+		}
+	}
 }
