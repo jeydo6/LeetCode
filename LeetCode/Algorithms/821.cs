78aefd0355efd936941edf1f28d067d375c3c43d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-21 11:57:44 +0300

    821

--- LeetCode/Algorithms/821.cs
+++ LeetCode/Algorithms/821.cs
@@ -1,52 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Leetcode.Algorithms
 {
-    public class _821
-    {
-        public Int32[] ShortestToChar(String S, Char C)
-        {
-            Int32[] arr = new Int32[S.Length];
-            for (Int32 i = 0; i < S.Length; i++)
-            {
-                arr[i] = 10000;
-            }
+	// EASY
+	internal class _821
+	{
+		public static int[] ShortestToChar(string str, char ch)
+		{
+			var arr = new int[str.Length];
+			for (var i = 0; i < str.Length; i++)
+			{
+				arr[i] = int.MaxValue;
+			}
 
-            Int32 distance = 10000;
-            for (Int32 i = 0; i < S.Length; i++)
-            {
-                if (S[i] == C)
-                {
-                    distance = 0;
-                }
-                else
-                {
-                    distance++;
-                }
+			var distance = int.MaxValue;
+			for (var i = 0; i < str.Length; i++)
+			{
+				if (str[i] == ch)
+				{
+					distance = 0;
+				}
+				else
+				{
+					distance++;
+				}
 
-                arr[i] = Math.Min(arr[i], distance);
-            }
+				arr[i] = Math.Min(arr[i], distance);
+			}
 
-            distance = 10000;
-            for (Int32 i = S.Length - 1; i >= 0; i--)
-            {
-                if (S[i] == C)
-                {
-                    distance = 0;
-                }
-                else
-                {
-                    distance++;
-                }
+			distance = int.MaxValue;
+			for (var i = str.Length - 1; i >= 0; i--)
+			{
+				if (str[i] == ch)
+				{
+					distance = 0;
+				}
+				else
+				{
+					distance++;
+				}
 
-                arr[i] = Math.Min(arr[i], distance);
-            }
+				arr[i] = Math.Min(arr[i], distance);
+			}
 
-            return arr;
-        }
-    }
+			return arr;
+		}
+	}
 }
