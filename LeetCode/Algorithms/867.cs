bf9641fc929a9d06c6b4579575053a248fd95e83
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-21 11:58:44 +0300

    867

--- LeetCode/Algorithms/867.cs
+++ LeetCode/Algorithms/867.cs
@@ -1,28 +1,26 @@
-﻿using System;
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _867
-    {
-        public Int32[][] Transpose(Int32[][] A)
-        {
-            Int32 n = A.Length;
-            Int32 m = A[0].Length;
+	// EASY
+	internal class _867
+	{
+		public static int[][] Transpose(int[][] matrix)
+		{
+			var n = matrix.Length;
+			var m = matrix[0].Length;
 
-            Int32[][] tA = new Int32[m][];
-            for (Int32 j = 0; j < m; j++)
-            {
-                tA[j] = new Int32[n];
-            }
-
-            for (Int32 i = 0; i < n; i++)
-            {
-                for(Int32 j = 0; j < m; j++)
-                {
-                    tA[j][i] = A[i][j];
-                }
-            }
-
-            return tA;
-        }
-    }
+			var transponse = new int[m][];
+			for (var j = 0; j < m; j++)
+			{
+				transponse[j] = new int[n];
+			}
+			for (var i = 0; i < n; i++)
+			{
+				for (var j = 0; j < m; j++)
+				{
+					transponse[j][i] = matrix[i][j];
+				}
+			}
+			return transponse;
+		}
+	}
 }
