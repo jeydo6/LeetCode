998cb4ea12366ef5098c24aaa1f6a526b6d8d745
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-12-26 18:52:23 +0300

    973

---
+++ LeetCode/Algorithms/973.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _973
+	{
+		public static int[][] KClosest(int[][] points, int k)
+		{
+			Array.Sort(points, (p1, p2) => p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
+			var result = new int[k][];
+			Array.Copy(points, result, k);
+			return result;
+		}
+	}
+}
