3757710b4fc09b97af5155151b21a34cbea215db
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-02 17:20:19 +0300

    1010

---
+++ LeetCode/Algorithms/1010.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1010
+	{
+		public static int NumPairsDivisibleBy60(int[] time)
+		{
+			var result = 0;
+			var arr = new int[60];
+			for (var i = 0; i < time.Length; i++)
+			{
+				result += arr[(600 - time[i]) % 60];
+				arr[time[i] % 60] += 1;
+			}
+			return result;
+		}
+	}
+}
