d3c7d1a5e2c928ab3eb4eb36c95de00b8fcfa697
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-06 17:40:46 +0300

    1094

---
+++ LeetCode/Algorithms/1094.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1094
+	{
+		public static bool CarPooling(int[][] trips, int capacity)
+		{
+			var stops = new int[1001];
+			for (var i = 0; i < trips.Length; i++)
+			{
+				stops[trips[i][1]] += trips[i][0];
+				stops[trips[i][2]] -= trips[i][0];
+			}
+			for (var i = 0; capacity >= 0 && i < 1001; i++)
+			{
+				capacity -= stops[i];
+			}
+			return capacity >= 0;
+		}
+	}
+}
