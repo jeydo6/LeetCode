da5262590ffaf012f56d28494fda3d3b2275b01c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-14 19:01:50 +0300

    8

---
+++ LeetCode/Algorithms/8.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _8
+	{
+		public static int MyAtoi(string s)
+		{
+			var index = 0;
+			while (index < s.Length && char.IsWhiteSpace(s[index]))
+			{
+				index++;
+			}
+
+			var sign = 1;
+			if (index < s.Length && (s[index] == '-' || s[index] == '+'))
+			{
+				if (s[index] == '-')
+				{
+					sign = -1;
+				}
+				index++;
+			}
+
+			var result = 0;
+			while (index < s.Length && char.IsDigit(s[index]))
+			{
+				var digit = s[index] - '0';
+				if (result > (int.MaxValue - digit) / 10)
+				{
+					return sign == -1 ? int.MinValue : int.MaxValue;
+				}
+				result = (result * 10) + digit;
+				index++;
+			}
+			return result * sign;
+		}
+	}
+}
