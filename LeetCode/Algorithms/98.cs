c15fcbd398313c7152516930bc046ccf0e5b2344
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-15 19:06:25 +0300

    98

---
+++ LeetCode/Algorithms/98.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _98
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static bool IsValidBST(TreeNode root)
+		{
+			if (root == null)
+			{
+				return true;
+			}
+			var stack = new Stack<TreeNode>();
+			TreeNode pre = null;
+			while (stack.Count > 0 || root != null)
+			{
+				while (root != null)
+				{
+					stack.Push(root);
+					root = root.left;
+				}
+				root = stack.Pop();
+				if (pre != null && root.val <= pre.val)
+				{
+					return false;
+				}
+				pre = root;
+				root = root.right;
+			}
+			return true;
+		}
+	}
+}
