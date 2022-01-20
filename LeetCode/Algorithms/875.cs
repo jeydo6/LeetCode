605517f89b52b314d809f0d6f733998dec3672c3
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-20 10:01:54 +0300

    875

---
+++ LeetCode/Algorithms/875.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _875
+	{
+		public static int MinEatingSpeed(int[] piles, int h)
+		{
+			var l = 1;
+			var r = 1000000000;
+			while (l < r)
+			{
+				var m = (l + r) / 2;
+				var total = 0;
+				for (var i = 0; i < piles.Length; i++)
+				{
+					total += (piles[i] + m - 1) / m;
+				}
+
+				if (total > h)
+				{
+					l = m + 1;
+				}
+				else
+				{
+					r = m;
+				}
+			}
+			return l;
+		}
+	}
+}
