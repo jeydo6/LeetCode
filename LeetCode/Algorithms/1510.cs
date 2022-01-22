6eca9542ac2d9e44eb5bf19d969766efe8fb0b6d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-22 21:49:01 +0300

    1510

---
+++ LeetCode/Algorithms/1510.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _1510
+	{
+		public static bool WinnerSquareGame(int n)
+		{
+			var dp = new bool[n + 1];
+			for (var i = 1; i <= n; ++i)
+			{
+				for (var k = 1; k * k <= i; ++k)
+				{
+					if (!dp[i - k * k])
+					{
+						dp[i] = true;
+						break;
+					}
+				}
+			}
+			return dp[n];
+		}
+	}
+}
