5f36a4520f2aa1c7ad01b5034b1cb10f1209722b
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-23 20:07:43 +0300

    1291

---
+++ LeetCode/Algorithms/1291.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1291
+	{
+		public static IList<int> SequentialDigits(int low, int high)
+		{
+			var result = new List<int>();
+			var b = 0;
+			var n = b;
+			var delta = 0;
+			while (n <= high)
+			{
+				if (n >= low)
+				{
+					result.Add(n);
+				}
+				n += delta;
+				if (n % 10 == 0)
+				{ // number of digits changed
+					b = b * 10 + (b % 10 + 1);
+					delta = delta * 10 + 1;
+					n = b;
+				}
+			}
+			return result;
+		}
+	}
+}
