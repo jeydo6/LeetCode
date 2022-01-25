e68b75cea5be6222239a680c8599e9ef2d2b83c0
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-01-25 10:15:34 +0300

    941

---
+++ LeetCode/Algorithms/941.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _941
+	{
+		public static bool ValidMountainArray(int[] arr)
+		{
+			var n = arr.Length;
+			var i = 0;
+			var j = n - 1;
+			while (i + 1 < n && arr[i] < arr[i + 1])
+			{
+				i++;
+			}
+			while (j > 0 && arr[j - 1] > arr[j])
+			{
+				j--;
+			}
+			return i > 0 && i == j && j < n - 1;
+		}
+	}
+}
