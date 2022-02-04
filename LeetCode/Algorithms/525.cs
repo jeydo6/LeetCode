7988c7b35041f3f68cbf0aa5a26631854d9442e2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-04 14:16:14 +0300

    525

---
+++ LeetCode/Algorithms/525.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _525
+	{
+		public static int FindMaxLength(int[] nums)
+		{
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] == 0) nums[i] = -1;
+			}
+
+			var dict = new Dictionary<int, int>
+			{
+				[0] = -1
+			};
+
+			var sum = 0;
+			var max = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				sum += nums[i];
+				if (dict.ContainsKey(sum))
+				{
+					max = Math.Max(max, i - dict[sum]);
+				}
+				else
+				{
+					dict[sum] = i;
+				}
+			}
+			return max;
+		}
+	}
+}
