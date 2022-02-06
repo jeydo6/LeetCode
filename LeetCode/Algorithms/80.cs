2cdf0ce2df91df09e08ea719ed9f536e47a6b381
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-06 21:34:41 +0300

    80

---
+++ LeetCode/Algorithms/80.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _80
+	{
+		public static int RemoveDuplicates(int[] nums)
+		{
+			var i = 0;
+			foreach (var num in nums)
+			{
+				if (i < 2 || num > nums[i - 2])
+				{
+					nums[i++] = num;
+				}
+			}
+			return i;
+		}
+	}
+}
