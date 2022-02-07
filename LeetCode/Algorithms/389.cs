c58ecf61b70401b33b75e123b493eb5595c6e89e
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-07 21:24:29 +0300

    389

---
+++ LeetCode/Algorithms/389.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _389
+	{
+		public static char FindTheDifference(string s, string t)
+		{
+			var n = t.Length;
+			var ch = t[^1];
+			for (var i = 0; i < n - 1; ++i)
+			{
+				ch ^= s[i];
+				ch ^= t[i];
+			}
+			return ch;
+		}
+	}
+}
