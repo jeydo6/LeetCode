0f290eba8b89edc3e92433811557f71b7ec8c846
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-19 20:53:24 +0300

    1675

---
+++ LeetCode/Algorithms/1675.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _1675
+	{
+		public static int MinimumDeviation(int[] nums)
+		{
+			var pqueue = new PriorityQueue<int, int>();
+			var min = int.MaxValue;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var num = nums[i];
+				if (num % 2 == 1)
+				{
+					num *= 2;
+				};
+				pqueue.Enqueue(num, -num);
+				min = Math.Min(min, num);
+			}
+
+			var result = int.MaxValue;
+			while (true)
+			{
+				var num = pqueue.Dequeue();
+				result = Math.Min(result, num - min);
+				if (num % 2 == 1)
+				{
+					break;
+				}
+				min = Math.Min(min, num / 2);
+				pqueue.Enqueue(num / 2, -num / 2);
+			}
+			return result;
+		}
+	}
+}
