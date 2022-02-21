a78483320e1335fc6d41d829f7fe7c1236d00a96
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-21 21:28:14 +0300

    169

---
+++ LeetCode/Algorithms/169.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _169
+	{
+		public static int MajorityElement(int[] nums)
+		{
+			var major = nums[0];
+			var count = 1;
+			for (var i = 1; i < nums.Length; i++)
+			{
+				if (count == 0)
+				{
+					count++;
+					major = nums[i];
+				}
+				else if (major == nums[i])
+				{
+					count++;
+				}
+				else count--;
+
+			}
+			return major;
+		}
+	}
+}
