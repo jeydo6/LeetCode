48d90389bf584301e7129db51aa3174e25378be3
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-22 20:09:41 +0300

    171

---
+++ LeetCode/Algorithms/171.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASy
+	internal class _171
+	{
+		public static int TitleToNumber(string columnTitle)
+		{
+			var result = 0;
+			for (var i = 0; i < columnTitle.Length; i++)
+			{
+				result = result * 26 + columnTitle[i] - 'A' + 1;
+			}
+			return result;
+		}
+	}
+}
