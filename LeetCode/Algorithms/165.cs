99076a804b646c01a8b9b601106eb3a0022579dc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-02-25 10:41:52 +0300

    165

---
+++ LeetCode/Algorithms/165.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _165
+	{
+		public static int CompareVersion(string version1, string version2)
+		{
+			var levels1 = version1.Split(".");
+			var levels2 = version2.Split(".");
+
+			var length = Math.Max(levels1.Length, levels2.Length);
+			for (var i = 0; i < length; i++)
+			{
+				var v1 = i < levels1.Length ? int.Parse(levels1[i]) : 0;
+				var v2 = i < levels2.Length ? int.Parse(levels2[i]) : 0;
+				var compare = v1.CompareTo(v2);
+				if (compare != 0)
+				{
+					return compare;
+				}
+			}
+			return 0;
+		}
+	}
+}
