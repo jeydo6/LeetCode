120b87ab63637a5f78345b1173b61668f3c99c29
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-03 00:52:20 +0300

    121

--- LeetCode/Algorithms/121.cs
+++ LeetCode/Algorithms/121.cs
@@ -1,19 +1,22 @@
-﻿using System;
-
-namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms
 {
 	// EASY
 	internal class _121
 	{
 		public static int MaxProfit(int[] prices)
 		{
-
 			var minPrice = int.MaxValue;
-			var maxProfit = int.MinValue;
+			var maxProfit = 0;
 			for (var i = 0; i < prices.Length; i++)
 			{
-				minPrice = Math.Min(minPrice, prices[i]);
-				maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+				if (prices[i] < minPrice)
+				{
+					minPrice = prices[i];
+				}
+				else if (prices[i] - minPrice > maxProfit)
+				{
+					maxProfit = prices[i] - minPrice;
+				}
 			}
 			return maxProfit;
 		}
