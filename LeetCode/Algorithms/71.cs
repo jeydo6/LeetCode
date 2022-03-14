d653f0d73c8022a6d5b8907772011f6c57c5c363
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-14 15:37:45 +0300

    71

---
+++ LeetCode/Algorithms/71.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _71
+	{
+		public static string SimplifyPath(string path)
+		{
+			var itemCount = 0;
+			var components = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+			for (var i = 0; i < components.Length; i++)
+			{
+				switch (components[i])
+				{
+					case ".":
+						break;
+					case "..":
+						if (itemCount > 0) itemCount--;
+						break;
+					default:
+						components[itemCount++] = components[i];
+						break;
+				}
+			}
+			return "/" + string.Join('/', components, 0, itemCount);
+		}
+	}
+}
