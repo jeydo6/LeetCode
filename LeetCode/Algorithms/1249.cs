546337ddf00f495ee33459cae9bb470780fe1e55
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-15 21:40:44 +0300

    1249

---
+++ LeetCode/Algorithms/1249.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1249
+	{
+		public static string MinRemoveToMakeValid(string s)
+		{
+			var indexesToRemove = new HashSet<int>();
+			var queue = new Queue<int>();
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (s[i] == '(')
+				{
+					queue.Enqueue(i);
+				}
+				if (s[i] == ')')
+				{
+					if (queue.Count == 0)
+					{
+						indexesToRemove.Add(i);
+					}
+					else
+					{
+						queue.Dequeue();
+					}
+				}
+			}
+
+			while (queue.Count > 0)
+			{
+				indexesToRemove.Add(
+					queue.Dequeue()
+				);
+			}
+
+			var sb = new StringBuilder();
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (!indexesToRemove.Contains(i))
+				{
+					sb.Append(s[i]);
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
