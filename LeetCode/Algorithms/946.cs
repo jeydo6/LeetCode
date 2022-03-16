2914ff02f3055639a8769ee8359cbe8141e5ecd7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-16 10:01:57 +0300

    946

---
+++ LeetCode/Algorithms/946.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _946
+	{
+		public static bool ValidateStackSequences(int[] pushed, int[] popped)
+		{
+			var j = 0;
+			var stack = new Stack<int>();
+			for (var i = 0; i < pushed.Length; i++)
+			{
+				stack.Push(pushed[i]);
+				while (stack.Count > 0 && j < pushed.Length && stack.Peek() == popped[j])
+				{
+					stack.Pop();
+					j++;
+				}
+			}
+			return j == pushed.Length;
+		}
+	}
+}
