30b64bc6b5ad05c2c6dffcc74ccd01e3fe62cd4a
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-18 23:11:51 +0300

    316

---
+++ LeetCode/Algorithms/316.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _316
+	{
+		public static string RemoveDuplicateLetters(string s)
+		{
+			var count = new int[26];
+			var position = 0;
+			for (var i = 0; i < s.Length; i++)
+			{
+				count[s[i] - 'a']++;
+			}
+
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (s[i] < s[position])
+				{
+					position = i;
+				}
+				if (count[s[i] - 'a']-- == 0)
+				{
+					break;
+				}
+			}
+			return s.Length == 0 ? "" : s[position] + RemoveDuplicateLetters(s[(position + 1)..].Replace("" + s[position], ""));
+		}
+	}
+}
