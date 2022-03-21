8d4107db52e304227359fa35ca928bf36996b106
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-21 09:56:50 +0300

    763

---
+++ LeetCode/Algorithms/763.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _763
+	{
+		public static IList<int> PartitionLabels(string s)
+		{
+			var last = new int[26];
+			for (var i = 0; i < s.Length; i++)
+			{
+				last[s[i] - 'a'] = i;
+			}
+
+			var j = 0;
+			var anchor = 0;
+			var result = new List<int>();
+			for (var i = 0; i < s.Length; i++)
+			{
+				j = Math.Max(j, last[s[i] - 'a']);
+				if (i == j)
+				{
+					result.Add(i - anchor + 1);
+					anchor = i + 1;
+				}
+			}
+			return result;
+		}
+	}
+}
