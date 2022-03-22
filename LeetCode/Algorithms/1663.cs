2ae2a1bb96732ea641ff04c88530d6993dac0e45
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-22 13:09:11 +0300

    1663

---
+++ LeetCode/Algorithms/1663.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1663
+	{
+		public static string GetSmallestString(int n, int k)
+		{
+			var result = new char[n];
+			for (var position = n - 1; position >= 0; position--)
+			{
+				var add = Math.Min(k - position, 26);
+				result[position] = (char)(add + 'a' - 1);
+				k -= add;
+			}
+			return new string(result);
+		}
+	}
+}
