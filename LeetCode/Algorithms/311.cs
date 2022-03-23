8c2384c125dff86288ea1aadc791e68beb0cca83
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-23 14:52:01 +0300

    311

---
+++ LeetCode/Algorithms/311.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _311
+	{
+		public static int[][] Multiply(int[][] matrix1, int[][] matrix2)
+		{
+			var n = matrix1.Length;
+			var k = matrix1[0].Length;
+			var m = matrix2[0].Length;
+
+			var matrix3 = new int[n][];
+			for (var rowIndex = 0; rowIndex < n; rowIndex++)
+			{
+				matrix3[rowIndex] = new int[m];
+				for (var elementIndex = 0; elementIndex < k; elementIndex++)
+				{
+					if (matrix1[rowIndex][elementIndex] != 0)
+					{
+						for (var columnIndex = 0; columnIndex < m; columnIndex++)
+						{
+							matrix3[rowIndex][columnIndex] += matrix1[rowIndex][elementIndex] * matrix2[elementIndex][columnIndex];
+						}
+					}
+				}
+			}
+			return matrix3;
+		}
+	}
+}
