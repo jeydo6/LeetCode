013e0b92d594e7fe211cc620fe9b290dfcbd50ca
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-24 22:15:09 +0300

    881

---
+++ LeetCode/Algorithms/881.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _881
+	{
+		public static int NumRescueBoats(int[] people, int limit)
+		{
+			Array.Sort(people);
+			var i = 0;
+			var j = people.Length - 1;
+
+			var result = 0;
+			while (i <= j)
+			{
+				result++;
+				if (people[i] + people[j] <= limit)
+				{
+					i++;
+				}
+				j--;
+			}
+			return result;
+		}
+	}
+}
