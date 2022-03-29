20f68fe0a2e7d5f44e1d41fd96676156a881c324
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-03-29 14:46:11 +0300

    287

---
+++ LeetCode/Algorithms/287.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _287
+	{
+		public static int FindDuplicate(int[] nums)
+		{
+			var slow = nums[0];
+			var fast = nums[0];
+			do
+			{
+				slow = nums[slow];
+				fast = nums[nums[fast]];
+			} while (slow != fast);
+
+			slow = nums[0];
+			while (slow != fast)
+			{
+				slow = nums[slow];
+				fast = nums[fast];
+			}
+			return fast;
+		}
+	}
+}
