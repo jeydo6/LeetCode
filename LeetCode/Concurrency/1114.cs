4d48b6a7f2fd5ee5ac5277dbf8ee29cf3e2a36f6
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-01 00:10:01 +0300

    1114

---
+++ LeetCode/Concurrency/1114.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace LeetCode.Concurrency
+{
+	internal class _1114
+	{
+		public class Foo
+		{
+			private readonly Semaphore _first;
+			private readonly Semaphore _second;
+			private readonly Semaphore _third;
+
+			public Foo()
+			{
+				_first = new Semaphore(1, 1);
+				_second = new Semaphore(0, 1);
+				_third = new Semaphore(0, 1);
+			}
+
+			public void First(Action printFirst)
+			{
+				_first.WaitOne();
+				printFirst();
+				_second.Release();
+			}
+
+			public void Second(Action printSecond)
+			{
+				_second.WaitOne();
+				printSecond();
+				_third.Release();
+			}
+
+			public void Third(Action printThird)
+			{
+				_third.WaitOne();
+				printThird();
+				_first.Release();
+			}
+		}
+	}
+}
