9ea35b16856a8b68ba0dd6d83f781d495cd26bd7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-02 23:23:11 +0300

    696

---
+++ LeetCode/Algorithms/696.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _696
+	{
+		public static int CountBinarySubstrings(string s)
+		{
+			var result = 0;
+			var previous = 0;
+			var current = 1;
+			for (var i = 1; i < s.Length; i++)
+			{
+				if (s[i - 1] != s[i])
+				{
+					result += Math.Min(previous, current);
+					previous = current;
+					current = 1;
+				}
+				else
+				{
+					current++;
+				}
+			}
+			return result + Math.Min(previous, current);
+		}
+	}
+}
