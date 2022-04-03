57f6b6a902c6087bdae0fabb0c8b8cb951ab1bb7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-03 16:42:28 +0300

    535

---
+++ LeetCode/Algorithms/535.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _535
+	{
+		public class Codec
+		{
+			private static readonly string baseUrl = "http://tinyurl.com/";
+
+			public static string Encode(string longUrl)
+			{
+				var bytes = Encoding.UTF8.GetBytes(longUrl);
+				return baseUrl + Convert.ToBase64String(bytes)
+					.Replace("/", "-")
+					.Replace("+", "_");
+			}
+
+			public static string Decode(string shortUrl)
+			{
+				var bytes = Convert.FromBase64String(
+					shortUrl[baseUrl.Length..]
+						.Replace("-", "/")
+						.Replace("_", "+")
+				);
+				return Encoding.UTF8.GetString(bytes);
+			}
+		}
+	}
+}
