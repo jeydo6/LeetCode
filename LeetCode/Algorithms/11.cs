c78d51f7f6caaf7695bd2798e0ff9c556d66b2f2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-05 20:57:49 +0300

    11

---
+++ LeetCode/Algorithms/11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _11
+	{
+		public static int MaxArea(int[] height)
+		{
+			var result = 0;
+			var lo = 0;
+			var hi = height.Length - 1;
+			while (lo < hi)
+			{
+				result = Math.Max(
+					result,
+					Math.Min(height[lo], height[hi]) * (hi - lo)
+				);
+
+				if (height[lo] < height[hi])
+				{
+					lo++;
+				}
+				else
+				{
+					hi--;
+				}
+			}
+			return result;
+		}
+	}
+}
