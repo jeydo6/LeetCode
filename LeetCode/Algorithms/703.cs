ab00a0ebbd146ab442793cbfcbc84c13779118c6
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-08 08:42:18 +0300

    703

---
+++ LeetCode/Algorithms/703.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _703
+	{
+		public class KthLargest
+		{
+			private readonly int _k;
+			private readonly PriorityQueue<int, int> _pQueue;
+
+			public KthLargest(int k, int[] nums)
+			{
+				_k = k;
+				_pQueue = new PriorityQueue<int, int>();
+				for (var i = 0; i < nums.Length; i++)
+				{
+					_pQueue.Enqueue(nums[i], nums[i]);
+				}
+
+				while (_pQueue.Count > k)
+				{
+					_pQueue.Dequeue();
+				}
+			}
+
+			public int Add(int val)
+			{
+				_pQueue.Enqueue(val, val);
+				while (_pQueue.Count > _k)
+				{
+					_pQueue.Dequeue();
+				}
+
+				return _pQueue.Peek();
+			}
+		}
+	}
+}
