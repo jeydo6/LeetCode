d3627f3b310152b474aeca5d638d9237da48b2d0
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-16 17:02:36 +0300

    538

---
+++ LeetCode/Algorithms/538.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _538
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static TreeNode ConvertBST(TreeNode root)
+		{
+			var stack = new Stack<TreeNode>();
+
+			var sum = 0;
+			var node = root;
+			while (stack.Count > 0 || node != null)
+			{
+				while (node != null)
+				{
+					stack.Push(node);
+					node = node.right;
+				}
+
+				node = stack.Pop();
+				sum += node.val;
+				node.val = sum;
+
+				node = node.left;
+			}
+
+			return root;
+		}
+	}
+}
