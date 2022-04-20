1b38f2a83f08974c3cd7244a19d02a6bdd5717b1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-20 10:27:42 +0300

    173

---
+++ LeetCode/Algorithms/173.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _173
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+		
+		public class BSTIterator
+		{
+			private readonly IList<int> _values;
+
+			private int _index;
+
+			public BSTIterator(TreeNode root)
+			{
+				_values = new List<int>();
+				_index = 0;
+
+				Flatten(root);
+			}
+
+			public int Next()
+			{
+				return _values[_index++];
+			}
+
+			public bool HasNext()
+			{
+				return _index + 1 < _values.Count;
+			}
+
+			private void Flatten(TreeNode root)
+			{
+				if (root == null)
+				{
+					return;
+				}
+
+				Flatten(root.left);
+				_values.Add(root.val);
+				Flatten(root.right);
+			}
+		}
+	}
+}
