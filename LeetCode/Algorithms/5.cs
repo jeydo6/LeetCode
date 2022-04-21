77e7416ac2fdb113eccb2babf97db48ffab82a40
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-21 22:51:58 +0300

    5

---
+++ LeetCode/Algorithms/5.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _5
+	{
+		public static string LongestPalindrome(string s)
+		{
+			var n = s.Length;
+			var dp = new bool[n][];
+			for (var i = 0; i < n; i++)
+			{
+				dp[i] = new bool[n];
+			}
+
+			string result = null;
+			for (var i = n - 1; i >= 0; i--)
+			{
+				for (var j = i; j < n; j++)
+				{
+					dp[i][j] = s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1]);
+
+					if (dp[i][j] && (result == null || j - i + 1 > result.Length))
+					{
+						result = s[i..(j + 1)];
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
