713fd4cf18c78d447cffacca5681759584c88f97
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-21 11:04:54 +0300

    13

---
+++ LeetCode/Algorithms/13.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _13
+	{
+		private readonly static IDictionary<char, int> _dictionary = new Dictionary<char, int>
+		{
+			['M'] = 1000,
+			['D'] = 500,
+			['C'] = 100,
+			['L'] = 50,
+			['X'] = 10,
+			['V'] = 5,
+			['I'] = 1
+		};
+
+		public static int RomanToInt(string s)
+		{
+			var last = _dictionary[s[^1]];
+			var result = last;
+			for (var i = s.Length - 2; i >= 0; i--)
+			{
+				var current = _dictionary[s[i]];
+				if (current < last)
+				{
+					result -= current;
+				}
+				else
+				{
+					result += current;
+				}
+				last = current;
+			}
+			return result;
+		}
+	}
+}
