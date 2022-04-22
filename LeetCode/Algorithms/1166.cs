e4dd74baad8982381e45d7faa6be61ad75d9de08
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-22 23:40:53 +0300

    1166

---
+++ LeetCode/Algorithms/1166.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	internal class _1166
+	{
+		public class FileSystem
+		{
+			private class TrieNode
+			{
+				public TrieNode(string name)
+				{
+					Name = name;
+					Value = -1;
+					Children = new Dictionary<string, TrieNode>();
+				}
+
+				public string Name { get; set; }
+
+				public int Value { get; set; }
+
+				public IDictionary<string, TrieNode> Children { get; }
+			}
+
+			private readonly TrieNode _root;
+
+			public FileSystem()
+			{
+				_root = new TrieNode("");
+			}
+
+			public bool CreatePath(string path, int value)
+			{
+				var node = _root;
+				var segments = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+				for (var i = 0; i < segments.Length; i++)
+				{
+					if (!node.Children.ContainsKey(segments[i]))
+					{
+						if (i == segments.Length - 1)
+						{
+							node.Children[segments[i]] = new TrieNode(segments[i]);
+						}
+						else
+						{
+							return false;
+						}
+						
+					}
+					node = node.Children[segments[i]];
+				}
+
+				if (node.Value != -1)
+				{
+					return false;
+				}
+
+				node.Value = value;
+				return true;
+			}
+
+			public int Get(string path)
+			{
+				var node = _root;
+				var segments = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+				for (var i = 0; i < segments.Length; i++)
+				{
+					if (!node.Children.ContainsKey(segments[i]))
+					{
+						return -1;
+					}
+					node = node.Children[segments[i]];
+				}
+				return node.Value;
+			}
+		}
+	}
+}
