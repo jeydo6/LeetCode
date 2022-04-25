d930dcde999a11145970622c9c85d501b7c48e39
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-04-25 18:39:40 +0300

    284

---
+++ LeetCode/Algorithms/284.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _284
+	{
+		public class PeekingIterator
+		{
+			private readonly IEnumerator<int> _iterator;
+
+			private bool _hasNext;
+
+			public PeekingIterator(IEnumerator<int> iterator)
+			{
+				_iterator = iterator;
+				_hasNext = true;
+			}
+
+			public int Peek()
+			{
+				return _iterator.Current;
+			}
+
+			public int Next()
+			{
+				var result = _iterator.Current;
+
+				_hasNext = _iterator.MoveNext();
+
+				return result;
+			}
+
+			public bool HasNext()
+			{
+				return _hasNext;
+			}
+		}
+	}
+}
