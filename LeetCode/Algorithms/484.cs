d6ec0915860a623b3eced2eb4ee496dd52b5c73f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-01 22:06:48 +0300

    484

---
+++ LeetCode/Algorithms/484.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _484
+	{
+		public static int[] FindPermutation(string s)
+		{
+			var result = new int[s.Length + 1];
+			result[0] = 1;
+
+			var i = 1;
+			while (i <= s.Length)
+			{
+				result[i] = i + 1;
+				var j = i;
+				if (s[i - 1] == 'D')
+				{
+					while (i <= s.Length && s[i - 1] == 'D')
+					{
+						i++;
+					}
+
+					var c = i;
+					var k = j - 1;
+					while (k <= i - 1)
+					{
+						result[k++] = c--;
+					}
+				}
+				else
+				{
+					i++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
