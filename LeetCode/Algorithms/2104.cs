4220f93f01fe02f221b67418c964f6354953b460
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-02 23:47:40 +0300

    2104

---
+++ LeetCode/Algorithms/2104.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _2104
+	{
+		public static long SubArrayRanges(int[] nums)
+		{
+			var result = 0L;
+
+			var stack = new Stack<int>();
+			for (var i = 0; i <= nums.Length; i++)
+			{
+				while (stack.Count > 0 && nums[stack.Peek()] > (i == nums.Length ? int.MinValue : nums[i]))
+				{
+					var j = stack.Pop();
+					var k = stack.Count > 0 ? stack.Peek() : -1;
+
+					result -= (long)nums[j] * (i - j) * (j - k);
+				}
+				stack.Push(i);
+			}
+
+			stack.Clear();
+			for (var i = 0; i <= nums.Length; i++)
+			{
+				while (stack.Count > 0 && nums[stack.Peek()] < (i == nums.Length ? int.MaxValue : nums[i]))
+				{
+					var j = stack.Pop();
+					var k = stack.Count > 0 ? stack.Peek() : -1;
+
+					result += (long)nums[j] * (i - j) * (j - k);
+				}
+				stack.Push(i);
+			}
+
+			return result;
+		}
+	}
+}
