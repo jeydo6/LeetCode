373e6e67da6e3ef153abc7b26d75a8ce1cb51486
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-04 08:16:06 +0300

    1679

---
+++ LeetCode/Algorithms/1679.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1679
+	{
+		public static int MaxOperations(int[] nums, int k)
+		{
+			var dict = new Dictionary<int, int>();
+			var count = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var current = nums[i];
+				var complement = k - current;
+
+				if (dict.ContainsKey(complement) && dict[complement] > 0)
+				{
+					dict[complement]--;
+					count++;
+				}
+				else
+				{
+					if (!dict.ContainsKey(current))
+					{
+						dict[current] = 0;
+					}
+					dict[current]++;
+				}
+			}
+			return count;
+		}
+	}
+}
