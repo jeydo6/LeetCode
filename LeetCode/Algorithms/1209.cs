f5e123419adc6080d6b6db73e7e3854ccf59b32f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-06 21:58:12 +0300

    1209

---
+++ LeetCode/Algorithms/1209.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1209
+	{
+		public static string RemoveDuplicates(string s, int k)
+		{
+			var sb = new StringBuilder(s);
+			var counts = new Stack<int>();
+			for (var i = 0; i < sb.Length; ++i)
+			{
+				if (i == 0 || sb[i] != sb[i - 1])
+				{
+					counts.Push(1);
+				}
+				else
+				{
+					var incremented = counts.Pop() + 1;
+					if (incremented == k)
+					{
+						sb.Remove(i - k + 1, k);
+						i -= k;
+					}
+					else
+					{
+						counts.Push(incremented);
+					}
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
