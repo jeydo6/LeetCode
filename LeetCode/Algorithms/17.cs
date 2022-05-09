e0996f96feab337c5ec9d0fc46a05e6c16978fb2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-09 14:45:18 +0300

    17

---
+++ LeetCode/Algorithms/17.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _17
+	{
+		public static IList<string> LetterCombinations(string digits)
+		{
+			if (digits == null || digits.Length == 0)
+			{
+				return new List<string>();
+			}
+
+			var mappings = new string[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+			var queue = new Queue<string>();
+			queue.Enqueue("");
+			while (queue.Peek().Length != digits.Length)
+			{
+				var item = queue.Dequeue();
+				var mapping = mappings[digits[item.Length] - '0'];
+				for (var i = 0; i < mapping.Length; i++)
+				{
+					queue.Enqueue(item + mapping[i]);
+				}
+			}
+			return new List<string>(queue);
+		}
+	}
+}
