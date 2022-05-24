d914dbb388b76218d89020827b619a1445baadd7
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-24 12:27:40 +0300

    32

---
+++ LeetCode/Algorithms/32.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _32
+	{
+		public static int LongestValidParentheses(string s)
+		{
+			var result = 0;
+			var stack = new Stack<int>();
+			stack.Push(-1);
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (s[i] == '(')
+				{
+					stack.Push(i);
+				}
+				else
+				{
+					stack.Pop();
+					if (stack.Count == 0)
+					{
+						stack.Push(i);
+					}
+					else
+					{
+						result = Math.Max(result, i - stack.Peek());
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
