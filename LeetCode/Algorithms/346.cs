bce62e0b37c9ce9ce775b850b21b662199e2cc90
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-27 18:17:30 +0300

    346

---
+++ LeetCode/Algorithms/346.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _346
+	{
+		public class MovingAverage
+		{
+			private readonly int _size;
+			private readonly Queue<int> _queue;
+
+			private int _total;
+
+			public MovingAverage(int size)
+			{
+				_size = size;
+				_queue = new Queue<int>();
+			}
+
+			public double Next(int val)
+			{
+				_queue.Enqueue(val);
+
+				_total += val - (_queue.Count > _size ? _queue.Dequeue() : 0);
+
+				return _total * 1.0 / Math.Min(_size, _queue.Count);
+			}
+		}
+	}
+}
