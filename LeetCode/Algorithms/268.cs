6abb2c2716e413fba7c3251cf38448835583a57d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-28 22:34:15 +0300

    268

---
+++ LeetCode/Algorithms/268.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _268
+	{
+		public static int MissingNumber(int[] nums)
+		{
+			var result = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				result += i + 1 - nums[i];
+			}
+			return result;
+		}
+	}
+}
