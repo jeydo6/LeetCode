de1e22b2c427da1492e8ecbf1f1c766dfff06676
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-30 19:45:04 +0300

    29

---
+++ LeetCode/Algorithms/29.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _29
+	{
+		private const int HalfMinValue = -1073741824;
+
+		public static int Divide(int dividend, int divisor)
+		{
+			if (dividend == int.MinValue && divisor == -1)
+			{
+				return int.MaxValue;
+			}
+
+			if (dividend == int.MinValue && divisor == 1)
+			{
+				return int.MinValue;
+			}
+
+			var negatives = 2;
+			if (dividend > 0)
+			{
+				negatives--;
+				dividend = -dividend;
+			}
+			if (divisor > 0)
+			{
+				negatives--;
+				divisor = -divisor;
+			}
+
+			var maxBit = 0;
+			while (divisor >= HalfMinValue && divisor + divisor >= dividend)
+			{
+				maxBit += 1;
+				divisor += divisor;
+			}
+
+			var quotient = 0;
+			for (var bit = maxBit; bit >= 0; bit--)
+			{
+				if (divisor >= dividend)
+				{
+					quotient -= (1 << bit);
+					dividend -= divisor;
+				}
+				divisor = (divisor + 1) >> 1;
+			}
+
+			if (negatives != 1)
+			{
+				quotient = -quotient;
+			}
+			return quotient;
+		}
+	}
+}
