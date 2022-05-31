ba5a7ea7cbd3a205667dd7a95f0c15fadd38d80d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-05-31 22:20:48 +0300

    1461

---
+++ LeetCode/Algorithms/1461.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEIUM
+	internal class _1461
+	{
+		public static bool HasAllCodes(string s, int k)
+		{
+			var need = 1 << k;
+			var set = new HashSet<string>();
+			for (var i = k; i <= s.Length; i++)
+			{
+				var substring = s[(i - k)..i];
+				if (!set.Contains(substring))
+				{
+					set.Add(substring);
+					need--;
+
+					if (need == 0)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
