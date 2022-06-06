1feaae2616a4ae60bc308f2cb833a42b7d4b952f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-06 21:43:54 +0300

    160

---
+++ LeetCode/Algorithms/160.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _160
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+		}
+
+		public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+		{
+			var nodeA = headA;
+			var nodeB = headB;
+			while (nodeA != nodeB)
+			{
+				nodeA = nodeA == null ? headB : nodeA.next;
+				nodeB = nodeB == null ? headA : nodeB.next;
+			}
+			return nodeA;
+		}
+	}
+}
