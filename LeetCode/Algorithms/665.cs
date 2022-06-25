8a6587bce917885478f17583e38c47cc9104badb
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-25 16:23:15 +0300

    665

---
+++ LeetCode/Algorithms/665.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _665
+	{
+		public static bool CheckPossibility(int[] nums)
+		{
+			var numViolations = 0;
+			for (var i = 1; i < nums.Length; i++)
+			{
+
+				if (nums[i - 1] > nums[i])
+				{
+
+					if (numViolations == 1)
+					{
+						return false;
+					}
+
+					numViolations++;
+
+					if (i < 2 || nums[i - 2] <= nums[i])
+					{
+						nums[i - 1] = nums[i];
+					}
+					else
+					{
+						nums[i] = nums[i - 1];
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
