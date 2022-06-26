cfc74d4ec525b49f4833feb63d62e22bf03ec8dc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-26 18:00:25 +0300

    1423

---
+++ LeetCode/Algorithms/1423.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1423
+	{
+		public static int MaxScore(int[] cardPoints, int k)
+		{
+			var frontScore = 0;
+			var rearScore = 0;
+
+			for (var i = 0; i < k; i++)
+			{
+				frontScore += cardPoints[i];
+			}
+
+			var maxScore = frontScore;
+
+			for (var i = k - 1; i >= 0; i--)
+			{
+				rearScore += cardPoints[^(k - i)];
+				frontScore -= cardPoints[i];
+
+				maxScore = Math.Max(maxScore, rearScore + frontScore);
+			}
+
+			return maxScore;
+		}
+	}
+}
