4bc1c8a85e69ab00e41cb85a313d3694b932c882
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-30 22:43:50 +0300

    462

---
+++ LeetCode/Algorithms/462.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _462
+	{
+		public static int MinMoves2(int[] nums)
+		{
+			Array.Sort(nums);
+
+			var result = 0;
+
+			var lo = 0;
+			var hi = nums.Length - 1;
+			while (lo < hi)
+			{
+				result += nums[hi--] - nums[lo++];
+			}
+
+			return result;
+		}
+	}
+}
