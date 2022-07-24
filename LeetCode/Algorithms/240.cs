c66e87e7ac474f3adde1a0f810f237b1bca7388d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-07-24 16:37:00 +0300

    240

---
+++ LeetCode/Algorithms/240.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _240
+	{
+		public static bool SearchMatrix(int[][] matrix, int target)
+		{
+			var row = matrix.Length - 1;
+			var col = 0;
+
+			while (row >= 0 && col < matrix[0].Length)
+			{
+				if (matrix[row][col] > target)
+				{
+					row--;
+				}
+				else if (matrix[row][col] < target)
+				{
+					col++;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
