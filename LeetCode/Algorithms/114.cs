ad206753462a936b916d1db682acd9761994466c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-07-27 22:07:37 +0300

    114

---
+++ LeetCode/Algorithms/114.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _114
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static void Flatten(TreeNode root)
+		{
+			if (root == null)
+			{
+				return;
+			}
+
+			var node = root;
+			while (node != null)
+			{
+
+				if (node.left != null)
+				{
+					var rightmost = node.left;
+					while (rightmost.right != null)
+					{
+						rightmost = rightmost.right;
+					}
+
+					rightmost.right = node.right;
+					node.right = node.left;
+					node.left = null;
+				}
+
+				node = node.right;
+			}
+		}
+	}
+}
