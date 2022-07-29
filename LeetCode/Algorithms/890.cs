4e0415a2c1f390523d8eb6d37bd3509572e55378
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-07-29 18:28:52 +0300

    890

---
+++ LeetCode/Algorithms/890.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _890
+	{
+		public static IList<string> FindAndReplacePattern(string[] words, string pattern)
+		{
+			var result = new List<string>();
+			for (var i = 0; i < words.Length; i++)
+			{
+				if (IsMatch(words[i], pattern))
+				{
+					result.Add(words[i]);
+				}
+			}
+
+			return result;
+		}
+
+		private static bool IsMatch(string word, string pattern)
+		{
+			var dict = new Dictionary<char, char>();
+			for (var i = 0; i < word.Length; i++)
+			{
+				if (!dict.ContainsKey(word[i]))
+				{
+					dict[word[i]] = pattern[i];
+				}
+				
+				if(dict[word[i]] != pattern[i])
+				{
+					return false;
+				}
+			}
+
+			var seen = new bool[26];
+			foreach (var p in dict.Values)
+			{
+				if (seen[p - 'a'])
+				{
+					return false;
+				}
+				seen[p - 'a'] = true;
+			}
+			return true;
+		}
+	}
+}
