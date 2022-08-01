ddf2cb74374a3d4444ccd8bc1b4d58425f0bbabc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-08-01 22:17:43 +0300

    62

--- LeetCode/Algorithms/62.cs
+++ LeetCode/Algorithms/62.cs
@@ -3,28 +3,28 @@
 	// MEDIUM
 	internal class _62
 	{
-		public static int UniquePaths(int n, int m)
+		public static int UniquePaths(int m, int n)
 		{
-			var dp = new int[n][];
+			var dp = new int[m][];
 
-			for (var i = 0; i < n; i++)
+			for (var i = 0; i < m; i++)
 			{
-				dp[i] = new int[m];
-				for (var j = 0; j < m; j++)
+				dp[i] = new int[n];
+				for (var j = 0; j < n; j++)
 				{
 					dp[i][j] = 1;
 				}
 			}
 
-			for (var i = 1; i < n; i++)
+			for (var i = 1; i < m; i++)
 			{
-				for (var j = 1; j < m; j++)
+				for (var j = 1; j < n; j++)
 				{
 					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 				}
 			}
 
-			return dp[n - 1][m - 1];
+			return dp[m - 1][n - 1];
 		}
 	}
 }
