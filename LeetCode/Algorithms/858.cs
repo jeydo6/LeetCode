b8f54e578fac29593d798c7329996a747045cd3e
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-08-04 21:51:32 +0300

    858

---
+++ LeetCode/Algorithms/858.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _858
+	{
+		public static int MirrorReflection(int p, int q)
+		{
+			while (p % 2 == 0 && q % 2 == 0)
+			{
+				p >>= 1;
+				q >>= 1;
+			}
+
+			return 1 - p % 2 + q % 2;
+		}
+	}
+}
