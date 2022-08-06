6a6da578a00727db6e07d875f5b39b55b3b4008d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-08-06 21:04:11 +0300

    458

---
+++ LeetCode/Algorithms/458.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _458
+	{
+		public static int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+		{
+			return (int)Math.Ceiling(
+				Math.Log(buckets) / Math.Log(minutesToTest / minutesToDie + 1)
+			);
+		}
+	}
+}
