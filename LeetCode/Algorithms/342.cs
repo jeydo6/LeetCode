15077098c8800b54221cf25853cf8b2082685729
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-08-22 14:30:47 +0300

    342

---
+++ LeetCode/Algorithms/342.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _342
+	{
+		public static bool IsPowerOfFour(int n)
+		{
+			return (n > 0) && (Math.Log(n) / Math.Log(2) % 2 == 0);
+		}
+	}
+}
