20852981b7aeb072043460bdeccfded05020a9bc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-08-23 22:51:55 +0300

    234

---
+++ LeetCode/Algorithms/234.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _234
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static bool IsPalindrome(ListNode head)
+		{
+			var stack = new Stack<ListNode>();
+			
+			var node = head;
+			while (node != null)
+			{
+				stack.Push(node);
+				node = node.next;
+			}
+
+			while (stack.Count > 0)
+			{
+				var current = stack.Pop();
+				if (head.val != current.val)
+				{
+					return false;
+				}
+
+				head = head.next;
+			}
+
+			return true;
+		}
+	}
+}
