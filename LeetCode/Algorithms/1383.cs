459c7f41684aad4316cae3eece40d45c5d3e5a62
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-09-11 22:49:08 +0300

    1383

---
+++ LeetCode/Algorithms/1383.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1383
+{
+	private const int Modulo = 1_000_000_007;
+
+	public static int MaxPerformance(int n, int[] speed, int[] efficiency, int k)
+	{
+		var candidates = new List<(int Efficiency, int Speed)>();
+		for (var i = 0; i < n; i++)
+		{
+			candidates.Add((efficiency[i], speed[i]));
+		}
+
+		candidates.Sort((o1, o2) => o1.Efficiency - o2.Efficiency);
+
+		var speedHeap = new PriorityQueue<int, int>();
+
+		var speedSum = 0L;
+		var maxPerformance = 0L;
+		for (var i = 0; i < candidates.Count; i++)
+		{
+			var (currentEfficiency, currentSpeed) = candidates[i];
+			if (speedHeap.Count > k - 1)
+			{
+				speedSum -= speedHeap.Dequeue();
+			}
+			speedHeap.Enqueue(currentSpeed, currentSpeed);
+
+			speedSum += currentSpeed;
+			maxPerformance = Math.Max(maxPerformance, speedSum * currentEfficiency);
+		}
+		return (int)(maxPerformance % Modulo);
+	}
+}
