cb9794a19066224dba1b09d42f74380d0cf52cf1
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-09-21 16:39:41 +0300

    985

--- LeetCode/Algorithms/985.cs
+++ LeetCode/Algorithms/985.cs
@@ -1,41 +1,40 @@
-﻿namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms;
+
+// EASY
+internal class _985
 {
-	// EASY
-	internal class _985
+	public static int[] SumEvenAfterQueries(int[] nums, int[][] queries)
 	{
-		public static int[] SumEvenAfterQueries(int[] arr, int[][] queries)
+		var result = new int[queries.Length];
+
+		var sum = 0;
+		foreach (var num in nums)
 		{
-			var result = new int[queries.Length];
-
-			var sum = 0;
-			foreach (var a in arr)
+			if (num % 2 == 0)
 			{
-				if (a % 2 == 0)
-				{
-					sum += a;
-				}
+				sum += num;
 			}
-
-			for (var i = 0; i < queries.Length; i++)
-			{
-				var index = queries[i][1];
-				var val = queries[i][0];
-
-				if (arr[index] % 2 == 0)
-				{
-					sum -= arr[index];
-				}
-
-				arr[index] += val;
-
-				if (arr[index] % 2 == 0)
-				{
-					sum += arr[index];
-				}
-
-				result[i] = sum;
-			}
-			return result;
 		}
+
+		for (var i = 0; i < queries.Length; i++)
+		{
+			var index = queries[i][1];
+			var val = queries[i][0];
+
+			if (nums[index] % 2 == 0)
+			{
+				sum -= nums[index];
+			}
+
+			nums[index] += val;
+
+			if (nums[index] % 2 == 0)
+			{
+				sum += nums[index];
+			}
+
+			result[i] = sum;
+		}
+		return result;
 	}
 }
