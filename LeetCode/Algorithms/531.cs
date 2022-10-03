806d8f5696b986904216764607bb34e6009db4a5
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-03 23:58:52 +0300

    531

---
+++ LeetCode/Algorithms/531.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _531
+{
+	public static int FindLonelyPixel(char[][] picture)
+	{
+		var n = picture.Length;
+		var m = picture[0].Length;
+
+		var rowCount = new int[n];
+		var columnCount = new int[m];
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < m; j++)
+			{
+				if (picture[i][j] == 'B')
+				{
+					rowCount[i]++;
+					columnCount[j]++;
+				}
+			}
+		}
+
+		var result = 0;
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < m; j++)
+			{
+				if (picture[i][j] == 'B' && rowCount[i] == 1 && columnCount[j] == 1)
+				{
+					result++;
+				}
+			}
+		}
+
+		return result;
+	}
+}
