82e28effd8af9c2f58e25c054fa5b1793367ff58
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-04 18:45:00 +0300

    112

--- LeetCode/Algorithms/112.cs
+++ LeetCode/Algorithms/112.cs
@@ -1,34 +1,33 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _112
 {
-	// EASY
-	internal class _112
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static bool HasPathSum(TreeNode root, int targetSum)
+	{
+		if (root == null)
+		{
+			return false;
 		}
 
-		public static bool HasPathSum(TreeNode root, int targetSum)
+		if (root.left == null && root.right == null && targetSum - root.val == 0)
 		{
-			if (root == null)
-			{
-				return false;
-			}
-
-			if (root.left == null && root.right == null && targetSum - root.val == 0)
-			{
-				return true;
-			}
-
-			return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+			return true;
 		}
+
+		return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
 	}
 }
