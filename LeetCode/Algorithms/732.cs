805a5b80a33b4789635f018001ea28f40c154fda
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-07 22:02:46 +0300

    732

---
+++ LeetCode/Algorithms/732.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _732
+{
+	public class MyCalendarThree
+	{
+		private readonly IDictionary<int, int> _dict;
+
+		public MyCalendarThree()
+		{
+			_dict = new SortedDictionary<int, int>();
+		}
+
+		public int Book(int start, int end)
+		{
+			if (!_dict.ContainsKey(start))
+			{
+				_dict[start] = 0;
+			}
+			_dict[start]++;
+
+			if (!_dict.ContainsKey(end))
+			{
+				_dict[end] = 0;
+			}
+			_dict[end]--;
+
+			var result = 0;
+			var current = 0;
+			foreach (var value in _dict.Values)
+			{
+				current += value;
+				if (current > result)
+				{
+					result = current;
+				}
+			}
+
+			return result;
+		}
+	}
+}
