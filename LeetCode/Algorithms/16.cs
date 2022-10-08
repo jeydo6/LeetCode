6621ec21f299d25d7b34a582cde42400c5fe9471
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-08 22:06:56 +0300

    16

--- LeetCode/Algorithms/16.cs
+++ LeetCode/Algorithms/16.cs
@@ -1,53 +1,53 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _16
 {
-	class _16
+	public static int ThreeSumClosest(int[] nums, int target)
 	{
-		public static int ThreeSumClosest(int[] numbers, int target)
+		var result = int.MaxValue;
+
+		Array.Sort(nums);
+
+		var min = int.MaxValue;
+		for (var i = 0; i < nums.Length - 2; i++)
 		{
-			var result = int.MaxValue;
+			var left = i + 1;
+			var right = nums.Length - 1;
 
-			Array.Sort(numbers);
-
-			var min = int.MaxValue;
-			for (var i = 0; i < numbers.Length - 2; i++)
+			while (left < right)
 			{
-				var left = i + 1;
-				var right = numbers.Length - 1;
+				var sum = nums[i] + nums[left] + nums[right];
 
-				while (left < right)
+				if (sum == target)
 				{
-					var sum = numbers[i] + numbers[left] + numbers[right];
-
-					if (sum == target)
+					min = 0;
+					result = target;
+					break;
+				}
+				else
+				{
+					var d = Math.Abs(sum - target);
+					if (d < min)
 					{
-						min = 0;
-						result = target;
-						break;
+						min = d;
+						result = sum;
+					}
+
+					if (sum < target)
+					{
+						left++;
 					}
 					else
 					{
-						var d = Math.Abs(sum - target);
-						if (d < min)
-						{
-							min = d;
-							result = sum;
-						}
-
-						if (sum < target)
-						{
-							left++;
-						}
-						else
-						{
-							right--;
-						}
+						right--;
 					}
 				}
 			}
-
-			return result;
 		}
+
+		return result;
 	}
 }
