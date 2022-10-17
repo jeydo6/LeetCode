f8a3592126869e1873415f61a29019cd01b7b4db
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-17 18:10:04 +0300

    1832

--- LeetCode/Algorithms/1832.cs
+++ LeetCode/Algorithms/1832.cs
@@ -1,29 +1,29 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1832
 {
-	class _1832
+	public static bool CheckIfPangram(string sentence)
 	{
-		public static bool CheckIfPangram(string sentence)
+		if (sentence == null || sentence.Length < 26)
 		{
-			if (sentence == null || sentence.Length < 26)
+			return false;
+		}
+
+		var dict = new int[26];
+		foreach (var ch in sentence)
+		{
+			dict[ch - 'a']++;
+		}
+
+		foreach (var item  in dict)
+		{
+			if (item == 0)
 			{
 				return false;
 			}
-
-			var dict = new int[26];
-			foreach (var ch in sentence)
-			{
-				dict[ch - 'a']++;
-			}
-
-			foreach (var item  in dict)
-			{
-				if (item == 0)
-				{
-					return false;
-				}
-			}
-
-			return true;
 		}
+
+		return true;
 	}
 }
