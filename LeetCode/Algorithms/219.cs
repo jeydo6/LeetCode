97e76976573c1e9c1dec80f0f26dba564b05c74e
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-21 22:46:31 +0300

    219

---
+++ LeetCode/Algorithms/219.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _219
+{
+	public static bool ContainsNearbyDuplicate(int[] nums, int k)
+	{
+		var hashSet = new HashSet<int>();
+		for (var i = 0; i < nums.Length; i++)
+		{
+			if (hashSet.Contains(nums[i]))
+			{
+				return true;
+			}
+
+			hashSet.Add(nums[i]);
+
+			if (hashSet.Count > k)
+			{
+				hashSet.Remove(nums[i - k]);
+			}
+		}
+
+		return false;
+	}
+}
