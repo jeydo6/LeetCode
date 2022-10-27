b1079ff338deabeb6ffd821a0d49cc57d7b0e44b
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-27 19:46:16 +0300

    835

---
+++ LeetCode/Algorithms/835.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _835
+{
+	public static int LargestOverlap(int[][] img1, int[][] img2)
+	{
+		var n = img1.Length;
+		var padded = new int[3 * n - 2][];
+		for (var i = 0; i < 3 * n - 2; i++)
+		{
+			padded[i] = new int[3 * n - 2];
+		}
+
+		for (var row = 0; row < n; row++)
+		{
+			for (var col = 0; col < n; col++)
+			{
+				padded[row + n - 1][col + n - 1] = img2[row][col];
+			}
+		}
+
+		var maxOverlaps = 0;
+		for (var x = 0; x < 2 * n - 1; x++)
+		{
+			for (var y = 0; y < 2 * n - 1; ++y)
+			{
+				maxOverlaps = Math.Max(
+					maxOverlaps,
+					LargestOverlap(img1, padded, x, y)
+				);
+			}
+		}
+
+		return maxOverlaps;
+	}
+
+	private static int LargestOverlap(int[][] img, int[][] kernel, int x, int y)
+	{
+		var n = img.Length;
+		var result = 0;
+		for (var row = 0; row < n; row++)
+		{
+			for (var col = 0; col < n; col++)
+			{
+				result += img[row][col] * kernel[row + y][col + x];
+			}
+		}
+
+		return result;
+	}
+}
