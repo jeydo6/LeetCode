d32cb9f504a6d1615962821d1c728ab57d669733
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-30 15:34:35 +0300

    487

---
+++ LeetCode/Algorithms/487.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _487
+{
+	public static int FindMaxConsecutiveOnes(int[] nums)
+	{
+		var result = 0;
+		var left = 0;
+		var right = 0;
+		var zeroes = 0;
+
+		while (right < nums.Length)
+		{
+			if (nums[right] == 0)
+			{
+				zeroes++;
+			}
+
+			while (zeroes == 2)
+			{
+				if (nums[left] == 0)
+				{
+					zeroes--;
+				}
+				left++;
+			}
+
+			result = Math.Max(result, right - left + 1);
+
+			right++;
+		}
+		return result;
+	}
+}
