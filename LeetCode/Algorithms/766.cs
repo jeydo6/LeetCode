cedb7b99f61ebb5eb31aa88e4a51bfdc0bc69503
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-10-31 20:00:07 +0300

    766

--- LeetCode/Algorithms/766.cs
+++ LeetCode/Algorithms/766.cs
@@ -1,32 +1,29 @@
-﻿using System;
+﻿namespace Leetcode.Algorithms;
 
-namespace Leetcode.Algorithms
+// EASY
+internal class _766
 {
-	// EASY
-	internal class _766
+	public static bool IsToeplitzMatrix(int[][] matrix)
 	{
-		public static bool IsToeplitzMatrix(int[][] matrix)
+		var n = matrix.Length;
+		var m = matrix[0].Length;
+
+		if (n == 1 || m == 1)
 		{
-			var n = matrix.Length;
-			var m = matrix[0].Length;
-
-			if (n == 1 || m == 1)
-			{
-				return true;
-			}
-
-			for (var i = 1; i < n; i++)
-			{
-				for (var j = 1; j < m; j++)
-				{
-					if (matrix[i - 1][j - 1] != matrix[i][j])
-					{
-						return false;
-					}
-				}
-			}
-
 			return true;
 		}
+
+		for (var i = 1; i < n; i++)
+		{
+			for (var j = 1; j < m; j++)
+			{
+				if (matrix[i - 1][j - 1] != matrix[i][j])
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }
