97022e1e58a5b5115c567504015854c3daaf2c2d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-01 19:33:08 +0300

    1198

---
+++ LeetCode/Algorithms/1198.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1198
+{
+	public static int SmallestCommonElement(int[][] mat)
+	{
+		var count = new int[10001];
+		var n = mat.Length;
+		var m = mat[0].Length;
+		for (var j = 0; j < m; ++j)
+		{
+			for (var i = 0; i < n; ++i)
+			{
+				if (++count[mat[i][j]] == n)
+				{
+					return mat[i][j];
+				}
+			}
+		}
+
+		return -1;
+	}
+}
