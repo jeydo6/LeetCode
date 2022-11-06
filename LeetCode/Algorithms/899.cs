48e4c7bd19b1842efa628e74e5001245466db18f
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-06 21:32:55 +0300

    899

--- LeetCode/Algorithms/899.cs
+++ LeetCode/Algorithms/899.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _899
 {
-	// HARD
-	class _899
+	public static string OrderlyQueue(string s, int k)
 	{
-		public static string OrderlyQueue(string s, int k)
+		if (k > 1)
 		{
-			if (k > 1)
+			var chars = s.ToCharArray();
+			Array.Sort(chars);
+			return new string(chars);
+		}
+		else
+		{
+			var result = s;
+			for (var i = 1; i < s.Length; i++)
 			{
-				var chars = s.ToCharArray();
-				Array.Sort(chars);
-				return new string(chars);
-			}
-			else
-			{
-				var result = s;
-				for (var i = 1; i < s.Length; i++)
+				var temp = s[i..] + s[..i];
+				if (string.Compare(result, temp) > 0)
 				{
-					var temp = s[i..] + s[..i];
-					if (string.Compare(result, temp) > 0)
-					{
-						result = temp;
-					}
+					result = temp;
 				}
-				return result;
 			}
+			return result;
 		}
 	}
 }
