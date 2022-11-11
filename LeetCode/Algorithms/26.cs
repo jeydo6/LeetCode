46f82c84a8ac1cb623dc046655ae657b7c81e435
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-11 14:33:35 +0300

    26

---
+++ LeetCode/Algorithms/26.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _26
+{
+	public static int RemoveDuplicates(int[] nums)
+	{
+		var result = 1;
+		for (var i = 1; i < nums.Length; i++)
+		{
+			if (nums[i - 1] != nums[i])
+			{
+				nums[result] = nums[i];
+				result++;
+			}
+		}
+		return result;
+	}
+}
