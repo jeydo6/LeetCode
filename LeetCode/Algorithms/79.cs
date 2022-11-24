6c0a996216bb3e55a2a3d1fd5f7a3e5934f1747d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-24 22:06:25 +0300

    79

--- LeetCode/Algorithms/79.cs
+++ LeetCode/Algorithms/79.cs
@@ -1,45 +1,44 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _79
 {
-	// MEDIUM
-	internal class _79
+	public static bool Exist(char[][] board, string word)
 	{
-		public static bool Exist(char[][] board, string word)
+		for (var x = 0; x < board.Length; x++)
 		{
-			for (var x = 0; x < board.Length; x++)
+			for (var y = 0; y < board[x].Length; y++)
 			{
-				for (var y = 0; y < board[x].Length; y++)
+				if (Exist(board, word, x, y, 0))
 				{
-					if (Exist(board, word, x, y, 0))
-					{
-						return true;
-					}
+					return true;
 				}
 			}
+		}
+		return false;
+	}
+
+	public static bool Exist(char[][] board, string word, int x, int y, int i)
+	{
+		if (i == word.Length)
+		{
+			return true;
+		}
+		if (x < 0 || x == board.Length || y < 0 || y == board[x].Length)
+		{
 			return false;
 		}
-
-		public static bool Exist(char[][] board, string word, int x, int y, int i)
+		if (board[x][y] != word[i])
 		{
-			if (i == word.Length)
-			{
-				return true;
-			}
-			if (x < 0 || x == board.Length || y < 0 || y == board[x].Length)
-			{
-				return false;
-			}
-			if (board[x][y] != word[i])
-			{
-				return false;
-			}
-			board[x][y] = (char)(board[x][y] ^ 256);
-			var exist =
-				Exist(board, word, x, y + 1, i + 1) ||
-				Exist(board, word, x, y - 1, i + 1) ||
-				Exist(board, word, x + 1, y, i + 1) ||
-				Exist(board, word, x - 1, y, i + 1);
-			board[x][y] = (char)(board[x][y] ^ 256);
-			return exist;
+			return false;
 		}
+		board[x][y] = (char)(board[x][y] ^ 256);
+		var exist =
+			Exist(board, word, x, y + 1, i + 1) ||
+			Exist(board, word, x, y - 1, i + 1) ||
+			Exist(board, word, x + 1, y, i + 1) ||
+			Exist(board, word, x - 1, y, i + 1);
+		board[x][y] = (char)(board[x][y] ^ 256);
+		return exist;
 	}
 }
