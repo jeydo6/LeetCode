ee622c953a38bbb9d4efa466a886f86f13b744bd
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-25 18:30:17 +0300

    907

---
+++ LeetCode/Algorithms/907.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _907
+{
+	private const int Modulo = 1000000007;
+
+	public static int SumSubarrayMins(int[] arr)
+	{
+		var stack = new Stack<int>();
+
+		var dp = new int[arr.Length];
+		for (var i = 0; i < arr.Length; i++)
+		{
+			while (stack.Count > 0 && arr[stack.Peek()] >= arr[i])
+			{
+				stack.Pop();
+			}
+
+			if (stack.Count > 0)
+			{
+				var previousSmaller = stack.Peek();
+				dp[i] = dp[previousSmaller] + (i - previousSmaller) * arr[i];
+			}
+			else
+			{
+				dp[i] = (i + 1) * arr[i];
+			}
+
+			stack.Push(i);
+		}
+
+		var result = 0L;
+		for (var i = 0; i < dp.Length; i++)
+		{
+			result += dp[i];
+			result %= Modulo;
+		}
+
+		return (int)result;
+	}
+}
