440fd788c8f01b6733eb4d6fd049547bec5a300d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-11-27 20:41:40 +0300

    446

--- LeetCode/Algorithms/446.cs
+++ LeetCode/Algorithms/446.cs
@@ -1,38 +1,37 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _446
 {
-	// HARD
-	class _446
+	public static int NumberOfArithmeticSlices(int[] nums)
 	{
-		public static int NumberOfArithmeticSlices(int[] nums)
+		var result = 0;
+
+		var dict = new Dictionary<int, int>[nums.Length];
+
+		for (var i = 0; i < nums.Length; i++)
 		{
-			var result = 0;
-
-			var dict = new Dictionary<int, int>[nums.Length];
-
-			for (var i = 0; i < nums.Length; i++)
+			dict[i] = new Dictionary<int, int>(i);
+			for (var j = 0; j < i; j++)
 			{
-				dict[i] = new Dictionary<int, int>(i);
-				for (var j = 0; j < i; j++)
+				var diff = (long)nums[i] - nums[j];
+				if (diff <= int.MinValue || diff > int.MaxValue)
 				{
-					var diff = (long)nums[i] - nums[j];
-					if (diff <= int.MinValue || diff > int.MaxValue)
-					{
-						continue;
-					}
-
-					var key = (int)diff;
-					dict[i].TryGetValue(key, out var value1);
-					dict[j].TryGetValue(key, out var value2);
-
-					result += value2;
-
-					dict[i][key] = value1 + value2 + 1;
+					continue;
 				}
-			}
 
-			return result;
+				var key = (int)diff;
+				dict[i].TryGetValue(key, out var value1);
+				dict[j].TryGetValue(key, out var value2);
+
+				result += value2;
+
+				dict[i][key] = value1 + value2 + 1;
+			}
 		}
+
+		return result;
 	}
 }
