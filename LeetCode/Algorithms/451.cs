0ee5fd8e396fa6e5190c1d292ee598bbe134e566
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-12-03 21:23:14 +0300

    451

--- LeetCode/Algorithms/451.cs
+++ LeetCode/Algorithms/451.cs
@@ -1,43 +1,43 @@
 using System.Collections.Generic;
+using System.Text;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _451
 {
-	// MEDIUM
-	internal class _451
+	public static string FrequencySort(string s)
 	{
-		public static string FrequencySort(string s)
+		var dict = new Dictionary<char, int>();
+		foreach (var ch in s)
 		{
-			var dict = new Dictionary<char, int>();
-			foreach (var ch in s)
+			if (!dict.ContainsKey(ch))
 			{
-				if (!dict.ContainsKey(ch))
-				{
-					dict[ch] = 0;
-				}
-				dict[ch]++;
+				dict[ch] = 0;
 			}
-        
-			var buckets = new string[s.Length + 1];
-			foreach (var (key, value) in dict)
-			{
-				buckets[value] ??= "";
-				buckets[value] += key;
-			}
-        
-			var result = "";
-			for (var i = buckets.Length - 1; i >= 0; i--)
-			{
-				if (buckets[i] == null)
-				{
-					continue;
-				}
-
-				for (var j = 0; j < buckets[i].Length; j++)
-				{
-					result += new string(buckets[i][j], i);
-				}
-			}
-			return result;
+			dict[ch]++;
 		}
+
+		var buckets = new string[s.Length + 1];
+		foreach (var (key, value) in dict)
+		{
+			buckets[value] ??= "";
+			buckets[value] += key;
+		}
+
+		var result = new StringBuilder();
+		for (var i = buckets.Length - 1; i >= 0; i--)
+		{
+			if (buckets[i] == null)
+			{
+				continue;
+			}
+
+			for (var j = 0; j < buckets[i].Length; j++)
+			{
+				result.Append(new string(buckets[i][j], i));
+			}
+		}
+		return result.ToString();
 	}
 }
