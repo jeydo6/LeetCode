8a2925b3d11223cad86bd45a495e529fb56e0303
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-12-04 18:50:52 +0300

    2256

---
+++ LeetCode/Algorithms/2256.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2256
+{
+	public static int MinimumAverageDifference(int[] nums)
+	{
+		var totalSum = 0L;
+		for (var index = 0; index < nums.Length; index++)
+		{
+			totalSum += nums[index];
+		}
+
+		var result = -1;
+		var minimumDifference = int.MaxValue;
+		var currentPrefixSum = 0L;
+		for (var index = 0; index < nums.Length; index++)
+		{
+			currentPrefixSum += nums[index];
+
+			var leftPartAverage = currentPrefixSum;
+			leftPartAverage /= index + 1;
+
+			var rightPartAverage = totalSum - currentPrefixSum;
+			if (index != nums.Length - 1)
+			{
+				rightPartAverage /= nums.Length - index - 1;
+			}
+
+			var currentDifference = (int)Math.Abs(leftPartAverage - rightPartAverage);
+			if (currentDifference < minimumDifference)
+			{
+				minimumDifference = currentDifference;
+				result = index;
+			}
+		}
+
+		return result;
+	}
+}
