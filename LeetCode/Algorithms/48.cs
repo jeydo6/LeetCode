2ad9ffa8c8b070721b00bc693f8f7f3954bb8d1a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:47 +0300

    48

---
+++ LeetCode/Algorithms/48.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _48
+	{
+		public static void Rotate(int[][] matrix)
+		{
+			var n = matrix.Length;
+
+			for (var i = 0; i < n; i++)
+			{
+				for (var j = i + 1; j < n; j++)
+				{
+					(matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+				}
+			}
+
+			for (var i = 0; i < n; i++)
+			{
+				for (var j = 0; j < n / 2; j++)
+				{
+					(matrix[i][j], matrix[i][n - j - 1]) = (matrix[i][n - j - 1], matrix[i][j]);
+				}
+			}
+		}
+	}
+}
