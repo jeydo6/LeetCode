eef325d884161a1ca57a85dbed5c5b41d0aeed09
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:04 +0300

    1832

---
+++ LeetCode/Algorithms/1832.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1832
+	{
+		public static bool CheckIfPangram(string sentence)
+		{
+			if (sentence == null || sentence.Length < 26)
+			{
+				return false;
+			}
+
+			var dict = new int[26];
+			foreach (var ch in sentence)
+			{
+				dict[ch - 'a']++;
+			}
+
+			foreach (var item  in dict)
+			{
+				if (item == 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
