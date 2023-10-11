17160c00db918cde14c5cd311eaa8da4a2e03f7d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:34 +0300

    617

--- LeetCode/Algorithms/617.cs
+++ LeetCode/Algorithms/617.cs
@@ -1,46 +1,47 @@
-using System;
-
-public class _617
+namespace LeetCode.Algorithms
 {
-    public class TreeNode
-    {
-        public TreeNode(Int32 x)
-        {
-            val = x;
-        }
-        public Int32 val;
-        public TreeNode left;
-        public TreeNode right;
-    }
+	public class _617
+	{
+		public class TreeNode
+		{
+			public TreeNode(int x)
+			{
+				val = x;
+			}
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+		}
 
-    public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
-    {
-        if (t1 == null && t2 == null)
-        {
-            return null;
-        }
+		public static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+		{
+			if (t1 == null && t2 == null)
+			{
+				return null;
+			}
 
-        if (t1 == null)
-        {
-            t1 = new TreeNode(0);
-        }
-        if (t2 == null)
-        {
-            t2 = new TreeNode(0);
-        }
+			if (t1 == null)
+			{
+				t1 = new TreeNode(0);
+			}
+			if (t2 == null)
+			{
+				t2 = new TreeNode(0);
+			}
 
-        TreeNode result = new TreeNode(t1.val + t2.val);
+			var result = new TreeNode(t1.val + t2.val);
 
-        if (t1.left != null || t2.left != null)
-        {
-            result.left = MergeTrees(t1.left, t2.left);
-        }
+			if (t1.left != null || t2.left != null)
+			{
+				result.left = MergeTrees(t1.left, t2.left);
+			}
 
-        if (t1.right != null || t2.right != null)
-        {
-            result.right = MergeTrees(t1.right, t2.right);
-        }
+			if (t1.right != null || t2.right != null)
+			{
+				result.right = MergeTrees(t1.right, t2.right);
+			}
 
-        return result;
-    }
+			return result;
+		}
+	}
 }
