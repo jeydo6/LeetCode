3be9962c0e14aaefa443bfd32f32a1880397e981
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:00 +0300

    1486

---
+++ LeetCode/Algorithms/1486.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1486
+	{
+		public static int XorOperation(int n, int start)
+		{
+			var result = start;
+			for (var i = 1; i < n; i++)
+			{
+				result ^= start + 2 * i;
+			}
+			return result;
+		}
+	}
+}
