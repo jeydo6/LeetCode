50eb69a307b8322f8c24cf3379f8fb26d2a34e1d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:03 +0300

    1658

--- LeetCode/Algorithms/1658.cs
+++ LeetCode/Algorithms/1658.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1658
 {
-	// MEDIUM
-	internal class _1658
+	public static int MinOperations(int[] nums, int x)
 	{
-		public static int MinOperations(int[] nums, int x)
+		var current = 0;
+		for (var i = 0; i < nums.Length; i++)
 		{
-			var current = 0;
-			for (var i = 0; i < nums.Length; i++)
-			{
-				current += nums[i];
-			}
-
-			var result = int.MaxValue;
-
-			var left = 0;
-			for (var right = 0; right < nums.Length; right++)
-			{
-				current -= nums[right];
-				while (current < x && left <= right)
-				{
-					current += nums[left];
-					left += 1;
-				}
-				if (current == x)
-				{
-					result = Math.Min(result, nums.Length + left - right - 1);
-				}
-			}
-
-			return result != int.MaxValue ? result : -1;
+			current += nums[i];
 		}
+
+		var result = int.MaxValue;
+
+		var left = 0;
+		for (var right = 0; right < nums.Length; right++)
+		{
+			current -= nums[right];
+			while (current < x && left <= right)
+			{
+				current += nums[left];
+				left += 1;
+			}
+			if (current == x)
+			{
+				result = Math.Min(result, nums.Length + left - right - 1);
+			}
+		}
+
+		return result != int.MaxValue ? result : -1;
 	}
 }
