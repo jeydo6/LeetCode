d595e25f76efedae5c8c2e0693814b51362dfb32
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:50 +0300

    81

---
+++ LeetCode/Algorithms/81.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _81
+	{
+		public static bool Search(int[] nums, int target)
+		{
+			if (nums.Length == 0)
+			{
+				return false;
+			}
+
+			var end = nums.Length - 1;
+			var start = 0;
+
+			while (start <= end)
+			{
+				var mid = (start + end) / 2;
+
+				if (nums[mid] == target)
+				{
+					return true;
+				}
+
+				if (nums[start] == nums[mid])
+				{
+					start++;
+					continue;
+				}
+				var pivotArray = nums[start] <= nums[mid];
+				var targetArray = nums[start] <= target;
+				if (pivotArray ^ targetArray)
+				{
+					if (pivotArray)
+					{
+						start = mid + 1;
+					}
+					else
+					{
+						end = mid - 1;
+					}
+				}
+				else
+				{
+					if (nums[mid] < target)
+					{
+						start = mid + 1;
+					}
+					else
+					{
+						end = mid - 1;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
