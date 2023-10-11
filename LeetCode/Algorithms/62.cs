dac939124918cad04322367300c94b2dd6d310cd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:49 +0300

    62

--- LeetCode/Algorithms/62.cs
+++ LeetCode/Algorithms/62.cs
@@ -1,30 +1,29 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _62
 {
-	// MEDIUM
-	internal class _62
+	public static int UniquePaths(int m, int n)
 	{
-		public static int UniquePaths(int m, int n)
+		var dp = new int[m][];
+
+		for (var i = 0; i < m; i++)
 		{
-			var dp = new int[m][];
-
-			for (var i = 0; i < m; i++)
+			dp[i] = new int[n];
+			for (var j = 0; j < n; j++)
 			{
-				dp[i] = new int[n];
-				for (var j = 0; j < n; j++)
-				{
-					dp[i][j] = 1;
-				}
+				dp[i][j] = 1;
 			}
-
-			for (var i = 1; i < m; i++)
-			{
-				for (var j = 1; j < n; j++)
-				{
-					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-				}
-			}
-
-			return dp[m - 1][n - 1];
 		}
+
+		for (var i = 1; i < m; i++)
+		{
+			for (var j = 1; j < n; j++)
+			{
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+
+		return dp[m - 1][n - 1];
 	}
 }
