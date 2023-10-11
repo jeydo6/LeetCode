b1339cf15a48d0cf2c9f7ec8e506e522f2c07e0a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:15 +0300

    121

---
+++ LeetCode/Algorithms/121.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _121
+	{
+		public static int MaxProfit(int[] prices)
+		{
+			var minPrice = int.MaxValue;
+			var maxProfit = int.MinValue;
+			for (var i = 0; i < prices.Length; i++)
+			{
+				minPrice = Math.Min(minPrice, prices[i]);
+				maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+			}
+			return maxProfit;
+		}
+	}
+}
