281d83cefffd05a3d7545b8091de80a7ee288786
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:33 +0300

    744

---
+++ LeetCode/Algorithms/744.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _744
+{
+	public static char NextGreatestLetter(char[] letters, char target)
+	{
+		var lo = 0;
+		var hi = letters.Length - 1;
+		while (lo <= hi)
+		{
+			var mid = lo + (hi - lo) / 2;
+			if (letters[mid] <= target)
+			{
+				lo = mid + 1;
+			}
+			else
+			{
+				hi = mid - 1;
+			}
+		}
+
+		return lo == letters.Length ? letters[0] : letters[lo];
+	}
+}
