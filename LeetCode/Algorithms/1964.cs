f7edb87381833b0cbede6f8cf9c3aa3320285777
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:08 +0300

    1964

---
+++ LeetCode/Algorithms/1964.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1964
+{
+	public static int[] LongestObstacleCourseAtEachPosition(int[] obstacles)
+	{
+		var result = new int[obstacles.Length];
+		var lis = new int[obstacles.Length];
+		var lisLength = 0;
+		for (var i = 0; i < obstacles.Length; i++)
+		{
+			var height = obstacles[i];
+			var index = BisectRight(lis, height, lisLength);
+			if (index == lisLength)
+			{
+				lisLength++;
+			}
+
+			lis[index] = height;
+			result[i] = index + 1;
+		}
+		return result;
+	}
+
+	private static int BisectRight(int[] arr, int target, int right)
+	{
+		if (right == 0)
+		{
+			return 0;
+		}
+
+		var left = 0;
+		while (left < right)
+		{
+			var mid = left + (right - left) / 2;
+			if (arr[mid] <= target)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid;
+			}
+		}
+		return left;
+	}
+}
