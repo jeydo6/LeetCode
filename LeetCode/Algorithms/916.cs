bee09faf5bf79cb883505ef7775d24b45a4c3a30
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:19 +0300

    916

---
+++ LeetCode/Algorithms/916.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _916
+	{
+		public static IList<string> WordSubsets(string[] words1, string[] words2)
+		{
+			var bMax = Count("");
+			for (var i = 0; i < words2.Length; i++)
+			{
+				var bCount = Count(words2[i]);
+				for (var j = 0; j < bCount.Length; j++)
+				{
+					bMax[j] = Math.Max(bMax[j], bCount[j]);
+				}
+			}
+
+			var result = new List<string>();
+			for (var i = 0; i < words1.Length; i++)
+			{
+				var aCount = Count(words1[i]);
+				for (var j = 0; j < aCount.Length; j++)
+				{
+					if (aCount[j] < bMax[j])
+					{
+						break;
+					}
+
+					if (j == aCount.Length - 1)
+					{
+						result.Add(words1[i]);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static int[] Count(string word)
+		{
+			var result = new int[26];
+			for (var i = 0; i < word.Length; i++)
+			{
+				result[word[i] - 'a']++;
+			}
+			return result;
+		}
+	}
+}
