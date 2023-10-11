62e9f2fc953435849a1e4d9512e84198c91a0b19
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:11:38 +0300

    1528

---
+++ LeetCode/Algorithms/1528.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1528
+	{
+		public static string RestoreString(string str, int[] indices)
+		{
+			var result = new char[str.Length];
+
+			for (var i = 0; i < str.Length; i++)
+			{
+				result[indices[i]] = str[i];
+			}
+
+			return new string(result);
+		}
+	}
+}
