b46a50ce718239de552f7665c50bd3c3287cdc2c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:53 +0300

    1431

--- LeetCode/Algorithms/1431.cs
+++ LeetCode/Algorithms/1431.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1431
 {
-	class _1431
+	public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
 	{
-		public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+		var maxCandies = candies[0];
+		for (var i = 1; i < candies.Length; i++)
 		{
-			var max = candies.Max();
-			return candies
-				.Select(c => c + extraCandies >= max)
-				.ToList();
+			if (candies[i] > maxCandies)
+			{
+				maxCandies = candies[i];
+			}
 		}
+
+		var result = new List<bool>(candies.Length);
+		for (var i = 0; i < candies.Length; i++)
+		{
+			result.Add(candies[i] + extraCandies >= maxCandies);
+		}
+		return result;
 	}
 }
