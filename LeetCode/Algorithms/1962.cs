e689dfc783b6438ace6a560db170bd78bf78c2a4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:13 +0300

    1962

---
+++ LeetCode/Algorithms/1962.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1962
+{
+	public static int MinStoneSum(int[] piles, int k)
+	{
+		var pqueue = new PriorityQueue<int, int>();
+		for (var i = 0; i < piles.Length; i++)
+		{
+			pqueue.Enqueue(piles[i], -piles[i]);
+		}
+
+		for (var i = 0; i < k; i++)
+		{
+			var current = pqueue.Dequeue();
+			var remove = current / 2;
+			pqueue.Enqueue(current - remove, remove - current);
+		}
+
+		var result = 0;
+		while (pqueue.Count > 0)
+		{
+			result += pqueue.Dequeue();
+		}
+		return result;
+	}
+}
