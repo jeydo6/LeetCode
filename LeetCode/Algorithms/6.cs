b82542d314557998af78314617ec1545e3aa309b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:52 +0300

    6

--- LeetCode/Algorithms/6.cs
+++ LeetCode/Algorithms/6.cs
@@ -1,32 +1,31 @@
 ﻿using System.Text;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _6
 {
-	// MEDIUM
-	internal class _6
+	public static string Convert(string s, int numRows)
 	{
-		public static string Convert(string s, int numRows)
+		if (numRows == 1)
 		{
-			if (numRows == 1)
-			{
-				return s;
-			}
-
-			var sb = new StringBuilder();
-			var count = 2 * numRows - 2;
-			for (var i = 0; i < numRows; i++)
-			{
-				for (var j = 0; j + i < s.Length; j += count)
-				{
-					sb.Append(s[j + i]);
-					if (i > 0 && i < numRows - 1 && j + count - i < s.Length)
-					{
-						sb.Append(s[j + count - i]);
-					}
-
-				}
-			}
-			return sb.ToString();
+			return s;
 		}
+
+		var sb = new StringBuilder();
+		var count = 2 * numRows - 2;
+		for (var i = 0; i < numRows; i++)
+		{
+			for (var j = 0; j + i < s.Length; j += count)
+			{
+				sb.Append(s[j + i]);
+				if (i > 0 && i < numRows - 1 && j + count - i < s.Length)
+				{
+					sb.Append(s[j + count - i]);
+				}
+
+			}
+		}
+		return sb.ToString();
 	}
 }
