41b5bb83adba468e5f807b293018c9e52b4ee0ae
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:00 +0300

    258

--- LeetCode/Algorithms/258.cs
+++ LeetCode/Algorithms/258.cs
@@ -1,11 +1,10 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _258
 {
-	// EASY
-	internal class _258
+	public static int AddDigits(int num)
 	{
-		public static int AddDigits(int num)
-		{
-			return 1 + (num - 1) % 9;
-		}
+		return 1 + (num - 1) % 9;
 	}
 }
