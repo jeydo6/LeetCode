de4be46840dd40b47afd78365f1489f0338b0d34
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:03 +0300

    704

---
+++ LeetCode/Algorithms/704.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _704
+	{
+		public static int Search(int[] nums, int target)
+		{
+			var lo = 0;
+			var hi = nums[^1];
+			while (lo < hi)
+			{
+				var mid = lo + (hi - lo) / 2;
+				if (nums[mid] < target)
+				{
+					lo = mid + 1;
+				}
+				else if (nums[mid] > target)
+				{
+					hi = mid - 1;
+				}
+				else
+				{
+					return mid;
+				}
+			}
+			return -1;
+		}
+	}
+}
