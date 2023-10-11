3a42fb857f815b30533ce7ff60ce202299eda4db
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:00 +0300

    1099

---
+++ LeetCode/Algorithms/1099.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1099
+{
+	public static int TwoSumLessThanK(int[] nums, int k)
+	{
+		var count = new int[1001];
+		for (var i = 0; i < nums.Length; i++)
+		{
+			count[nums[i]]++;
+		}
+
+		var result = -1;
+
+		var lo = 1;
+		var hi = 1000;
+		while (lo <= hi)
+		{
+			if (lo + hi >= k || count[hi] == 0)
+			{
+				hi--;
+			}
+			else
+			{
+				if (count[lo] > (lo < hi ? 0 : 1))
+				{
+					result = Math.Max(result, lo + hi);
+				}
+				lo++;
+			}
+		}
+
+		return result;
+	}
+}
