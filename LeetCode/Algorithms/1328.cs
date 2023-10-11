371df60452833adadf6e4ba773d98c190466d2a5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:23 +0300

    1328

--- LeetCode/Algorithms/1328.cs
+++ LeetCode/Algorithms/1328.cs
@@ -1,34 +1,34 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1328
 {
-	internal class _1328
+	public static string BreakPalindrome(string palindrome)
 	{
-		public static string BreakPalindrome(string palindrome)
+		if (palindrome.Length == 1)
 		{
-			if (palindrome.Length == 1)
-			{
-				return "";
-			}
-
-			var index = -1;
-			var chars = palindrome.ToCharArray();
-			for (var i = 0; i < palindrome.Length / 2; i++)
-			{
-				if (chars[i] == 'a')
-				{
-					continue;
-				}
-
-				index = i;
-				chars[i] = 'a';
-
-				break;
-			}
-
-			if (index == -1)
-			{
-				chars[palindrome.Length - 1] = 'b';
-			}
-			return new string(chars);
+			return "";
 		}
+
+		var index = -1;
+		var chars = palindrome.ToCharArray();
+		for (var i = 0; i < palindrome.Length / 2; i++)
+		{
+			if (chars[i] == 'a')
+			{
+				continue;
+			}
+
+			index = i;
+			chars[i] = 'a';
+
+			break;
+		}
+
+		if (index == -1)
+		{
+			chars[palindrome.Length - 1] = 'b';
+		}
+		return new string(chars);
 	}
 }
