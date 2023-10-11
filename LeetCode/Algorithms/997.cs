e1cce475fff2725f39b7c58461eda4b43a067e59
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:42 +0300

    997

--- LeetCode/Algorithms/997.cs
+++ LeetCode/Algorithms/997.cs
@@ -1,24 +1,23 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _997
 {
-	// EASY
-	internal class _997
+	public static int FindJudge(int n, int[][] trust)
 	{
-		public static int FindJudge(int n, int[][] trust)
+		var count = new int[n + 1];
+		for (var i = 0; i < trust.Length; i++)
 		{
-			var count = new int[n + 1];
-			for (var i = 0; i < trust.Length; i++)
-			{
-				count[trust[i][0]]--;
-				count[trust[i][1]]++;
-			}
-			for (var i = 1; i <= n; i++)
-			{
-				if (count[i] == n - 1)
-				{
-					return i;
-				}
-			}
-			return -1;
+			count[trust[i][0]]--;
+			count[trust[i][1]]++;
 		}
+		for (var i = 1; i <= n; i++)
+		{
+			if (count[i] == n - 1)
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 }
