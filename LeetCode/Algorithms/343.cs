98d2462f8f65e9668d45c383e6b3d201c15d7bd5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:14 +0300

    343

---
+++ LeetCode/Algorithms/343.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _343
+{
+	public static int IntegerBreak(int n)
+	{
+		if (n <= 3)
+		{
+			return n - 1;
+		}
+
+		return (n % 3) switch
+		{
+			0 => (int)Math.Pow(3, n / 3),
+			1 => (int)Math.Pow(3, n / 3 - 1) * 4,
+			_ => (int)Math.Pow(3, n / 3) * 2
+		};
+	}
+}
