f639ef1e70eac2e2a9b5b6d85f9fed1da1216851
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:35 +0300

    1351

---
+++ LeetCode/Algorithms/1351.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1351
+	{
+		public static int CountNegatives(int[][] grid)
+		{
+			var n = grid.Length;
+			var m = grid[0].Length;
+
+			var i = n - 1;
+			var j = 0;
+
+			var result = 0;
+			while (i >= 0 && j < m)
+			{
+				if (grid[i][j] < 0)
+				{
+					result += m - j;
+					i--;
+				}
+				else
+				{
+					j++;
+				}
+			}
+			return result;
+		}
+	}
+}
