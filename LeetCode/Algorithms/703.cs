444036aa4f28a81ab4cf1cf3935e922190a65e94
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:21 +0300

    703

--- LeetCode/Algorithms/703.cs
+++ LeetCode/Algorithms/703.cs
@@ -1,40 +1,39 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _703
 {
-	// EASY
-	internal class _703
+	public class KthLargest
 	{
-		public class KthLargest
+		private readonly int _k;
+		private readonly PriorityQueue<int, int> _pQueue;
+
+		public KthLargest(int k, int[] nums)
 		{
-			private readonly int _k;
-			private readonly PriorityQueue<int, int> _pQueue;
-
-			public KthLargest(int k, int[] nums)
+			_k = k;
+			_pQueue = new PriorityQueue<int, int>();
+			for (var i = 0; i < nums.Length; i++)
 			{
-				_k = k;
-				_pQueue = new PriorityQueue<int, int>();
-				for (var i = 0; i < nums.Length; i++)
-				{
-					_pQueue.Enqueue(nums[i], nums[i]);
-				}
-
-				while (_pQueue.Count > k)
-				{
-					_pQueue.Dequeue();
-				}
+				_pQueue.Enqueue(nums[i], nums[i]);
 			}
 
-			public int Add(int val)
+			while (_pQueue.Count > k)
 			{
-				_pQueue.Enqueue(val, val);
-				while (_pQueue.Count > _k)
-				{
-					_pQueue.Dequeue();
-				}
-
-				return _pQueue.Peek();
+				_pQueue.Dequeue();
 			}
+		}
+
+		public int Add(int val)
+		{
+			_pQueue.Enqueue(val, val);
+			while (_pQueue.Count > _k)
+			{
+				_pQueue.Dequeue();
+			}
+
+			return _pQueue.Peek();
 		}
 	}
 }
