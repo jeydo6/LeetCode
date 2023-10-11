ecc5a34fcabeba2c18d677b8e06df406cf19eade
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:16 +0300

    1704

---
+++ LeetCode/Algorithms/1704.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1704
+	{
+		public bool HalvesAreAlike(string s)
+		{
+			var vowels = "aeiouAEIOU";
+
+			var left = 0;
+			var right = 0;
+			for (var i = 0; i < s.Length / 2; i++)
+			{
+				if (vowels.Contains(s[i]))
+				{
+					left++;
+				}
+
+				if (vowels.Contains(s[^(i + 1)]))
+				{
+					right++;
+				}
+			}
+			return left == right;
+		}
+	}
+}
