a2d15413cec51faa016d89c98e5e4469d10380ad
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:37 +0300

    583

---
+++ LeetCode/Algorithms/583.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _583
+	{
+		public static int MinDistance(string word1, string word2)
+		{
+			var dp = new int[word2.Length + 1];
+			for (var i = 0; i <= word1.Length; i++)
+			{
+				var temp = new int[word2.Length + 1];
+				for (var j = 0; j <= word2.Length; j++)
+				{
+					if (i == 0 || j == 0)
+					{
+						temp[j] = i + j;
+					}
+					else if (word1[i - 1] == word2[j - 1])
+					{
+						temp[j] = dp[j - 1];
+					}
+					else
+					{
+						temp[j] = 1 + Math.Min(dp[j], temp[j - 1]);
+					}
+				}
+				dp = temp;
+			}
+			return dp[word2.Length];
+		}
+	}
+}
