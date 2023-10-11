3d1daaa7f76139b3204ecdecdae1598d07479ceb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:41 +0300

    881

--- LeetCode/Algorithms/881.cs
+++ LeetCode/Algorithms/881.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _881
-	{
-		public static int NumRescueBoats(int[] people, int limit)
-		{
-			Array.Sort(people);
-			var i = 0;
-			var j = people.Length - 1;
+namespace LeetCode.Algorithms;
 
-			var result = 0;
-			while (i <= j)
+// MEDIUM
+internal class _881
+{
+	public static int NumRescueBoats(int[] people, int limit)
+	{
+		Array.Sort(people);
+		var i = 0;
+		var j = people.Length - 1;
+
+		var result = 0;
+		while (i <= j)
+		{
+			result++;
+			if (people[i] + people[j] <= limit)
 			{
-				result++;
-				if (people[i] + people[j] <= limit)
-				{
-					i++;
-				}
-				j--;
+				i++;
 			}
-			return result;
+			j--;
 		}
+		return result;
 	}
 }
