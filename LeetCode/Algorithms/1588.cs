04f26662ed9f1ed052233cda6b3aebc3e4c806f6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:07 +0300

    1588

---
+++ LeetCode/Algorithms/1588.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1588
+	{
+		public static int SumOddLengthSubarrays(int[] arr)
+		{
+			var result = 0;
+			for (var i = 0; i < arr.Length; i++)
+			{
+				var temp = 0;
+				for (var j = i; j < arr.Length; j++)
+				{
+					temp += arr[j];
+					if ((j - i + 1) % 2 == 1)
+					{
+						result += temp;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
