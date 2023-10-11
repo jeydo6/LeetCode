c92f22af6faa44c8f1cfd21d91727573d6df09df
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:18 +0300

    1339

--- LeetCode/Algorithms/1339.cs
+++ LeetCode/Algorithms/1339.cs
@@ -1,46 +1,46 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1339
 {
-	class _1339
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	private static long _result = 0;
+
+	public static int MaxProduct(TreeNode root)
+	{
+		var total = Sum(root);
+		Sum(root, total);
+
+		return (int)(_result % 1000000007);
+	}
+
+	private static long Sum(TreeNode root, long total = 0)
+	{
+		if (root == null)
+		{
+			return 0;
 		}
 
-		private long _result = 0;
-
-		public int MaxProduct(TreeNode root)
-		{
-			var total = Sum(root);
-			Sum(root, total);
-
-			return (int)(_result % 1000000007);
-		}
-
-		private long Sum(TreeNode root, long total = 0)
-		{
-			if (root == null)
-			{
-				return 0;
-			}
-
-			var left = Sum(root.left, total);
-			var right = Sum(root.right, total);
-			_result = Math.Max(
-				_result,
-				Math.Max(left * (total - left), right * (total - right))
-			);
-			return left + right + root.val;
-		}
+		var left = Sum(root.left, total);
+		var right = Sum(root.right, total);
+		_result = Math.Max(
+			_result,
+			Math.Max(left * (total - left), right * (total - right))
+		);
+		return left + right + root.val;
 	}
 }
