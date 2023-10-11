88576013ed7893da96d6cdfdd5cf159ac1da65ab
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:14 +0300

    55

---
+++ LeetCode/Algorithms/55.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	internal class _55
+	{
+		public static bool CanJump(int[] nums)
+		{
+			var reach = 0;
+			for (var i = 0; i <= reach; i++)
+			{
+				reach = Math.Max(reach, i + nums[i]);
+
+				if (reach >= nums.Length - 1)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
