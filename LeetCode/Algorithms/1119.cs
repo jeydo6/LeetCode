92be3912eff833c2bcc9c222df85c6445666a9d7
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:30 +0300

    1119

---
+++ LeetCode/Algorithms/1119.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _1119
+	{
+		public static string RemoveVowels(string s)
+		{
+			var sb = new StringBuilder(s.Length);
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (
+					s[i] == 'a' ||
+					s[i] == 'e' ||
+					s[i] == 'i' ||
+					s[i] == 'o' ||
+					s[i] == 'u'
+				)
+				{
+					continue;
+				}
+
+				sb.Append(s[i]);
+			}
+			return sb.ToString();
+		}
+	}
+}
