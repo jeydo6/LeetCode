d3dde28c18a93ab75f5a06acfec92d0eb33567fe
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:16 +0300

    980

--- LeetCode/Algorithms/980.cs
+++ LeetCode/Algorithms/980.cs
@@ -1,54 +1,53 @@
-﻿namespace LeetCode.Algorithms
-{
-	// HARD
-	internal class _980
-	{
-		public static int UniquePathsIII(int[][] grid)
-		{
-			var zero = 0;
-			var sx = 0;
-			var sy = 0;
+﻿namespace LeetCode.Algorithms;
 
-			for (int r = 0; r < grid.Length; r++)
+// HARD
+internal class _980
+{
+	public static int UniquePathsIII(int[][] grid)
+	{
+		var zero = 0;
+		var sx = 0;
+		var sy = 0;
+
+		for (var r = 0; r < grid.Length; r++)
+		{
+			for (var c = 0; c < grid[0].Length; c++)
 			{
-				for (int c = 0; c < grid[0].Length; c++)
+				if (grid[r][c] == 0) zero++;
+				else if (grid[r][c] == 1)
 				{
-					if (grid[r][c] == 0) zero++;
-					else if (grid[r][c] == 1)
-					{
-						sx = r;
-						sy = c;
-					}
+					sx = r;
+					sy = c;
 				}
 			}
-
-			return DFS(grid, sx, sy, zero);
 		}
 
-		public static int DFS(int[][] grid, int x, int y, int zero)
+		return UniquePathsIII(grid, sx, sy, zero);
+	}
+
+	private static int UniquePathsIII(int[][] grid, int x, int y, int zero)
+	{
+		if (x < 0 || y < 0 || x >= grid.Length || y >= grid[0].Length || grid[x][y] == -1)
 		{
-			if (x < 0 || y < 0 || x >= grid.Length || y >= grid[0].Length || grid[x][y] == -1)
-			{
-				return 0;
-			}
-
-			if (grid[x][y] == 2)
-			{
-				return zero == -1 ? 1 : 0;
-			}
-
-			grid[x][y] = -1;
-			zero--;
-
-			var totalPaths =
-				DFS(grid, x + 1, y, zero) +
-				DFS(grid, x - 1, y, zero) +
-				DFS(grid, x, y + 1, zero) +
-				DFS(grid, x, y - 1, zero);
-
-			grid[x][y] = 0;
-
-			return totalPaths;
+			return 0;
 		}
+
+		if (grid[x][y] == 2)
+		{
+			return zero == -1 ? 1 : 0;
+		}
+
+		grid[x][y] = -1;
+		zero--;
+
+		var totalPaths =
+			UniquePathsIII(grid, x + 1, y, zero) +
+			UniquePathsIII(grid, x - 1, y, zero) +
+			UniquePathsIII(grid, x, y + 1, zero) +
+			UniquePathsIII(grid, x, y - 1, zero);
+
+		grid[x][y] = 0;
+
+		return totalPaths;
 	}
 }
