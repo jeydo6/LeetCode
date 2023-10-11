5dc2925de35098fb8ae00c022a5dae0dd8115757
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:07 +0300

    1498

---
+++ LeetCode/Algorithms/1498.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1498
+{
+	private const int Modulo = 1_000_000_007;
+
+	public static int NumSubseq(int[] nums, int target)
+	{
+		Array.Sort(nums);
+
+		var powers = new int[nums.Length];
+		powers[0] = 1;
+		for (var i = 1; i < powers.Length; ++i)
+		{
+			powers[i] = powers[i - 1] * 2 % Modulo;
+		}
+
+		var result = 0;
+		var left = 0;
+		var right = nums.Length - 1;
+		while (left <= right)
+		{
+			if (nums[left] + nums[right] <= target)
+			{
+				result += powers[right - left];
+				result %= Modulo;
+				left++;
+			}
+			else
+			{
+				right--;
+			}
+		}
+
+		return result;
+	}
+}
