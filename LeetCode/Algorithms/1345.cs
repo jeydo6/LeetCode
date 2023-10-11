2f56309c1b5d3fa7da3b52e659358ef168818215
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:17 +0300

    1345

--- LeetCode/Algorithms/1345.cs
+++ LeetCode/Algorithms/1345.cs
@@ -1,54 +1,53 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1345
 {
-	// HARD
-	internal class _1345
+	public static int MinJumps(int[] arr)
 	{
-		public static int MinJumps(int[] arr)
+		var dict = new Dictionary<int, IList<int>>();
+		for (int i = 0; i < arr.Length; i++)
 		{
-			var dict = new Dictionary<int, IList<int>>();
-			for (int i = 0; i < arr.Length; i++)
+			if (!dict.ContainsKey(arr[i]))
 			{
-				if (!dict.ContainsKey(arr[i]))
-				{
-					dict[arr[i]] = new List<int>();
-				}
-				dict[arr[i]].Add(i);
+				dict[arr[i]] = new List<int>();
 			}
-
-			var step = 0;
-			var visited = new bool[arr.Length];
-			visited[0] = true;
-			var queue = new Queue<int>();
-			queue.Enqueue(0);
-			while (queue.Count > 0)
-			{
-				for (var size = queue.Count; size > 0; size--)
-				{
-					var i = queue.Dequeue();
-					if (i == arr.Length - 1)
-					{
-						return step;
-					}
-
-					var next = dict[arr[i]];
-					next.Add(i - 1);
-					next.Add(i + 1);
-					foreach (var j in next)
-					{
-						if (j >= 0 && j < arr.Length && !visited[j])
-						{
-							visited[j] = true;
-							queue.Enqueue(j);
-						}
-					}
-					next.Clear();
-				}
-				step++;
-			}
-
-			return 0;
+			dict[arr[i]].Add(i);
 		}
+
+		var step = 0;
+		var visited = new bool[arr.Length];
+		visited[0] = true;
+		var queue = new Queue<int>();
+		queue.Enqueue(0);
+		while (queue.Count > 0)
+		{
+			for (var size = queue.Count; size > 0; size--)
+			{
+				var i = queue.Dequeue();
+				if (i == arr.Length - 1)
+				{
+					return step;
+				}
+
+				var next = dict[arr[i]];
+				next.Add(i - 1);
+				next.Add(i + 1);
+				foreach (var j in next)
+				{
+					if (j >= 0 && j < arr.Length && !visited[j])
+					{
+						visited[j] = true;
+						queue.Enqueue(j);
+					}
+				}
+				next.Clear();
+			}
+			step++;
+		}
+
+		return 0;
 	}
 }
