fe2ce3852b8819c6f263be65712f908e78ecd85f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:31 +0300

    143

---
+++ LeetCode/Algorithms/143.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _143
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static void ReorderList(ListNode head)
+		{
+			if (head == null || head.next == null)
+			{
+				return;
+			}
+
+			var p1 = head;
+			var p2 = head;
+			while (p2.next != null && p2.next.next != null)
+			{
+				p1 = p1.next;
+				p2 = p2.next.next;
+			}
+
+			var preMiddle = p1;
+			var preCurrent = p1.next;
+			while (preCurrent.next != null)
+			{
+				var current = preCurrent.next;
+				preCurrent.next = current.next;
+				current.next = preMiddle.next;
+				preMiddle.next = current;
+			}
+
+			p1 = head;
+			p2 = preMiddle.next;
+			while (p1 != preMiddle)
+			{
+				preMiddle.next = p2.next;
+				p2.next = p1.next;
+				p1.next = p2;
+				p1 = p2.next;
+				p2 = preMiddle.next;
+			}
+		}
+	}
+}
