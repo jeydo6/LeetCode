e152d22b81a671e649e29d880013bb4cb0052476
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:41 +0300

    326

---
+++ LeetCode/Algorithms/326.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _326
+	{
+		public static bool IsPowerOfThree(int n)
+		{
+			if (n < 1)
+			{
+				return false;
+			}
+
+			while (n % 3 == 0)
+			{
+				n /= 3;
+			}
+
+			return n == 1;
+		}
+	}
+}
