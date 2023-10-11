dba8accd84795f17f09fcc175f66ca551954ab8e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:19 +0300

    21

--- LeetCode/Algorithms/21.cs
+++ LeetCode/Algorithms/21.cs
@@ -1,39 +1,42 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _21
 {
-	// EASY
-	internal class _21
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+	{
+		var result = new ListNode();
+
+		var prev = result;
+		while (list1 != null && list2 != null)
+		{
+			if (list1.val <= list2.val)
 			{
-				this.val = val;
-				this.next = next;
+				prev.next = list1;
+				list1 = list1.next;
 			}
+			else
+			{
+				prev.next = list2;
+				list2 = list2.next;
+			}
+
+			prev = prev.next;
 		}
 
-		public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
-		{
-			var temp = new ListNode(0);
-			var current = temp;
-			while (list1 != null && list2 != null)
-			{
-				if (list1.val <= list2.val)
-				{
-					current.next = list1;
-					list1 = list1.next;
-				}
-				else
-				{
-					current.next = list2;
-					list2 = list2.next;
-				}
-				current = current.next;
-			}
-			current.next = list1 ?? list2;
-			return temp.next;
-		}
+		prev.next = list1 ?? list2;
+
+		return result.next;
 	}
 }
