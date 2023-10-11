067c139ac016f1f0969cb309da5f2c06cc346ca3
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:31 +0300

    144

---
+++ LeetCode/Algorithms/144.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _144
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static IList<int> PreorderTraversal(TreeNode node)
+		{
+			var list = new List<int>();
+			var stack = new Stack<TreeNode>();
+			while (node != null)
+			{
+				list.Add(node.val);
+				if (node.right != null)
+				{
+					stack.Push(node.right);
+				}
+				node = node.left;
+				if (node == null && stack.Count > 0)
+				{
+					node = stack.Pop();
+				}
+			}
+			return list;
+		}
+	}
+}
