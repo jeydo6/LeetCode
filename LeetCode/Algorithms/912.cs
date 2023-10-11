40d5224641275c669907227c0666daac0f5d0d71
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:13 +0300

    912

---
+++ LeetCode/Algorithms/912.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _912
+{
+	public static int[] SortArray(int[] nums)
+	{
+		var counts = new Dictionary<int, int>();
+
+		var minVal = nums[0];
+		var maxVal = nums[0];
+		for (var i = 0; i < nums.Length; i++)
+		{
+			minVal = Math.Min(minVal, nums[i]);
+			maxVal = Math.Max(maxVal, nums[i]);
+
+			if (!counts.ContainsKey(nums[i]))
+			{
+				counts[nums[i]] = 0;
+			}
+			counts[nums[i]]++;
+		}
+
+		var index = 0;
+		for (var val = minVal; val <= maxVal; ++val)
+		{
+			while (counts.ContainsKey(val))
+			{
+				nums[index] = val;
+				index++;
+				counts[val]--;
+			}
+		}
+
+		return nums;
+	}
+}
