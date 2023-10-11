8bba757e95a94552182fa4fd22f5e5b32c366222
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:46 +0300

    1326

---
+++ LeetCode/Algorithms/1326.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1326
+{
+	public static int MinTaps(int n, int[] ranges)
+	{
+		var maxReach = new int[n + 1];
+		for (var i = 0; i < ranges.Length; i++)
+		{
+			var start = Math.Max(0, i - ranges[i]);
+			var end = Math.Min(n, i + ranges[i]);
+
+			maxReach[start] = Math.Max(maxReach[start], end);
+		}
+
+		var result = 0;
+		var currentEnd = 0;
+		var nextEnd = 0;
+		for (var i = 0; i < maxReach.Length; i++)
+		{
+			if (i > nextEnd)
+			{
+				return -1;
+			}
+			if (i > currentEnd)
+			{
+				result++;
+				currentEnd = nextEnd;
+			}
+			nextEnd = Math.Max(nextEnd, maxReach[i]);
+		}
+		return result;
+	}
+}
