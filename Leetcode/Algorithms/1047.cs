c15364e80cdb518e5161738d5e849f51387d4e26
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:11:33 +0300

    1. 1025, 1030, 1047, 1160, 1185, 344, 557, 821;

---
+++ Leetcode/Algorithms/1047.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Algorithms
+{
+    public class _1047
+    {
+        public String RemoveDuplicates(String S)
+        {
+            while (S.Length > 1)
+            {
+                StringBuilder sb = new StringBuilder(S);
+
+                Int32 i = 0;
+                Int32 j = 0;
+
+                while (i < S.Length - 1)
+                {
+                    if (S[i] == S[i + 1])
+                    {
+                        sb.Remove(j, 2);
+
+                        i += 2;
+                    }
+                    else
+                    {
+                        i++;
+                        j++;
+                    }
+                }
+
+                if (sb.Length != S.Length)
+                {
+                    S = sb.ToString();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return S;
+        }
+    }
+}
---
+++ Leetcode/Algorithms/344.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Algorithms
+{
+    public class _344
+    {
+        public void ReverseString(Char[] s)
+        {
+            Int32 length = s.Length / 2;
+            for (Int32 i = 0; i < length; i++)
+            {
+                Char temp = s[i];
+                s[i] = s[s.Length - 1 - i];
+                s[s.Length - 1 - i] = temp;
+            }
+        }
+    }
+}
