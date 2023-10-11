33407c43ac9a6b9877d5ade32c646d8f8c44b0e0
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:38 +0300

    403

---
+++ LeetCode/Algorithms/403.cs
@@ -0,0 +1,55 @@
+namespace LeetCode.Algorithms;
+
+// HARD
+internal static class _403
+{
+    public bool CanCross(int[] stones)
+    {
+        var n = stones.Length;
+        var mark = new Dictionary<int, int>();
+        for (var i = 0 ; i < n; i++)
+        {
+            mark[stones[i]] = i;
+        }
+
+        var dp = new bool[2001][];
+        for (var i = 0; i < dp.Length; i++)
+        {
+            dp[i] = new bool[2001];
+        }
+        dp[0][0] = true;
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j <= n; j++)
+            {
+                if (dp[i][j])
+                {
+                    if (mark.ContainsKey(stones[i] + j))
+                    {
+                        dp[mark[stones[i] + j]][j] = true;
+                    }
+                    if (mark.ContainsKey(stones[i] + j + 1))
+                    {
+                        dp[mark[stones[i] + j + 1]][j + 1] = true;
+                    }
+                    if (mark.ContainsKey(stones[i] + j - 1))
+                    {
+                        dp[mark[stones[i] + j - 1]][j - 1] = true;
+                    }
+                }
+
+
+            }
+        }
+
+        for (var i = 0; i <= n; i++)
+        {
+            if (dp[n - 1][i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
