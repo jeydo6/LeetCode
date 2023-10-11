de28262466a4263b81e39c3b457ef9c4ed18e0c5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:52 +0300

    814

---
+++ LeetCode/Algorithms/814.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _814
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static TreeNode PruneTree(TreeNode root)
+	{
+		return ContainsOne(root) ? root : null;
+	}
+
+	private static bool ContainsOne(TreeNode node)
+	{
+		if (node == null)
+		{
+			return false;
+		}
+
+		var leftContainsOne = ContainsOne(node.left);
+		if (!leftContainsOne)
+		{
+			node.left = null;
+		}
+
+		var rightContainsOne = ContainsOne(node.right);
+		if (!rightContainsOne)
+		{
+			node.right = null;
+		}
+
+		return node.val == 1 || leftContainsOne || rightContainsOne;
+	}
+}
