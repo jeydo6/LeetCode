9d603e02a3d2b005f683be53271c009d02539484
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:30 +0300

    1450

---
+++ LeetCode/Algorithms/1450.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1450
+	{
+		public static int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+		{
+			var result = 0;
+			for (var i = 0; i < startTime.Length; i++)
+			{
+				if (queryTime >= startTime[i] && queryTime <= endTime[i])
+				{
+					result++;
+				}
+			}
+			return result;
+		}
+	}
+}
