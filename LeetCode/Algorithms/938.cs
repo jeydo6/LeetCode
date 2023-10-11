8f0c9c59220a31adde9e4392d1523db73328c4f1
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:11:58 +0300

    938

--- LeetCode/Algorithms/938.cs
+++ LeetCode/Algorithms/938.cs
@@ -1,71 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Leetcode.Algorithms
 {
-    public class _938
-    {
-        public class TreeNode
-        {
-            public TreeNode(Int32 x)
-            {
-                val = x;
-            }
+	public class _938
+	{
+		public class TreeNode
+		{
+			public TreeNode(int value)
+			{
+				val = value;
+			}
 
-            public Int32 val;
+			public int val;
 
-            public TreeNode left;
+			public TreeNode left;
 
-            public TreeNode right;
+			public TreeNode right;
 
-        }
+		}
 
-        public Int32 RangeSumBST(TreeNode root, Int32 L, Int32 R)
-        {
-            Int32 result = 0;
+		public static int RangeSumBST(TreeNode root, int low, int high)
+		{
+			int result = 0;
 
-            Stack<TreeNode> stack = new Stack<TreeNode>();
-            stack.Push(root);
+			Stack<TreeNode> stack = new Stack<TreeNode>();
+			stack.Push(root);
 
-            while(stack.Count > 0)
-            {
-                TreeNode node = stack.Pop();
-                if (node != null)
-                {
-                    if (node.val >= L && node.val <= R)
-                    {
-                        result += node.val;
-                    }
+			while (stack.Count > 0)
+			{
+				TreeNode node = stack.Pop();
+				if (node != null)
+				{
+					if (node.val >= low && node.val <= high)
+					{
+						result += node.val;
+					}
 
-                    if (node.val > L)
-                    {
-                        stack.Push(node.left);
-                    }
+					if (node.val > low)
+					{
+						stack.Push(node.left);
+					}
 
-                    if (node.val < R)
-                    {
-                        stack.Push(node.right);
-                    }
-                }
-            }
+					if (node.val < high)
+					{
+						stack.Push(node.right);
+					}
+				}
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        //private int sum = 0;
-        //public int RangeSumBST(TreeNode root, int L, int R)
-        //{
-        //    if (root != null)
-        //    {
-        //        RangeSumBST(root.left, L, R);
-        //        sum += (root.val >= L && root.val <= R ? root.val : 0);
-        //        RangeSumBST(root.right, L, R);
-        //    }
+		//private int sum = 0;
+		//public int RangeSumBST(TreeNode root, int L, int R)
+		//{
+		//    if (root != null)
+		//    {
+		//        RangeSumBST(root.left, L, R);
+		//        sum += (root.val >= L && root.val <= R ? root.val : 0);
+		//        RangeSumBST(root.right, L, R);
+		//    }
 
-        //    return sum;
-        //}
-    }
+		//    return sum;
+		//}
+	}
 }
