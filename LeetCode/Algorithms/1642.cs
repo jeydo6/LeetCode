1a5f641f1c7232af6eee45a35f7f3a132d201367
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:42 +0300

    1642

---
+++ LeetCode/Algorithms/1642.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1642
+	{
+		public static int FurthestBuilding(int[] heights, int bricks, int ladders)
+		{
+			var ladderAllocations = new PriorityQueue<int, int>();
+			for (var i = 0; i < heights.Length - 1; i++)
+			{
+				var climb = heights[i + 1] - heights[i];
+				if (climb <= 0)
+				{
+					continue;
+				}
+
+				ladderAllocations.Enqueue(climb, climb);
+				if (ladderAllocations.Count <= ladders)
+				{
+					continue;
+				}
+
+				bricks -= ladderAllocations.Dequeue();
+				if (bricks < 0)
+				{
+					return i;
+				}
+			}
+			return heights.Length - 1;
+		}
+	}
+}
