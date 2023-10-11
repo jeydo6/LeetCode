9af4491136d0ddad63a0d3e0c094d08d0fde7c0d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:17 +0300

    290

--- LeetCode/Algorithms/290.cs
+++ LeetCode/Algorithms/290.cs
@@ -1,51 +1,50 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+internal class _290
 {
-	internal class _290
+	// EASY
+	public static bool WordPattern(string pattern, string s)
 	{
-		// EASY
-		public static bool WordPattern(string pattern, string s)
+		var words = s.Split(" ");
+		if (words.Length != pattern.Length)
 		{
-			var words = s.Split(" ");
-			if (words.Length != pattern.Length)
-			{
-				return false;
-			}
-
-			var pw = new Dictionary<char, string>();
-			var wp = new Dictionary<string, char>();
-			for (var i = 0; i < words.Length; i++)
-			{
-				var w = words[i];
-				var p = pattern[i];
-
-				if (pw.ContainsKey(p))
-				{
-					if (pw[p] != w)
-					{
-						return false;
-					}
-				}
-				else
-				{
-					pw[p] = w;
-				}
-
-				if (wp.ContainsKey(w))
-				{
-					if (wp[w] != p)
-					{
-						return false;
-					}
-				}
-				else
-				{
-					wp[w] = p;
-				}
-			}
-
-			return true;
+			return false;
 		}
+
+		var pw = new Dictionary<char, string>();
+		var wp = new Dictionary<string, char>();
+		for (var i = 0; i < words.Length; i++)
+		{
+			var w = words[i];
+			var p = pattern[i];
+
+			if (pw.ContainsKey(p))
+			{
+				if (pw[p] != w)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				pw[p] = w;
+			}
+
+			if (wp.ContainsKey(w))
+			{
+				if (wp[w] != p)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				wp[w] = p;
+			}
+		}
+
+		return true;
 	}
 }
