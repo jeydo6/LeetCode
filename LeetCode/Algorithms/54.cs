1dbd051fcd28b4a1c5e74d01cff23ab4ec1cf683
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:10 +0300

    54

--- LeetCode/Algorithms/54.cs
+++ LeetCode/Algorithms/54.cs
@@ -1,61 +1,57 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _54
 {
-	// MEDIUM
-	internal class _54
+	public static IList<int> SpiralOrder(int[][] matrix)
 	{
-		public static IList<int> SpiralOrder(int[][] matrix)
+		var result = new List<int>();
+
+		if (matrix.Length == 0)
 		{
-			var result = new List<int>();
-
-			if (matrix.Length == 0)
-			{
-				return result;
-			}
-
-			int rowBegin = 0;
-			int rowEnd = matrix.Length - 1;
-			int columnBegin = 0;
-			int columnEnd = matrix[0].Length - 1;
-
-			while (rowBegin <= rowEnd && columnBegin <= columnEnd)
-			{
-				for (int j = columnBegin; j <= columnEnd; j++)
-				{
-					result.Add(matrix[rowBegin][j]);
-				}
-				rowBegin++;
-
-				// Traverse Down
-				for (int j = rowBegin; j <= rowEnd; j++)
-				{
-					result.Add(matrix[j][columnEnd]);
-				}
-				columnEnd--;
-
-				if (rowBegin <= rowEnd)
-				{
-					// Traverse Left
-					for (int j = columnEnd; j >= columnBegin; j--)
-					{
-						result.Add(matrix[rowEnd][j]);
-					}
-				}
-				rowEnd--;
-
-				if (columnBegin <= columnEnd)
-				{
-					// Traver Up
-					for (int j = rowEnd; j >= rowBegin; j--)
-					{
-						result.Add(matrix[j][columnBegin]);
-					}
-				}
-				columnBegin++;
-			}
-
 			return result;
 		}
+
+		var rowBegin = 0;
+		var rowEnd = matrix.Length - 1;
+		var columnBegin = 0;
+		var columnEnd = matrix[0].Length - 1;
+
+		while (rowBegin <= rowEnd && columnBegin <= columnEnd)
+		{
+			for (var j = columnBegin; j <= columnEnd; j++)
+			{
+				result.Add(matrix[rowBegin][j]);
+			}
+			rowBegin++;
+
+			for (var j = rowBegin; j <= rowEnd; j++)
+			{
+				result.Add(matrix[j][columnEnd]);
+			}
+			columnEnd--;
+
+			if (rowBegin <= rowEnd)
+			{
+				for (var j = columnEnd; j >= columnBegin; j--)
+				{
+					result.Add(matrix[rowEnd][j]);
+				}
+			}
+			rowEnd--;
+
+			if (columnBegin <= columnEnd)
+			{
+				for (var j = rowEnd; j >= rowBegin; j--)
+				{
+					result.Add(matrix[j][columnBegin]);
+				}
+			}
+			columnBegin++;
+		}
+
+		return result;
 	}
 }
