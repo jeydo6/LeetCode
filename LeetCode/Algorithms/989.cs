3cfc8d8ed936285a1a40dc97a46e259ca54ee22f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:03 +0300

    989

---
+++ LeetCode/Algorithms/989.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _989
+{
+	public static IList<int> AddToArrayForm(int[] num, int k)
+	{
+		var result = new List<int>();
+
+		var current = k;
+		for (var i = num.Length - 1; i >= 0 || current > 0; i--)
+		{
+			if (i >= 0)
+			{
+				current += num[i];
+			}
+
+			result.Add(current % 10);
+			current /= 10;
+		}
+
+		for (var i = 0; i < result.Count / 2; i++)
+		{
+			(result[i], result[^(i + 1)]) = (result[^(i + 1)], result[i]);
+		}
+		return result;
+	}
+}
