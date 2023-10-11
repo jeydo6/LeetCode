848e19da2636704d1c32cb57899fd95ef121749b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:06 +0300

    9

---
+++ LeetCode/Algorithms/9.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _9
+	{
+		public static bool IsPalindrome(int x)
+		{
+			if (x < 0 || (x % 10 == 0 && x != 0))
+			{
+				return false;
+			}
+
+			var y = Reverse(x);
+			return x == y || x == y / 10;
+		}
+
+		public static int Reverse(int x)
+		{
+			var y = 0;
+			while (x > 0)
+			{
+				y = y * 10 + x % 10;
+				x /= 10;
+			}
+			return y;
+		}
+	}
+}
