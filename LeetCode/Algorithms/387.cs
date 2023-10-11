cae90606949df9fbbaa7764ff7c7096acf6c0e32
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:34 +0300

    387

--- LeetCode/Algorithms/387.cs
+++ LeetCode/Algorithms/387.cs
@@ -5,18 +5,20 @@
 	{
 		public static int FirstUniqChar(string s)
 		{
-			var freq = new int[26];
+			var counts = new int[26];
 			for (var i = 0; i < s.Length; i++)
 			{
-				freq[s[i] - 'a']++;
+				counts[s[i] - 'a']++;
 			}
+
 			for (var i = 0; i < s.Length; i++)
 			{
-				if (freq[s[i] - 'a'] == 1)
+				if (counts[s[i] - 'a'] == 1)
 				{
 					return i;
 				}
 			}
+
 			return -1;
 		}
 	}
