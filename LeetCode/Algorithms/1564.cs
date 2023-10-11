3fbc014cd968d6ea37dcb3bf8cd3d0dde2735fa4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:51 +0300

    1564

---
+++ LeetCode/Algorithms/1564.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1564
+	{
+		public static int MaxBoxesInWarehouse(int[] boxes, int[] warehouse)
+		{
+			for (var i = 1; i < warehouse.Length; i++)
+			{
+				warehouse[i] = Math.Min(warehouse[i - 1], warehouse[i]);
+			}
+
+			Array.Sort(boxes);
+
+			var result = 0;
+			for (var i = warehouse.Length - 1; i >= 0; i--)
+			{
+				if (result < boxes.Length && boxes[result] <= warehouse[i])
+				{
+					result++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
