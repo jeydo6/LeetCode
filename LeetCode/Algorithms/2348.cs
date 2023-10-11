88104a15aa7aa67aefa873a9a43f9974e3df0653
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:30 +0300

    2348

---
+++ LeetCode/Algorithms/2348.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2348
+{
+	public static long ZeroFilledSubarray(int[] nums)
+	{
+		var result = 0L;
+		var j = 0;
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] != 0)
+			{
+				j = i + 1;
+			}
+			result += i - j + 1;
+		}
+		return result;
+	}
+}
