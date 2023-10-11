72380360b876c388ee2c4c65ba485c54a15caa49
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:45 +0300

    133

--- LeetCode/Algorithms/133.cs
+++ LeetCode/Algorithms/133.cs
@@ -1,55 +1,54 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _133
 {
-	// MEDIUM
-	internal class _133
+	public class Node
 	{
-		public class Node
+		public int val;
+		public IList<Node> neighbors;
+
+		public Node()
 		{
-			public int val;
-			public IList<Node> neighbors;
-
-			public Node()
-			{
-				val = 0;
-				neighbors = new List<Node>();
-			}
-
-			public Node(int _val)
-			{
-				val = _val;
-				neighbors = new List<Node>();
-			}
-
-			public Node(int _val, List<Node> _neighbors)
-			{
-				val = _val;
-				neighbors = _neighbors;
-			}
+			val = 0;
+			neighbors = new List<Node>();
 		}
 
-		private static readonly IDictionary<int, Node> _dict = new Dictionary<int, Node>();
-
-		public static Node CloneGraph(Node node)
+		public Node(int _val)
 		{
-			if (node == null)
-			{
-				return null;
-			}
-
-			if (_dict.ContainsKey(node.val))
-			{
-				return _dict[node.val];
-			}
-
-			var clone = new Node(node.val);
-			_dict[clone.val] = clone;
-			foreach (var neighbor in node.neighbors)
-			{
-				clone.neighbors.Add(CloneGraph(neighbor));
-			}
-			return clone;
+			val = _val;
+			neighbors = new List<Node>();
 		}
+
+		public Node(int _val, List<Node> _neighbors)
+		{
+			val = _val;
+			neighbors = _neighbors;
+		}
+	}
+
+	private static readonly IDictionary<int, Node> _dict = new Dictionary<int, Node>();
+
+	public static Node CloneGraph(Node node)
+	{
+		if (node == null)
+		{
+			return null;
+		}
+
+		if (_dict.ContainsKey(node.val))
+		{
+			return _dict[node.val];
+		}
+
+		var clone = new Node(node.val);
+		_dict[clone.val] = clone;
+		foreach (var neighbor in node.neighbors)
+		{
+			clone.neighbors.Add(CloneGraph(neighbor));
+		}
+		return clone;
 	}
 }
