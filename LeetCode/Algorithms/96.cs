b52e6aa4bc283a85bba081a18e0922d4871d4464
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:46 +0300

    96

---
+++ LeetCode/Algorithms/96.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _96
+	{
+		public static int NumTrees(int n)
+		{
+			var graph = new int[n + 1];
+			graph[0] = graph[1] = 1;
+
+			for (var i = 2; i <= n; ++i)
+			{
+				for (var j = 1; j <= i; ++j)
+				{
+					graph[i] += graph[j - 1] * graph[i - j];
+				}
+			}
+
+			return graph[n];
+		}
+	}
+}
