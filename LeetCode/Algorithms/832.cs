468baf9f343c01873a96d3c0aec5c9556a7f0b98
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:16 +0300

    832

--- LeetCode/Algorithms/832.cs
+++ LeetCode/Algorithms/832.cs
@@ -1,21 +1,43 @@
-using System;
-
-public class _832
+namespace LeetCode.Algorithms
 {
-    public Int32[][] FlipAndInvertImage(Int32[][] A)
-    {
-        foreach (Int32[] row in A)
-        {
-            Array.Reverse(row);
-        }
+	public class _832
+	{
+		//public Int32[][] FlipAndInvertImage(Int32[][] A)
+		//{
+		//    foreach (Int32[] row in A)
+		//    {
+		//        Array.Reverse(row);
+		//    }
 
-        foreach (Int32[] row in A)
-        {
-            for (Int32 i = 0; i < row.Length; i++)
-            {
-                row[i] = (row[i] + 1) % 2;
-            }
-        }
-        return A;
-    }
+		//    foreach (Int32[] row in A)
+		//    {
+		//        for (Int32 i = 0; i < row.Length; i++)
+		//        {
+		//            row[i] = (row[i] + 1) % 2;
+		//        }
+		//    }
+		//    return A;
+		//}
+
+		public static int[][] FlipAndInvertImage(int[][] image)
+		{
+			var length = image.Length;
+			for (var i = 0; i < length; i++)
+			{
+				for (var j = 0; j < length / 2; j++)
+				{
+					var temp = image[i][j];
+					image[i][j] = (image[i][^(j + 1)] + 1) % 2;
+					image[i][^(j + 1)] = (temp + 1) % 2;
+				}
+
+				if (length % 2 == 1)
+				{
+					image[i][length / 2] = (image[i][length / 2] + 1) % 2;
+				}
+			}
+
+			return image;
+		}
+	}
 }
