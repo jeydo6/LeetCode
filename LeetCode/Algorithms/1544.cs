ef76fcab6bd390f490ff725ec2fb106e19bbee23
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:50 +0300

    1544

---
+++ LeetCode/Algorithms/1544.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1544
+{
+	public static string MakeGood(string s)
+	{
+		var end = 0;
+		var chars = s.ToCharArray();
+		for (var i = 0; i < chars.Length; i++)
+		{
+			if (end > 0 && Math.Abs(chars[i] - chars[end - 1]) == 32)
+			{
+				end--;
+			}
+			else
+			{
+				chars[end] = chars[i];
+				end++;
+			}
+		}
+
+		return new string(chars[..end]);
+	}
+}
