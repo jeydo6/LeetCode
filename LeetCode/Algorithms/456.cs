30d1b612c9f8fb85aae1dd271ed9ff9266c25669
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:00 +0300

    456

---
+++ LeetCode/Algorithms/456.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _456
+	{
+		public static bool Find132pattern(int[] nums)
+		{
+			if (nums.Length < 3)
+			{
+				return false;
+			}
+
+			var min = new int[nums.Length];
+			min[0] = nums[0];
+			for (var i = 1; i < nums.Length; i++)
+			{
+				min[i] = Math.Min(min[i - 1], nums[i]);
+			}
+
+			var k = nums.Length;
+			for (var i = nums.Length - 1; i >= 0; i--)
+			{
+				if (nums[i] > min[i])
+				{
+					while (k < nums.Length && nums[k] <= min[i])
+					{
+						k++;
+					}
+
+					if (k < nums.Length && nums[k] < nums[i])
+					{
+						return true;
+					}
+
+					nums[--k] = nums[i];
+				}
+			}
+			return false;
+		}
+	}
+}
