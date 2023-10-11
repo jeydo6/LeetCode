7e4d2118eaf37fc61bde03b2de8537f30e14d982
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:08 +0300

    1967

---
+++ LeetCode/Algorithms/1967.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1967
+	{
+		public static int NumOfStrings(string[] patterns, string word)
+		{
+			var count = 0;
+			foreach (var pattern in patterns)
+			{
+				if (word.Contains(pattern))
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+}
