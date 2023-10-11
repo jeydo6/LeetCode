0e9f7bc19f67d8fb70e6111289a80c35d7897a74
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:29 +0300

    1812

---
+++ LeetCode/Algorithms/1812.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1812
+	{
+		public static bool SquareIsWhite(string coordinates)
+		{
+			return (coordinates[0] - 'a' + coordinates[1] - '1') % 2 == 1;
+		}
+	}
+}
