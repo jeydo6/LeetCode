58fe42aefe1a2d105381b4c181b3d7114277b58e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:07 +0300

    557

--- LeetCode/Algorithms/557.cs
+++ LeetCode/Algorithms/557.cs
@@ -1,24 +1,24 @@
 ﻿using System.Text;
 
-namespace Leetcode.Algorithms
+namespace Leetcode.Algorithms;
+
+// EASY
+internal class _557
 {
-	internal class _557
+	public static string ReverseWords(string s)
 	{
-		public static string ReverseWords(string s)
+		var result = new StringBuilder(s.Length);
+		var words = s.Split(' ');
+		for (var i = 0; i < words.Length; i++)
 		{
-			var result = new StringBuilder(s.Length);
-			var words = s.Split(' ');
-			for (var i = 0; i < words.Length; i++)
+			for (var j = 0; j < words[i].Length; j++)
 			{
-				for (var j = 0; j < words[i].Length; j++)
-				{
-					result.Append(words[i][^(j + 1)]);
-				}
-				result.Append(' ');
+				result.Append(words[i][^(j + 1)]);
 			}
-			return result
-				.ToString()
-				.TrimEnd();
+			result.Append(' ');
 		}
+		return result
+			.ToString()
+			.TrimEnd();
 	}
 }
