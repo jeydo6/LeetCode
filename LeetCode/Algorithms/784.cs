df2128cadeeb6501b0059bd40aa13096ae1dc16b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:43 +0300

    784

---
+++ LeetCode/Algorithms/784.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _784
+	{
+		public static IList<string> LetterCasePermutation(string s)
+		{
+			var result = new List<string>();
+			if (string.IsNullOrEmpty(s))
+			{
+				return result;
+			}
+
+			var queue = new Queue<string>();
+			queue.Enqueue(s);
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (char.IsLetter(s[i]))
+				{
+					for (var j = queue.Count; j > 0; j--)
+					{
+						var temp = queue.Dequeue();
+						queue.Enqueue(temp[..i] + char.ToUpper(temp[i]) + temp[(i + 1)..]);
+						queue.Enqueue(temp[..i] + char.ToLower(temp[i]) + temp[(i + 1)..]);
+					}
+				}
+			}
+			return new List<string>(queue);
+		}
+	}
+}
