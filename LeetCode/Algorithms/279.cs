d1ed433650134d8712092a82a6d7f84f614b7cc3
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:02 +0300

    279

--- LeetCode/Algorithms/279.cs
+++ LeetCode/Algorithms/279.cs
@@ -1,32 +1,31 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _279
 {
-	// MEDIUM
-	internal class _279
+	public static int NumSquares(int n)
 	{
-		public static int NumSquares(int n)
+		var values = new int[n + 1];
+		for (var i = 0; i < n + 1; i++)
 		{
-			var values = new int[n + 1];
-			for (var i = 0; i < n + 1; i++)
-			{
-				values[i] = int.MaxValue;
-			}
-			values[0] = 0;
-
-			var count = 1;
-			var square = count * count;
-			while (square <= n)
-			{
-				for (var i = square; i < n + 1; i++)
-				{
-					values[i] = Math.Min(values[i], values[i - square] + 1);
-				}
-				count++;
-				square = count * count;
-			}
-
-			return values[n];
+			values[i] = int.MaxValue;
 		}
+		values[0] = 0;
+
+		var count = 1;
+		var square = count * count;
+		while (square <= n)
+		{
+			for (var i = square; i < n + 1; i++)
+			{
+				values[i] = Math.Min(values[i], values[i - square] + 1);
+			}
+			count++;
+			square = count * count;
+		}
+
+		return values[n];
 	}
 }
