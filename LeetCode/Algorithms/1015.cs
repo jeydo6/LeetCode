bebe93a7eee64f972c042acb9f449b29e92e10e6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:37 +0300

    1015

---
+++ LeetCode/Algorithms/1015.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1015
+	{
+		public static int SmallestRepunitDivByK(int k)
+		{
+			if (k % 2 == 0 || k % 5 == 0)
+			{
+				return -1;
+			}
+
+			var r = 0;
+			for (var n = 1; n <= k; n++)
+			{
+				r = (r * 10 + 1) % k;
+				if (r == 0)
+				{
+					return n;
+				}
+			}
+			return -1;
+		}
+	}
+}
