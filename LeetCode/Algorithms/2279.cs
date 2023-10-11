ec2b864d75ddb7afeab64661a557780319f860de
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:12 +0300

    2279

---
+++ LeetCode/Algorithms/2279.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2279
+{
+	public static int MaximumBags(int[] capacity, int[] rocks, int additionalRocks)
+	{
+		var result = 0;
+
+		var remainingCapacity = new int[capacity.Length];
+		for (var i = 0; i < capacity.Length; i++)
+		{
+			remainingCapacity[i] = capacity[i] - rocks[i];
+		}
+		Array.Sort(remainingCapacity);
+
+		for (var i = 0; i < capacity.Length; i++)
+		{
+			if (additionalRocks >= remainingCapacity[i])
+			{
+				additionalRocks -= remainingCapacity[i];
+				result++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return result;
+	}
+}
