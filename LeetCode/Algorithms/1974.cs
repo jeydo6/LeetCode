95e82d6952697fbb326c88b2b8f478c383de11d9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:10 +0300

    1974

---
+++ LeetCode/Algorithms/1974.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1974
+	{
+		public static int MinTimeToType(string word)
+		{
+			var result = 0;
+			var prev = 'a';
+			foreach (var ch in word)
+			{
+				result += 1 + Math.Min(
+					Math.Abs(ch - prev),
+					26 - Math.Abs(ch - prev)
+				);
+				prev = ch;
+			}
+			return result;
+		}
+	}
+}
