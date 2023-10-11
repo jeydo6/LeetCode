3bc723c1e04871b444b3d2f493e7c19ad6ab256f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:59 +0300

    225

---
+++ LeetCode/Algorithms/225.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _225
+	{
+		public class MyStack
+		{
+			private readonly LinkedList<int> _list;
+
+			public MyStack()
+			{
+				_list = new LinkedList<int>();
+			}
+
+			public void Push(int x)
+			{
+				_list.AddLast(x);
+			}
+
+			public int Pop()
+			{
+				var x = _list.Last;
+				_list.RemoveLast();
+				return x.Value;
+			}
+
+			public int Top()
+			{
+				return _list.Last.Value;
+			}
+
+			public bool Empty()
+			{
+				return _list.Count == 0;
+			}
+		}
+	}
+}
