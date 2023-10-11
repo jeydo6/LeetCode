4af6c8e7dc7424d05bfd32340a581ce7ffdf6791
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:14 +0300

    658

---
+++ LeetCode/Algorithms/658.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _658
+{
+	public static IList<int> FindClosestElements(int[] arr, int k, int x)
+	{
+		var lo = 0;
+		var hi = arr.Length - k;
+
+		while (lo < hi)
+		{
+			var mid = (lo + hi) / 2;
+			if (x - arr[mid] > arr[mid + k] - x)
+			{
+				lo = mid + 1;
+			}
+			else
+			{
+				hi = mid;
+			}
+		}
+
+		var result = new List<int>();
+		for (var i = lo; i < lo + k; i++)
+		{
+			result.Add(arr[i]);
+		}
+
+		return result;
+	}
+}
