838b598df543d8dcf1de8e4d58e1e5a91ccc2b16
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:58 +0300

    209

---
+++ LeetCode/Algorithms/209.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _209
+{
+	public static int MinSubArrayLen(int target, int[] nums)
+	{
+		var result = int.MaxValue;
+		var left = 0;
+		var sum = 0;
+		for (var right = 0; right < nums.Length; right++)
+		{
+			sum += nums[right];
+
+			while (sum >= target)
+			{
+				result = Math.Min(result, right - left + 1);
+				sum -= nums[left++];
+			}
+		}
+
+		return result == int.MaxValue ? 0 : result;
+	}
+}
