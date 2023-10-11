93d4d655bca7fc15f3e73467d97d303f21dbf130
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:02 +0300

    330

---
+++ LeetCode/Algorithms/330.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _330
+	{
+		public static int MinPatches(int[] nums, int n)
+		{
+			var miss = 1L;
+			var added = 0;
+			var i = 0;
+			while (miss <= n)
+			{
+				if (i < nums.Length && nums[i] <= miss)
+				{
+					miss += nums[i++];
+				}
+				else
+				{
+					miss += miss;
+					added++;
+				}
+			}
+			return added;
+		}
+	}
+}
