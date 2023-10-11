2cbbf58e2b686ea5f5c83e2b8a70f14e62036337
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:13 +0300

    1721

---
+++ LeetCode/Algorithms/1721.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1721
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode SwapNodes(ListNode head, int k)
+		{
+			ListNode frontNode = null;
+			ListNode endNode = null;
+
+			var length = 0;
+			var currentNode = head;
+			while (currentNode != null)
+			{
+				length++;
+				if (endNode != null)
+				{
+					endNode = endNode.next;
+				}
+
+				if (length == k)
+				{
+					frontNode = currentNode;
+					endNode = head;
+				}
+				currentNode = currentNode.next;
+			}
+
+			(frontNode.val, endNode.val) = (endNode.val, frontNode.val);
+
+			return head;
+		}
+	}
+}
