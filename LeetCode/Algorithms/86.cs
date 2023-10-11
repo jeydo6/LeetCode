4933af8be89c856c79654cbe249828319de2f5ef
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:28 +0300

    86

--- LeetCode/Algorithms/86.cs
+++ LeetCode/Algorithms/86.cs
@@ -1,46 +1,45 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _86
 {
-	// MEDIUM
-	internal class _86
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
 		}
+	}
 
-		public static ListNode Partition(ListNode head, int x)
+	public static ListNode Partition(ListNode head, int x)
+	{
+		var beforeHead = new ListNode(0);
+		var before = beforeHead;
+		var afterHead = new ListNode(0);
+		var after = afterHead;
+
+		while (head != null)
 		{
-			var beforeHead = new ListNode(0);
-			var before = beforeHead;
-			var afterHead = new ListNode(0);
-			var after = afterHead;
-
-			while (head != null)
+			if (head.val < x)
 			{
-				if (head.val < x)
-				{
-					before.next = head;
-					before = before.next;
-				}
-				else
-				{
-					after.next = head;
-					after = after.next;
-				}
-
-				head = head.next;
+				before.next = head;
+				before = before.next;
+			}
+			else
+			{
+				after.next = head;
+				after = after.next;
 			}
 
-			after.next = null;
-			before.next = afterHead.next;
-
-			return beforeHead.next;
+			head = head.next;
 		}
+
+		after.next = null;
+		before.next = afterHead.next;
+
+		return beforeHead.next;
 	}
 }
