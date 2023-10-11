8f7d03019cd0434add604ee2555509efbe32d01f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:59 +0300

    1046

--- LeetCode/Algorithms/1046.cs
+++ LeetCode/Algorithms/1046.cs
@@ -1,56 +1,55 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1046
 {
-	// EASY
-	internal class _1046
+	public static int LastStoneWeight(int[] stones)
 	{
-		public static int LastStoneWeight(int[] stones)
+		var maxWeight = stones[0];
+		for (var i = 0; i < stones.Length; i++)
 		{
-			var maxWeight = stones[0];
-			for (var i = 0; i < stones.Length; i++)
-			{
-				maxWeight = Math.Max(maxWeight, stones[i]);
-			}
+			maxWeight = Math.Max(maxWeight, stones[i]);
+		}
 
-			var buckets = new int[maxWeight + 1];
-			for (var i = 0; i < stones.Length; i++)
-			{
-				buckets[stones[i]]++;
-			}
+		var buckets = new int[maxWeight + 1];
+		for (var i = 0; i < stones.Length; i++)
+		{
+			buckets[stones[i]]++;
+		}
 
-			var biggestWeight = 0;
-			var currentWeight = maxWeight;
-			while (currentWeight > 0)
+		var biggestWeight = 0;
+		var currentWeight = maxWeight;
+		while (currentWeight > 0)
+		{
+			if (buckets[currentWeight] == 0)
 			{
-				if (buckets[currentWeight] == 0)
+				currentWeight--;
+			}
+			else if (biggestWeight == 0)
+			{
+				buckets[currentWeight] %= 2;
+				if (buckets[currentWeight] == 1)
 				{
-					currentWeight--;
+					biggestWeight = currentWeight;
 				}
-				else if (biggestWeight == 0)
+				currentWeight--;
+			}
+			else
+			{
+				buckets[currentWeight]--;
+				if (biggestWeight - currentWeight <= currentWeight)
 				{
-					buckets[currentWeight] %= 2;
-					if (buckets[currentWeight] == 1)
-					{
-						biggestWeight = currentWeight;
-					}
-					currentWeight--;
+					buckets[biggestWeight - currentWeight]++;
+					biggestWeight = 0;
 				}
 				else
 				{
-					buckets[currentWeight]--;
-					if (biggestWeight - currentWeight <= currentWeight)
-					{
-						buckets[biggestWeight - currentWeight]++;
-						biggestWeight = 0;
-					}
-					else
-					{
-						biggestWeight -= currentWeight;
-					}
+					biggestWeight -= currentWeight;
 				}
 			}
-			return biggestWeight;
 		}
+		return biggestWeight;
 	}
 }
