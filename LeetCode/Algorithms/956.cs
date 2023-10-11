fae46b1835df17870cd218059a8aef0af6cf7080
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:45 +0300

    956

---
+++ LeetCode/Algorithms/956.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+public class _956
+{
+	public static int TallestBillboard(int[] rods)
+	{
+		var dp = new Dictionary<int, int> { [0] = 0 };
+		
+		foreach (var r in rods)
+		{
+			var newDp = new Dictionary<int, int>(dp);
+			foreach (var (diff, taller) in dp)
+			{
+				newDp[diff + r] = Math.Max(newDp.ContainsKey(diff + r) ? newDp[diff + r] : 0, taller + r);
+
+				var shorter = taller - diff;
+				var newDiff = Math.Abs(shorter + r - taller);
+				var newTaller = Math.Max(shorter + r, taller);
+				newDp[newDiff] =  Math.Max(newTaller, newDp.TryGetValue(newDiff, out var value) ? value : 0);
+			}
+			dp = newDp;
+		}
+		
+		return dp.ContainsKey(0) ? dp[0] : 0;
+	}
+}
