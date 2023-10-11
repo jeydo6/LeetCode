098e6bd4f6c6396d32dbb568531d27bfd143a202
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:32 +0300

    1318

---
+++ LeetCode/Algorithms/1318.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1318
+{
+	public static int MinFlips(int a, int b, int c)
+	{
+		var result = 0;
+		while (a != 0 | b != 0 | c != 0)
+		{
+			if ((c & 1) == 1)
+			{
+				if ((a & 1) == 0 && (b & 1) == 0)
+				{
+					result++;
+				}
+			}
+			else
+			{
+				result += (a & 1) + (b & 1);
+			}
+
+			a >>= 1;
+			b >>= 1;
+			c >>= 1;
+		}
+
+		return result;
+	}
+}
