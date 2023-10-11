186f76187c174d87c36de222aa460a67051b5593
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:03 +0300

    1650

---
+++ LeetCode/Algorithms/1650.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	internal class _1650
+	{
+		public class Node
+		{
+			public int val;
+			public Node left;
+			public Node right;
+			public Node parent;
+		}
+
+		public static Node LowestCommonAncestor(Node p, Node q)
+		{
+			var a = p;
+			var b = q;
+			while (a != b)
+			{
+				a = a == null ? q : a.parent;
+				b = b == null ? p : b.parent;
+			}
+			return a;
+		}
+	}
+}
