1712eaced548d53e5520a2b485dfd225e8429aba
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:24 +0300

    104

--- LeetCode/Algorithms/104.cs
+++ LeetCode/Algorithms/104.cs
@@ -1,70 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Leetcode.Algorithms
 {
-    public class _104
-    {
-        public class TreeNode
-        {
-            public TreeNode(Int32 x)
-            {
-                val = x;
-            }
+	public class _104
+	{
+		public class TreeNode
+		{
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
 
-            public Int32 val;
+			public int val;
 
-            public TreeNode left;
+			public TreeNode left;
 
-            public TreeNode right;
+			public TreeNode right;
+		}
 
-        }
+		//public Int32 MaxDepth(TreeNode root)
+		//      {
+		//          if (root == null)
+		//          {
+		//              return 0;
+		//          }
 
-        public Int32 MaxDepth(TreeNode root)
-        {
-            if (root == null)
-            {
-                return 0;
-            }
+		//          Int32 depth = 0;
+		//          Stack<TreeNode> nodes = new Stack<TreeNode>();
+		//          Stack<TreeNode> path = new Stack<TreeNode>();
 
-            Int32 depth = 0;
-            Stack<TreeNode> nodes = new Stack<TreeNode>();
-            Stack<TreeNode> path = new Stack<TreeNode>();
+		//          TreeNode node = root;
+		//          nodes.Push(node);
 
-            TreeNode node = root;
-            nodes.Push(node);
+		//          while (nodes.Count > 0)
+		//          {
+		//              node = nodes.Peek();
+		//              if (path.Count > 0 && node == path.Peek())
+		//              {
+		//                  if (path.Count > depth)
+		//                  {
+		//                      depth = path.Count;
+		//                  }
 
-            while (nodes.Count > 0)
-            {
-                node = nodes.Peek();
-                if (path.Count > 0 && node == path.Peek())
-                {
-                    if (path.Count > depth)
-                    {
-                        depth = path.Count;
-                    }
+		//                  path.Pop();
+		//                  nodes.Pop();
+		//              }
+		//              else
+		//              {
+		//                  path.Push(node);
+		//                  if (node.left != null)
+		//                  {
+		//                      nodes.Push(node.left);
+		//                  }
+		//                  if (node.right != null)
+		//                  {
+		//                      nodes.Push(node.right);
+		//                  }
+		//              }
+		//          }
 
-                    path.Pop();
-                    nodes.Pop();
-                }
-                else
-                {
-                    path.Push(node);
-                    if (node.left != null)
-                    {
-                        nodes.Push(node.left);
-                    }
-                    if (node.right != null)
-                    {
-                        nodes.Push(node.right);
-                    }
-                }
-            }
+		//          return depth;
+		//      }
 
-            return depth;
-        }
-    }
+		public static int MaxDepth(TreeNode root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			var depth = 0;
+			var path = new Stack<TreeNode>();
+
+			var stack = new Stack<TreeNode>();
+			stack.Push(root);
+			while (stack.Count > 0)
+			{
+				var node = stack.Peek();
+				if (path.Count > 0 && node == path.Peek())
+				{
+					if (path.Count > depth)
+					{
+						depth = path.Count;
+					}
+
+					path.Pop();
+					stack.Pop();
+				}
+				else
+				{
+					path.Push(node);
+					if (node.left != null)
+					{
+						stack.Push(node.left);
+					}
+					if (node.right != null)
+					{
+						stack.Push(node.right);
+					}
+				}
+			}
+
+			return depth;
+		}
+	}
 }
