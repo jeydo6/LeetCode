8dfea6f94e0b9759316b65fd5ab35c61fbf07840
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:54 +0300

    1470

--- LeetCode/Algorithms/1470.cs
+++ LeetCode/Algorithms/1470.cs
@@ -1,16 +1,15 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+internal class _1470
 {
-	class _1470
+	public static int[] Shuffle(int[] nums, int n)
 	{
-		public static int[] Shuffle(int[] numbers, int n)
+		var result = new int[n * 2];
+		for (var i = 0; i < n; i++)
 		{
-			var result = new int[n * 2];
-			for (var i = 0; i < n; i++)
-			{
-				result[2 * i] = numbers[i];
-				result[2 * i + 1] = numbers[n + i];
-			}
-			return result;
+			result[2 * i] = nums[i];
+			result[2 * i + 1] = nums[n + i];
 		}
+		return result;
 	}
 }
