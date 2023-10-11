ff0b626652d86a24c3ac8228f3ce7694bf0c8f4f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:16 +0300

    2130

---
+++ LeetCode/Algorithms/2130.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2130
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static int PairSum(ListNode head)
+	{
+		var slow = head;
+		var fast = head;
+
+		while (fast != null && fast.next != null)
+		{
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		var prev = default(ListNode);
+		while (slow != null)
+		{
+			var next = slow.next;
+			slow.next = prev;
+			prev = slow;
+			slow = next;
+		}
+
+		var start = head;
+		var result = 0;
+		while (prev != null)
+		{
+			result = Math.Max(result, start.val + prev.val);
+			prev = prev.next;
+			start = start.next;
+		}
+
+		return result;
+	}
+}
