1fc19107bb0b24abc074c01de38977b4e6de4b84
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:29 +0300

    211

--- LeetCode/Algorithms/211.cs
+++ LeetCode/Algorithms/211.cs
@@ -1,77 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LeetCode.Algorithms;
 
-namespace LeetCode.Algorithms
+// MEDIUM
+internal class _211
 {
-	// MEDIUM
-	internal class _211
+	public class WordDictionary
 	{
-		public static void GetResult()
+		public class TrieNode
 		{
-			var word = "bad";
-			var obj = new WordDictionary();
-			obj.AddWord(word);
-			_ = obj.Search(word);
+			public TrieNode[] children = new TrieNode[26];
+			public string item = "";
 		}
 
-		public class WordDictionary
+		private readonly TrieNode _root = new TrieNode();
+
+		public void AddWord(string word)
 		{
-			public class TrieNode
+			var node = _root;
+			foreach (var ch in word)
 			{
-				public TrieNode[] children = new TrieNode[26];
-				public string item = "";
+				if (node.children[ch - 'a'] == null)
+				{
+					node.children[ch - 'a'] = new TrieNode();
+				}
+				node = node.children[ch - 'a'];
+			}
+			node.item = word;
+		}
+
+		public bool Search(string word)
+		{
+			return Search(word, 0, _root);
+		}
+
+		private static bool Search(string word, int k, TrieNode node)
+		{
+			if (k == word.Length)
+			{
+				return node.item != "";
 			}
 
-			private readonly TrieNode _root = new TrieNode();
-
-			public void AddWord(string word)
+			if (word[k] != '.')
 			{
-				var node = _root;
-				foreach (var ch in word)
+				return node.children[word[k] - 'a'] != null && Search(word, k + 1, node.children[word[k] - 'a']);
+			}
+			else
+			{
+				for (int i = 0; i < node.children.Length; i++)
 				{
-					if (node.children[ch - 'a'] == null)
+					if (node.children[i] != null)
 					{
-						node.children[ch - 'a'] = new TrieNode();
-					}
-					node = node.children[ch - 'a'];
-				}
-				node.item = word;
-			}
-
-			public bool Search(string word)
-			{
-				return Search(word, 0, _root);
-			}
-
-			private static bool Search(string word, int k, TrieNode node)
-			{
-				if (k == word.Length)
-				{
-					return node.item != "";
-				}
-
-				if (word[k] != '.')
-				{
-					return node.children[word[k] - 'a'] != null && Search(word, k + 1, node.children[word[k] - 'a']);
-				}
-				else
-				{
-					for (int i = 0; i < node.children.Length; i++)
-					{
-						if (node.children[i] != null)
+						if (Search(word, k + 1, node.children[i]))
 						{
-							if (Search(word, k + 1, node.children[i]))
-							{
-								return true;
-							}
+							return true;
 						}
 					}
 				}
-				return false;
 			}
+			return false;
 		}
 	}
 }
