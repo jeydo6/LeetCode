e0029e76166a64093fa1a66e7be615ad20a0179e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:18 +0300

    77

--- LeetCode/Algorithms/77.cs
+++ LeetCode/Algorithms/77.cs
@@ -1,37 +1,36 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _77
 {
-	// MEDIUM
-	class _77
+	public static IList<IList<int>> Combine(int n, int k, int start = 1, IList<int> current = null)
 	{
-		public static IList<IList<int>> Combine(int n, int k, int start = 1, IList<int> current = null)
+		var result = new List<IList<int>>();
+
+		if (current == null)
 		{
-			var result = new List<IList<int>>();
-
-			if (current == null)
-			{
-				current = new List<int>();
-			}
-
-			if (k == 0)
-			{
-				result.Add(new List<int>(current));
-			}
-			else
-			{
-				for (var i = start; i <= n - k + 1; i++)
-				{
-					current.Add(i);
-					var temp = Combine(n, k - 1, i + 1, current);
-					foreach (var combination in temp)
-					{
-						result.Add(combination);
-					}
-					current.RemoveAt(current.Count - 1);
-				}
-			}
-			return result;
+			current = new List<int>();
 		}
+
+		if (k == 0)
+		{
+			result.Add(new List<int>(current));
+		}
+		else
+		{
+			for (var i = start; i <= n - k + 1; i++)
+			{
+				current.Add(i);
+				var temp = Combine(n, k - 1, i + 1, current);
+				foreach (var combination in temp)
+				{
+					result.Add(combination);
+				}
+				current.RemoveAt(current.Count - 1);
+			}
+		}
+		return result;
 	}
 }
