ca21aaf0f350174c8f187a7454f705a9feafd134
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:58 +0300

    263

---
+++ LeetCode/Algorithms/263.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _263
+{
+	public static bool IsUgly(int n)
+	{
+		if (n <= 0)
+		{
+			return false;
+		}
+
+		foreach (var factor in new[] { 2, 3, 5 })
+		{
+			n = KeepDividingWhenDivisible(n, factor);
+		}
+
+		return n == 1;
+	}
+
+	private static int KeepDividingWhenDivisible(int dividend, int divisor)
+	{
+		while (dividend % divisor == 0)
+		{
+			dividend /= divisor;
+		}
+		return dividend;
+	}
+}
