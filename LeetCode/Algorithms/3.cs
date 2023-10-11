139b68f9973783949c71afde7b39667bd61646b9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:30 +0300

    3

---
+++ LeetCode/Algorithms/3.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _3
+	{
+		public static int LengthOfLongestSubstring(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return 0;
+			}
+
+			var dict = new Dictionary<char, int>();
+
+			var j = 0;
+			var max = 0;
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (dict.ContainsKey(s[i]))
+				{
+					j = Math.Max(j, dict[s[i]] + 1);
+				}
+
+				dict[s[i]] = i;
+				max = Math.Max(max, i - j + 1);
+			}
+			return max;
+		}
+	}
+}
