1d25ac3cfd62f4260a9aa06f68b1a32caa8a5584
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:28 +0300

    876

--- LeetCode/Algorithms/876.cs
+++ LeetCode/Algorithms/876.cs
@@ -1,36 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _876
-    {
-        public class ListNode
-        {
-            public ListNode(Int32 x)
-            {
-                val = x;
-            }
+	public class _876
+	{
+		public class ListNode
+		{
+			public ListNode(int x)
+			{
+				val = x;
+			}
 
-            public Int32 val;
+			public int val;
 
-            public ListNode next;
-        }
-        public ListNode MiddleNode(ListNode head)
-        {
-            ListNode slow = head;
-            ListNode fast = head;
+			public ListNode next;
+		}
 
-            while (fast != null && fast.next != null)
-            {
-                slow = slow.next;
-                fast = fast.next.next;
-            }
+		public static ListNode MiddleNode(ListNode head)
+		{
+			var slow = head;
+			var fast = head;
 
-            return slow;
-        }
-    }
+			while (fast != null && fast.next != null)
+			{
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+
+			return slow;
+		}
+	}
 }
