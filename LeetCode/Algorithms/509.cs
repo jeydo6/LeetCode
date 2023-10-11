1af70eab5c248c8a429117fe4399bcf17bfddf19
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:57 +0300

    509

--- LeetCode/Algorithms/509.cs
+++ LeetCode/Algorithms/509.cs
@@ -5,26 +5,22 @@
 	{
 		public static int Fib(int n)
 		{
-			var array = new int[]
+			if (n <= 1)
 			{
-				0,
-				1,
-				0
-			};
+				return n;
+			}
+
+			var current = 0;
+			var prev1 = 1;
+			var prev2 = 0;
 
 			for (var i = 2; i <= n; i++)
 			{
-				array[2] = array[0] + array[1];
-				array[0] = array[1];
-				array[1] = array[2];
+				current = prev1 + prev2;
+				prev2 = prev1;
+				prev1 = current;
 			}
-
-			if (n <= 2)
-			{
-				return array[n];
-			}
-
-			return array[2];
+			return current;
 		}
 	}
 }
