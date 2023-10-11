5eeb8e3c0af7492c74da5abf717a309ae6260ef9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:52 +0300

    368

---
+++ LeetCode/Algorithms/368.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _368
+	{
+		public static IList<int> LargestDivisibleSubset(int[] nums)
+		{
+			Array.Sort(nums);
+
+			var lengths = new int[nums.Length];
+			var prevs = new int[nums.Length];
+
+			var maxLength = 0;
+			var index = -1;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				lengths[i] = 1;
+				prevs[i] = -1;
+				for (var j = i - 1; j >= 0; j--)
+				{
+					if (nums[i] % nums[j] == 0 && lengths[j] + 1 > lengths[i])
+					{
+						lengths[i] = lengths[j] + 1;
+						prevs[i] = j;
+					}
+				}
+				if (lengths[i] > maxLength)
+				{
+					maxLength = lengths[i];
+					index = i;
+				}
+			}
+			var result = new List<int>();
+			while (index != -1)
+			{
+				result.Add(nums[index]);
+				index = prevs[index];
+			}
+			return result;
+		}
+	}
+}
