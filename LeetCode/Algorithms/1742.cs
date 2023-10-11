355552819047e1da9bb6dbeb31ba477e1055e96c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:09 +0300

    1742

---
+++ LeetCode/Algorithms/1742.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1742
+	{
+		public static int CountBalls(int lowLimit, int highLimit)
+		{
+			var dict = new Dictionary<int, int>();
+			for (var i = lowLimit; i <= highLimit; i++)
+			{
+				var sum = 0;
+				var number = i;
+				while (number > 0)
+				{
+					sum += number % 10;
+					number /= 10;
+				}
+
+				if (dict.ContainsKey(sum))
+				{
+					dict[sum]++;
+				}
+				else
+				{
+					dict[sum] = 1;
+				}
+			}
+
+			var max = int.MinValue;
+			foreach (var key in dict.Keys)
+			{
+				if (dict[key] > max) max = dict[key];
+			}
+			return max;
+		}
+	}
+}
