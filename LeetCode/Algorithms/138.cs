c38628891e504fc190b0fdc40a7a3f4b575b3ed4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:36 +0300

    138

---
+++ LeetCode/Algorithms/138.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _138
+	{
+		public class Node
+		{
+			public int val;
+			public Node next;
+			public Node random;
+
+			public Node(int _val)
+			{
+				val = _val;
+				next = null;
+				random = null;
+			}
+		}
+
+		public static Node CopyRandomList(Node head)
+		{
+			if (head == null)
+			{
+				return null;
+			}
+
+			var ptr = head;
+			while (ptr != null)
+			{
+				var newNode = new Node(ptr.val)
+				{
+					next = ptr.next
+				};
+
+				ptr.next = newNode;
+				ptr = newNode.next;
+			}
+
+			ptr = head;
+			while (ptr != null)
+			{
+				ptr.next.random = ptr.random?.next;
+				ptr = ptr.next.next;
+			}
+
+			var ptrOldList = head;
+			var ptrNewList = head.next;
+			var prev = head.next;
+			while (ptrOldList != null)
+			{
+				ptrOldList.next = ptrOldList.next.next;
+				ptrNewList.next = ptrNewList.next?.next;
+				ptrOldList = ptrOldList.next;
+				ptrNewList = ptrNewList.next;
+			}
+			return prev;
+		}
+	}
+}
