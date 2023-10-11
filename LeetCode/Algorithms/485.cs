13ed7618a72cd0245ae1d19cd599aa70219312c2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:01 +0300

    485

---
+++ LeetCode/Algorithms/485.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	internal class _485
+	{
+		public static int FindMaxConsecutiveOnes(int[] nums)
+		{
+			var max = 0;
+			var count = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] == 0)
+				{
+					count = 0;
+				}
+				else
+				{
+					count++;
+					max = Math.Max(max, count);
+				}
+			}
+			return max;
+		}
+	}
+}
