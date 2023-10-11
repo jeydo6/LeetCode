df778e8ace90bbe12e97143bb0ace8edebc08316
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:11 +0300

    1265

---
+++ LeetCode/Algorithms/1265.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1265
+	{
+		public abstract class ImmutableListNode
+		{
+			public abstract void PrintValue();
+			public abstract ImmutableListNode GetNext();
+		}
+
+		public static void PrintLinkedListInReverse(ImmutableListNode head)
+		{
+			var stack = new Stack<ImmutableListNode>();
+			var node = head;
+			while (node != null)
+			{
+				stack.Push(node);
+				node = node.GetNext();
+			}
+
+			while (stack.Count > 0)
+			{
+				node = stack.Pop();
+				node.PrintValue();
+			}
+		}
+	}
+}
