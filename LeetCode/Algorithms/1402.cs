6be4355fcbd0c14b5a24fd308e1a547f80784fb3
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:37 +0300

    1402

---
+++ LeetCode/Algorithms/1402.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1402
+{
+	public static int MaxSatisfaction(int[] satisfaction)
+	{
+		Array.Sort(satisfaction);
+
+		var result = 0;
+		var suffixSum = 0;
+		for (var i = satisfaction.Length - 1; i >= 0; i--)
+		{
+			if (suffixSum + satisfaction[i] <= 0)
+			{
+				break;
+			}
+
+			suffixSum += satisfaction[i];
+			result += suffixSum;
+		}
+		return result;
+	}
+}
