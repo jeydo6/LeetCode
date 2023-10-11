744bc0e2d369017dc48b1b37a32feb99d6a35e28
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:25 +0300

    234

--- LeetCode/Algorithms/234.cs
+++ LeetCode/Algorithms/234.cs
@@ -1,44 +1,102 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _234
 {
-	// EASY
-	internal class _234
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static bool IsPalindrome(ListNode head)
+	{
+		var stack = new Stack<ListNode>();
+		
+		var node = head;
+		while (node != null)
+		{
+			stack.Push(node);
+			node = node.next;
 		}
 
-		public static bool IsPalindrome(ListNode head)
+		while (stack.Count > 0)
 		{
-			var stack = new Stack<ListNode>();
-			
-			var node = head;
-			while (node != null)
+			var current = stack.Pop();
+			if (head.val != current.val)
 			{
-				stack.Push(node);
-				node = node.next;
+				return false;
 			}
 
-			while (stack.Count > 0)
-			{
-				var current = stack.Pop();
-				if (head.val != current.val)
-				{
-					return false;
-				}
+			head = head.next;
+		}
 
-				head = head.next;
-			}
+		return true;
+	}
 
+	public static bool IsPalindromeO1(ListNode head)
+	{
+		if (head == null)
+		{
 			return true;
 		}
+
+		var mid = GetListMiddle(head);
+		var second = ReverseList(mid.next);
+
+		var p1 = head;
+		var p2 = second;
+
+		var result = true;
+		while (result && p2 != null)
+		{
+			if (p1.val != p2.val)
+			{
+				result = false;
+			}
+
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+
+		mid.next = ReverseList(second);
+
+		return result;
+	}
+
+	private static ListNode GetListMiddle(ListNode head)
+	{
+		var slow = head;
+		var fast = head;
+
+		while (fast.next != null && fast.next.next != null)
+		{
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+
+		return slow;
+	}
+
+	private static ListNode ReverseList(ListNode head)
+	{
+		var previous = default(ListNode);
+		var current = head;
+		while (current != null)
+		{
+			var next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
+		}
+
+		return previous;
 	}
 }
