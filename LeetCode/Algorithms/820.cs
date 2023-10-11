dc5c14686ce8dad7215c2e9595d174eb5f20adec
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:42 +0300

    820

---
+++ LeetCode/Algorithms/820.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _820
+	{
+		public static int MinimumLengthEncoding(string[] words)
+		{
+			var hashSet = new HashSet<string>(words);
+			for (var i = 0; i < words.Length; i++)
+			{
+				for (var j = 1; j < words[i].Length; j++)
+				{
+					hashSet.Remove(words[i][j..]);
+				}
+			}
+
+			var result = 0;
+			foreach (var item in hashSet)
+			{
+				result += item.Length + 1;
+			}
+			return result;
+		}
+	}
+}
