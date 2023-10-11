078ce1e05a2408e6782c9c54b6ca79bf1928c47c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:15 +0300

    443

---
+++ LeetCode/Algorithms/443.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms;
+internal class _443
+{
+	public static int Compress(char[] chars)
+	{
+		var i = 0;
+		var result = 0;
+		while (i < chars.Length)
+		{
+			var groupLength = 1;
+			while (i + groupLength < chars.Length && chars[i + groupLength] == chars[i])
+			{
+				groupLength++;
+			}
+			chars[result++] = chars[i];
+			if (groupLength > 1)
+			{
+				foreach (var ch in groupLength.ToString())
+				{
+					chars[result++] = ch;
+				}
+			}
+			i += groupLength;
+		}
+		return result;
+	}
+}
