0a763387a0fc4a805a781d02957b9df9bcf56c86
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:34 +0300

    2

---
+++ LeetCode/Algorithms/2.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _2
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+		{
+			var dummy = new ListNode(0);
+			var p = l1;
+			var q = l2;
+			var current = dummy;
+			var carry = 0;
+			while (p != null || q != null)
+			{
+				var x = (p != null) ? p.val : 0;
+				var y = (q != null) ? q.val : 0;
+				var sum = carry + x + y;
+				carry = sum / 10;
+				current.next = new ListNode(sum % 10);
+				current = current.next;
+				if (p != null) p = p.next;
+				if (q != null) q = q.next;
+			}
+			if (carry > 0)
+			{
+				current.next = new ListNode(carry);
+			}
+			return dummy.next;
+		}
+	}
+}
