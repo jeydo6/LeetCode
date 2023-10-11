04288cd4ebcd04e913b8fe0b18e916625b1573d4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:29 +0300

    1374

---
+++ LeetCode/Algorithms/1374.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1374
+	{
+		public static string GenerateTheString(int n)
+		{
+			var chars = new char[n];
+			Array.Fill(chars, 'a');
+			if (n % 2 == 0)
+			{
+				chars[0] = 'b';
+			}
+			return new string(chars);
+		}
+	}
+}
