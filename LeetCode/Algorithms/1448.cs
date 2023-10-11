b571b3b0fabef6b05bd460b416ab4c48168db2c8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:53 +0300

    1448

---
+++ LeetCode/Algorithms/1448.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1448
+	{
+		public class TreeNode
+		{
+			public int val;
+
+			public TreeNode left;
+
+			public TreeNode right;
+
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static int GoodNodes(TreeNode root, int val = int.MinValue)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			var result = root.val >= val ? 1 : 0;
+			result += GoodNodes(root.left, Math.Max(val, root.val));
+			result += GoodNodes(root.right, Math.Max(val, root.val));
+			return result;
+		}
+	}
+}
