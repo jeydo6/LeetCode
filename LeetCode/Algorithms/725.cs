ccbc12284e4080e45cae14df67783fabe8a547e0
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:51 +0300

    725

--- LeetCode/Algorithms/725.cs
+++ LeetCode/Algorithms/725.cs
@@ -1,44 +1,43 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _725
 {
-	// MEDIUM
-	internal class _725
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode[] SplitListToParts(ListNode head, int k)
+	{
+		var length = 0;
+		for (var item = head; item != null; item = item.next)
+		{
+			length++;
 		}
 
-		public static ListNode[] SplitListToParts(ListNode head, int k)
+		var n = length / k;
+		var r = length % k;
+
+		var node = head;
+		var prev = default(ListNode);
+		var parts = new ListNode[k];
+		for (var i = 0; node != null && i < k; i++, r--)
 		{
-			var length = 0;
-			for (var item = head; item != null; item = item.next)
+			parts[i] = node;
+			for (var j = 0; j < n + (r > 0 ? 1 : 0); j++)
 			{
-				length++;
+				prev = node;
+				node = node.next;
 			}
-
-			var n = length / k;
-			var r = length % k;
-
-			var node = head;
-			var prev = default(ListNode);
-			ListNode[] parts = new ListNode[k];
-			for (var i = 0; node != null && i < k; i++, r--)
-			{
-				parts[i] = node;
-				for (var j = 0; j < n + (r > 0 ? 1 : 0); j++)
-				{
-					prev = node;
-					node = node.next;
-				}
-				prev.next = null;
-			}
-			return parts;
+			prev.next = null;
 		}
+		return parts;
 	}
 }
