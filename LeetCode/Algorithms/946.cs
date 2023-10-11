f1d54c66f6f4af719a60cb7b521fa71b1f23b70a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:50 +0300

    946

--- LeetCode/Algorithms/946.cs
+++ LeetCode/Algorithms/946.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _946
 {
-	// MEDIUM
-	internal class _946
+	public static bool ValidateStackSequences(int[] pushed, int[] popped)
 	{
-		public static bool ValidateStackSequences(int[] pushed, int[] popped)
+		var j = 0;
+		var stack = new Stack<int>();
+		for (var i = 0; i < pushed.Length; i++)
 		{
-			var j = 0;
-			var stack = new Stack<int>();
-			for (var i = 0; i < pushed.Length; i++)
+			stack.Push(pushed[i]);
+			while (stack.Count > 0 && j < pushed.Length && stack.Peek() == popped[j])
 			{
-				stack.Push(pushed[i]);
-				while (stack.Count > 0 && j < pushed.Length && stack.Peek() == popped[j])
-				{
-					stack.Pop();
-					j++;
-				}
+				stack.Pop();
+				j++;
 			}
-			return j == pushed.Length;
 		}
+		return j == pushed.Length;
 	}
 }
