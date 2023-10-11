c8f5236a27b094185f7f9f0e19f37c3b8f891e57
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:16 +0300

    664

--- LeetCode/Algorithms/664.cs
+++ LeetCode/Algorithms/664.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace LeetCode.Algorithms;
 
 // HARD
@@ -5,6 +7,43 @@ internal class _664
 {
 	public static int StrangePrinter(string s)
 	{
-        //
+		var n = s.Length;
+		var dp = new int[n][];
+		for (var i = 0; i < n; i++)
+		{
+			dp[i] = new int[n];
+		}
+
+		for (var length = 1; length <= n; length++)
+		{
+			for (var left = 0; left <= n - length; left++)
+			{
+				var right = left + length - 1;
+				var i = -1;
+				dp[left][right] = n;
+				for (var j = left; j < right; j++)
+				{
+					if (s[j] != s[right] && i == -1)
+					{
+						i = j;
+					}
+
+					if (i != -1)
+					{
+						dp[left][right] = Math.Min(
+							dp[left][right],
+							dp[i][j] + dp[j + 1][right] + 1
+						);
+					}
+				}
+
+				if (i == -1)
+				{
+					dp[left][right] = 0;
+				}
+			}
+		}
+
+		return dp[0][^1] + 1;
 	}
 }
