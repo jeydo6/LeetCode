cd34282b7e3d51555b7bec3ae63bd8b4cb8b281f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:50 +0300

    1277

---
+++ LeetCode/Algorithms/1277.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1277
+	{
+		public static int CountSquares(int[][] matrix)
+		{
+			var result = 0;
+			for (var i = 0; i < matrix.Length; i++)
+			{
+				for (var j = 0; j < matrix[0].Length; j++)
+				{
+					if (i > 0 && j > 0 && matrix[i][j] > 0)
+					{
+						matrix[i][j] = Math.Min(
+							matrix[i - 1][j - 1],
+							Math.Min(matrix[i - 1][j], matrix[i][j - 1])
+						) + 1;
+					}
+					result += matrix[i][j];
+				}
+			}
+			return result;
+		}
+	}
+}
