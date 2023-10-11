9fc95b0c2f66f5e122d6773385e7bbec0c9972eb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:27 +0300

    1396

--- LeetCode/Algorithms/1396.cs
+++ LeetCode/Algorithms/1396.cs
@@ -1,49 +1,48 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1396
 {
-	// MEDIUM
-	internal class _1396
+	public class UndergroundSystem
 	{
-		public class UndergroundSystem
+		private readonly IDictionary<string, (double TotalTime, double TotalTrips)> _journeyData;
+		private readonly IDictionary<int, (string Station, int Time)> _checkInData;
+
+		public UndergroundSystem()
 		{
-			private readonly IDictionary<string, (double TotalTime, double TotalTrips)> _journeyData;
-			private readonly IDictionary<int, (string Station, int Time)> _checkInData;
+			_journeyData = new Dictionary<string, (double TotalTime, double TotalTrips)>();
+			_checkInData = new Dictionary<int, (string StationName, int CheckInTime)>();
+		}
 
-			public UndergroundSystem()
-			{
-				_journeyData = new Dictionary<string, (double TotalTime, double TotalTrips)>();
-				_checkInData = new Dictionary<int, (string StationName, int CheckInTime)>();
-			}
+		public void CheckIn(int id, string stationName, int t)
+		{
+			_checkInData[id] = (stationName, t);
+		}
 
-			public void CheckIn(int id, string stationName, int t)
-			{
-				_checkInData[id] = (stationName, t);
-			}
+		public void CheckOut(int id, string stationName, int t)
+		{
+			(var startStation, var checkInTime) = _checkInData[id];
+			var tripTime = t - checkInTime;
 
-			public void CheckOut(int id, string stationName, int t)
-			{
-				(var startStation, var checkInTime) = _checkInData[id];
-				var tripTime = t - checkInTime;
+			var routeKey = GetRouteKey(startStation, stationName);
+			(var totalTime, var totalTrips) = _journeyData.ContainsKey(routeKey) ? _journeyData[routeKey] : (0.0, 0.0);
 
-				var routeKey = GetRouteKey(startStation, stationName);
-				(var totalTime, var totalTrips) = _journeyData.ContainsKey(routeKey) ? _journeyData[routeKey] : (0.0, 0.0);
+			_journeyData[routeKey] = (totalTime + tripTime, ++totalTrips);
+			_checkInData.Remove(id);
+		}
 
-				_journeyData[routeKey] = (totalTime + tripTime, ++totalTrips);
-				_checkInData.Remove(id);
-			}
+		public double GetAverageTime(string startStation, string endStation)
+		{
+			var routeKey = GetRouteKey(startStation, endStation);
+			(var totalTime, var totalTrips) = _journeyData[routeKey];
+			return totalTime / totalTrips;
+		}
 
-			public double GetAverageTime(string startStation, string endStation)
-			{
-				var routeKey = GetRouteKey(startStation, endStation);
-				(var totalTime, var totalTrips) = _journeyData[routeKey];
-				return totalTime / totalTrips;
-			}
-
-			private static string GetRouteKey(string startStation, string endStation)
-			{
-				return $"{startStation}->{endStation}";
-			}
+		private static string GetRouteKey(string startStation, string endStation)
+		{
+			return $"{startStation}->{endStation}";
 		}
 	}
 }
