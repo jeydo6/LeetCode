aa9e17dd09e2712af92091004c3e08fd1acbaafd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:51 +0300

    559

--- LeetCode/Algorithms/559.cs
+++ LeetCode/Algorithms/559.cs
@@ -1,48 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Leetcode.Algorithms
 {
-    public class _559
-    {
-        public class Node
-        {
-            public Int32 val;
-            public IList<Node> children;
+	public class _559
+	{
+		public class Node
+		{
+			public int val;
+			public IList<Node> children;
 
-            public Node()
-            {
-                //
-            }
-            public Node(Int32 _val, IList<Node> _children)
-            {
-                val = _val;
-                children = _children;
-            }
-        }
+			public Node()
+			{
+				//
+			}
+			public Node(int val, IList<Node> children)
+			{
+				this.val = val;
+				this.children = children;
+			}
+		}
 
-        public Int32 MaxDepth(Node root)
-        {
-            if (root == null)
-            {
-                return 0;
-            }
-            else
-            {
-                Int32 depth = 0;
-                if (root.children != null)
-                {
-                    foreach (Node child in root.children)
-                    {
-                        Int32 childDepth = MaxDepth(child);
-                        depth = Math.Max(depth, childDepth);
-                    }
-                }
-                return 1 + depth;
-            }
-        }
-    }
+		public int MaxDepth(Node root)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+			else
+			{
+				int depth = 0;
+				if (root.children != null)
+				{
+					foreach (var child in root.children)
+					{
+						depth = Math.Max(depth, MaxDepth(child));
+					}
+				}
+				return 1 + depth;
+			}
+		}
+	}
 }
