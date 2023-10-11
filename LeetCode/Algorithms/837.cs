f26034fca8fa66682c8f9fdfe539108a5ee2d3b8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:22 +0300

    837

---
+++ LeetCode/Algorithms/837.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _837
+{
+	public static double New21Game(int n, int k, int maxPts)
+	{
+		var dp = new double[n + 1];
+		dp[0] = 1;
+
+		var s = k > 0 ? 1d : 0d;
+		for (var i = 1; i <= n; i++)
+		{
+			dp[i] = s / maxPts;
+			if (i < k)
+			{
+				s += dp[i];
+			}
+			if (i - maxPts >= 0 && i - maxPts < k)
+			{
+				s -= dp[i - maxPts];
+			}
+		}
+
+		var result = 0d;
+		for (var i = k; i <= n; i++)
+		{
+			result += dp[i];
+		}
+		return result;
+	}
+}
