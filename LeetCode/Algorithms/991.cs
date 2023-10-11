e501a6b411c9539dddfd30ca73c048ca956919d9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:45 +0300

    991

---
+++ LeetCode/Algorithms/991.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _991
+	{
+		public static int BrokenCalc(int startValue, int target)
+		{
+			var result = 0;
+			while (target > startValue)
+			{
+				if (target % 2 == 1)
+				{
+					target++;
+				}
+				else
+				{
+					target /= 2;
+				}
+				result++;
+			}
+			return result + startValue - target;
+		}
+	}
+}
