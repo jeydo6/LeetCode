8362f71c5922790847e95cec5aabdacb4c6b24ae
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:03 +0300

    36

--- LeetCode/Algorithms/36.cs
+++ LeetCode/Algorithms/36.cs
@@ -1,29 +1,29 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _36
 {
-	class _36
+	public static bool IsValidSudoku(char[][] board)
 	{
-		public static bool IsValidSudoku(char[][] board)
+		var seen = new HashSet<string>();
+		for (var i = 0; i < 9; i++)
 		{
-			var seen = new HashSet<string>();
-			for (var i = 0; i < 9; i++)
+			for (var j = 0; j < 9; j++)
 			{
-				for (var j = 0; j < 9; j++)
+				var number = board[i][j];
+				if (number != '.')
 				{
-					var number = board[i][j];
-					if (number != '.')
+					if (!seen.Add(number + " in row " + i) ||
+						!seen.Add(number + " in column " + j) ||
+						!seen.Add(number + " in block " + i / 3 + "-" + j / 3))
 					{
-						if (!seen.Add(number + " in row " + i) ||
-							!seen.Add(number + " in column " + j) ||
-							!seen.Add(number + " in block " + i / 3 + "-" + j / 3))
-						{
-							return false;
-						}
+						return false;
 					}
 				}
 			}
-			return true;
 		}
+		return true;
 	}
 }
