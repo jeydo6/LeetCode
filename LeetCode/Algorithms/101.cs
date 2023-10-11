ba4661a62cadbb5354f377ab4c9edf8ff6ba44dd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:37 +0300

    101

---
+++ LeetCode/Algorithms/101.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _101
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static bool IsSymmetric(TreeNode root)
+		{
+			if (root == null)
+			{
+				return true;
+			}
+
+			var stack = new Stack<TreeNode>();
+			if (root.left != null)
+			{
+				if (root.right == null)
+				{
+					return false;
+				}
+				stack.Push(root.left);
+				stack.Push(root.right);
+			}
+			else if (root.right != null)
+			{
+				return false;
+			}
+
+			while (stack.Count > 0)
+			{
+				if (stack.Count % 2 != 0)
+				{
+					return false;
+				}
+				var right = stack.Pop();
+				var left = stack.Pop();
+				if (right.val != left.val)
+				{
+					return false;
+				}
+
+				if (left.left != null)
+				{
+					if (right.right == null)
+					{
+						return false;
+					}
+
+					stack.Push(left.left);
+					stack.Push(right.right);
+				}
+				else if (right.right != null)
+				{
+					return false;
+				}
+
+				if (left.right != null)
+				{
+					if (right.left == null)
+					{
+						return false;
+					}
+
+					stack.Push(left.right);
+					stack.Push(right.left);
+				}
+				else if (right.left != null)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
