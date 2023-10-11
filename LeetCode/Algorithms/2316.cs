310cc75229fea6ffc0e056de40d362c0b7e0eb1f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:34 +0300

    2316

---
+++ LeetCode/Algorithms/2316.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2316
+{
+	public static long CountPairs(int n, int[][] edges)
+	{
+		var graph = new IList<int>[n];
+		for (var i = 0; i < n; i++)
+		{
+			graph[i] = new List<int>();
+		}
+
+		for (var i = 0; i < edges.Length; i++)
+		{
+			graph[edges[i][0]].Add(edges[i][1]);
+			graph[edges[i][1]].Add(edges[i][0]);
+		}
+
+		var result = 0L;
+		var visited = new bool[n];
+		var visitedCount = 0L;
+		for (var from = 0; from < n; from++)
+		{
+			if (visited[from])
+			{
+				var count = CountPairs(from, graph, new bool[n]);
+				result += visitedCount * count;
+				visitedCount += count;
+			}
+		}
+		return result;
+	}
+
+	private static long CountPairs(int from, IList<int>[] graph, bool[] visited)
+	{
+		visited[from] = true;
+		var result = 1L;
+		foreach (var to in graph[from])
+		{
+			if (!visited[to])
+			{
+				result += CountPairs(to, graph, visited);
+			}
+		}
+		return result;
+	}
+}
