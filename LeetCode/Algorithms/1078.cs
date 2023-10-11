05f139e9811c290f6fbfc60bc4dec3a7f28061ff
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:46 +0300

    1078

--- LeetCode/Algorithms/1078.cs
+++ LeetCode/Algorithms/1078.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Leetcode.Algorithms
 {
-    public class _1078
-    {
-        public String[] FindOcurrences(String text, String first, String second)
-        {
-            ICollection<String> resultCollection = new List<String>();
-
-            String[] words = text.Split(' ');
-
-            for (Int32 i = 0; i < words.Length - 2; i++)
-            {
-                if (words[i] == first && words[i + 1] == second)
-                {
-                    resultCollection.Add(words[i + 2]);
-                }
-            }
-
-            return resultCollection.ToArray();
-        }
-    }
+	internal class _1078
+	{
+		public static string[] FindOcurrences(string text, string first, string second)
+		{
+			var result = new List<string>();
+			var words = text.Split(' ');
+			for (var i = 0; i < words.Length - 2; i++)
+			{
+				if (words[i] == first && words[i + 1] == second)
+				{
+					result.Add(words[i + 2]);
+				}
+			}
+			return result.ToArray();
+		}
+	}
 }
