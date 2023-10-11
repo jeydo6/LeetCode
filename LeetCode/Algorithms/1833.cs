83bd0d86f5df7c85dd619258204527336ec31efe
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:26 +0300

    1833

---
+++ LeetCode/Algorithms/1833.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1833
+{
+	public static int MaxIceCream(int[] costs, int coins)
+	{
+		var result = 0;
+
+		var maxCost = costs[0];
+		for (var i = 0; i < costs.Length; i++)
+		{
+			maxCost = Math.Max(maxCost, costs[i]);
+		}
+
+		var costsFrequencies = new int[maxCost + 1];
+		for (var i = 0; i < costs.Length; i++)
+		{
+			costsFrequencies[costs[i]]++;
+		}
+
+		for (var cost = 1; cost <= maxCost; cost++)
+		{
+			if (costsFrequencies[cost] == 0)
+			{
+				continue;
+			}
+
+			if (cost > coins)
+			{
+				break;
+			}
+
+			var count = Math.Min(costsFrequencies[cost], coins / cost);
+			coins -= cost * count;
+			result += count;
+		}
+
+		return result;
+	}
+}
