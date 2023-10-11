64195266bcd99c6249684bf248cf4e7241af4da9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:49 +0300

    71

--- LeetCode/Algorithms/71.cs
+++ LeetCode/Algorithms/71.cs
@@ -1,30 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _71
 {
-	// MEDIUM
-	internal class _71
+	public static string SimplifyPath(string path)
 	{
-		public static string SimplifyPath(string path)
+		var itemCount = 0;
+		var components = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+		for (var i = 0; i < components.Length; i++)
 		{
-			var itemCount = 0;
-			var components = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
-			for (var i = 0; i < components.Length; i++)
+			switch (components[i])
 			{
-				switch (components[i])
-				{
-					case ".":
-						break;
-					case "..":
-						if (itemCount > 0) itemCount--;
-						break;
-					default:
-						components[itemCount++] = components[i];
-						break;
-				}
+				case ".":
+					break;
+				case "..":
+					if (itemCount > 0) itemCount--;
+					break;
+				default:
+					components[itemCount++] = components[i];
+					break;
 			}
-			return "/" + string.Join('/', components, 0, itemCount);
 		}
+		return "/" + string.Join('/', components, 0, itemCount);
 	}
 }
