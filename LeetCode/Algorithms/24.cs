98de6486fad41690c71066da7d5e00213e9ac5ae
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:16 +0300

    24

---
+++ LeetCode/Algorithms/24.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _24
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode SwapPairs(ListNode head)
+		{
+			if (head == null || head.next == null)
+			{
+				return head;
+			}
+
+			var next = head.next;
+			head.next = SwapPairs(head.next.next);
+			next.next = head;
+			return next;
+		}
+	}
+}
