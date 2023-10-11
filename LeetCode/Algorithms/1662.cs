b59102770225d6ba58181ac3ee3d60b2ca1168be
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:38 +0300

    1662

--- LeetCode/Algorithms/1662.cs
+++ LeetCode/Algorithms/1662.cs
@@ -1,10 +1,37 @@
 ﻿namespace LeetCode.Algorithms
 {
-	class _1662
+	// EASY
+	internal class _1662
 	{
 		public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
 		{
-			return string.Concat(word1) == string.Concat(word2);
+			var word1Pointer = 0;
+			var word2Pointer = 0;
+
+			var string1Pointer = 0;
+			var string2Pointer = 0;
+
+			while (word1Pointer < word1.Length && word2Pointer < word2.Length)
+			{
+				if (word1[word1Pointer][string1Pointer++] != word2[word2Pointer][string2Pointer++])
+				{
+					return false;
+				}
+
+				if (string1Pointer == word1[word1Pointer].Length)
+				{
+					word1Pointer++;
+					string1Pointer = 0;
+				}
+
+				if (string2Pointer == word2[word2Pointer].Length)
+				{
+					word2Pointer++;
+					string2Pointer = 0;
+				}
+			}
+
+			return word1Pointer == word1.Length && word2Pointer == word2.Length;
 		}
 	}
 }
