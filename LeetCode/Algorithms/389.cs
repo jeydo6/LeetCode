8f037639a4eca4811fc96e6a7bbf3727977b19af
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:06 +0300

    389

--- LeetCode/Algorithms/389.cs
+++ LeetCode/Algorithms/389.cs
@@ -1,18 +1,17 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _389
 {
-	// EASY
-	internal class _389
+	public static char FindTheDifference(string s, string t)
 	{
-		public static char FindTheDifference(string s, string t)
+		var n = t.Length;
+		var ch = t[^1];
+		for (var i = 0; i < n - 1; ++i)
 		{
-			var n = t.Length;
-			var ch = t[^1];
-			for (var i = 0; i < n - 1; ++i)
-			{
-				ch ^= s[i];
-				ch ^= t[i];
-			}
-			return ch;
+			ch ^= s[i];
+			ch ^= t[i];
 		}
+		return ch;
 	}
 }
