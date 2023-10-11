bef29500a43ee1d9e6254ec52e4dfb9aae3b8abb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:49 +0300

    700

--- LeetCode/Algorithms/700.cs
+++ LeetCode/Algorithms/700.cs
@@ -1,46 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _700
-    {
-        public class TreeNode
-        {
-            public TreeNode(Int32 x)
-            {
-                val = x;
-            }
+	public class _700
+	{
+		public class TreeNode
+		{
+			public TreeNode(int x)
+			{
+				val = x;
+			}
 
-            public Int32 val;
-            public TreeNode left;
-            public TreeNode right;
-        }
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+		}
 
-        public TreeNode SearchBST(TreeNode root, Int32 val)
-        {
-            if (root == null)
-            {
-                return null;
-            }
+		public static TreeNode SearchBST(TreeNode root, int val)
+		{
+			if (root == null)
+			{
+				return null;
+			}
 
-            if (val == root.val)
-            {
-                return root;
-            }
-            else if (val < root.val)
-            {
-                return SearchBST(root.left, val);
-            }
-            else if (val > root.val)
-            {
-                return SearchBST(root.right, val);
-            }
+			if (val == root.val)
+			{
+				return root;
+			}
+			else if (val < root.val)
+			{
+				return SearchBST(root.left, val);
+			}
+			else if (val > root.val)
+			{
+				return SearchBST(root.right, val);
+			}
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 }
