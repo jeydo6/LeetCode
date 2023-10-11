4ff6499c204b65371873ebc6447dbfa78b853379
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:41 +0300

    1009

---
+++ LeetCode/Algorithms/1009.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _1009
+	{
+		public static int BitwiseComplement(int n)
+		{
+			var x = 1;
+			while (n > x)
+			{
+				x = x * 2 + 1;
+			}
+			return x - n;
+		}
+	}
+}
