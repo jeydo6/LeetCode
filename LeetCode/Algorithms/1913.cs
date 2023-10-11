ae1e20727f007be178e060fe53844c04a387fb10
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:03 +0300

    1913

---
+++ LeetCode/Algorithms/1913.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1913
+	{
+		public static int MaxProductDifference(int[] numbers)
+		{
+			Array.Sort(numbers);
+			return numbers[^1] * numbers[^2] - numbers[1] * numbers[0];
+		}
+	}
+}
