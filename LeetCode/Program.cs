515cbb5a4525e86e4f6b82e93feff9a9b54cea8e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:17 +0300

    fix security vulnerability

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,5 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
+using System.Text.Json;
 
 namespace LeetCode
 {
@@ -7,9 +7,17 @@ namespace LeetCode
 	{
 		private static void Main()
 		{
-			var json = JsonConvert.SerializeObject(
-				""
-			);
+			var result = "";
+			WriteLine(result);
+		}
+
+		private static void WriteLine(object result)
+		{
+			var json = JsonSerializer.Serialize(result, new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				WriteIndented = true
+			});
 			Console.WriteLine(json);
 		}
 	}
