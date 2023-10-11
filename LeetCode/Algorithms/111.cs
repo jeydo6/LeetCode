67126c19c4b88655ad68940769c858a2394344fa
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:01 +0300

    111

--- LeetCode/Algorithms/111.cs
+++ LeetCode/Algorithms/111.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _111
 {
-	// EASY
-	internal class _111
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static int MinDepth(TreeNode root)
+	{
+		if (root == null)
+		{
+			return 0;
 		}
 
-		public static int MinDepth(TreeNode root)
-		{
-			if (root == null)
-			{
-				return 0;
-			}
-			var left = MinDepth(root.left);
-			var right = MinDepth(root.right);
-			return (left == 0 || right == 0) ? left + right + 1 : Math.Min(left, right) + 1;
-		}
+		var left = MinDepth(root.left);
+		var right = MinDepth(root.right);
+		return left == 0 || right == 0 ? left + right + 1 : Math.Min(left, right) + 1;
 	}
 }
