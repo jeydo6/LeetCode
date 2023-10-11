2b8cdcd35ec15510d0b13d48ddff637ecb2aeb11
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:29 +0300

    2214

---
+++ LeetCode/Algorithms/2214.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2214
+{
+	public static long MinimumHealth(int[] damage, int armor)
+	{
+		var maxDamage = 0;
+		var totalDamage = 0L;
+
+		for (var i = 0; i < damage.Length; i++)
+		{
+			totalDamage += damage[i];
+			if (damage[i] > maxDamage)
+			{
+				maxDamage = damage[i];
+			}
+		}
+
+		return totalDamage - Math.Min(armor, maxDamage) + 1;
+	}
+}
