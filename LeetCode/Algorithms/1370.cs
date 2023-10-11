551f87fac6755b25e248ac932f3a34b1e90f925a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:25 +0300

    1370

---
+++ LeetCode/Algorithms/1370.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	class _1370
+	{
+		public static string SortString(string s)
+		{
+			var dict = new int[26];
+			for (var i = 0; i < s.Length; i++)
+			{
+				dict[s[i] - 'a']++;
+			}
+
+			var sb = new StringBuilder(s.Length);
+			int count = 0;
+			while (count < s.Length)
+			{
+				for (var i = 0; i < 26; i++)
+				{
+					if (dict[i] > 0)
+					{
+						sb.Append((char)(i + 'a'));
+						dict[i] = dict[i] - 1;
+						count++;
+					}
+				}
+				for (var i = 25; i >= 0; i--)
+				{
+					if (dict[i] > 0)
+					{
+						sb.Append((char)(i + 'a'));
+						dict[i] = dict[i] - 1;
+						count++;
+					}
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
