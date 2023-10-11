809e7e584a013d33a2a0e19833bdf5b59f995fec
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:03 +0300

    598

---
+++ LeetCode/Algorithms/598.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _598
+	{
+		public static int MaxCount(int m, int n, int[][] ops)
+		{
+			if (ops == null || ops.Length == 0)
+			{
+				return m * n;
+			}
+
+			var row = int.MaxValue;
+			var col = int.MaxValue;
+			foreach (var op in ops)
+			{
+				row = Math.Min(row, op[0]);
+				col = Math.Min(col, op[1]);
+			}
+			return row * col;
+		}
+	}
+}
