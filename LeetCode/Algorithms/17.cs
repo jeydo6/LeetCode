f2ddd5e870ff6b5ed4ae0946d5acc10188ebee7a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:19 +0300

    17

--- LeetCode/Algorithms/17.cs
+++ LeetCode/Algorithms/17.cs
@@ -1,31 +1,30 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _17
 {
-	// MEDIUM
-	internal class _17
+	private static readonly string[] _mappings = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+	
+	public static IList<string> LetterCombinations(string digits)
 	{
-		public static IList<string> LetterCombinations(string digits)
+		if (string.IsNullOrEmpty(digits))
 		{
-			if (digits == null || digits.Length == 0)
-			{
-				return new List<string>();
-			}
-
-			var mappings = new string[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-
-			var queue = new Queue<string>();
-			queue.Enqueue("");
-			while (queue.Peek().Length != digits.Length)
-			{
-				var item = queue.Dequeue();
-				var mapping = mappings[digits[item.Length] - '0'];
-				for (var i = 0; i < mapping.Length; i++)
-				{
-					queue.Enqueue(item + mapping[i]);
-				}
-			}
-			return new List<string>(queue);
+			return new List<string>();
 		}
+
+		var queue = new Queue<string>();
+		queue.Enqueue("");
+		while (queue.Peek().Length != digits.Length)
+		{
+			var item = queue.Dequeue();
+			var mapping = _mappings[digits[item.Length] - '0'];
+			for (var i = 0; i < mapping.Length; i++)
+			{
+				queue.Enqueue(item + mapping[i]);
+			}
+		}
+		return new List<string>(queue);
 	}
 }
