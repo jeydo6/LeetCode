318bf350e5c3e36c552c1336f63a940061ff8cde
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:39 +0300

    974

---
+++ LeetCode/Algorithms/974.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _974
+{
+	public static int SubarraysDivByK(int[] nums, int k)
+	{
+		var result = 0;
+
+		var modGroups = new int[k];
+		modGroups[0] = 1;
+
+		var prefixMod = 0;
+		for (var i = 0; i < nums.Length; i++)
+		{
+			prefixMod = (prefixMod + nums[i] % k + k) % k;
+			result += modGroups[prefixMod];
+			modGroups[prefixMod]++;
+		}
+
+		return result;
+	}
+}
