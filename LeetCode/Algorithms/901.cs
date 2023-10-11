c76e8028cdd51b2d862b604c44749c818c68d3ec
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:51 +0300

    901

---
+++ LeetCode/Algorithms/901.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _901
+{
+	public class StockSpanner
+	{
+		private readonly Stack<int[]> _stack;
+
+		public StockSpanner()
+		{
+			_stack = new Stack<int[]>();
+		}
+
+		public int Next(int price)
+		{
+			var result = 1;
+			while (_stack.Count > 0 && _stack.Peek()[0] <= price)
+			{
+				result += _stack.Pop()[1];
+			}
+
+			_stack.Push(new int[] { price, result });
+
+			return result;
+		}
+	}
+}
