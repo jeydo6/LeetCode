50dcf9a3b5977a58ee23222dd1ca58c85a971a7f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:22 +0300

    565

---
+++ LeetCode/Algorithms/565.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _565
+	{
+		public static int ArrayNesting(int[] nums)
+		{
+			int maxSize = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var size = 0;
+				for (var j = i; nums[j] >= 0; size++)
+				{
+					var temp = nums[j];
+					nums[j] = -1;
+					j = temp;
+				}
+				maxSize = Math.Max(maxSize, size);
+			}
+			return maxSize;
+		}
+	}
+}
