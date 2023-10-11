343e8f08d73f56f4ab6a2cc8e62de91646f2fedb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:27 +0300

    2095

---
+++ LeetCode/Algorithms/2095.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2095
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode DeleteMiddle(ListNode head)
+	{
+		if (head.next == null)
+		{
+			return null;
+		}
+
+		var slow = head;
+		var fast = head.next.next;
+
+		while (fast != null && fast.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		slow.next = slow.next.next;
+		return head;
+	}
+}
