42087cfcf7dad8b96255147e9df8f2301e082799
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:11 +0300

    560

---
+++ LeetCode/Algorithms/560.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _560
+	{
+		public static int SubarraySum(int[] nums, int k)
+		{
+			var result = 0;
+
+			var sum = 0;
+			var preSum = new Dictionary<int, int>
+			{
+				[0] = 1
+			};
+			for (var i = 0; i < nums.Length; i++)
+			{
+				sum += nums[i];
+				if (preSum.ContainsKey(sum - k))
+				{
+					result += preSum[sum - k];
+				}
+				if (!preSum.ContainsKey(sum))
+				{
+					preSum[sum] = 0;
+				}
+				preSum[sum]++;
+			}
+
+			return result;
+		}
+	}
+}
