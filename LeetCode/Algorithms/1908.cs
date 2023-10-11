3eb89fd91450ac72bec3093c5257bc442abb88c2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:50 +0300

    1908

---
+++ LeetCode/Algorithms/1908.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1908
+{
+	public static bool NimGame(int[] piles)
+	{
+		var result = 0;
+		for (var i = 0; i < piles.Length; i++)
+		{
+			result ^= piles[i];
+		}
+		return result != 0;
+	}
+}
