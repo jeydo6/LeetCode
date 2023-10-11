50e02b06ae82e72cbae4c143d458198a4b61fb14
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:29 +0300

    1376

---
+++ LeetCode/Algorithms/1376.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1376
+{
+	public static int NumOfMinutes(int n, int headID, int[] manager, int[] informTime)
+	{
+		var graph = new List<int>[n];
+		for (var i = 0; i < n; i++)
+		{
+			graph[i] = new List<int>();
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (manager[i] != -1)
+			{
+				graph[manager[i]].Add(i);
+			}
+		}
+
+		var result = int.MinValue;
+
+		var queue = new Queue<(int Parent, int Time)>();
+		queue.Enqueue((headID, 0));
+		while (queue.Count > 0)
+		{
+			var (parent, time) = queue.Dequeue();
+			result = Math.Max(result, time);
+			foreach (var child in graph[parent])
+			{
+				queue.Enqueue((child, time + informTime[parent]));
+			}
+		}
+
+		return result;
+	}
+}
