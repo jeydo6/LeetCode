54c56922799fe0096e924bde97f9cbd226bbe833
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:29 +0300

    144

--- LeetCode/Algorithms/144.cs
+++ LeetCode/Algorithms/144.cs
@@ -1,42 +1,41 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
-{
-	// EASY
-	internal class _144
-	{
-		public class TreeNode
-		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
-		}
+namespace LeetCode.Algorithms;
 
-		public static IList<int> PreorderTraversal(TreeNode node)
+// EASY
+internal class _144
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			var list = new List<int>();
-			var stack = new Stack<TreeNode>();
-			while (node != null || stack.Count > 0)
-			{
-				if (node != null)
-				{
-					list.Add(node.val);
-					stack.Push(node);
-					node = node.left;
-				}
-				else
-				{
-					var temp = stack.Pop();
-					node = temp.right;
-				}
-			}
-			return list;
+			this.val = val;
+			this.left = left;
+			this.right = right;
 		}
+	}
+
+	public static IList<int> PreorderTraversal(TreeNode root)
+	{
+		var list = new List<int>();
+		var stack = new Stack<TreeNode>();
+		var node = root;
+		while (node != null || stack.Count > 0)
+		{
+			if (node != null)
+			{
+				list.Add(node.val);
+				stack.Push(node);
+				node = node.left;
+			}
+			else
+			{
+				node = stack.Pop().right;
+			}
+		}
+		return list;
 	}
 }
