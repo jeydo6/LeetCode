5f73c8b17e759d407655af1f3837dee0826465f8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:52 +0300

    953

---
+++ LeetCode/Algorithms/953.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _953
+{
+	public static bool IsAlienSorted(string[] words, string order)
+	{
+		var dict = new int[26];
+		for (var i = 0; i < order.Length; i++)
+		{
+			dict[order[i] - 'a'] = i;
+		}
+
+		for (var i = 0; i < words.Length - 1; i++)
+		{
+			for (var j = 0; j < words[i].Length; j++)
+			{
+				if (j >= words[i + 1].Length)
+				{
+					return false;
+				}
+
+				if (words[i][j] != words[i + 1][j])
+				{
+					if (dict[words[i][j] - 'a'] > dict[words[i + 1][j] - 'a'])
+					{
+						return false;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+}
