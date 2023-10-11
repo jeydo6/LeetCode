3e27ed9e83a2fb76280dbaca3a1765b526960202
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:19 +0300

    347

---
+++ LeetCode/Algorithms/347.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _347
+	{
+		public static int[] TopKFrequent(int[] nums, int k)
+		{
+			if (k == nums.Length)
+			{
+				return nums;
+			}
+
+			var count = new Dictionary<int, int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (!count.ContainsKey(nums[i]))
+				{
+					count[nums[i]] = 0;
+				}
+				count[nums[i]]++;
+			}
+
+			var pQueue = new PriorityQueue<int, int>();
+			foreach (var key in count.Keys)
+			{
+				pQueue.Enqueue(key, -count[key]);
+				if (pQueue.Count > k)
+				{
+					pQueue.Dequeue();
+				}
+			}
+
+			var result = new int[k];
+			for (var i = k - 1; i >= 0; --i)
+			{
+				result[i] = pQueue.Dequeue();
+			}
+			return result;
+		}
+	}
+}
