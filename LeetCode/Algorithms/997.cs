04c2e4ffbb6648359b17fc390830ad7bfb29bf66
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:40 +0300

    997

---
+++ LeetCode/Algorithms/997.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _997
+	{
+		public static int FindJudge(int n, int[][] trust)
+		{
+			var count = new int[n + 1];
+			for (var i = 0; i < trust.Length; i++)
+			{
+				count[trust[i][0]]--;
+				count[trust[i][1]]++;
+			}
+			for (var i = 1; i <= n; i++)
+			{
+				if (count[i] == n - 1)
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+}
