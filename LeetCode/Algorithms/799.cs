5147fd8f26ebad66aeba52cfd4c61983ee0ef1fb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:06 +0300

    799

--- LeetCode/Algorithms/799.cs
+++ LeetCode/Algorithms/799.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _799
 {
-	// MEDIUM
-	internal class _799
+	public static double ChampagneTower(int poured, int query_row, int query_glass)
 	{
-		public static double ChampagneTower(int poured, int query_row, int query_glass)
+		var result = new double[query_row + 2];
+		result[0] = poured;
+		for (var row = 1; row <= query_row; row++)
 		{
-			var result = new double[query_row + 2];
-			result[0] = poured;
-			for (var row = 1; row <= query_row; row++)
+			for (var i = row; i >= 0; i--)
 			{
-				for (var i = row; i >= 0; i--)
-				{
-					result[i] = Math.Max(0.0, (result[i] - 1) / 2);
-					result[i + 1] += result[i];
-				}
+				result[i] = Math.Max(0.0, (result[i] - 1) / 2);
+				result[i + 1] += result[i];
 			}
-			return Math.Min(result[query_glass], 1.0);
 		}
+		return Math.Min(result[query_glass], 1.0);
 	}
 }
