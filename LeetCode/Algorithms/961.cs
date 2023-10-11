5994177540edb4119238bb29ab381d4dfd9b24e1
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:04 +0300

    961

--- LeetCode/Algorithms/961.cs
+++ LeetCode/Algorithms/961.cs
@@ -2,7 +2,8 @@
 
 namespace Leetcode.Algorithms
 {
-	public class _961
+	// EASY
+	internal class _961
 	{
 		public static int RepeatedNTimes(int[] numbers)
 		{
@@ -20,27 +21,5 @@ namespace Leetcode.Algorithms
 			}
 			return 0;
 		}
-
-		//public Int32 RepeatedNTimes(Int32[] A)
-		//{
-		//	Int32 result = 0;
-
-		//	Int32 count = A.Length / 2;
-		//	Int32[] keys = A
-		//		.Distinct()
-		//		.ToArray();
-
-		//	foreach (Int32 key in keys)
-		//	{
-		//		Int32 keyCount = A.Count(x => x == key);
-		//		if (keyCount == count)
-		//		{
-		//			result = key;
-		//			break;
-		//		}
-		//	}
-
-		//	return result;
-		//}
 	}
 }
