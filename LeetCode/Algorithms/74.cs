83eeb0c8652496d86bfecf6739d575d9a3ed12e8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:22 +0300

    74

--- LeetCode/Algorithms/74.cs
+++ LeetCode/Algorithms/74.cs
@@ -1,28 +1,27 @@
-﻿namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _74
-	{
-		public static bool SearchMatrix(int[][] matrix, int target)
-		{
-			var n = matrix.Length;
-			var m = matrix[0].Length;
+﻿namespace LeetCode.Algorithms;
 
-			var lo = 0;
-			var hi = m * n - 1;
-			while (lo != hi)
+// MEDIUM
+internal class _74
+{
+	public static bool SearchMatrix(int[][] matrix, int target)
+	{
+		var n = matrix.Length;
+		var m = matrix[0].Length;
+
+		var lo = 0;
+		var hi = m * n - 1;
+		while (lo != hi)
+		{
+			var mid = lo + (hi - lo) / 2;
+			if (matrix[mid / m][mid % m] < target)
 			{
-				var mid = lo + (hi - lo) / 2;
-				if (matrix[mid / m][mid % m] < target)
-				{
-					lo = mid + 1;
-				}
-				else
-				{
-					hi = mid;
-				}
+				lo = mid + 1;
 			}
-			return matrix[hi / m][hi % m] == target;
+			else
+			{
+				hi = mid;
+			}
 		}
+		return matrix[hi / m][hi % m] == target;
 	}
 }
