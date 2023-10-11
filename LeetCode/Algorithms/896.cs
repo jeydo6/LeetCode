5084a83075c75e495fa69a3e2ac3b61bbdc20772
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:09 +0300

    896

---
+++ LeetCode/Algorithms/896.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _896
+{
+	public static bool IsMonotonic(int[] nums)
+	{
+		var inc = true;
+		var dec = true;
+		for (var i = 1; i < nums.Length; i++)
+		{
+			inc &= nums[i - 1] <= nums[i];
+			dec &= nums[i - 1] >= nums[i];
+		}
+		return inc || dec;
+	}
+}
