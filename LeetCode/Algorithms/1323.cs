f5d385992a04eaa7ce6661e185b9a7abc186b3f5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:49 +0300

    1323

--- LeetCode/Algorithms/1323.cs
+++ LeetCode/Algorithms/1323.cs
@@ -1,55 +1,24 @@
-﻿using System;
+﻿namespace LeetCode.Algorithms;
 
-namespace LeetCode.Algorithms
+// EASY
+internal class _1323
 {
-	class _1323
+	public static int Maximum69Number(int number)
 	{
-		//public static int Maximum69Number(int number)
-		//{
-		//	var length = (int)Math.Ceiling(Math.Log10(number));
-		//	var array = new int[length];
-		//	for (var i = 0; i < length; i++)
-		//	{
-		//		array[i] = number % 10;
-		//		number /= 10;
-		//	}
+		var chars = number
+			.ToString()
+			.ToCharArray();
 
-		//	for (var i = 0; i < length; i++)
-		//	{
-		//		if (array[^(i + 1)] == 6)
-		//		{
-		//			array[^(i + 1)] = 9;
-		//			break;
-		//		}
-		//	}
-
-		//	var result = 0;
-		//	var b = 1;
-		//	for (var i = 0; i < length; i++)
-		//	{
-		//		result += array[i] * b;
-		//		b *= 10;
-		//	}
-		//	return result;
-		//}
-
-		public static int Maximum69Number(int number)
+		for (var i = 0; i < chars.Length; i++)
 		{
-			var chars = number
-				.ToString()
-				.ToCharArray();
-
-			for (var i = 0; i < chars.Length; i++)
+			if (chars[i] == '6')
 			{
-				if (chars[i] == '6')
-				{
-					chars[i] = '9';
-					break;
-				}
+				chars[i] = '9';
+				break;
 			}
-
-			var result = new string(chars);
-			return int.Parse(result);
 		}
+
+		var result = new string(chars);
+		return int.Parse(result);
 	}
 }
