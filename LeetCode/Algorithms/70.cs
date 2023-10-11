aff463ae82858f60ba42885eb50e5ac3df37b904
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:21 +0300

    70

--- LeetCode/Algorithms/70.cs
+++ LeetCode/Algorithms/70.cs
@@ -1,27 +1,27 @@
-﻿namespace LeetCode.Algorithms
-{
-	class _70
-	{
-		public static int ClimbStairs(int n)
-		{
-			if (n == 1)
-			{
-				return 1;
-			}
-			if (n == 2)
-			{
-				return 2;
-			}
+﻿namespace LeetCode.Algorithms;
 
-			var dp = new int[n + 1];
-			dp[0] = 0;
-			dp[1] = 1;
-			dp[2] = 2;
-			for (var i = 3; i < n + 1; i++)
-			{
-				dp[i] = dp[i - 1] + dp[i - 2];
-			}
-			return dp[n];
+// EASY
+internal class _70
+{
+	public static int ClimbStairs(int n)
+	{
+		if (n == 1)
+		{
+			return 1;
 		}
+		if (n == 2)
+		{
+			return 2;
+		}
+
+		var dp = new int[n + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		for (var i = 3; i < n + 1; i++)
+		{
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
 	}
 }
