2596d0687c1142f64aa700df69509df5da3ff485
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:13 +0300

    1306

---
+++ LeetCode/Algorithms/1306.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1306
+	{
+		public static bool CanReach(int[] arr, int start)
+		{
+			return
+				start >= 0 &&
+				start < arr.Length &&
+				arr[start] >= 0 &&
+				(
+					(arr[start] = -arr[start]) == 0 ||
+					CanReach(arr, start + arr[start]) ||
+					CanReach(arr, start - arr[start])
+				);
+		}
+	}
+}
