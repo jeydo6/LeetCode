8163aa78ff670cf9e4272347f87def0e5573bdb8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:55 +0300

    709

--- LeetCode/Algorithms/709.cs
+++ LeetCode/Algorithms/709.cs
@@ -1,26 +1,7 @@
 ﻿namespace Leetcode.Algorithms
 {
-	public class _709
+	internal class _709
 	{
-		//public String ToLowerCase(String str)
-		//{
-		//	String result = "";
-
-		//	foreach (Char c in str)
-		//	{
-		//		Int32 diff = 0;
-
-		//		if (Char.IsUpper(c))
-		//		{
-		//			diff = 'a' - 'A';
-		//		}
-
-		//		result += (Char)(c + diff);
-		//	}
-
-		//	return result;
-		//}
-
 		public static string ToLowerCase(string s)
 		{
 			return s.ToLower();
