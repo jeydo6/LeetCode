0b96ae126fd8bcf80d71ba350997ec184e80ed75
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:22 +0300

    2244

---
+++ LeetCode/Algorithms/2244.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2244
+{
+	public static int MinimumRounds(int[] tasks)
+	{
+		var frequncies = new Dictionary<int, int>();
+		for (var i = 0; i < tasks.Length; i++)
+		{
+			var task = tasks[i];
+			if (!frequncies.ContainsKey(task))
+			{
+				frequncies[task] = 0;
+			}
+			frequncies[task]++;
+		}
+
+		var result = 0;
+		foreach (var count in frequncies.Values)
+		{
+			if (count == 1)
+			{
+				return -1;
+			}
+
+			if (count % 3 == 0)
+			{
+				result += count / 3;
+			}
+			else
+			{
+				result += count / 3 + 1;
+			}
+		}
+
+		return result;
+	}
+}
