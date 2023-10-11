3fa48096a06f6332dfc2695e1bfd67a3882d3b06
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:38 +0300

    961

--- LeetCode/Algorithms/961.cs
+++ LeetCode/Algorithms/961.cs
@@ -1,33 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Leetcode.Algorithms
 {
-    public class _961
-    {
-        public Int32 RepeatedNTimes(Int32[] A)
-        {
-            Int32 result = 0;
+	public class _961
+	{
+		public static int RepeatedNTimes(int[] numbers)
+		{
+			var hashSet = new HashSet<int>();
+			foreach (int number in numbers)
+			{
+				if (hashSet.Contains(number))
+				{
+					return number;
+				}
+				else
+				{
+					hashSet.Add(number);
+				}
+			}
+			return 0;
+		}
 
-            Int32 count = A.Length / 2;
-            Int32[] keys = A
-                .Distinct()
-                .ToArray();
+		//public Int32 RepeatedNTimes(Int32[] A)
+		//{
+		//	Int32 result = 0;
 
-            foreach (Int32 key in keys)
-            {
-                Int32 keyCount = A.Count(x => x == key);
-                if (keyCount == count)
-                {
-                    result = key;
-                    break;
-                }
-            }
+		//	Int32 count = A.Length / 2;
+		//	Int32[] keys = A
+		//		.Distinct()
+		//		.ToArray();
 
-            return result;
-        }
-    }
+		//	foreach (Int32 key in keys)
+		//	{
+		//		Int32 keyCount = A.Count(x => x == key);
+		//		if (keyCount == count)
+		//		{
+		//			result = key;
+		//			break;
+		//		}
+		//	}
+
+		//	return result;
+		//}
+	}
 }
