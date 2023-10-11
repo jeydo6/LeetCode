788894717bc0645ab3a4ed9ad842d2f5f5463495
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:38 +0300

    848

---
+++ LeetCode/Algorithms/848.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	class _848
+	{
+		public static string ShiftingLetters(string s, int[] shifts)
+		{
+			for (var i = shifts.Length - 2; i >= 0; i--)
+			{
+				shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+			}
+
+			var sb = new StringBuilder();
+			for (var i = 0; i < s.Length; i++)
+			{
+				sb.Insert(i, (char)((s[i] - 'a' + shifts[i]) % 26 + 'a'));
+			}
+			return sb.ToString();
+		}
+	}
+}
