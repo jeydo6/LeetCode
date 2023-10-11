1d3d5fd49838c7a54f36b95f67184cb6c74ef68e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:02 +0300

    1614

---
+++ LeetCode/Algorithms/1614.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1614
+	{
+		public static int MaxDepth(string s)
+		{
+			var max = 0;
+			var depth = 0;
+			foreach (var ch in s)
+			{
+				if (ch == '(')
+				{
+					depth++;
+				}
+				if (ch == ')')
+				{
+					if (depth > max)
+					{
+						max = depth;
+					}
+					depth--;
+				}
+			}
+			return max;
+		}
+	}
+}
