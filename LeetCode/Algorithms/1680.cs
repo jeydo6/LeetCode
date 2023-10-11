a0e84d24928cb7f99e087fe0cbf7ffe1e8629170
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:08 +0300

    1680

---
+++ LeetCode/Algorithms/1680.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1680
+{
+	private const int Modulo = 1_000_000_007;
+
+	public static int ConcatenatedBinary(int n)
+	{
+		var length = 0;
+		var result = 0L;
+		for (var i = 1; i <= n; i++)
+		{
+			if (Math.Pow(2, (int)(Math.Log(i) / Math.Log(2))) == i)
+			{
+				length++;
+			}
+			result = ((result * (int)Math.Pow(2, length)) + i) % Modulo;
+		}
+
+		return (int)result;
+	}
+}
