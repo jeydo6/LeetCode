93a183e41883d6ec948c117e1bf2d707408a0c76
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:17 +0300

    520

--- LeetCode/Algorithms/520.cs
+++ LeetCode/Algorithms/520.cs
@@ -1,19 +1,18 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _520
 {
-	// EASY
-	internal class _520
+	public static bool DetectCapitalUse(string word)
 	{
-		public static bool DetectCapitalUse(string word)
+		var count = 0;
+		for (var i = 0; i < word.Length; i++)
 		{
-			var count = 0;
-			for (var i = 0; i < word.Length; i++)
+			if ('Z' - word[i] >= 0)
 			{
-				if ('Z' - word[i] >= 0)
-				{
-					count++;
-				}
+				count++;
 			}
-			return count == 0 || count == word.Length || (count == 1 && 'Z' - word[0] >= 0);
 		}
+		return count == 0 || count == word.Length || (count == 1 && 'Z' - word[0] >= 0);
 	}
 }
