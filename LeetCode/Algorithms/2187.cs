1f476629676326580456a1030808b4a0fd1d8ef2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:18 +0300

    2187

---
+++ LeetCode/Algorithms/2187.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2187
+{
+	public static long MinimumTime(int[] time, int totalTrips)
+	{
+		var start = 1L;
+		var end = long.MaxValue;
+		while (start <= end)
+		{
+			var trip = 0L;
+			var mid = start + (end - start) / 2;
+			for (var i = 0; i < time.Length; i++)
+			{
+				trip += mid / time[i];
+			}	
+			if (trip < totalTrips)
+			{
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		return start;
+	}
+}
