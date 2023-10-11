5ed68f121d8b901ab6fa4c96d5fe6c323ef5e751
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:01 +0300

    1337

--- LeetCode/Algorithms/1337.cs
+++ LeetCode/Algorithms/1337.cs
@@ -1,36 +1,35 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1337
 {
-	// EASY
-	internal class _1337
+	public static int[] KWeakestRows(int[][] mat, int k)
 	{
-		public static int[] KWeakestRows(int[][] mat, int k)
+		var n = mat.Length;
+		var m = mat[0].Length;
+
+		var indexes = new int[k];
+		var nextInsertIndex = 0;
+		for (var c = 0; c < m && nextInsertIndex < k; c++)
 		{
-			var n = mat.Length;
-			var m = mat[0].Length;
-
-			var indexes = new int[k];
-			var nextInsertIndex = 0;
-			for (var c = 0; c < m && nextInsertIndex < k; c++)
+			for (var r = 0; r < n && nextInsertIndex < k; r++)
 			{
-				for (var r = 0; r < n && nextInsertIndex < k; r++)
-				{
-					if (mat[r][c] == 0 && (c == 0 || mat[r][c - 1] == 1))
-					{
-						indexes[nextInsertIndex] = r;
-						nextInsertIndex++;
-					}
-				}
-			}
-
-			for (var r = 0; nextInsertIndex < k; r++)
-			{
-				if (mat[r][m - 1] == 1)
+				if (mat[r][c] == 0 && (c == 0 || mat[r][c - 1] == 1))
 				{
 					indexes[nextInsertIndex] = r;
 					nextInsertIndex++;
 				}
 			}
-			return indexes;
 		}
+
+		for (var r = 0; nextInsertIndex < k; r++)
+		{
+			if (mat[r][m - 1] == 1)
+			{
+				indexes[nextInsertIndex] = r;
+				nextInsertIndex++;
+			}
+		}
+		return indexes;
 	}
 }
