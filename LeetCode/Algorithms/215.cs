b222fa0d520b4ffe52556dd85934c8b04cc86163
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:44 +0300

    215

---
+++ LeetCode/Algorithms/215.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _215
+	{
+		public static int FindKthLargest(int[] nums, int k)
+		{
+			var pqueue = new PriorityQueue<int, int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				pqueue.Enqueue(nums[i], nums[i]);
+				if (pqueue.Count > k)
+				{
+					pqueue.Dequeue();
+				}
+			}
+
+			return pqueue.Dequeue();
+		}
+	}
+}
