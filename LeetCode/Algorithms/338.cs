8a97b328eefe8a1ccac68eb43a8ede6ba6644c3f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:26 +0300

    338

---
+++ LeetCode/Algorithms/338.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _338
+	{
+		public static int[] CountBits(int n)
+		{
+			var result = new int[n + 1];
+			for (var i = 1; i <= n; i++)
+			{
+				result[i] = result[i >> 1] + (i & 1);
+			}
+			return result;
+		}
+	}
+}
