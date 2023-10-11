df9a191493232edaddc232fc7b6c55dc3de3488c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:23 +0300

    1143

--- LeetCode/Algorithms/1143.cs
+++ LeetCode/Algorithms/1143.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _1143
-	{
-		public static int LongestCommonSubsequence(string text1, string text2)
-		{
-			var dp = new int[text1.Length + 1][];
-			for (int i = 0; i < text1.Length + 1; i++)
-			{
-				dp[i] = new int[text2.Length + 1];
-			}
+namespace LeetCode.Algorithms;
 
-			for (int i = 0; i < text1.Length; i++)
+// MEDIUM
+internal class _1143
+{
+	public static int LongestCommonSubsequence(string text1, string text2)
+	{
+		var dp = new int[text1.Length + 1][];
+		for (var i = 0; i < text1.Length + 1; i++)
+		{
+			dp[i] = new int[text2.Length + 1];
+		}
+
+		for (var i = 0; i < text1.Length; i++)
+		{
+			for (var j = 0; j < text2.Length; j++)
 			{
-				for (int j = 0; j < text2.Length; j++)
+				if (text1[i] == text2[j])
 				{
-					if (text1[i] == text2[j])
-					{
-						dp[i + 1][j + 1] = 1 + dp[i][j];
-					}
-					else
-					{
-						dp[i + 1][j + 1] = Math.Max(dp[i][j + 1], dp[i + 1][j]);
-					}
+					dp[i + 1][j + 1] = 1 + dp[i][j];
+				}
+				else
+				{
+					dp[i + 1][j + 1] = Math.Max(dp[i][j + 1], dp[i + 1][j]);
 				}
 			}
-			return dp[text1.Length][text2.Length];
 		}
+		return dp[text1.Length][text2.Length];
 	}
 }
