bcfafacb10e44bce75a3ca65e289a90d63ffdc08
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:50 +0300

    141

--- LeetCode/Algorithms/141.cs
+++ LeetCode/Algorithms/141.cs
@@ -1,37 +1,36 @@
-﻿namespace LeetCode.Algorithms
-{
-	// EASY
-	internal class _141
-	{
-		public class ListNode
-		{
-			public int val;
-			public ListNode next;
-			public ListNode(int x)
-			{
-				val = x;
-				next = null;
-			}
-		}
+﻿namespace LeetCode.Algorithms;
 
-		public static bool HasCycle(ListNode head)
+// EASY
+internal class _141
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x)
 		{
-			if (head == null)
-			{
-				return false;
-			}
-			var slow = head;
-			var fast = head;
-			while (fast.next != null && fast.next.next != null)
-			{
-				slow = slow.next;
-				fast = fast.next.next;
-				if (slow == fast)
-				{
-					return true;
-				}
-			}
+			val = x;
+			next = null;
+		}
+	}
+
+	public static bool HasCycle(ListNode head)
+	{
+		if (head == null)
+		{
 			return false;
 		}
+		var slow = head;
+		var fast = head;
+		while (fast.next != null && fast.next.next != null)
+		{
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 }
