01d1e3f92b9b562b6a1ccd248e1220b765cbb2f4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:17 +0300

    1155

---
+++ LeetCode/Algorithms/1155.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1155
+{
+	private const int Modulo = 1000000007;
+
+	public static int NumRollsToTarget(int n, int k, int target)
+	{
+		var dp = new int[n + 1][];
+		for (var i = 0; i < n + 1; i++)
+		{
+			dp[i] = new int[target + 1];
+		}
+
+		dp[n][target] = 1;
+		for (var diceIndex = n - 1; diceIndex >= 0; diceIndex--)
+		{
+			for (var sum = 0; sum <= target; sum++)
+			{
+				var ways = 0;
+
+				for (var i = 1; i <= Math.Min(k, target - sum); i++)
+				{
+					ways = (ways + dp[diceIndex + 1][sum + i]) % Modulo;
+				}
+
+				dp[diceIndex][sum] = ways;
+			}
+		}
+
+		return dp[0][0];
+	}
+}
