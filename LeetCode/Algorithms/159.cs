17d49c7e56ca2261ada626957489adf74e73d73d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:01 +0300

    159

---
+++ LeetCode/Algorithms/159.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _159
+{
+	public static int LengthOfLongestSubstringTwoDistinct(string s)
+	{
+		if (s.Length < 3)
+		{
+			return s.Length;
+		}
+
+		var result = 2;
+
+		var dict = new Dictionary<char, int>();
+		
+		var left = 0;
+		var right = 0;
+		while (right < s.Length)
+		{
+			dict[s[right]] = right++;
+
+			if (dict.Count == 3)
+			{
+				var index = GetMinValue(dict.Values);
+				dict.Remove(s[index]);
+
+				left = index + 1;
+			}
+
+			result = Math.Max(result, right - left);
+		}
+		return result;
+	}
+
+	private static int GetMinValue(IEnumerable<int> values)
+	{
+		var result = int.MaxValue;
+		foreach (var value in values)
+		{
+			if (value < result)
+			{
+				result = value;
+			}
+		}
+
+		return result;
+	}
+}
