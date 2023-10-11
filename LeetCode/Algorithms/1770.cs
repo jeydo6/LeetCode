4721282e702bce33a17e078612b73a4c400375c5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:02 +0300

    1770

---
+++ LeetCode/Algorithms/1770.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1770
+{
+	public static int MaximumScore(int[] nums, int[] multipliers)
+	{
+		var dp = new int[multipliers.Length + 1];
+
+		for (var op = multipliers.Length - 1; op >= 0; op--)
+		{
+			var next = new List<int>(dp);
+			for (var left = op; left >= 0; left--)
+			{
+				dp[left] = Math.Max(
+					multipliers[op] * nums[left] + next[left + 1],
+					multipliers[op] * nums[nums.Length - 1 - (op - left)] + next[left]
+				);
+			}
+		}
+
+		return dp[0];
+	}
+}
