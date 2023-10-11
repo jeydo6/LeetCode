72a6b6f4aa6796821b6dd791a08f3b21c556a3a6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:55 +0300

    142

---
+++ LeetCode/Algorithms/142.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _142
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x)
+			{
+				val = x;
+				next = null;
+			}
+		}
+
+		public static ListNode DetectCycle(ListNode head)
+		{
+			var slow = head;
+			var fast = head;
+			while (fast != null && fast.next != null)
+			{
+				fast = fast.next.next;
+				slow = slow.next;
+
+				if (fast == slow)
+				{
+					var slow2 = head;
+					while (slow2 != slow)
+					{
+						slow = slow.next;
+						slow2 = slow2.next;
+					}
+					return slow;
+				}
+			}
+			return null;
+		}
+	}
+}
