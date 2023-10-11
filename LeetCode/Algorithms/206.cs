ec74b5d74d82281917f326fae047bfb2e8614ad6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:36 +0300

    206

---
+++ LeetCode/Algorithms/206.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _206
+	{
+
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+		public static ListNode ReverseList(ListNode head)
+		{
+			ListNode newHead = null;
+			while (head != null)
+			{
+				var next = head.next;
+				head.next = newHead;
+				newHead = head;
+				head = next;
+			}
+			return newHead;
+		}
+	}
+}
