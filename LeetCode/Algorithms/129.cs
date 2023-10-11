a8fb5a30ad5e19c2bb4b051ab6f07d8bfaa7b436
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:25 +0300

    129

--- LeetCode/Algorithms/129.cs
+++ LeetCode/Algorithms/129.cs
@@ -1,34 +1,33 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _129
 {
-	// MEDIUM
-	internal class _129
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static int SumNumbers(TreeNode root, int sum = 0)
+	{
+		if (root == null)
+		{
+			return 0;
 		}
 
-		public static int SumNumbers(TreeNode root, int sum = 0)
+		if (root.left == null && root.right == null)
 		{
-			if (root == null)
-			{
-				return 0;
-			}
-
-			if (root.left == null && root.right == null)
-			{
-				return sum * 10 + root.val;
-			}
-
-			return SumNumbers(root.left, sum * 10 + root.val) + SumNumbers(root.right, sum * 10 + root.val);
+			return sum * 10 + root.val;
 		}
+
+		return SumNumbers(root.left, sum * 10 + root.val) + SumNumbers(root.right, sum * 10 + root.val);
 	}
 }
