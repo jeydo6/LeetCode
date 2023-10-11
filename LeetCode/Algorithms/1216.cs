6bc3b66ea6f7a383751c858eb14a29b8cb2e49ca
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:08 +0300

    1216

---
+++ LeetCode/Algorithms/1216.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _1216
+{
+	public static bool IsValidPalindrome(string s, int k)
+	{
+		var memo = new int[s.Length];
+
+		for (var i = s.Length - 2; i >= 0; i--)
+		{
+			var prev = 0;
+			for (var j = i + 1; j < s.Length; j++)
+			{
+				var temp = memo[j];
+
+				if (s[i] == s[j])
+				{
+					memo[j] = prev;
+				}
+				else
+				{
+					memo[j] = 1 + Math.Min(memo[j], memo[j - 1]);
+				}
+
+				prev = temp;
+			}
+		}
+
+		return memo[^1] <= k;
+	}
+}
