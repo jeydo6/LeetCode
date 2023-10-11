e6852af21b2126b924cf57a098303f154143894a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:02 +0300

    280

---
+++ LeetCode/Algorithms/280.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _280
+{
+	public static void WiggleSort(int[] nums)
+	{
+		for (var i = 0; i < nums.Length - 1; i++)
+		{
+			if (
+				((i % 2 == 0) && nums[i] > nums[i + 1]) ||
+				((i % 2 == 1) && nums[i] < nums[i + 1])
+			)
+			{
+				(nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+			}
+		}
+	}
+}
