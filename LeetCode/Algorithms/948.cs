019821b44bd03d5ffe8a640babc49274a2c6375f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:58 +0300

    948

---
+++ LeetCode/Algorithms/948.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _948
+{
+	public static int BagOfTokensScore(int[] tokens, int power)
+	{
+		Array.Sort(tokens);
+		var lo = 0;
+		var hi = tokens.Length - 1;
+
+		var points = 0;
+		var result = 0;
+		while (lo <= hi && (power >= tokens[lo] || points > 0))
+		{
+			while (lo <= hi && power >= tokens[lo])
+			{
+				power -= tokens[lo++];
+				points++;
+			}
+
+			if (points > result)
+			{
+				result = points;
+			}
+
+			if (lo <= hi && points > 0)
+			{
+				power += tokens[hi--];
+				points--;
+			}
+		}
+
+		return result;
+	}
+}
