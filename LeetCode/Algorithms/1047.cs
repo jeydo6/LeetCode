13e1f3d16264199f47293731e70164734ef6cbb4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:52 +0300

    1047

--- LeetCode/Algorithms/1047.cs
+++ LeetCode/Algorithms/1047.cs
@@ -4,40 +4,21 @@ namespace Leetcode.Algorithms
 {
 	internal class _1047
 	{
-		public static string RemoveDuplicates(string str)
+		public static string RemoveDuplicates(string s)
 		{
-			while (str.Length > 1)
+			var sb = new StringBuilder();
+			for (var i = 0; i < s.Length; i++)
 			{
-				var sb = new StringBuilder(str);
-
-				var i = 0;
-				var j = 0;
-
-				while (i < str.Length - 1)
+				if (sb.Length != 0 && s[i] == sb[^1])
 				{
-					if (str[i] == str[i + 1])
-					{
-						sb.Remove(j, 2);
-
-						i += 2;
-					}
-					else
-					{
-						i++;
-						j++;
-					}
-				}
-
-				if (sb.Length != str.Length)
-				{
-					str = sb.ToString();
+					sb.Remove(sb.Length - 1, 1);
 				}
 				else
 				{
-					break;
+					sb.Append(s[i]);
 				}
 			}
-			return str;
+			return sb.ToString();
 		}
 	}
 }
