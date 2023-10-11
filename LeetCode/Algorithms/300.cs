aab79f9ec4413e35baae653e53e1bf68e2593fdf
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:27 +0300

    300

---
+++ LeetCode/Algorithms/300.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _300
+	{
+		public static int LengthOfLIS(int[] nums)
+		{
+			var dp = new int[nums.Length];
+			Array.Fill(dp, 1);
+
+			for (var i = 1; i < nums.Length; i++)
+			{
+				for (var j = 0; j < i; j++)
+				{
+					if (nums[i] > nums[j])
+					{
+						dp[i] = Math.Max(dp[i], dp[j] + 1);
+					}
+				}
+			}
+
+			var result = 0;
+			for (var i = 0; i < dp.Length; i++)
+			{
+				result = Math.Max(result, dp[i]);
+			}
+
+			return result;
+		}
+	}
+}
