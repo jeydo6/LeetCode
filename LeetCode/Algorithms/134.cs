64dc57b5c213bb9977350cf658b4dd76b9bb13a9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:57 +0300

    134

---
+++ LeetCode/Algorithms/134.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _134
+	{
+		public static int CanCompleteCircuit(int[] gas, int[] cost)
+		{
+			var sumGas = 0;
+			var sumCost = 0;
+			var start = 0;
+			var tank = 0;
+			for (var i = 0; i < gas.Length; i++)
+			{
+				sumGas += gas[i];
+				sumCost += cost[i];
+				tank += gas[i] - cost[i];
+				if (tank < 0)
+				{
+					start = i + 1;
+					tank = 0;
+				}
+			}
+			if (sumGas < sumCost)
+			{
+				return -1;
+			}
+			else
+			{
+				return start;
+			}
+		}
+	}
+}
