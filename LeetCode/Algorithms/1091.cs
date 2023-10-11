8f2e23a11a70171f2e4d973c0f9c55b6c2aea967
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:27 +0300

    1091

--- LeetCode/Algorithms/1091.cs
+++ LeetCode/Algorithms/1091.cs
@@ -1,99 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1091
 {
-	// MEDIUM
-	internal class _1091
+	private static readonly int[][] _directions = new int[][]
 	{
-		private static readonly int[][] _directions = new int[][]
+		new int[] { -1, -1 },
+		new int[] { -1, 0 },
+		new int[] { -1, 1 },
+		new int[] { 0, -1 },
+		new int[] { 0, 1 },
+		new int[] { 1, -1 },
+		new int[] { 1, 0 },
+		new int[] { 1, 1 }
+	};
+
+	public static int ShortestPathBinaryMatrix(int[][] grid)
+	{
+		if (grid[0][0] != 0 || grid[^1][^1] != 0)
 		{
-			new int[] { -1, -1 },
-			new int[] { -1, 0 },
-			new int[] { -1, 1 },
-			new int[] { 0, -1 },
-			new int[] { 0, 1 },
-			new int[] { 1, -1 },
-			new int[] { 1, 0 },
-			new int[] { 1, 1 }
-		};
-
-		public static int ShortestPathBinaryMatrix(int[][] grid)
-		{
-			if (grid[0][0] != 0 || grid[^1][^1] != 0)
-			{
-				return -1;
-			}
-
-			var visited = new bool[grid.Length][];
-			for (var i = 0; i < grid.Length; i++)
-			{
-				visited[i] = new bool[grid[i].Length];
-			}
-
-			var pqueue = new PriorityQueue<(int Row, int Column, int Distance), int>();
-			pqueue.Enqueue((0, 0, 1), GetEstimation(grid, 0, 0));
-
-			while (pqueue.Count > 0)
-			{
-				var (row, column, distance) = pqueue.Dequeue();
-				if (row == grid.Length - 1 && column == grid.Length - 1)
-				{
-					return distance;
-				}
-
-				if (visited[row][column])
-				{
-					continue;
-				}
-
-				visited[row][column] = true;
-
-				var neighbours = GetNeighbours(grid, row, column);
-				for (var i = 0; i < neighbours.Length; i++)
-				{
-					var (neighbourRow, neighbourColumn) = neighbours[i];
-
-					if (visited[neighbourRow][neighbourColumn])
-					{
-						continue;
-					}
-
-					var neighbourDistance = distance + 1;
-					var neighbourEstimation = neighbourDistance + GetEstimation(grid, neighbourRow, neighbourColumn);
-					pqueue.Enqueue((neighbourRow, neighbourColumn, neighbourDistance), neighbourEstimation);
-				}
-			}
-
 			return -1;
 		}
 
-		private static (int Row, int Column)[] GetNeighbours(int[][] grid, int row, int column)
+		var visited = new bool[grid.Length][];
+		for (var i = 0; i < grid.Length; i++)
 		{
-			var neighbours = new List<(int Row, int Column)>();
-			for (int i = 0; i < _directions.Length; i++)
+			visited[i] = new bool[grid[i].Length];
+		}
+
+		var pqueue = new PriorityQueue<(int Row, int Column, int Distance), int>();
+		pqueue.Enqueue((0, 0, 1), GetEstimation(grid, 0, 0));
+
+		while (pqueue.Count > 0)
+		{
+			var (row, column, distance) = pqueue.Dequeue();
+			if (row == grid.Length - 1 && column == grid.Length - 1)
 			{
-				var newRow = row + _directions[i][0];
-				var newColumn = column + _directions[i][1];
-				if (
-					newRow < 0 || newRow >= grid.Length ||
-					newColumn < 0 || newColumn >= grid[0].Length ||
-					grid[newRow][newColumn] != 0
-				)
+				return distance;
+			}
+
+			if (visited[row][column])
+			{
+				continue;
+			}
+
+			visited[row][column] = true;
+
+			var neighbours = GetNeighbours(grid, row, column);
+			for (var i = 0; i < neighbours.Length; i++)
+			{
+				var (neighbourRow, neighbourColumn) = neighbours[i];
+
+				if (visited[neighbourRow][neighbourColumn])
 				{
 					continue;
 				}
-				neighbours.Add((newRow, newColumn));
+
+				var neighbourDistance = distance + 1;
+				var neighbourEstimation = neighbourDistance + GetEstimation(grid, neighbourRow, neighbourColumn);
+				pqueue.Enqueue((neighbourRow, neighbourColumn, neighbourDistance), neighbourEstimation);
 			}
-			return neighbours.ToArray();
 		}
 
-		private static int GetEstimation(int[][] grid, int row, int col)
+		return -1;
+	}
+
+	private static (int Row, int Column)[] GetNeighbours(int[][] grid, int row, int column)
+	{
+		var neighbours = new List<(int Row, int Column)>();
+		for (int i = 0; i < _directions.Length; i++)
 		{
-			return Math.Max(
-				grid.Length - row - 1,
-				grid[0].Length - col - 1
-			);
+			var newRow = row + _directions[i][0];
+			var newColumn = column + _directions[i][1];
+			if (
+				newRow < 0 || newRow >= grid.Length ||
+				newColumn < 0 || newColumn >= grid[0].Length ||
+				grid[newRow][newColumn] != 0
+			)
+			{
+				continue;
+			}
+			neighbours.Add((newRow, newColumn));
 		}
+		return neighbours.ToArray();
+	}
+
+	private static int GetEstimation(int[][] grid, int row, int col)
+	{
+		return Math.Max(
+			grid.Length - row - 1,
+			grid[0].Length - col - 1
+		);
 	}
 }
