e2b4f4ed99127fba5707f3337006058e38cf2e2b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:50 +0300

    138

--- LeetCode/Algorithms/138.cs
+++ LeetCode/Algorithms/138.cs
@@ -1,59 +1,58 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _138
 {
-	// MEDIUM
-	internal class _138
+	public class Node
 	{
-		public class Node
-		{
-			public int val;
-			public Node next;
-			public Node random;
+		public int val;
+		public Node next;
+		public Node random;
 
-			public Node(int _val)
-			{
-				val = _val;
-				next = null;
-				random = null;
-			}
+		public Node(int _val)
+		{
+			val = _val;
+			next = null;
+			random = null;
+		}
+	}
+
+	public static Node CopyRandomList(Node head)
+	{
+		if (head == null)
+		{
+			return null;
 		}
 
-		public static Node CopyRandomList(Node head)
+		var ptr = head;
+		while (ptr != null)
 		{
-			if (head == null)
+			var newNode = new Node(ptr.val)
 			{
-				return null;
-			}
+				next = ptr.next
+			};
 
-			var ptr = head;
-			while (ptr != null)
-			{
-				var newNode = new Node(ptr.val)
-				{
-					next = ptr.next
-				};
-
-				ptr.next = newNode;
-				ptr = newNode.next;
-			}
-
-			ptr = head;
-			while (ptr != null)
-			{
-				ptr.next.random = ptr.random?.next;
-				ptr = ptr.next.next;
-			}
-
-			var ptrOldList = head;
-			var ptrNewList = head.next;
-			var prev = head.next;
-			while (ptrOldList != null)
-			{
-				ptrOldList.next = ptrOldList.next.next;
-				ptrNewList.next = ptrNewList.next?.next;
-				ptrOldList = ptrOldList.next;
-				ptrNewList = ptrNewList.next;
-			}
-			return prev;
+			ptr.next = newNode;
+			ptr = newNode.next;
 		}
+
+		ptr = head;
+		while (ptr != null)
+		{
+			ptr.next.random = ptr.random?.next;
+			ptr = ptr.next.next;
+		}
+
+		var ptrOldList = head;
+		var ptrNewList = head.next;
+		var prev = head.next;
+		while (ptrOldList != null)
+		{
+			ptrOldList.next = ptrOldList.next.next;
+			ptrNewList.next = ptrNewList.next?.next;
+			ptrOldList = ptrOldList.next;
+			ptrNewList = ptrNewList.next;
+		}
+		return prev;
 	}
 }
