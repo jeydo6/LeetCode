74b292f0bfe6c474d07caf76b279f795b4fa6fb2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:27 +0300

    437

---
+++ LeetCode/Algorithms/437.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _437
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static int PathSum(TreeNode root, int targetSum)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			return PathSum(root.left, targetSum) + PathSum(root.right, targetSum) + PathSum(root, targetSum, 0);
+		}
+
+		private static int PathSum(TreeNode root, int targetSum, int current)
+		{
+			if (root == null)
+			{
+				return 0;
+			}
+
+			current += root.val;
+
+			return (current == targetSum ? 1 : 0) + PathSum(root.left, targetSum, current) + PathSum(root.right, targetSum, current);
+		}
+	}
+}
