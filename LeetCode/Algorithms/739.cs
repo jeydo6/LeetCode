2e647f9c7b1b7743171e1151c60d0a84972ee792
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:50 +0300

    739

---
+++ LeetCode/Algorithms/739.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _739
+	{
+		public static int[] DailyTemperatures(int[] temperatures)
+		{
+			var result = new int[temperatures.Length];
+
+			var stack = new Stack<int>();
+			for (var i = 0; i < temperatures.Length; i++)
+			{
+				while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
+				{
+					var index = stack.Pop();
+					result[index] = i - index;
+				}
+				stack.Push(i);
+			}
+
+			return result;
+		}
+	}
+}
