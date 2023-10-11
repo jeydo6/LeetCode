ff336a3062c2455e2995f512612d6a2fb6e138c2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:42 +0300

    1748

---
+++ LeetCode/Algorithms/1748.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1748
+	{
+		public static int SumOfUnique(int[] numbers)
+		{
+			var dict = new Dictionary<int, int>();
+			foreach (int number in numbers)
+			{
+				if (!dict.ContainsKey(number))
+				{
+					dict[number] = 0;
+				}
+				dict[number]++;
+			}
+
+			var result = 0;
+			foreach (var key in dict.Keys)
+			{
+				if (dict[key] == 1)
+				{
+					result += key;
+				}
+			}
+			return result;
+		}
+	}
+}
