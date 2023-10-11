c15e5bb14d62f4d548decdcb062823e3d02281c5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:56 +0300

    540

---
+++ LeetCode/Algorithms/540.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _540
+	{
+		public static int SingleNonDuplicate(int[] nums)
+		{
+			var left = 0;
+			var right = nums.Length - 1;
+			while (left < right)
+			{
+				var mid = (left + right) / 2;
+				if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+				{
+					left = mid + 1;
+				}
+				else
+				{
+					right = mid;
+				}
+			}
+			return nums[left];
+		}
+	}
+}
