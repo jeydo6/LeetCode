9451825118d8687e1f5fdd55960cf683c778663d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:03 +0300

    104

--- LeetCode/Algorithms/104.cs
+++ LeetCode/Algorithms/104.cs
@@ -1,67 +1,66 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _104
 {
-	// EASY
-	internal class _104
+	public class TreeNode
 	{
-		public class TreeNode
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
-
-			public int val;
-
-			public TreeNode left;
-
-			public TreeNode right;
+			this.val = val;
+			this.left = left;
+			this.right = right;
 		}
 
-		public static int MaxDepth(TreeNode root)
+		public int val;
+
+		public TreeNode left;
+
+		public TreeNode right;
+	}
+
+	public static int MaxDepth(TreeNode root)
+	{
+		if (root == null)
 		{
-			if (root == null)
-			{
-				return 0;
-			}
-
-			var depth = 0;
-			var path = new Stack<TreeNode>();
-
-			var stack = new Stack<TreeNode>();
-			stack.Push(root);
-			while (stack.Count > 0)
-			{
-				var node = stack.Peek();
-				if (path.Count > 0 && node == path.Peek())
-				{
-					if (path.Count > depth)
-					{
-						depth = path.Count;
-					}
-
-					path.Pop();
-					stack.Pop();
-				}
-				else
-				{
-					path.Push(node);
-					if (node.left != null)
-					{
-						stack.Push(node.left);
-					}
-					if (node.right != null)
-					{
-						stack.Push(node.right);
-					}
-				}
-			}
-
-			return depth;
+			return 0;
 		}
+
+		var depth = 0;
+		var path = new Stack<TreeNode>();
+
+		var stack = new Stack<TreeNode>();
+		stack.Push(root);
+		while (stack.Count > 0)
+		{
+			var node = stack.Peek();
+			if (path.Count > 0 && node == path.Peek())
+			{
+				if (path.Count > depth)
+				{
+					depth = path.Count;
+				}
+
+				path.Pop();
+				stack.Pop();
+			}
+			else
+			{
+				path.Push(node);
+				if (node.left != null)
+				{
+					stack.Push(node.left);
+				}
+				if (node.right != null)
+				{
+					stack.Push(node.right);
+				}
+			}
+		}
+
+		return depth;
 	}
 }
 
