32ba9a85bb766d5fd892de0bc032752d113da068
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:56 +0300

    45

---
+++ LeetCode/Algorithms/45.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _45
+{
+	public static int Jump(int[] nums)
+	{
+		var result = 0;
+
+		var currentEnd = 0;
+		var currentFar = 0;
+		for (var i = 0; i < nums.Length - 1; ++i)
+		{
+			currentFar = Math.Max(currentFar, i + nums[i]);
+
+			if (i == currentEnd)
+			{
+				result++;
+				currentEnd = currentFar;
+			}
+		}
+
+		return result;
+	}
+}
