ea79c727c013037ba84499a98ddd940a28fc6a94
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:19 +0300

    1446

---
+++ LeetCode/Algorithms/1446.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _1446
+	{
+		public static int MaxPower(string s)
+		{
+			var result = 1;
+			var count = 1;
+			for (var i = 1; i < s.Length; i++)
+			{
+				if (s[i] == s[i - 1])
+				{
+					if (++count > result)
+					{
+						result = count;
+					}
+				}
+				else
+				{
+					count = 1;
+				}
+			}
+			return result;
+		}
+	}
+}
