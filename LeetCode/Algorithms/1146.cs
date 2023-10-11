ee5e8c33f84691d70809523b8f58f6c5bef1f308
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:34 +0300

    1146

---
+++ LeetCode/Algorithms/1146.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1146
+{
+	public class SnapshotArray
+	{
+		private readonly IDictionary<int, int>[] _array;
+
+		private int _snapshotId = 0;
+
+		public SnapshotArray(int length)
+		{
+			_array = new Dictionary<int, int>[length];
+			for (var i = 0; i < length; i++)
+			{
+				_array[i] = new Dictionary<int, int> { [0] = 0 };
+			}
+		}
+
+		public void Set(int index, int val)
+		{
+			_array[index][_snapshotId] = val;
+		}
+
+		public int Snap()
+		{
+			return _snapshotId++;
+		}
+
+		public int Get(int index, int snapshotId)
+		{
+			var dictionary = _array[index];
+			while (!dictionary.ContainsKey(snapshotId))
+			{
+				--snapshotId;
+			}
+
+			return dictionary[snapshotId];
+		}
+	}
+}
