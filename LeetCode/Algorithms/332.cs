e71c5d85a9e67bd6cea9d1813b51651e1a1162ee
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:58 +0300

    332

---
+++ LeetCode/Algorithms/332.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _332
+{
+	public static IList<string> FindItinerary(IList<IList<string>> tickets)
+	{
+		var targets = new Dictionary<string, PriorityQueue<string, string>>();
+		foreach (var ticket in tickets)
+		{
+			if (!targets.ContainsKey(ticket[0]))
+			{
+				targets[ticket[0]] = new PriorityQueue<string, string>();
+			}
+			targets[ticket[0]].Enqueue(ticket[1], ticket[1]);
+		}
+
+		var route = new LinkedList<string>();
+		var stack = new Stack<string>();
+		stack.Push("JFK");
+		while (stack.Count > 0)
+		{
+			while (targets.ContainsKey(stack.Peek()) && targets[stack.Peek()].Count > 0)
+			{
+				stack.Push(targets[stack.Peek()].Dequeue());
+			}
+
+			route.AddFirst(stack.Pop());
+		}
+		return new List<string>(route);
+	}
+}
