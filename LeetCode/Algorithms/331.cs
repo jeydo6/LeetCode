dcb99397d75b6caf06972f751a5fbfbdd735178e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:00 +0300

    331

---
+++ LeetCode/Algorithms/331.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _331
+	{
+		public static bool IsValidSerialization(string preorder)
+		{
+			var diff = 1;
+			var nodes = preorder.Split(",");
+			foreach (var node in nodes)
+			{
+				if (diff-- < 0)
+				{
+					return false;
+				}
+
+				if (node != "#")
+				{
+					diff += 2;
+				}
+			}
+			return diff == 0;
+		}
+	}
+}
