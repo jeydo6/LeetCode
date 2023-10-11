32091c7afd3a42af6dc8d041872149062b42e13f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:30 +0300

    1232

---
+++ LeetCode/Algorithms/1232.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1232
+{
+	public static bool CheckStraightLine(int[][] coordinates)
+	{
+		var dx0 = coordinates[1][0] - coordinates[0][0];
+		var dy0 = coordinates[1][1] - coordinates[0][1];
+		for (var i = 2; i < coordinates.Length; i++)
+		{
+			var dxi = coordinates[i][0] - coordinates[0][0];
+			var dyi = coordinates[i][1] - coordinates[0][1];
+			if (dy0 * dxi != dx0 * dyi)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
