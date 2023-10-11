6e7b12c568bc35a16ae52442ae55ab2ead378ecd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:31 +0300

    1359

---
+++ LeetCode/Algorithms/1359.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _1359
+	{
+		public static int CountOrders(int n)
+		{
+			var result = 1L;
+			var MOD = 1_000_000_007;
+			for (var i = 1; i <= 2 * n; ++i)
+			{
+				result *= i;
+				if (i % 2 == 0)
+				{
+					result /= 2;
+				}
+				result %= MOD;
+			}
+			return (int)result;
+		}
+	}
+}
