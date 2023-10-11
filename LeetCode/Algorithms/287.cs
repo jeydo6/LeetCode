b4df85b52cca9c60e4b2842e62d5931f4678943e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:02 +0300

    287

--- LeetCode/Algorithms/287.cs
+++ LeetCode/Algorithms/287.cs
@@ -1,25 +1,24 @@
-﻿namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _287
-	{
-		public static int FindDuplicate(int[] nums)
-		{
-			var slow = nums[0];
-			var fast = nums[0];
-			do
-			{
-				slow = nums[slow];
-				fast = nums[nums[fast]];
-			} while (slow != fast);
+﻿namespace LeetCode.Algorithms;
 
-			slow = nums[0];
-			while (slow != fast)
-			{
-				slow = nums[slow];
-				fast = nums[fast];
-			}
-			return fast;
+// MEDIUM
+internal class _287
+{
+	public static int FindDuplicate(int[] nums)
+	{
+		var slow = nums[0];
+		var fast = nums[0];
+		do
+		{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		} while (slow != fast);
+
+		slow = nums[0];
+		while (slow != fast)
+		{
+			slow = nums[slow];
+			fast = nums[fast];
 		}
+		return fast;
 	}
 }
