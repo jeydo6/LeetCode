d6a255336dbd4947da1bb21a112f8213533881d2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:29 +0300

    413

---
+++ LeetCode/Algorithms/413.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _413
+	{
+		public static int NumberOfArithmeticSlices(int[] nums)
+		{
+			var count = 0;
+			var sum = 0;
+			for (var i = 2; i < nums.Length; i++)
+			{
+				if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+				{
+					count++;
+				}
+				else
+				{
+					sum += (count + 1) * (count) / 2;
+					count = 0;
+				}
+			}
+			return sum += count * (count + 1) / 2;
+		}
+	}
+}
