0dc3eccbeff433af1df501d703b619c49a40439e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:24 +0300

    95

---
+++ LeetCode/Algorithms/95.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	class _95
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public IList<TreeNode> GenerateTrees(int n)
+		{
+			return GenerateTrees(1, n);
+		}
+
+		private IList<TreeNode> GenerateTrees(int start, int end)
+		{
+			if (start > end)
+			{
+				return new List<TreeNode>
+				{
+					null
+				};
+			}
+			else if (start == end)
+			{
+				return new List<TreeNode>
+				{
+					new TreeNode(start)
+				};
+			}
+			else
+			{
+				var result = new List<TreeNode>();
+				for (var i = start; i <= end; i++)
+				{
+					foreach (var l in GenerateTrees(start, i - 1))
+					{
+						foreach (var r in GenerateTrees(i + 1, end))
+						{
+							result.Add(new TreeNode(i, l, r));
+						}
+					}
+				}
+				return result;
+			}
+		}
+	}
+}
