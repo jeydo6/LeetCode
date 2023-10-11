da2f2d76c3d83936ebb8938102b36be0a7f80172
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:56 +0300

    222

--- LeetCode/Algorithms/222.cs
+++ LeetCode/Algorithms/222.cs
@@ -1,54 +1,54 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _222
 {
-	internal class _222
+	public class TreeNode
 	{
-		public class TreeNode
-		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
 
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static int CountNodes(TreeNode root)
+	{
+		if (root == null)
+		{
+			return 0;	
 		}
 
-		public static int CountNodes(TreeNode root)
+		TreeNode node;
+		
+		var left = 0;
+		node = root;
+		while (node.left != null)
 		{
-			if (root == null)
-			{
-				return 0;	
-			}
-
-			TreeNode node;
-			
-			var left = 0;
-			node = root;
-			while (node.left != null)
-			{
-				left++;
-				node = node.left;
-			}
-			
-			var right = 0;
-			node = root;
-			while (node.right != null)
-			{
-				right++;
-				node = node.right;
-			}
-
-			if (left == right)
-			{
-				return 2 * ((int)Math.Pow(2, left) - 1) + 1;
-			}
-
-			return CountNodes(root.left) + CountNodes(root.right) + 1;
+			left++;
+			node = node.left;
 		}
+		
+		var right = 0;
+		node = root;
+		while (node.right != null)
+		{
+			right++;
+			node = node.right;
+		}
+
+		if (left == right)
+		{
+			return 2 * ((int)Math.Pow(2, left) - 1) + 1;
+		}
+
+		return CountNodes(root.left) + CountNodes(root.right) + 1;
 	}
 }
