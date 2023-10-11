6c99e869ada18b77ca675d7c18c82bd83418b3a8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:13 +0300

    706

--- LeetCode/Algorithms/706.cs
+++ LeetCode/Algorithms/706.cs
@@ -1,106 +1,105 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _706
 {
-	// EASY
-	internal class _706
+	public class MyHashMap
 	{
-		public class MyHashMap
+		private class Bucket
 		{
-			private class Bucket
+			private class Pair
 			{
-				private class Pair
+				public Pair(int key, int value)
 				{
-					public Pair(int key, int value)
-					{
-						Key = key;
-						Value = value;
-					}
-
-					public int Key { get; set; }
-
-					public int Value { get; set; }
+					Key = key;
+					Value = value;
 				}
 
-				private readonly LinkedList<Pair> _container;
+				public int Key { get; set; }
 
-				public Bucket()
-				{
-					_container = new LinkedList<Pair>();
-				}
-
-				public void Put(int key, int value)
-				{
-					foreach (var pair in _container)
-					{
-						if (pair.Key == key)
-						{
-							pair.Value = value;
-							return;
-						}
-					}
-
-					_container.AddFirst(new Pair(key, value));
-				}
-
-				public int Get(int key)
-				{
-					foreach (var pair in _container)
-					{
-						if (pair.Key == key)
-						{
-							return pair.Value;
-						}
-					}
-					return -1;
-				}
-
-				public void Remove(int key)
-				{
-					foreach (var pair in _container)
-					{
-						if (pair.Key == key)
-						{
-							_container.Remove(pair);
-							return;
-						}
-					}
-				}
+				public int Value { get; set; }
 			}
 
-			private readonly Bucket[] _buckets;
+			private readonly LinkedList<Pair> _container;
 
-			public MyHashMap()
+			public Bucket()
 			{
-				_buckets = new Bucket[2069];
-				for (var i = 0; i < _buckets.Length; i++)
-				{
-					_buckets[i] = new Bucket();
-				}
+				_container = new LinkedList<Pair>();
 			}
 
 			public void Put(int key, int value)
 			{
-				var index = GetHash(key);
-				_buckets[index].Put(key, value);
+				foreach (var pair in _container)
+				{
+					if (pair.Key == key)
+					{
+						pair.Value = value;
+						return;
+					}
+				}
+
+				_container.AddFirst(new Pair(key, value));
 			}
 
 			public int Get(int key)
 			{
-				var index = GetHash(key);
-				return _buckets[index].Get(key);
+				foreach (var pair in _container)
+				{
+					if (pair.Key == key)
+					{
+						return pair.Value;
+					}
+				}
+				return -1;
 			}
 
 			public void Remove(int key)
 			{
-				var index = GetHash(key);
-				_buckets[index].Remove(key);
+				foreach (var pair in _container)
+				{
+					if (pair.Key == key)
+					{
+						_container.Remove(pair);
+						return;
+					}
+				}
 			}
+		}
 
-			private int GetHash(int key)
+		private readonly Bucket[] _buckets;
+
+		public MyHashMap()
+		{
+			_buckets = new Bucket[2069];
+			for (var i = 0; i < _buckets.Length; i++)
 			{
-				return key % _buckets.Length;
+				_buckets[i] = new Bucket();
 			}
+		}
+
+		public void Put(int key, int value)
+		{
+			var index = GetHash(key);
+			_buckets[index].Put(key, value);
+		}
+
+		public int Get(int key)
+		{
+			var index = GetHash(key);
+			return _buckets[index].Get(key);
+		}
+
+		public void Remove(int key)
+		{
+			var index = GetHash(key);
+			_buckets[index].Remove(key);
+		}
+
+		private int GetHash(int key)
+		{
+			return key % _buckets.Length;
 		}
 	}
 }
