e675279d14dc84f02aa4bebcb65fc89a90a60f03
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:57 +0300

    806

--- LeetCode/Algorithms/806.cs
+++ LeetCode/Algorithms/806.cs
@@ -1,31 +1,25 @@
-﻿using System;
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _806
-    {
-        public Int32[] NumberOfLines(Int32[] widths, String S)
-        {
-            Int32[] result =
-            {
-                1,
-                0
-            };
-
-            foreach (Char c in S)
-            {
-                Int32 width = widths[c - 'a'];
-                if (result[1] + width <= 100)
-                {
-                    result[1] += width;
-                }
-                else
-                {
-                    result[0]++;
-                    result[1] = width;
-                }
-            }
-
-            return result;
-        }
-    }
+	// EASY
+	internal class _806
+	{
+		public static int[] NumberOfLines(int[] widths, string str)
+		{
+			var result = new int[] { 1, 0 };
+			foreach (var ch in str)
+			{
+				var width = widths[ch - 'a'];
+				if (result[1] + width <= 100)
+				{
+					result[1] += width;
+				}
+				else
+				{
+					result[0]++;
+					result[1] = width;
+				}
+			}
+			return result;
+		}
+	}
 }
