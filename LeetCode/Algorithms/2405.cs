752a677f0451802212e23c7738558801fe2eb0fa
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:42 +0300

    2405

---
+++ LeetCode/Algorithms/2405.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2405
+{
+	public static int PartitionString(string s)
+	{
+		var seen = new int[26];
+		for (var i = 0; i < seen.Length; i++)
+		{
+			seen[i] = -1;
+		}
+
+		var result = 1;
+		var start = 0;
+		for (var i = 0; i < s.Length; i++)
+		{
+			if (seen[s[i] - 'a'] >= start)
+			{
+				result++;
+				start = i;
+			}
+			seen[s[i] - 'a'] = i;
+		}
+
+		return result;
+	}
+}
