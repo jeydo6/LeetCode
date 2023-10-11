fd578b46da9028e45ae2e394ff5903cd5f29ac8a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:16 +0300

    2444

---
+++ LeetCode/Algorithms/2444.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _2444
+{
+	public static long CountSubarrays(int[] nums, int minK, int maxK)
+	{
+		var result = 0L;
+
+		var leftBound = -1;
+		var lastMin = -1;
+		var lastMax = -1;
+		for (var i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] >= minK && nums[i] <= maxK)
+			{
+				lastMin = (nums[i] == minK) ? i : lastMin;
+				lastMax = (nums[i] == maxK) ? i : lastMax;
+				result += Math.Max(0, Math.Min(lastMin, lastMax) - leftBound);
+			}
+			else
+			{
+				leftBound = i;
+				lastMin = -1;
+				lastMax = -1;
+			}
+		}
+
+		return result;
+	}
+}
