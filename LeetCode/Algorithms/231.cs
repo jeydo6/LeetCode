b692029af04321695580b1be8f2a325258094000
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:49 +0300

    231

---
+++ LeetCode/Algorithms/231.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _231
+	{
+		public static bool IsPowerOfTwo(int n)
+		{
+			if (n <= 0)
+			{
+				return false;
+			}
+			while (n % 2 == 0)
+			{
+				n /= 2;
+			}
+			return n == 1;
+		}
+	}
+}
