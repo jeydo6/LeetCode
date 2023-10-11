77050a9f0aced32671e8fb1696c94937471ac9b5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:18 +0300

    1026

--- LeetCode/Algorithms/1026.cs
+++ LeetCode/Algorithms/1026.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1026
 {
-	// MEDIUM
-	internal class _1026
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static int MaxAncestorDiff(TreeNode root)
+	{
+		return MaxAncestorDiff(root, root.val, root.val);
+	}
+
+	private static int MaxAncestorDiff(TreeNode root, int min, int max)
+	{
+		if (root == null)
+		{
+			return max - min;
 		}
 
-		public static int MaxAncestorDiff(TreeNode root)
-		{
-			return MaxAncestorDiff(root, root.val, root.val);
-		}
+		min = Math.Min(min, root.val);
+		max = Math.Max(max, root.val);
 
-		private static int MaxAncestorDiff(TreeNode root, int min, int max)
-		{
-			if (root == null)
-			{
-				return max - min;
-			}
-			min = Math.Min(min, root.val);
-			max = Math.Max(max, root.val);
-			return Math.Max(
-				MaxAncestorDiff(root.left, min, max),
-				MaxAncestorDiff(root.right, min, max)
-			);
-		}
+		return Math.Max(
+			MaxAncestorDiff(root.left, min, max),
+			MaxAncestorDiff(root.right, min, max)
+		);
 	}
 }
