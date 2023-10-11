0db36434de9de8823b39813f19cb559af24c0003
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:35 +0300

    2360

---
+++ LeetCode/Algorithms/2360.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _2360
+{
+	public static int LongestCycle(int[] edges)
+	{
+		var result = -1;
+		var timeStep = 1;
+		var visited = new int[edges.Length];
+		for (var current = 0; current < edges.Length; current++)
+		{
+			if (visited[current] > 0)
+			{
+				continue;
+			}
+
+			var startTime = timeStep;
+			var next = current;
+			while (next != -1 && visited[next] == 0)
+			{
+				visited[next] = timeStep++;
+				next = edges[next];
+			}
+
+			if (next != -1 && visited[next] >= startTime)
+			{
+				result = Math.Max(result, timeStep - visited[next]);
+			}
+		}
+
+		return result;
+	}
+}
