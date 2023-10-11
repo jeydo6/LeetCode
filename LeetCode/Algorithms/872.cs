87d510627f8f10045aef1fe6c638b8044392066d
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:16 +0300

    872

--- LeetCode/Algorithms/872.cs
+++ LeetCode/Algorithms/872.cs
@@ -1,75 +1,74 @@
 ﻿using System.Collections.Generic;
 
-namespace Leetcode.Algorithms
+namespace Leetcode.Algorithms;
+
+// EASY
+internal class _872
 {
-	// EASY
-	internal class _872
+	public class TreeNode
 	{
-		public class TreeNode
+		public TreeNode(int val)
 		{
-			public TreeNode(int val)
-			{
-				this.val = val;
-			}
-
-			public int val;
-
-			public TreeNode left;
-
-			public TreeNode right;
+			this.val = val;
 		}
 
-		public static bool LeafSimilar(TreeNode root1, TreeNode root2)
-		{
-			var nodes1 = GetLeaves(root1);
-			var nodes2 = GetLeaves(root2);
+		public int val;
 
-			if (nodes1.Length != nodes2.Length)
+		public TreeNode left;
+
+		public TreeNode right;
+	}
+
+	public static bool LeafSimilar(TreeNode root1, TreeNode root2)
+	{
+		var nodes1 = GetLeaves(root1);
+		var nodes2 = GetLeaves(root2);
+
+		if (nodes1.Length != nodes2.Length)
+		{
+			return false;
+		}
+
+		for (var i = 0; i < nodes1.Length; i++)
+		{
+			if (nodes1[i].val != nodes2[i].val)
 			{
 				return false;
 			}
-
-			for (var i = 0; i < nodes1.Length; i++)
-			{
-				if (nodes1[i].val != nodes2[i].val)
-				{
-					return false;
-				}
-			}
-
-			return true;
 		}
 
-		private static TreeNode[] GetLeaves(TreeNode root)
+		return true;
+	}
+
+	private static TreeNode[] GetLeaves(TreeNode root)
+	{
+		var result = new List<TreeNode>();
+
+		var stack = new Stack<TreeNode>();
+		stack.Push(root);
+
+		while (stack.Count > 0)
 		{
-			var result = new List<TreeNode>();
+			var node = stack.Pop();
 
-			var stack = new Stack<TreeNode>();
-			stack.Push(root);
-
-			while (stack.Count > 0)
+			if (node.left != null || node.right != null)
 			{
-				var node = stack.Pop();
-
-				if (node.left != null || node.right != null)
+				if (node.right != null)
 				{
-					if (node.right != null)
-					{
-						stack.Push(node.right);
-					}
-
-					if (node.left != null)
-					{
-						stack.Push(node.left);
-					}
+					stack.Push(node.right);
 				}
-				else
+
+				if (node.left != null)
 				{
-					result.Add(node);
+					stack.Push(node.left);
 				}
 			}
-
-			return result.ToArray();
+			else
+			{
+				result.Add(node);
+			}
 		}
+
+		return result.ToArray();
 	}
 }
