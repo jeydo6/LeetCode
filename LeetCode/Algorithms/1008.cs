9f6432740372f8490ba07ebb119dd9bdbf9de003
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:24 +0300

    1008

---
+++ LeetCode/Algorithms/1008.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1008
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static TreeNode BstFromPreorder(int[] preorder)
+		{
+			return BstFromPreorder(preorder, 0, preorder.Length - 1);
+		}
+
+		public static TreeNode BstFromPreorder(int[] preorder, int start, int end)
+		{
+			if (start > end)
+			{
+				return null;
+			}
+
+			var root = new TreeNode(preorder[start]);
+
+			int index;
+			for (index = start; index <= end; index++)
+			{
+				if (preorder[index] > preorder[start])
+				{
+					break;
+				}
+			}
+
+			root.left = BstFromPreorder(preorder, start + 1, index - 1);
+			root.right = BstFromPreorder(preorder, index, end);
+
+			return root;
+		}
+	}
+}
