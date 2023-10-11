738485004260ef5481ef0dcf2d1df174445ef20e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:05 +0300

    2215

---
+++ LeetCode/Algorithms/2215.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _2215
+{
+	public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+	{
+		return new List<IList<int>>
+		{
+			Except(nums1, nums2),
+			Except(nums2, nums1)
+		};
+	}
+
+	private static IList<int> Except(int[] nums1, int[] nums2)
+	{
+		var set1 = new HashSet<int>(nums1);
+		var set2 = new HashSet<int>(nums2);
+
+		set1.ExceptWith(set2);
+
+		return new List<int>(set1);
+	}
+}
