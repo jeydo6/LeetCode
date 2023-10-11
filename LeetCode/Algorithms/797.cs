38675da2e179ae7d773740b951700341329205d5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:15 +0300

    797

--- LeetCode/Algorithms/797.cs
+++ LeetCode/Algorithms/797.cs
@@ -1,39 +1,28 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _797
 {
-	// MEDIUM
-	internal class _797
+	public static IList<IList<int>> AllPathsSourceTarget(int[][] graph, int node = 0, IList<IList<int>> result = null, IList<int> path = null)
 	{
-		public static IList<IList<int>> AllPathsSourceTarget(int[][] graph, int node = 0, IList<IList<int>> result = null, IList<int> path = null)
+		result ??= new List<IList<int>>();
+		path ??= new List<int> { 0 };
+
+		if (node == graph.Length - 1)
 		{
-			if (result == null)
-			{
-				result = new List<IList<int>>();
-			}
-
-			if (path == null)
-			{
-				path = new List<int>
-				{
-					0
-				};
-			}
-
-			if (node == graph.Length - 1)
-			{
-				result.Add(new List<int>(path));
-				return result;
-			}
-
-			for (var i = 0; i < graph[node].Length; i++)
-			{
-				path.Add(graph[node][i]);
-				AllPathsSourceTarget(graph, graph[node][i], result, path);
-				path.RemoveAt(path.Count - 1);
-			}
-
+			result.Add(new List<int>(path));
 			return result;
 		}
+
+		for (var i = 0; i < graph[node].Length; i++)
+		{
+			path.Add(graph[node][i]);
+			AllPathsSourceTarget(graph, graph[node][i], result, path);
+			path.RemoveAt(path.Count - 1);
+		}
+
+		return result;
 	}
 }
