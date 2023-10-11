d6098440ed533491f2624a28d77155e8e57f9f1b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:57 +0300

    128

---
+++ LeetCode/Algorithms/128.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _128
+	{
+		public static int LongestConsecutive(int[] nums)
+		{
+			var hashSet = new HashSet<int>();
+
+			for (var i = 0; i < nums.Length; i++)
+			{
+				hashSet.Add(nums[i]);
+			}
+
+			var result = 0;
+
+			for (var i = 0; i < nums.Length; i++)
+			{
+				var currentStreak = 1;
+				var currentNum = nums[i];
+
+				if (hashSet.Contains(currentNum - 1))
+				{
+					continue;
+				}
+
+				while (hashSet.Contains(currentNum + 1))
+				{
+					currentNum += 1;
+					currentStreak += 1;
+				}
+
+				result = Math.Max(result, currentStreak);
+			}
+
+			return result;
+		}
+	}
+}
