7a17d73e2b0f1f919aee2d5fce54bd125e2377dc
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:06 +0300

    1641

---
+++ LeetCode/Algorithms/1641.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1641
+	{
+		public static int CountVowelStrings(int n)
+		{
+			var dp = new int[n + 1][];
+			for (var i = 0; i < dp.Length; i++)
+			{
+				dp[i] = new int[6];
+			}
+
+			for (var vowels = 1; vowels <= 5; vowels++)
+			{
+				dp[1][vowels] = vowels;
+			}
+			for (var nValue = 2; nValue <= n; nValue++)
+			{
+				dp[nValue][1] = 1;
+				for (var vowels = 2; vowels <= 5; vowels++)
+				{
+					dp[nValue][vowels] = dp[nValue][vowels - 1] + dp[nValue - 1][vowels];
+				}
+			}
+			return dp[n][5];
+		}
+	}
+}
