4b34888e558263ab77432503ca64956244315b15
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:54 +0300

    438

--- LeetCode/Algorithms/438.cs
+++ LeetCode/Algorithms/438.cs
@@ -1,62 +1,61 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _438
 {
-	// MEDIUM
-	internal class _438
+	public static IList<int> FindAnagrams(string s, string p)
 	{
-		public static IList<int> FindAnagrams(string s, string p)
+		var result = new List<int>();
+		if (p.Length > s.Length)
 		{
-			var result = new List<int>();
-			if (p.Length > s.Length)
-			{
-				return result;
-			}
-
-			var dict = new Dictionary<char, int>();
-			for (var i = 0; i < p.Length; i++)
-			{
-				if (!dict.ContainsKey(p[i]))
-				{
-					dict[p[i]] = 0;
-				}
-				dict[p[i]]++;
-			}
-			var count = dict.Count;
-
-			var begin = 0;
-			var end = 0;
-
-			while (end < s.Length)
-			{
-				if (dict.ContainsKey(s[end]))
-				{
-					dict[s[end]]--;
-					if (dict[s[end]] == 0)
-					{
-						count--;
-					}
-				}
-				end++;
-
-				while (count == 0)
-				{
-					if (dict.ContainsKey(s[begin]))
-					{
-						dict[s[begin]]++;
-						if (dict[s[begin]] > 0)
-						{
-							count++;
-						}
-					}
-					if (end - begin == p.Length)
-					{
-						result.Add(begin);
-					}
-					begin++;
-				}
-			}
 			return result;
 		}
+
+		var dict = new Dictionary<char, int>();
+		for (var i = 0; i < p.Length; i++)
+		{
+			if (!dict.ContainsKey(p[i]))
+			{
+				dict[p[i]] = 0;
+			}
+			dict[p[i]]++;
+		}
+		var count = dict.Count;
+
+		var begin = 0;
+		var end = 0;
+
+		while (end < s.Length)
+		{
+			if (dict.ContainsKey(s[end]))
+			{
+				dict[s[end]]--;
+				if (dict[s[end]] == 0)
+				{
+					count--;
+				}
+			}
+			end++;
+
+			while (count == 0)
+			{
+				if (dict.ContainsKey(s[begin]))
+				{
+					dict[s[begin]]++;
+					if (dict[s[begin]] > 0)
+					{
+						count++;
+					}
+				}
+				if (end - begin == p.Length)
+				{
+					result.Add(begin);
+				}
+				begin++;
+			}
+		}
+		return result;
 	}
 }
