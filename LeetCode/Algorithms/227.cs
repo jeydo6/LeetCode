dc69c428314470451b6c17990765d89e6f402b6c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:34 +0300

    227

---
+++ LeetCode/Algorithms/227.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _227
+	{
+		public static int Calculate(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return 0;
+			}
+
+			var stack = new Stack<int>();
+			var value = 0;
+			var op = '+';
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (char.IsDigit(s[i]))
+				{
+					value = value * 10 + s[i] - '0';
+				}
+				if ((!char.IsDigit(s[i]) && s[i] != ' ') || i == s.Length - 1)
+				{
+					if (op == '-')
+					{
+						stack.Push(-value);
+					}
+					if (op == '+')
+					{
+						stack.Push(value);
+					}
+					if (op == '*')
+					{
+						stack.Push(stack.Pop() * value);
+					}
+					if (op == '/')
+					{
+						stack.Push(stack.Pop() / value);
+					}
+
+					op = s[i];
+					value = 0;
+				}
+			}
+
+			var result = 0;
+			while (stack.Count > 0)
+			{
+				result += stack.Pop();
+			}
+			return result;
+		}
+	}
+}
