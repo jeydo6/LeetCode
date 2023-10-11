2e5650c610ae80e5f82aba6bb44397ca48d2cbaf
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:42 +0300

    2439

---
+++ LeetCode/Algorithms/2439.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2439
+{
+	public static int MinimizeArrayValue(int[] nums)
+	{
+		var result = 0L;
+		var prefixSum = 0;
+		for (var i = 0; i < nums.Length; ++i)
+		{
+			prefixSum += nums[i];
+			result = Math.Max(result, (prefixSum + i) / (i + 1));
+		}
+
+		return (int)result;
+	}
+}
