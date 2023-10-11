2895daff5214c266c73055dd4b470bc1e0842f66
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:10 +0300

    532

---
+++ LeetCode/Algorithms/532.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _532
+	{
+		public static int FindPairs(int[] nums, int k)
+		{
+			if (nums == null || nums.Length == 0 || k < 0)
+			{
+				return 0;
+			}
+
+			var dict = new Dictionary<int, int>();
+			var count = 0;
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (!dict.ContainsKey(nums[i]))
+				{
+					dict[nums[i]] = 0;
+				}
+
+				dict[nums[i]]++;
+			}
+
+			foreach (var pair in dict)
+			{
+				if (k == 0)
+				{
+					if (pair.Value >= 2)
+					{
+						count++;
+					}
+				}
+				else
+				{
+					if (dict.ContainsKey(pair.Key + k))
+					{
+						count++;
+					}
+				}
+			}
+
+			return count;
+		}
+	}
+}
