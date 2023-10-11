9a43511eb72f4684c2d82f053843d8c689826bfc
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:26 +0300

    958

---
+++ LeetCode/Algorithms/958.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _958
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+		{
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static bool IsCompleteTree(TreeNode root)
+	{
+		var queue = new Queue<TreeNode>();
+		queue.Enqueue(root);
+
+		while (queue.Peek() != null)
+		{
+			var node = queue.Dequeue();
+			queue.Enqueue(node.left);
+			queue.Enqueue(node.right);
+		}
+
+		while (queue.Count > 0 && queue.Peek() == null)
+		{
+			queue.Dequeue();
+		}
+
+		return queue.Count == 0;
+	}
+}
