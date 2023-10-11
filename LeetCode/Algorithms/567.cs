b8cc33399c25d9277ed602f912a49b90a16094f8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:53 +0300

    567

--- LeetCode/Algorithms/567.cs
+++ LeetCode/Algorithms/567.cs
@@ -1,45 +1,44 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _567
 {
-	// MEDIUM
-	internal class _567
+	public static bool CheckInclusion(string s1, string s2)
 	{
-		public static bool CheckInclusion(string s1, string s2)
+		if (s1.Length > s2.Length)
 		{
-			if (s1.Length > s2.Length)
-			{
-				return false;
-			}
-
-			var freq = new int[26];
-			for (var i = 0; i < s1.Length; i++)
-			{
-				freq[s1[i] - 'a']++;
-			}
-			for (var i = 0; i < s2.Length; i++)
-			{
-				freq[s2[i] - 'a']--;
-
-				if (i >= s1.Length)
-				{
-					freq[s2[i - s1.Length] - 'a']++;
-				}
-
-				var allZero = true;
-				for (var j = 0; j < 26; j++)
-				{
-					if (freq[j] != 0)
-					{
-						allZero = false;
-						break;
-					}
-				}
-				if (allZero)
-				{
-					return true;
-				}
-			}
-
 			return false;
 		}
+
+		var frequencies = new int[26];
+		for (var i = 0; i < s1.Length; i++)
+		{
+			frequencies[s1[i] - 'a']++;
+		}
+		for (var i = 0; i < s2.Length; i++)
+		{
+			frequencies[s2[i] - 'a']--;
+
+			if (i >= s1.Length)
+			{
+				frequencies[s2[i - s1.Length] - 'a']++;
+			}
+
+			var allZero = true;
+			for (var j = 0; j < 26; j++)
+			{
+				if (frequencies[j] != 0)
+				{
+					allZero = false;
+					break;
+				}
+			}
+			if (allZero)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
