87ca920638244bef0651f8b57fe8071cf78af6d6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:27:17 +0300

    2009

---
+++ LeetCode/Algorithms/2009.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _2009
+{
+	public static int MinOperations(int[] nums)
+	{
+		var hashSet = new HashSet<int>(nums);
+		var newNums = new int[hashSet.Count];
+
+		var index = 0;
+		foreach (var num in hashSet)
+		{
+			newNums[index++] = num;
+		}
+
+		Array.Sort(newNums);
+
+		var result = nums.Length;
+
+		var j = 0;
+		for (var i = 0; i < newNums.Length; i++)
+		{
+			while (j < newNums.Length && newNums[j] < newNums[i] + nums.Length)
+			{
+				j++;
+			}
+
+			var count = j - i;
+			result = Math.Min(result, nums.Length - count);
+		}
+
+		return result;
+	}
+}
