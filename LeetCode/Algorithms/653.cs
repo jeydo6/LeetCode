bb1bc783f56fd8c2bc64a7acec78c0b80debbefa
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:22 +0300

    653

--- LeetCode/Algorithms/653.cs
+++ LeetCode/Algorithms/653.cs
@@ -1,43 +1,41 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _653
 {
-	// EASY
-	internal class _653
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static bool FindTarget(TreeNode root, int k)
+	{
+		return FindTarget(root, k, new HashSet<int>());
+	}
+
+	private static bool FindTarget(TreeNode root, int k, HashSet<int> hashSet)
+	{
+		if (root == null)
+		{
+			return false;
 		}
 
-		public static bool FindTarget(TreeNode root, int k)
+		if (hashSet.Contains(k - root.val))
 		{
-			var hashSet = new HashSet<int>();
-			return FindTarget(root, k, hashSet);
+			return true;
 		}
+		hashSet.Add(root.val);
 
-		private static bool FindTarget(TreeNode root, int k, HashSet<int> hashSet)
-		{
-			if (root == null)
-			{
-				return false;
-			}
-
-			if (hashSet.Contains(k - root.val))
-			{
-				return true;
-			}
-			hashSet.Add(root.val);
-
-			return FindTarget(root.left, k, hashSet) || FindTarget(root.right, k, hashSet);
-		}
+		return FindTarget(root.left, k, hashSet) || FindTarget(root.right, k, hashSet);
 	}
 }
