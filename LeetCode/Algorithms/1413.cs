f37e189bf3b40d41c1994be5fad5e8ff48de4eb8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:49 +0300

    1413

---
+++ LeetCode/Algorithms/1413.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _1413
+	{
+		public static int MinStartValue(int[] nums)
+		{
+			var sum = 0;
+			var min = 0;
+			foreach (var num in nums)
+			{
+				sum += num;
+				min = Math.Min(min, sum);
+			}
+			return 1 - min;
+		}
+	}
+}
