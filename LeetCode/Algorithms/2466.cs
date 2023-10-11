c960f445fc59e3f9037a63d07a17c7fd43f31aa0
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:13 +0300

    2466

---
+++ LeetCode/Algorithms/2466.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2466
+{
+	private const int Modulo = 1_000_000_007;
+
+	public static int CountGoodStrings(int low, int high, int zero, int one)
+	{
+		var dp = new int[high + 1];
+		dp[0] = 1;
+		for (var end = 1; end <= high; end++)
+		{
+			if (end >= zero)
+			{
+				dp[end] += dp[end - zero];
+			}
+			if (end >= one)
+			{
+				dp[end] += dp[end - one];
+			}
+			dp[end] %= Modulo;
+		}
+
+		var result = 0;
+		for (var i = low; i <= high; ++i)
+		{
+			result += dp[i];
+			result %= Modulo;
+		}
+		return result;
+	}
+}
