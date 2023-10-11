cbd2470d4cb5124710a8c2466edcc0551cf9e921
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:07 +0300

    1979

---
+++ LeetCode/Algorithms/1979.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1979
+	{
+		public static int FindGCD(int[] nums)
+		{
+			var min = int.MaxValue;
+			var max = int.MinValue;
+			foreach (var num in nums)
+			{
+				if (num < min) min = num;
+				if (num > max) max = num;
+			}
+			for (var divisor = min; divisor >= 1; divisor--)
+			{
+				if (min % divisor == 0 && max % divisor == 0)
+				{
+					return divisor;
+				}
+			}
+			return 1;
+		}
+	}
+}
