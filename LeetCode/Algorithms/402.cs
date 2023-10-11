00e10ed1bb5a830e75ba780e0b2a0f297fc915f4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:18 +0300

    402

---
+++ LeetCode/Algorithms/402.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _402
+	{
+		public static string RemoveKdigits(string num, int k)
+		{
+			if (num.Length == k)
+			{
+				return "0";
+			}
+
+			var stack = new Stack<char>();
+			var i = 0;
+			while (i < num.Length)
+			{
+				while (k > 0 && stack.Count > 0 && stack.Peek() > num[i])
+				{
+					stack.Pop();
+					k--;
+				}
+				stack.Push(num[i]);
+				i++;
+			}
+
+			while (k > 0)
+			{
+				stack.Pop();
+				k--;
+			}
+
+			var sb = new StringBuilder();
+			while (stack.Count > 0)
+			{
+				sb.Append(stack.Pop());
+			}
+
+			var reverse = sb.ToString();
+			sb = new StringBuilder();
+			for (var j = 0; j < reverse.Length; j++)
+			{
+				sb.Append(reverse[^(j + 1)]);
+			}
+			while (sb.Length > 1 && sb[0] == '0')
+			{
+				sb.Remove(0, 1);
+			}
+			return sb.ToString();
+		}
+	}
+}
