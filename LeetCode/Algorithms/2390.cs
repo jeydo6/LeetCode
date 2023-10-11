201c587f17dc2b666ad0ac125d2c54656ff4a055
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:48 +0300

    2390

---
+++ LeetCode/Algorithms/2390.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _2390
+{
+	public static string RemoveStars(string s)
+	{
+		var chars = new char[s.Length];
+		var p = 0;
+		for (var i = 0; i < s.Length; i++)
+		{
+			if (s[i] == '*')
+			{
+				p--;
+			}
+			else
+			{
+				chars[p++] = s[i];
+			}
+		}
+
+		var sb = new StringBuilder();
+		for (var i = 0; i < p; i++)
+		{
+			sb.Append(chars[i]);
+		}
+
+		return sb.ToString();
+	}
+}
