5ae07e83a0255da14f15876a7a0832f3defaee44
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:41 +0300

    589

---
+++ LeetCode/Algorithms/589.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _589
+	{
+		public class Node
+		{
+			public int val;
+
+			public IList<Node> children;
+
+			public Node()
+			{
+				//
+			}
+
+			public Node(int _val, IList<Node> _children)
+			{
+				val = _val;
+				children = _children;
+			}
+		}
+
+		public static IList<int> Preorder(Node root)
+		{
+			var result = new List<int>();
+
+			if (root == null)
+			{
+				return result;
+			}
+
+			var stack = new Stack<Node>();
+			stack.Push(root);
+
+			while (stack.Count > 0)
+			{
+				var current = stack.Pop();
+				result.Add(current.val);
+
+				if (current.children != null && current.children.Count > 0)
+				{
+					for (var i = 0; i < current.children.Count; i++)
+					{
+						stack.Push(current.children[^(i + 1)]);
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
