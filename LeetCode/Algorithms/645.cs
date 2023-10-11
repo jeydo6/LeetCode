c59db9342fd21e1da5e06e7faa88198dafd5027e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:36 +0300

    645

---
+++ LeetCode/Algorithms/645.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _645
+{
+	public static int[] FindErrorNums(int[] nums)
+	{
+		var array = new int[nums.Length + 1];
+		for (var i = 0; i < nums.Length; i++)
+		{
+			array[nums[i]]++;
+		}
+
+		var duplicate = -1;
+		var missing = 1;
+		for (var i = 1; i < array.Length; i++)
+		{
+			if (array[i] == 0)
+			{
+				missing = i;
+			}
+			else if (array[i] == 2)
+			{
+				duplicate = i;
+			}
+		}
+
+		return new int[] { duplicate, missing };
+	}
+}
