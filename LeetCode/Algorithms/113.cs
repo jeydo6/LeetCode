0b3c064d50ad289ffaebfef8d42ebe3e13b91551
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:09 +0300

    113

--- LeetCode/Algorithms/113.cs
+++ LeetCode/Algorithms/113.cs
@@ -1,85 +1,85 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _113
 {
-	class _113
+	public class TreeNode
 	{
-		public class TreeNode
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
-			
-			public int val;
-			
-			public TreeNode left;
-			
-			public TreeNode right;
+			this.val = val;
+			this.left = left;
+			this.right = right;
 		}
 
-		public static IList<IList<int>> PathSum(TreeNode root, int targetSum)
+		public int val;
+
+		public TreeNode left;
+
+		public TreeNode right;
+	}
+
+	public static IList<IList<int>> PathSum(TreeNode root, int targetSum)
+	{
+		var result = new List<IList<int>>();
+
+		if (root == null)
 		{
-			var result = new List<IList<int>>();
-
-			if (root == null)
-			{
-				return result;
-			}
-
-			var stack = new Stack<(TreeNode node, List<int> path, int pathSum)>();
-			stack.Push((root, new List<int> { root.val }, root.val));
-
-			while (stack.Count > 0)
-			{
-				var (node, path, pathSum) = stack.Pop();
-
-				if (node.left == null && node.right == null && pathSum == targetSum)
-				{
-					result.Add(path);
-					continue;
-				}
-
-				if (node.left != null)
-				{
-					stack.Push((node.left, new List<int>(path) { node.left.val }, pathSum + node.left.val));
-				}
-				
-				if (node.right != null)
-				{
-					stack.Push((node.right, new List<int>(path) { node.right.val }, pathSum + node.right.val));
-				}
-			}
-
 			return result;
 		}
 
-		private static IList<IList<int>> DFS(TreeNode root, int targetSum, IList<int> path)
-		{
-			if (root == null)
-			{
-				return new List<IList<int>>();
-			}
+		var stack = new Stack<(TreeNode node, List<int> path, int pathSum)>();
+		stack.Push((root, new List<int> { root.val }, root.val));
 
-			var result = new List<IList<int>>();
-			if (
-				root.left == null &&
-				root.right == null &&
-				root.val == targetSum
-			)
+		while (stack.Count > 0)
+		{
+			var (node, path, pathSum) = stack.Pop();
+
+			if (node.left == null && node.right == null && pathSum == targetSum)
 			{
-				path.Add(root.val);
 				result.Add(path);
+				continue;
 			}
-			result.AddRange(
-				DFS(root.left, targetSum - root.val, new List<int>(path) { root.val })
-			);
-			result.AddRange(
-				DFS(root.right, targetSum - root.val, new List<int>(path) { root.val })
-			);
-			return result;
+
+			if (node.left != null)
+			{
+				stack.Push((node.left, new List<int>(path) { node.left.val }, pathSum + node.left.val));
+			}
+
+			if (node.right != null)
+			{
+				stack.Push((node.right, new List<int>(path) { node.right.val }, pathSum + node.right.val));
+			}
 		}
+
+		return result;
+	}
+
+	private static IList<IList<int>> DFS(TreeNode root, int targetSum, IList<int> path)
+	{
+		if (root == null)
+		{
+			return new List<IList<int>>();
+		}
+
+		var result = new List<IList<int>>();
+		if (
+			root.left == null &&
+			root.right == null &&
+			root.val == targetSum
+		)
+		{
+			path.Add(root.val);
+			result.Add(path);
+		}
+		result.AddRange(
+			DFS(root.left, targetSum - root.val, new List<int>(path) { root.val })
+		);
+		result.AddRange(
+			DFS(root.right, targetSum - root.val, new List<int>(path) { root.val })
+		);
+		return result;
 	}
 }
