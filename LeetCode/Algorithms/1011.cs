54445749a1a0037a4b50cd8dd04492a6bf4c80bd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:08 +0300

    1011

---
+++ LeetCode/Algorithms/1011.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1011
+{
+	public static int ShipWithinDays(int[] weights, int days)
+	{
+		var totalLoad = 0;
+		var maxLoad = 0;
+		for (var i = 0; i < weights.Length; i++)
+		{
+			totalLoad += weights[i];
+			maxLoad = Math.Max(maxLoad, weights[i]);
+		}
+
+		var lo = maxLoad;
+		var hi = totalLoad;
+		while (lo < hi)
+		{
+			var mid = lo + (hi - lo) / 2;
+			if (IsFeasible(weights, days, mid))
+			{
+				hi = mid;
+			}
+			else
+			{
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static bool IsFeasible(int[] weights, int days, int capacity)
+	{
+		var daysNeeded = 1;
+		var currentLoad = 0;
+		for (var i = 0; i < weights.Length; i++)
+		{
+			currentLoad += weights[i];
+			if (currentLoad > capacity)
+			{
+				daysNeeded++;
+				currentLoad = weights[i];
+			}
+		}
+
+		return daysNeeded <= days;
+	}
+}
