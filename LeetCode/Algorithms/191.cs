2da8b120c1ba5879fad115220ad9e2478d95febf
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:49 +0300

    191

---
+++ LeetCode/Algorithms/191.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _191
+	{
+		public static int HammingWeight(uint n)
+		{
+			var count = 0U;
+			while (n != 0)
+			{
+				count += n & 1;
+				n >>= 1;
+			}
+			return (int)count;
+		}
+	}
+}
