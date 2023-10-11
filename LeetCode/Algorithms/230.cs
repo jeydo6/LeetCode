bcb3fa5243bb53b8cfe4210281e7debf1de41fed
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:33 +0300

    230

---
+++ LeetCode/Algorithms/230.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _230
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+		}
+
+		public static int KthSmallest(TreeNode root, int k)
+		{
+			var stack = new Stack<TreeNode>();
+			while (true)
+			{
+				while (root != null)
+				{
+					stack.Push(root);
+					root = root.left;
+				}
+				root = stack.Pop();
+				if (--k == 0)
+				{
+					return root.val;
+				}
+				root = root.right;
+			}
+		}
+	}
+}
