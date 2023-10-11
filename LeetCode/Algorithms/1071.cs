aaeac1378d42b9ee8890212471a0cf279d7449f8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:23:51 +0300

    1071

---
+++ LeetCode/Algorithms/1071.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1071
+{
+	public static string GcdOfStrings(string str1, string str2)
+	{
+		if (str1 + str2 != str2 + str1)
+		{
+			return "";
+		}
+
+		return str1[..GcdOfInts(str1.Length, str2.Length)];
+	}
+
+	private static int GcdOfInts(int val1, int val2)
+	{
+		while (val1 > 0 && val2 > 0)
+		{
+			if (val1 > val2)
+			{
+				val1 %= val2;
+			}
+			else
+			{
+				val2 %= val1;
+			}
+		}
+
+		return val1 | val2;
+	}
+}
