3ed2e357f6f46b68b79919c3321385b918ca099a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:17 +0300

    1578

---
+++ LeetCode/Algorithms/1578.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+internal class _1578
+{
+	public static int MinCost(string colors, int[] neededTime)
+	{
+		var totalTime = 0;
+		var currentMaxTime = 0;
+
+		for (var i = 0; i < colors.Length; i++)
+		{
+			if (i > 0 && colors[i] != colors[i - 1])
+			{
+				currentMaxTime = 0;
+			}
+
+			totalTime += Math.Min(currentMaxTime, neededTime[i]);
+			currentMaxTime = Math.Max(currentMaxTime, neededTime[i]);
+		}
+
+		return totalTime;
+	}
+}
