859c6331c50f5ff7107a6ca2a3db8002c2c5e743
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:15 +0300

    1165

---
+++ LeetCode/Algorithms/1165.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1165
+{
+	public static int CalculateTime(string keyboard, string word)
+	{
+		var indexes = new int[26];
+		for (var i = 0; i < keyboard.Length; i++)
+		{
+			indexes[keyboard[i] - 'a'] = i;
+		}
+
+		var prev = 0;
+		var result = 0;
+		for (var i = 0; i < word.Length; i++)
+		{
+			result += Math.Abs(prev - indexes[word[i] - 'a']);
+			prev = indexes[word[i] - 'a'];
+		}
+		return result;
+	}
+}
