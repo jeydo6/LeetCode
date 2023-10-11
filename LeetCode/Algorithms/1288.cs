c4d6b5a30d32112e55bdd90399579a806a28856f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:19 +0300

    1288

---
+++ LeetCode/Algorithms/1288.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1288
+	{
+		public static int RemoveCoveredIntervals(int[][] intervals)
+		{
+			var result = 0;
+			var left = -1;
+			var right = -1;
+			Array.Sort(intervals, (a, b) => a[0] - b[0]);
+			for (var i = 0; i < intervals.Length; i++)
+			{
+				if (intervals[i][0] > left && intervals[i][1] > right)
+				{
+					left = intervals[i][0];
+					result++;
+				}
+				right = Math.Max(right, intervals[i][1]);
+			}
+			return result;
+		}
+	}
+}
