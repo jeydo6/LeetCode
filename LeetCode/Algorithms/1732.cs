b634fb7719c3b344ab4f1f8e13d8aa69037b91a4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:15 +0300

    1732

---
+++ LeetCode/Algorithms/1732.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1732
+	{
+		public static int LargestAltitude(int[] gain)
+		{
+			var result = 0;
+			var altitude = 0;
+			foreach (var item in gain)
+			{
+				altitude += item;
+				if (altitude > result)
+				{
+					result = altitude;
+				}
+			}
+			return result;
+		}
+	}
+}
