b9bff3335565b4bf8903b71423b5dd3c233ed0f3
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:47 +0300

    452

---
+++ LeetCode/Algorithms/452.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _452
+	{
+		public static int FindMinArrowShots(int[][] points)
+		{
+			if (points.Length == 0)
+			{
+				return 0;
+			}
+			Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+			var position = points[0][1];
+			var count = 1;
+			for (var i = 1; i < points.Length; i++)
+			{
+				if (position >= points[i][0])
+				{
+					continue;
+				}
+				count++;
+				position = points[i][1];
+			}
+			return count;
+		}
+	}
+}
