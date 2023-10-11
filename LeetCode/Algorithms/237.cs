551e63d8d5675ed6c6c477492219f768bbff75e7
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:26 +0300

    237

---
+++ LeetCode/Algorithms/237.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _237
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x) { val = x; }
+	}
+
+	public static void DeleteNode(ListNode node)
+	{
+		var nextNode = node.next;
+		node.val = nextNode.val;
+		node.next = nextNode.next;
+		nextNode.next = null;
+	}
+}
