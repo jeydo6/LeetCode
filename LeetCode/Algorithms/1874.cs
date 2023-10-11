2e2006e32dc2ac85d2f150c99572d9154c28f0b1
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:21 +0300

    1874

---
+++ LeetCode/Algorithms/1874.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1874
+	{
+		public static int MinProductSum(int[] nums1, int[] nums2)
+		{
+			var counter1 = new int[101];
+			for (var i = 0; i < nums1.Length; i++)
+			{
+				counter1[nums1[i]]++;
+			}
+
+			var counter2 = new int[101];
+			for (var i = 0; i < nums2.Length; i++)
+			{
+				counter2[nums2[i]]++;
+			}
+
+			var result = 0;
+			var lo = 0;
+			var hi = 100;
+			while (lo < 101 && hi > 0)
+			{
+				while (lo < 101 && counter1[lo] == 0)
+				{
+					lo += 1;
+				}
+
+				while (hi > 0 && counter2[hi] == 0)
+				{
+					hi -= 1;
+				}
+
+				if (lo == 101 || hi == 0)
+				{
+					break;
+				}
+
+				var count = Math.Min(counter1[lo], counter2[hi]);
+				result += count * lo * hi;
+				counter1[lo] -= count;
+				counter2[hi] -= count;
+			}
+			return result;
+		}
+	}
+}
