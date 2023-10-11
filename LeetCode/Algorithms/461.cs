987af023236f46dfcc8b9229162a90d2f4fb7f16
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:51 +0300

    461

--- LeetCode/Algorithms/461.cs
+++ LeetCode/Algorithms/461.cs
@@ -1,22 +1,21 @@
-using System;
-
-public class _461
+namespace LeetCode.Algorithms
 {
-    public Int32 HammingDistance(Int32 x, Int32 y)
-    {
-        Int32 result = 0;
+	public class _461
+	{
+		public static int HammingDistance(int x, int y)
+		{
+			var result = 0;
+			while (x > 0 || y > 0)
+			{
+				if (x % 2 != y % 2)
+				{
+					result++;
+				}
 
-        while (x > 0 || y > 0)
-        {
-            if (x % 2 != y % 2)
-            {
-                result++;
-            }
-
-            x = x / 2;
-            y = y / 2;
-        }
-
-        return result;
-    }
+				x = x / 2;
+				y = y / 2;
+			}
+			return result;
+		}
+	}
 }
