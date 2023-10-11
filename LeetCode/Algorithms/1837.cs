909501d2487e51a754a2849e03231890b877c478
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:37 +0300

    1837

---
+++ LeetCode/Algorithms/1837.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1837
+	{
+		public static int SumBase(int n, int k)
+		{
+			var result = 0;
+			while (n > 0)
+			{
+				result += n % k;
+				n /= k;
+			}
+			return result;
+		}
+	}
+}
