3e53918e6f737e424b3e04eaddb903dd55dcee39
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:13 +0300

    73

---
+++ LeetCode/Algorithms/73.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _73
+	{
+		public static void SetZeroes(int[][] matrix)
+		{
+			var rows = new HashSet<int>();
+			var columns = new HashSet<int>();
+
+			for (var i = 0; i < matrix.Length; i++)
+			{
+				for (var j = 0; j < matrix[0].Length; j++)
+				{
+					if (matrix[i][j] == 0)
+					{
+						rows.Add(i);
+						columns.Add(j);
+					}
+				}
+			}
+
+			for (var i = 0; i < matrix.Length; i++)
+			{
+				for (var j = 0; j < matrix[0].Length; j++)
+				{
+					if (rows.Contains(i) || columns.Contains(j))
+					{
+						matrix[i][j] = 0;
+					}
+				}
+			}
+		}
+	}
+}
