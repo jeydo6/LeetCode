cc25cdc301bdcdc574877cc2941f7bb8b1d009e1
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:56 +0300

    1282

---
+++ LeetCode/Algorithms/1282.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1282
+{
+	public static IList<IList<int>> GroupThePeople(int[] groupSizes)
+	{
+		var result = new List<IList<int>>();
+		var groupSizesDict = new Dictionary<int, IList<int>>();
+		for (var i = 0; i < groupSizes.Length; i++)
+		{
+			if (!groupSizesDict.ContainsKey(groupSizes[i]))
+			{
+				groupSizesDict[groupSizes[i]] = new List<int>();
+			}
+
+			var group = groupSizesDict[groupSizes[i]];
+			group.Add(i);
+			if (group.Count == groupSizes[i])
+			{
+				result.Add(group);
+				groupSizesDict.Remove(groupSizes[i]);
+			}
+		}
+
+		return result;
+	}
+}
