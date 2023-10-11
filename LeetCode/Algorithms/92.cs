d2e14e1b8b824c5ac1e4471ff0089c3270b30493
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:52 +0300

    92

--- LeetCode/Algorithms/92.cs
+++ LeetCode/Algorithms/92.cs
@@ -1,59 +1,58 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _92
 {
-	// MEDIUM
-	internal class _92
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode ReverseBetween(ListNode head, int left, int right)
+	{
+		if (head == null)
+		{
+			return null;
 		}
 
-		public static ListNode ReverseBetween(ListNode head, int left, int right)
+		var current = head;
+		var previous = default(ListNode);
+		while (left > 1)
 		{
-			if (head == null)
-			{
-				return null;
-			}
-
-			var current = head;
-			var previous = default(ListNode);
-			while (left > 1)
-			{
-				previous = current;
-				current = current.next;
-				left--;
-				right--;
-			}
-
-			var connection = previous;
-			var tail = current;
-
-			while (right > 0)
-			{
-				var third = current.next;
-				current.next = previous;
-				previous = current;
-				current = third;
-				right--;
-			}
-
-			if (connection != null)
-			{
-				connection.next = previous;
-			}
-			else
-			{
-				head = previous;
-			}
-
-			tail.next = current;
-			return head;
+			previous = current;
+			current = current.next;
+			left--;
+			right--;
 		}
+
+		var connection = previous;
+		var tail = current;
+
+		while (right > 0)
+		{
+			var third = current.next;
+			current.next = previous;
+			previous = current;
+			current = third;
+			right--;
+		}
+
+		if (connection != null)
+		{
+			connection.next = previous;
+		}
+		else
+		{
+			head = previous;
+		}
+
+		tail.next = current;
+		return head;
 	}
 }
