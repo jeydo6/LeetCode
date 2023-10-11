1a4d7c1932ce010871a361b94a88699ccfb561c8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:36 +0300

    1239

--- LeetCode/Algorithms/1239.cs
+++ LeetCode/Algorithms/1239.cs
@@ -1,60 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1239
 {
-	// MEDIUM
-	internal class _1239
+	public static int MaxLength(IList<string> arr)
 	{
-		public static int MaxLength(IList<string> arr)
+		var result = 0;
+
+		var dp = new List<int>
 		{
-			var result = 0;
+			0
+		};
 
-			var dp = new List<int>
+		for (var i = 0; i < arr.Count; i++)
+		{
+			var a = 0;
+			var d = 0;
+
+			for (var j = 0; j < arr[i].Length; j++)
 			{
-				0
-			};
+				d |= a & (1 << (arr[i][j] - 'a'));
+				a |= 1 << (arr[i][j] - 'a');
+			}
 
-			for (var i = 0; i < arr.Count; i++)
+			if (d > 0)
 			{
-				var a = 0;
-				var d = 0;
+				continue;
+			}
 
-				for (var j = 0; j < arr[i].Length; j++)
-				{
-					d |= a & (1 << (arr[i][j] - 'a'));
-					a |= 1 << (arr[i][j] - 'a');
-				}
-
-				if (d > 0)
+			for (var j = dp.Count - 1; j >= 0; j--)
+			{
+				if ((dp[j] & a) > 0)
 				{
 					continue;
 				}
 
-				for (var j = dp.Count - 1; j >= 0; j--)
-				{
-					if ((dp[j] & a) > 0)
-					{
-						continue;
-					}
-
-					dp.Add(dp[j] | a);
-					result = Math.Max(result, BitCount(dp[j] | a));
-				}
+				dp.Add(dp[j] | a);
+				result = Math.Max(result, BitCount(dp[j] | a));
 			}
-
-			return result;
 		}
 
-		private static int BitCount(int n)
+		return result;
+	}
+
+	private static int BitCount(int n)
+	{
+		var count = 0;
+		while (n != 0)
 		{
-			var count = 0;
-			while (n != 0)
-			{
-				count++;
-				n &= n - 1;
-			}
-			return count;
+			count++;
+			n &= n - 1;
 		}
+		return count;
 	}
 }
