d70040f35857ebd5cd68ed965a3114da069f1088
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:17 +0300

    785

--- LeetCode/Algorithms/785.cs
+++ LeetCode/Algorithms/785.cs
@@ -1,47 +1,46 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _785
 {
-	// MEDIUM
-	internal class _785
+	public static bool IsBipartite(int[][] graph)
 	{
-		public static bool IsBipartite(int[][] graph)
+		var color = new int[graph.Length];
+		for (var i = 0; i < graph.Length; i++)
 		{
-			var color = new int[graph.Length];
-			for (var i = 0; i < graph.Length; i++)
-			{
-				color[i] = -1;
-			}
+			color[i] = -1;
+		}
 
-			for (var i = 0; i < graph.Length; ++i)
+		for (var i = 0; i < graph.Length; ++i)
+		{
+			if (color[i] == -1)
 			{
-				if (color[i] == -1)
+				var stack = new Stack<int>();
+				stack.Push(i);
+				color[i] = 0;
+
+				while (stack.Count > 0)
 				{
-					var stack = new Stack<int>();
-					stack.Push(i);
-					color[i] = 0;
-
-					while (stack.Count > 0)
+					var node = stack.Pop();
+					var neighbours = graph[node];
+					for (var j = 0; j < neighbours.Length; j++)
 					{
-						var node = stack.Pop();
-						var neighbours = graph[node];
-						for (var j = 0; j < neighbours.Length; j++)
+						var neighbour = neighbours[j];
+						if (color[neighbour] == -1)
 						{
-							var neighbour = neighbours[j];
-							if (color[neighbour] == -1)
-							{
-								stack.Push(neighbour);
-								color[neighbour] = color[node] ^ 1;
-							}
-							else if (color[neighbour] == color[node])
-							{
-								return false;
-							}
+							stack.Push(neighbour);
+							color[neighbour] = color[node] ^ 1;
+						}
+						else if (color[neighbour] == color[node])
+						{
+							return false;
 						}
 					}
 				}
 			}
-			return true;
 		}
+		return true;
 	}
 }
