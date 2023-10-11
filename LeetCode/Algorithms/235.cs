c8d5fd3c5006942d7cc2bdd7343fe3161098c980
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:52 +0300

    235

---
+++ LeetCode/Algorithms/235.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _235
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+
+		public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+		{
+			while ((root.val - p.val) * (root.val - q.val) > 0)
+			{
+				root = p.val < root.val ? root.left : root.right;
+			}
+			return root;
+		}
+	}
+}
