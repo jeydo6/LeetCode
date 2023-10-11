38ba21a98090e734100e280f8d30b258e095a2fc
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:11:35 +0300

    1470. 1672. 1945. 1946. 3829

---
+++ LeetCode/Algorithms/1672.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms
+{
+	class _1672
+	{
+		public static int MaximumWealth(int[][] accounts)
+		{
+			return accounts
+				.Select(a => a.Sum())
+				.OrderByDescending(s => s)
+				.First();
+		}
+	}
+}
