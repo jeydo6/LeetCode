1e08b8af8abee696c1680153d3401e82ac5b64dd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:04 +0300

    1491

---
+++ LeetCode/Algorithms/1491.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1491
+{
+	public static double Average(int[] salary)
+	{
+		var minSalary = int.MaxValue;
+		var maxSalary = int.MinValue;
+
+		var sum = 0d;
+		for (var i = 0; i < salary.Length; i++)
+		{
+			sum += salary[i];
+			minSalary = Math.Min(minSalary, salary[i]);
+			maxSalary = Math.Max(maxSalary, salary[i]);
+		}
+
+		return (sum - minSalary - maxSalary) / (salary.Length - 2);
+	}
+}
