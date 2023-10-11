846648cef06f7f6d65251df0c80cb87ed5c5b28f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:24 +0300

    976

---
+++ LeetCode/Algorithms/976.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// EASY
+internal class _976
+{
+	public static int LargestPerimeter(int[] nums)
+	{
+		Array.Sort(nums);
+
+		for (var i = nums.Length - 3; i >= 0; i--)
+		{
+			if (nums[i] + nums[i + 1] > nums[i + 2])
+			{
+				return nums[i] + nums[i + 1] + nums[i + 2];
+			}
+		}
+
+		return 0;
+	}
+}
