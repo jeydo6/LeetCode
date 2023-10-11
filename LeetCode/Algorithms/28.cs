889a402df6f1ff36611bf626b8d3cd7090981196
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:15 +0300

    28

---
+++ LeetCode/Algorithms/28.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _28
+{
+	public static int StrStr(string haystack, string needle)
+	{
+		if (haystack.Length < needle.Length)
+		{
+			return -1;
+		}
+
+		for (var i = 0; i <= haystack.Length - needle.Length; i++)
+		{
+			var j = 0;
+			while (j < needle.Length && haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+
+			if (j == needle.Length)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
