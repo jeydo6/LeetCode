ba5b1c071ac95b3b52823bb89e3e30de00e9eaa5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:43 +0300

    260

---
+++ LeetCode/Algorithms/260.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Algorithms
+{
+	internal class _260
+	{
+		public static int[] SingleNumber(int[] nums)
+		{
+			var diff = 0;
+			foreach (var num in nums)
+			{
+				diff ^= num;
+			}
+			diff &= -diff;
+
+			var result = new[] { 0, 0 };
+			foreach (var num in nums)
+			{
+				if ((num & diff) == 0)
+				{
+					result[0] ^= num;
+				}
+				else
+				{
+					result[1] ^= num;
+				}
+			}
+			return result;
+		}
+	}
+}
