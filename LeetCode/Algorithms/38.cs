09a44474745f81e1d69e26f944110b46e69bba68
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:31 +0300

    38

---
+++ LeetCode/Algorithms/38.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _38
+{
+	public static string CountAndSay(int n)
+	{
+		var result = "1";
+
+		for (var i = 2; i <= n; i++)
+		{
+			var sb = new StringBuilder();
+
+			var k = 0;
+			for (var j = 0; j < result.Length; j = k)
+			{
+				while (k < result.Length && result[k] == result[j])
+				{
+					k++;
+				}
+				sb.Append($"{k - j}{result[j]}");
+			}
+			result = sb.ToString();
+		}
+
+		return result;
+	}
+}
