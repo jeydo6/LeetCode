d9f4cf184c44835444d89b8d8a84a6127b376122
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:44 +0300

    1329

---
+++ LeetCode/Algorithms/1329.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1329
+	{
+		public static int[][] DiagonalSort(int[][] mat)
+		{
+			var rows = mat.Length;
+			var cols = mat[0].Length;
+
+			var diagonals = new Dictionary<int, PriorityQueue<int, int>>();
+
+			for (var row = 0; row < rows; row++)
+			{
+				for (var col = 0; col < cols; col++)
+				{
+					if (!diagonals.ContainsKey(row - col))
+					{
+						diagonals[row - col] = new PriorityQueue<int, int>();
+					}
+					diagonals[row - col].Enqueue(mat[row][col], mat[row][col]);
+				}
+			}
+
+			for (var row = 0; row < rows; row++)
+			{
+				for (var col = 0; col < cols; col++)
+				{
+					mat[row][col] = diagonals[row - col].Dequeue();
+				}
+			}
+
+			return mat;
+		}
+	}
+}
