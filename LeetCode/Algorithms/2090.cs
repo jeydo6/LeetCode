6a2b76331336973de4b13427658f9f45ee3c88da
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:42 +0300

    2090

--- LeetCode/Algorithms/2090.cs
+++ LeetCode/Algorithms/2090.cs
@@ -22,20 +22,15 @@ internal class _2090
 		}
 
 		var sum = 0L;
-		for (int i = 0; i < (2 * k + 1); ++i)
+		for (var i = 0; i < (2 * k + 1); i++)
 		{
 			sum += nums[i];
 		}
 		result[k] = (int)(sum / (2 * k + 1));
 
-		// Iterate on rest indices which have at least 'k' elements 
-		// on its left and right sides.
-		for (int i = (2 * k + 1); i < n; ++i)
+		for (var i = 2 * k + 1; i < result.Length; i++)
 		{
-			// We remove the discarded element and add the new element to get current window sum.
-			// 'i' is the index of new inserted element, and
-			// 'i - (window size)' is the index of the last removed element.
-			sum = sum - nums[i - (2 * k + 1)] + nums[i];
+			sum -= nums[i - (2 * k + 1)] - nums[i];
 			result[i - k] = (int)(sum / (2 * k + 1));
 		}
 
