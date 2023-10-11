de2b43465848d524da18050c432d729c3e1f748e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:02 +0300

    510

---
+++ LeetCode/Algorithms/510.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _510
+	{
+		public class Node
+		{
+			public int val;
+			public Node left;
+			public Node right;
+			public Node parent;
+		}
+
+		public static Node InorderSuccessor(Node x)
+		{
+			if (x.right != null)
+			{
+				x = x.right;
+				while (x.left != null)
+				{
+					x = x.left;
+				}
+				return x;
+			}
+
+			while (x.parent != null && x == x.parent.right)
+			{
+				x = x.parent;
+			}
+
+			return x.parent;
+		}
+	}
+}
