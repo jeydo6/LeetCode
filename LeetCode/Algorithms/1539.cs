41e6057147042ed3cc49b1b8060153fab8a3a0c4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:17 +0300

    1539

---
+++ LeetCode/Algorithms/1539.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1539
+{
+	public static int FindKthPositive(int[] arr, int k)
+	{
+		var start = 0;
+		var end = arr.Length - 1;
+		while (start <= end)
+		{
+			var mid = start + (end - start) / 2;
+			if (arr[mid] - (mid + 1) < k)
+			{
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		return start + k;
+	}
+}
