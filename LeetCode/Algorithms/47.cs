d710b5150eb9c8e279957be37c29bf498a213c5a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:07 +0300

    47

---
+++ LeetCode/Algorithms/47.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _47
+	{
+		public static IList<IList<int>> PermuteUnique(int[] nums)
+		{
+			var result = new List<IList<int>>();
+
+			var counter = new Dictionary<int, int>();
+			for (var i = 0; i < nums.Length; i++)
+			{
+				if (!counter.ContainsKey(nums[i]))
+				{
+					counter[nums[i]] = 0;
+				}
+				counter[nums[i]]++;
+			}
+
+			var combination = new LinkedList<int>();
+			Backtrack(combination, nums.Length, counter, result);
+			return result;
+		}
+
+		private static void Backtrack(LinkedList<int> combination, int n, IDictionary<int, int> counter, IList<IList<int>> result)
+		{
+
+			if (combination.Count == n)
+			{
+				result.Add(new List<int>(combination));
+				return;
+			}
+
+			foreach (var (num, count) in counter)
+			{
+
+				if (count == 0)
+				{
+					continue;
+				}
+
+				combination.AddLast(num);
+				counter[num] = count - 1;
+
+				Backtrack(combination, n, counter, result);
+
+				combination.RemoveLast();
+				counter[num] = count;
+			}
+		}
+	}
+}
