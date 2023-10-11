dac418b81d3264cc3599075b2a591db09fcbfd98
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:50 +0300

    967

---
+++ LeetCode/Algorithms/967.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _967
+	{
+		public static int[] NumsSameConsecDiff(int n, int k)
+		{
+			var result = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+			for (var i = 2; i <= n; i++)
+			{
+				var temp = new List<int>();
+				for (var j = 0; j < result.Count; j++)
+				{
+					var digit = result[j] % 10;
+					if (digit + k < 10)
+					{
+						temp.Add(result[j] * 10 + digit + k);
+					}
+
+					if (k > 0 && digit - k >= 0)
+					{
+						temp.Add(result[j] * 10 + digit - k);
+					}
+				}
+				result = temp;
+			}
+
+			return result.ToArray();
+		}
+	}
+}
