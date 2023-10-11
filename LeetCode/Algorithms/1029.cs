c2715cd816625b7785abb61212744cd954974077
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:47 +0300

    1029

---
+++ LeetCode/Algorithms/1029.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1029
+	{
+		public static int TwoCitySchedCost(int[][] costs)
+		{
+			Array.Sort(costs, (c1, c2) => c1[0] - c2[0] - c1[1] + c2[1]);
+
+			var n = costs.Length / 2;
+			var total = 0;
+			for (var i = 0; i < n; i++)
+			{
+				total += costs[i][0] + costs[i + n][1];
+			}
+			return total;
+		}
+	}
+}
