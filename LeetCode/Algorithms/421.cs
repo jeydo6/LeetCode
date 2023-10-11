3584120f50d02fd7a895d3a8c8f57a16228f037b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:17:01 +0300

    421

---
+++ LeetCode/Algorithms/421.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _421
+	{
+		public static int FindMaximumXOR(int[] nums)
+		{
+			var max = 0;
+			var mask = 0;
+			for (var i = 31; i >= 0; i--)
+			{
+				mask |= 1 << i;
+				var hashSet = new HashSet<int>();
+				for (var j = 0; j < nums.Length; j++)
+				{
+					hashSet.Add(nums[j] & mask);
+				}
+				var temp = max | (1 << i);
+				foreach (var item in hashSet)
+				{
+					if (hashSet.Contains(temp ^ item))
+					{
+						max = temp;
+						break;
+					}
+				}
+			}
+			return max;
+		}
+	}
+}
