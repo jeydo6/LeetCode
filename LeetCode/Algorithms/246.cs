a37487d498bc101b77947ac5c7214b7e40b9d125
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:22:06 +0300

    246

---
+++ LeetCode/Algorithms/246.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+internal class _246
+{
+	private static IDictionary<char, char> _digits = new Dictionary<char, char>
+	{
+		['0'] = '0',
+		['1'] = '1',
+		['6'] = '9',
+		['8'] = '8',
+		['9'] = '6'
+	};
+
+	public static bool IsStrobogrammatic(string num)
+	{
+		var lo = 0;
+		var hi = num.Length - 1;
+		while (lo <= hi)
+		{
+			if (!_digits.ContainsKey(num[lo]) || _digits[num[lo]] != num[hi])
+			{
+				return false;
+			}
+
+			lo++;
+			hi--;
+		}
+
+		return true;
+	}
+}
