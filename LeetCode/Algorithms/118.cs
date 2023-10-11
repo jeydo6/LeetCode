71a522287c2348b3541513ba14218481296d4af4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:16 +0300

    118

---
+++ LeetCode/Algorithms/118.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _118
+	{
+		public static IList<IList<int>> Generate(int n)
+		{
+			var rows = new List<IList<int>>();
+			var row = new List<int>();
+			for (var i = 0; i < n; i++)
+			{
+				row.Insert(0, 1);
+				for (var j = 1; j < row.Count - 1; j++)
+				{
+					row[j] = row[j] + row[j + 1];
+				}
+				rows.Add(new List<int>(row));
+			}
+			return rows;
+		}
+	}
+}
