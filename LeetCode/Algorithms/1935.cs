697258f737d4efff39ba0d5eeaf9b54e8c9a6f88
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:12 +0300

    1935

---
+++ LeetCode/Algorithms/1935.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _1935
+	{
+		public static int CanBeTypedWords(string text, string brokenLetters)
+		{
+			var words = text.Split();
+			var result = words.Length;
+			foreach (var word in words)
+			{
+				foreach (var ch in word)
+				{
+					if (brokenLetters.Contains(ch))
+					{
+						result--;
+						break;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
