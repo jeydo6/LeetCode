32b1ded9df86c7768d8477afb8ba336eac58dea2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:48 +0300

    122

---
+++ LeetCode/Algorithms/122.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _122
+	{
+		public static int MaxProfit(int[] prices)
+		{
+			var profit = 0;
+
+			var i = 0;
+			var n = prices.Length - 1;
+			while (i < n)
+			{
+				while (i < n && prices[i + 1] <= prices[i])
+				{
+					i++;
+				}
+				var buy = prices[i];
+
+				while (i < n && prices[i + 1] > prices[i])
+				{
+					i++;
+				}
+				var sell = prices[i];
+
+				profit += sell - buy;
+			}
+
+			return profit;
+		}
+	}
+}
