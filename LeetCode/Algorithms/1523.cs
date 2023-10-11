ba0add657e0e0e05e5c78aaeeb7fc49533ed6ffc
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:00 +0300

    1523

---
+++ LeetCode/Algorithms/1523.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1523
+{
+	public static int CountOdds(int low, int high)
+	{
+		if ((low & 2) == 0)
+		{
+			low++;
+		}
+
+		return low > high ? 0 : (high - low) / 2 + 1;
+	}
+}
