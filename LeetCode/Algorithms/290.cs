83711b1a8979c5aea76d849f7f84135d2a3b73b6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:16:54 +0300

    290

---
+++ LeetCode/Algorithms/290.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	internal class _290
+	{
+		// EASY
+		public static bool WordPattern(string pattern, string s)
+		{
+			var words = s.Split(" ");
+			if (words.Length != pattern.Length)
+			{
+				return false;
+			}
+
+			var pw = new Dictionary<char, string>();
+			var wp = new Dictionary<string, char>();
+			for (var i = 0; i < words.Length; i++)
+			{
+				var w = words[i];
+				var p = pattern[i];
+
+				if (pw.ContainsKey(p))
+				{
+					if (pw[p] != w)
+					{
+						return false;
+					}
+				}
+				else
+				{
+					pw[p] = w;
+				}
+
+				if (wp.ContainsKey(w))
+				{
+					if (wp[w] != p)
+					{
+						return false;
+					}
+				}
+				else
+				{
+					wp[w] = p;
+				}
+			}
+
+			return true;
+		}
+	}
+}
