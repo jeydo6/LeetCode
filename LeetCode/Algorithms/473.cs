3b541cd239d73fd8700d122db9b1b33fe533b020
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:01 +0300

    473

---
+++ LeetCode/Algorithms/473.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _473
+	{
+		public static bool Makesquare(int[] matchsticks)
+		{
+			if (matchsticks == null || matchsticks.Length == 0)
+			{
+				return false;
+			}
+
+			var perimeter = 0;
+			for (int i = 0; i < matchsticks.Length; i++)
+			{
+				perimeter += matchsticks[i];
+			}
+
+			var possibleSquareSide = perimeter / 4;
+			if (possibleSquareSide * 4 != perimeter)
+			{
+				return false;
+			}
+
+			Array.Sort(matchsticks, (a, b) => b - a);
+			return Makesquare(matchsticks, possibleSquareSide, 0, new int[4]);
+		}
+
+		public static bool Makesquare(int[] matchsticks, int possibleSquareSide, int index, int[] sums)
+		{
+			if (index == matchsticks.Length)
+			{
+				return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+			}
+
+			for (var i = 0; i < 4; i++)
+			{
+				if (sums[i] + matchsticks[index] <= possibleSquareSide)
+				{
+					sums[i] += matchsticks[index];
+					if (Makesquare(matchsticks, possibleSquareSide, index + 1, sums))
+					{
+						return true;
+					}
+					sums[i] -= matchsticks[index];
+				}
+			}
+
+			return false;
+		}
+	}
+}
