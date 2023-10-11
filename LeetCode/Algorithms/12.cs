55bbf939590603dd20f554a59c64f829b7b15a79
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:32 +0300

    12

--- LeetCode/Algorithms/12.cs
+++ LeetCode/Algorithms/12.cs
@@ -1,45 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _12
 {
-	// MEDIUM
-	internal class _12
+	private readonly static IDictionary<string, int> _dictionary = new Dictionary<string, int>
 	{
-		private readonly static IDictionary<string, int> _dictionary = new Dictionary<string, int>
-		{
-			["M"] = 1000,
-			["CM"] = 900,
-			["D"] = 500,
-			["CD"] = 400,
-			["C"] = 100,
-			["XC"] = 90,
-			["L"] = 50,
-			["XL"] = 40,
-			["X"] = 10,
-			["IX"] = 9,
-			["V"] = 5,
-			["IV"] = 4,
-			["I"] = 1
-		};
+		["M"] = 1000,
+		["CM"] = 900,
+		["D"] = 500,
+		["CD"] = 400,
+		["C"] = 100,
+		["XC"] = 90,
+		["L"] = 50,
+		["XL"] = 40,
+		["X"] = 10,
+		["IX"] = 9,
+		["V"] = 5,
+		["IV"] = 4,
+		["I"] = 1
+	};
 
-		public static string IntToRoman(int num)
+	public static string IntToRoman(int num)
+	{
+		var sb = new StringBuilder();
+		foreach (var pair in _dictionary)
 		{
-			var sb = new StringBuilder();
-			foreach (var pair in _dictionary)
+			if (num <= 0)
 			{
-				if (num <= 0)
-				{
-					break;
-				}
-
-				while (pair.Value <= num)
-				{
-					num -= pair.Value;
-					sb.Append(pair.Key);
-				}
+				break;
 			}
-			return sb.ToString();
+
+			while (pair.Value <= num)
+			{
+				num -= pair.Value;
+				sb.Append(pair.Key);
+			}
 		}
+		return sb.ToString();
 	}
 }
