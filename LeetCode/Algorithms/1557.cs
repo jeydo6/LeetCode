00d3e31bfd48481e6cc19c9f366dd70a19e83d81
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:17 +0300

    1557

---
+++ LeetCode/Algorithms/1557.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1557
+{
+	public static IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+	{
+		var isExists = new bool[n];
+		foreach (var edge in edges)
+		{
+			isExists[edge[1]] = true;
+		}
+
+		var result = new List<int>();
+		for (var i = 0; i < n; i++)
+		{
+			if (!isExists[i])
+			{
+				result.Add(i);
+			}
+		}
+		return result;
+	}
+}
