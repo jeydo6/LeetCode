8dd96c28e042e6cbbf583c5fba0220f08e104510
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:41 +0300

    1200

--- LeetCode/Algorithms/1200.cs
+++ LeetCode/Algorithms/1200.cs
@@ -3,50 +3,28 @@ using System.Collections.Generic;
 
 namespace Leetcode.Algorithms
 {
-    public class _1200
-    {
-        public IList<IList<Int32>> MinimumAbsDifference(Int32[] arr)
-        {
-            Int32 minDiff = Int32.MaxValue;
-
-            Array.Sort(arr);
-            for (Int32 i = 0; i < arr.Length - 1; i++)
-            {
-                Int32 diff = Math.Abs(arr[i] - arr[i + 1]);
-
-                if (diff < minDiff)
-                {
-                    minDiff = diff;
-                }
-            }
-
-            for (Int32 i = 0; i < arr.Length - 1; i++)
-            {
-                Int32 diff = Math.Abs(arr[i] - arr[i + 1]);
-
-                if (diff < minDiff)
-                {
-                    minDiff = diff;
-                }
-            }
-
-            IList<IList<Int32>> result = new List<IList<Int32>>();
-
-            for (Int32 i = 0; i < arr.Length - 1; i++)
-            {
-                Int32 diff = Math.Abs(arr[i] - arr[i + 1]);
-
-                if (diff == minDiff)
-                {
-                    result.Add(new List<Int32>
-                    {
-                        arr[i],
-                        arr[i + 1]
-                    });
-                }
-            }
-
-            return result;
-        }
-    }
+	public class _1200
+	{
+		public static IList<IList<int>> MinimumAbsDifference(int[] arr)
+		{
+			Array.Sort(arr);
+			var min = int.MaxValue;
+			var result = new List<IList<int>>();
+			for (var i = 0; i < arr.Length - 1; i++)
+			{
+				var diff = arr[i + 1] - arr[i];
+				if (diff < min)
+				{
+					min = diff;
+					result.Clear();
+					result.Add(new List<int> { arr[i], arr[i + 1] });
+				}
+				else if (diff == min)
+				{
+					result.Add(new List<int> { arr[i], arr[i + 1] });
+				}
+			}
+			return result;
+		}
+	}
 }
