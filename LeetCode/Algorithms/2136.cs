177d6f69b974e36fe220eb81d2af0807698d3de2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:41 +0300

    2136

---
+++ LeetCode/Algorithms/2136.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _2136
+{
+	public static int EarliestFullBloom(int[] plantTime, int[] growTime)
+	{
+		var indices = new List<int>();
+		for (var i = 0; i < growTime.Length; i++)
+		{
+			indices.Add(i);
+		}
+
+		indices.Sort((i1, i2) => growTime[i1] - growTime[i2]);
+
+		var currentPlantTime = 0;
+		var result = 0;
+		for (var i = 0; i < growTime.Length; i++)
+		{
+			var index = indices[i];
+
+			result = Math.Max(result, currentPlantTime + plantTime[index] + growTime[index]);
+
+			currentPlantTime += plantTime[index];
+		}
+
+		return result;
+	}
+}
