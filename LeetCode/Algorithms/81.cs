85561b7a66cd11694e99ea734e6659649ff1608a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:25 +0300

    81

--- LeetCode/Algorithms/81.cs
+++ LeetCode/Algorithms/81.cs
@@ -1,58 +1,57 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _81
 {
-	// MEDIUM
-	internal class _81
+	public static bool Search(int[] nums, int target)
 	{
-		public static bool Search(int[] nums, int target)
+		if (nums.Length == 0)
 		{
-			if (nums.Length == 0)
+			return false;
+		}
+
+		var end = nums.Length - 1;
+		var start = 0;
+
+		while (start <= end)
+		{
+			var mid = (start + end) / 2;
+
+			if (nums[mid] == target)
 			{
-				return false;
+				return true;
 			}
 
-			var end = nums.Length - 1;
-			var start = 0;
-
-			while (start <= end)
+			if (nums[start] == nums[mid])
 			{
-				var mid = (start + end) / 2;
-
-				if (nums[mid] == target)
+				start++;
+				continue;
+			}
+			var pivotArray = nums[start] <= nums[mid];
+			var targetArray = nums[start] <= target;
+			if (pivotArray ^ targetArray)
+			{
+				if (pivotArray)
 				{
-					return true;
-				}
-
-				if (nums[start] == nums[mid])
-				{
-					start++;
-					continue;
-				}
-				var pivotArray = nums[start] <= nums[mid];
-				var targetArray = nums[start] <= target;
-				if (pivotArray ^ targetArray)
-				{
-					if (pivotArray)
-					{
-						start = mid + 1;
-					}
-					else
-					{
-						end = mid - 1;
-					}
+					start = mid + 1;
 				}
 				else
 				{
-					if (nums[mid] < target)
-					{
-						start = mid + 1;
-					}
-					else
-					{
-						end = mid - 1;
-					}
+					end = mid - 1;
 				}
 			}
-			return false;
+			else
+			{
+				if (nums[mid] < target)
+				{
+					start = mid + 1;
+				}
+				else
+				{
+					end = mid - 1;
+				}
+			}
 		}
+		return false;
 	}
 }
