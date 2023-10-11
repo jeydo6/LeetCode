a76ae89c6aba7eef9cd59138ed080c0a97d4bdc3
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:08 +0300

    337

---
+++ LeetCode/Algorithms/337.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _337
+	{
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+			{
+				this.val = val;
+				this.left = left;
+				this.right = right;
+			}
+
+			public static int Rob(TreeNode root)
+			{
+				var result = RobRecursive(root);
+
+				return Math.Max(result[0], result[1]);
+			}
+
+			private static int[] RobRecursive(TreeNode root)
+			{
+				if (root == null)
+				{
+					return new int[2];
+				}
+
+				var left = RobRecursive(root.left);
+				var right = RobRecursive(root.right);
+
+				var result = new int[2];
+				result[0] = Math.Max(left[0], left[1]) + Math.Max(right[0], right[1]);
+				result[1] = root.val + left[0] + right[0];
+
+				return result;
+			}
+		}
+	}
+}
