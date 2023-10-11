7548382b55477541a04c6298857323369b9e8ded
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:23 +0300

    33

--- LeetCode/Algorithms/33.cs
+++ LeetCode/Algorithms/33.cs
@@ -1,43 +1,43 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _33
 {
-	// MEDIUM
-	internal class _33
+	public static int Search(int[] nums, int target)
 	{
-		public static int Search(int[] nums, int target)
+		var lo = 0;
+		var hi = nums.Length - 1;
+		while (lo <= hi)
 		{
-			var lo = 0;
-			var hi = nums.Length - 1;
-			while (lo <= hi)
+			var mid = lo + (hi - lo) / 2;
+			if (nums[mid] == target)
 			{
-				var mid = (lo + hi) / 2;
-				if (nums[mid] == target)
+				return mid;
+			}
+			else if (nums[mid] < nums[hi])
+			{
+				if (target <= nums[hi] && target > nums[mid])
 				{
-					return mid;
-				}
-				else if (nums[mid] < nums[hi])
-				{
-					if (target <= nums[hi] && target > nums[mid])
-					{
-						lo = mid + 1;
-					}
-					else
-					{
-						hi = mid - 1;
-					}
+					lo = mid + 1;
 				}
 				else
 				{
-					if (target >= nums[lo] && target < nums[mid])
-					{
-						hi = mid - 1;
-					}
-					else
-					{
-						lo = mid + 1;
-					}
+					hi = mid - 1;
 				}
 			}
-			return -1;
+			else
+			{
+				if (target >= nums[lo] && target < nums[mid])
+				{
+					hi = mid - 1;
+				}
+				else
+				{
+					lo = mid + 1;
+				}
+			}
 		}
+
+		return -1;
 	}
 }
