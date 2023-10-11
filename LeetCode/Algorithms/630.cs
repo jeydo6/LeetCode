f8ba0965273bf4d378ba8aae9d36327e7056241b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:45 +0300

    630

---
+++ LeetCode/Algorithms/630.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _630
+	{
+		public static int ScheduleCourse(int[][] courses)
+		{
+			Array.Sort(courses, (a, b) => a[1] - b[1]);
+
+			var pqueue = new PriorityQueue<int, int>();
+			var time = 0;
+			for (var i = 0; i < courses.Length; i++)
+			{
+				if (time + courses[i][0] <= courses[i][1])
+				{
+					time += courses[i][0];
+					pqueue.Enqueue(courses[i][0], -courses[i][0]);
+				}
+				else if (pqueue.Count > 0 && pqueue.Peek() > courses[i][0])
+				{
+					time += courses[i][0] - pqueue.Dequeue();
+					pqueue.Enqueue(courses[i][0], -courses[i][0]);
+				}
+			}
+
+			return pqueue.Count;
+		}
+	}
+}
