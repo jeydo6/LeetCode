3fa666a864f7a9af8c163d1e97b1b7296221af48
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:13:52 +0300

    1189

--- LeetCode/Algorithms/1189.cs
+++ LeetCode/Algorithms/1189.cs
@@ -1,42 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leetcode.Algorithms
+﻿namespace Leetcode.Algorithms
 {
-    public class _1189
-    {
-        public Int32 MaxNumberOfBalloons(String text)
-        {
-            String templateString = "balloon";
+	public class _1189
+	{
+		public static int MaxNumberOfBalloons(string text)
+		{
+			var chars = new int[26];
+			for (var i = 0; i < text.Length; i++)
+			{
+				chars[text[i] - 'a']++;
+			}
 
-            Int32[] templateArray = new Int32[26];
-            for (Int32 i = 0; i < templateString.Length; i++)
-            {
-                templateArray[templateString[i] - 'a']++;
-            }
-
-            Int32[] textArray = new Int32[26];
-            for (Int32 i = 0; i < text.Length; i++)
-            {
-                textArray[text[i] - 'a']++;
-            }
-
-            Int32 result = Int32.MaxValue;
-            for (Int32 i = 0; i < templateArray.Length; i++)
-            {
-                if (templateArray[i] > 0)
-                {
-                    Int32 count = textArray[i] / templateArray[i];
-                    if (count < result)
-                    {
-                        result = count;
-                    }
-                }
-            }
-            return result;
-        }
-    }
+			var min = Math.Min(chars[0], chars[1]);
+			min = Math.Min(min, chars[11] / 2);
+			min = Math.Min(min, chars[14] / 2);
+			return Math.Min(min, chars[13]);
+		}
+	}
 }
