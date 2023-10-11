770c4e04943ce0047ab211729d8e7278bb33ff4b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:58 +0300

    135

--- LeetCode/Algorithms/135.cs
+++ LeetCode/Algorithms/135.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _135
 {
-	// HARD
-	internal class _135
+	public static int Candy(int[] ratings)
 	{
-		public static int Candy(int[] ratings)
+		var candies = new int[ratings.Length];
+		for (var i = 0; i < ratings.Length; i++)
 		{
-			var candies = new int[ratings.Length];
-			for (var i = 0; i < ratings.Length; i++)
-			{
-				candies[i] = 1;
-			}
-
-			for (var i = 1; i < ratings.Length; i++)
-			{
-				if (ratings[i] > ratings[i - 1])
-				{
-					candies[i] = candies[i - 1] + 1;
-				}
-			}
-
-			var result = candies[ratings.Length - 1];
-			for (var i = ratings.Length - 2; i >= 0; i--)
-			{
-				if (ratings[i] > ratings[i + 1])
-				{
-					candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
-				}
-				result += candies[i];
-			}
-			return result;
+			candies[i] = 1;
 		}
+
+		for (var i = 1; i < ratings.Length; i++)
+		{
+			if (ratings[i] > ratings[i - 1])
+			{
+				candies[i] = candies[i - 1] + 1;
+			}
+		}
+
+		var result = candies[ratings.Length - 1];
+		for (var i = ratings.Length - 2; i >= 0; i--)
+		{
+			if (ratings[i] > ratings[i + 1])
+			{
+				candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+			}
+			result += candies[i];
+		}
+		return result;
 	}
 }
