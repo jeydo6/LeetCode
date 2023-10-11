a647bc50bbb87009879d0c7014db7d1a7c9398bf
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:21 +0300

    201

---
+++ LeetCode/Algorithms/201.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _201
+	{
+		public static int RangeBitwiseAnd(int left, int right)
+		{
+			var count = 0;
+			while (left != right)
+			{
+				count++;
+				left /= 2;
+				right /= 2;
+			}
+			return left << count;
+		}
+	}
+}
