0ddd88d2540adbe4559f7edb44655d4e9d6051bb
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:30 +0300

    151

---
+++ LeetCode/Algorithms/151.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _151
+	{
+		public static string ReverseWords(string s)
+		{
+			var result = "";
+			for (var i = s.Length - 1; i >= 0; i--)
+			{
+				var current = "";
+				while (i >= 0 && s[i] != ' ')
+				{
+					current = s[i] + current;
+					i--;
+				}
+
+				if (current.Length > 0)
+				{
+					result += (result.Length == 0 ? "" : " ") + current;
+				}
+			}
+			return result;
+		}
+	}
+}
