93c894328d726180fe910e6580a0cf31ac6d3632
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:12 +0300

    804

--- LeetCode/Algorithms/804.cs
+++ LeetCode/Algorithms/804.cs
@@ -1,67 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Leetcode.Algorithms
 {
-    public class _804
-    {
-        public Int32 UniqueMorseRepresentations(String[] words)
-        {
-            Dictionary<Char, String> letterToMorse = new Dictionary<Char, String>
-            {
-                ['a'] = ".-",
-                ['b'] = "-...",
-                ['c'] = "-.-.",
-                ['d'] = "-..",
-                ['e'] = ".",
-                ['f'] = "..-.",
-                ['g'] = "--.",
-                ['h'] = "....",
-                ['i'] = "..",
-                ['j'] = ".---",
-                ['k'] = "-.-",
-                ['l'] = ".-..",
-                ['m'] = "--",
-                ['n'] = "-.",
-                ['o'] = "---",
-                ['p'] = ".--.",
-                ['q'] = "--.-",
-                ['r'] = ".-.",
-                ['s'] = "...",
-                ['t'] = "-",
-                ['u'] = "..-",
-                ['v'] = "...-",
-                ['w'] = ".--",
-                ['x'] = "-..-",
-                ['y'] = "-.--",
-                ['z'] = "--.."
-            };
+	public class _804
+	{
+		//public static int UniqueMorseRepresentations(String[] words)
+		//{
+		//	Dictionary<Char, String> letterToMorse = new Dictionary<Char, String>
+		//	{
+		//		['a'] = ".-",
+		//		['b'] = "-...",
+		//		['c'] = "-.-.",
+		//		['d'] = "-..",
+		//		['e'] = ".",
+		//		['f'] = "..-.",
+		//		['g'] = "--.",
+		//		['h'] = "....",
+		//		['i'] = "..",
+		//		['j'] = ".---",
+		//		['k'] = "-.-",
+		//		['l'] = ".-..",
+		//		['m'] = "--",
+		//		['n'] = "-.",
+		//		['o'] = "---",
+		//		['p'] = ".--.",
+		//		['q'] = "--.-",
+		//		['r'] = ".-.",
+		//		['s'] = "...",
+		//		['t'] = "-",
+		//		['u'] = "..-",
+		//		['v'] = "...-",
+		//		['w'] = ".--",
+		//		['x'] = "-..-",
+		//		['y'] = "-.--",
+		//		['z'] = "--.."
+		//	};
 
-            ICollection<String> morseWords = new List<String>();
-            foreach (String word in words)
-            {
-                String morseWord = "";
-                foreach (Char letter in word)
-                {
-                    Char lowerLetter = Char.ToLower(letter);
-                    if (letterToMorse.ContainsKey(lowerLetter))
-                    {
-                        morseWord += letterToMorse[lowerLetter];
-                    }
-                }
+		//	ICollection<String> morseWords = new List<String>();
+		//	foreach (String word in words)
+		//	{
+		//		String morseWord = "";
+		//		foreach (Char letter in word)
+		//		{
+		//			Char lowerLetter = Char.ToLower(letter);
+		//			if (letterToMorse.ContainsKey(lowerLetter))
+		//			{
+		//				morseWord += letterToMorse[lowerLetter];
+		//			}
+		//		}
 
-                if (!morseWords.Contains(morseWord))
-                {
-                    morseWords.Add(morseWord);
-                }
-            }
+		//		if (!morseWords.Contains(morseWord))
+		//		{
+		//			morseWords.Add(morseWord);
+		//		}
+		//	}
 
-            Int32 result = morseWords.Count;
+		//	Int32 result = morseWords.Count;
 
-            return result;
-        }
-    }
+		//	return result;
+		//}
+
+		public static int UniqueMorseRepresentations(String[] words)
+		{
+			var dict = new string[26]
+			{
+				".-",
+				"-...",
+				"-.-.",
+				"-..",
+				".",
+				"..-.",
+				"--.",
+				"....",
+				"..",
+				".---",
+				"-.-",
+				".-..",
+				"--",
+				"-.",
+				"---",
+				".--.",
+				"--.-",
+				".-.",
+				"...",
+				"-",
+				"..-",
+				"...-",
+				".--",
+				"-..-",
+				"-.--",
+				"--.."
+			};
+
+			var morseWords = new HashSet<string>();
+			foreach (var word in words)
+			{
+				var morseWord = new StringBuilder();
+				foreach (var letter in word)
+				{
+					morseWord.Append(dict[letter - 'a']);
+				}
+				morseWords.Add(
+					morseWord.ToString()
+				);
+			}
+			return morseWords.Count;
+		}
+
+	}
 }
