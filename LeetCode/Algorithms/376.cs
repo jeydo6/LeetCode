4d5d43a1623f90fa7fc05f10760aee24ac66c059
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:55 +0300

    376

---
+++ LeetCode/Algorithms/376.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _376
+	{
+		public static int WiggleMaxLength(int[] nums)
+		{
+			if (nums.Length < 2)
+			{
+				return nums.Length;
+			}
+
+			var down = 1;
+			var up = 1;
+			for (var i = 1; i < nums.Length; i++)
+			{
+				if (nums[i] > nums[i - 1])
+				{
+					up = down + 1;
+				}
+				else if (nums[i] < nums[i - 1])
+				{
+					down = up + 1;
+				}
+			}
+
+			return Math.Max(down, up);
+		}
+	}
+}
