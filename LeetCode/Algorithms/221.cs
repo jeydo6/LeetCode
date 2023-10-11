4490b4efa1c000a24a537cd1a7fcc73c583dd59e
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:15:39 +0300

    221

---
+++ LeetCode/Algorithms/221.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _221
+	{
+		public static int MaximalSquare(char[][] matrix)
+		{
+			if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+			{
+				return 0;
+			}
+
+			var n = matrix.Length;
+			var m = matrix[0].Length;
+						
+			var temp = new int[n + 1][];
+			for (var i = 0; i < n + 1; i++)
+			{
+				temp[i] = new int[m + 1];
+			}
+
+			var result = 0;
+			for (var i = 1; i <= n; i++)
+			{
+				for (var j = 1; j <= m; j++)
+				{
+					if (matrix[i - 1][j - 1] == '1')
+					{
+						temp[i][j] = Math.Min(
+							Math.Min(temp[i][j - 1], temp[i - 1][j - 1]),
+							temp[i - 1][j]
+						) + 1;
+						result = Math.Max(temp[i][j], result);
+					}
+				}
+			}
+			return result * result;
+		}
+	}
+}
