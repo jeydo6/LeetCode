53c05025957bcb013c9286b0023643ba4904b267
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:24:20 +0300

    875

--- LeetCode/Algorithms/875.cs
+++ LeetCode/Algorithms/875.cs
@@ -1,31 +1,30 @@
-﻿namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _875
-	{
-		public static int MinEatingSpeed(int[] piles, int h)
-		{
-			var l = 1;
-			var r = 1000000000;
-			while (l < r)
-			{
-				var m = (l + r) / 2;
-				var total = 0;
-				for (var i = 0; i < piles.Length; i++)
-				{
-					total += (piles[i] + m - 1) / m;
-				}
+﻿namespace LeetCode.Algorithms;
 
-				if (total > h)
-				{
-					l = m + 1;
-				}
-				else
-				{
-					r = m;
-				}
+// MEDIUM
+internal class _875
+{
+	public static int MinEatingSpeed(int[] piles, int h)
+	{
+		var l = 1;
+		var r = 1000000000;
+		while (l < r)
+		{
+			var m = (l + r) / 2;
+			var total = 0;
+			for (var i = 0; i < piles.Length; i++)
+			{
+				total += (piles[i] + m - 1) / m;
 			}
-			return l;
+
+			if (total > h)
+			{
+				l = m + 1;
+			}
+			else
+			{
+				r = m;
+			}
 		}
+		return l;
 	}
 }
