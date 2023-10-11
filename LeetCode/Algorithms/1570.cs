1325f46da1cdc1d986047c538853c27d76801ae6
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:12 +0300

    1570

---
+++ LeetCode/Algorithms/1570.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1570
+	{
+		public class SparseVector
+		{
+			private readonly IDictionary<int, int> _nums;
+
+			public SparseVector(int[] nums)
+			{
+				_nums = new Dictionary<int, int>();
+				for (var i = 0; i < nums.Length; i++)
+				{
+					if (nums[i] != 0)
+					{
+						_nums[i] = nums[i];
+					}
+				}
+			}
+
+			public int DotProduct(SparseVector vec)
+			{
+				var result = 0;
+				foreach (var i in vec._nums.Keys)
+				{
+					if (_nums.ContainsKey(i))
+					{
+						result += _nums[i] * vec._nums[i];
+					}
+				}
+				return result;
+			}
+		}
+	}
+}
