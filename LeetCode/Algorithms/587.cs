565da385e92d711436be97fa8e020869160be3f4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:58 +0300

    587

--- LeetCode/Algorithms/587.cs
+++ LeetCode/Algorithms/587.cs
@@ -1,44 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _587
 {
-	// HARD
-	class _587
+	public static int[][] OuterTrees(int[][] trees)
 	{
-		public static int[][] OuterTrees(int[][] trees)
+		Array.Sort(trees, (p, q) => q[0] - p[0] == 0 ? q[1] - p[1] : q[0] - p[0]);
+
+		var list = new LinkedList<int[]>();
+		for (var i = 0; i < trees.Length; i++)
 		{
-			Array.Sort(trees, (p, q) => q[0] - p[0] == 0 ? q[1] - p[1] : q[0] - p[0]);
-
-			var stack = new Stack<int[]>();
-			for (var i = 0; i < trees.Length; i++)
+			while (list.Count >= 2 && Orientation(list.Last.Value, list.Last.Previous.Value, trees[i]) > 0)
 			{
-				while (stack.Count >= 2 && Orientation(stack.ElementAt(1), stack.Peek(), trees[i]) > 0)
-				{
-					stack.Pop();
-				}
-				stack.Push(trees[i]);
+				list.RemoveLast();
 			}
-
-			stack.Pop();
-
-			for (var i = trees.Length - 1; i >= 0; i--)
-			{
-				while (stack.Count >= 2 && Orientation(stack.ElementAt(1), stack.Peek(), trees[i]) > 0)
-				{
-					stack.Pop();
-				}
-				stack.Push(trees[i]);
-			}
-
-			var hashSet = new HashSet<int[]>(stack);
-			return hashSet.ToArray();
+			list.AddLast(trees[i]);
 		}
 
-		private static int Orientation(int[] p, int[] q, int[] r)
+		list.RemoveLast();
+
+		for (var i = trees.Length - 1; i >= 0; i--)
 		{
-			return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
+			while (list.Count >= 2 && Orientation(list.Last.Value, list.Last.Previous.Value, trees[i]) > 0)
+			{
+				list.RemoveLast();
+			}
+			list.AddLast(trees[i]);
 		}
+
+		var result = new List<int[]>(list.Count);
+		foreach (var item in new HashSet<int[]>(list))
+		{
+			result.Add(item);
+		}
+		return result.ToArray();
+	}
+
+	private static int Orientation(int[] p, int[] q, int[] r)
+	{
+		return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1]);
 	}
 }
