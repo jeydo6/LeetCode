139181b1d720556e250dc809a3d956815612423f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:26:56 +0300

    1647

--- LeetCode/Algorithms/1647.cs
+++ LeetCode/Algorithms/1647.cs
@@ -1,43 +1,42 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1647
 {
-	// MEDIUM
-	internal class _1647
+	public static int MinDeletions(string s)
 	{
-		public static int MinDeletions(string s)
+		var frequency = new int[26];
+		for (var i = 0; i < s.Length; i++)
 		{
-			var frequency = new int[26];
-			for (var i = 0; i < s.Length; i++)
-			{
-				frequency[s[i] - 'a']++;
-			}
-
-			var pqueue = new PriorityQueue<int, int>();
-			for (var i = 0; i < frequency.Length; i++)
-			{
-				if (frequency[i] > 0)
-				{
-					pqueue.Enqueue(frequency[i], -frequency[i]);
-				}
-			}
-
-			var result = 0;
-			while (pqueue.Count > 1)
-			{
-				var item = pqueue.Dequeue();
-
-				if (item == pqueue.Peek())
-				{
-					if (item - 1 > 0)
-					{
-						pqueue.Enqueue(item - 1, -(item - 1));
-					}
-					result++;
-				}
-			}
-
-			return result;
+			frequency[s[i] - 'a']++;
 		}
+
+		var pqueue = new PriorityQueue<int, int>();
+		for (var i = 0; i < frequency.Length; i++)
+		{
+			if (frequency[i] > 0)
+			{
+				pqueue.Enqueue(frequency[i], -frequency[i]);
+			}
+		}
+
+		var result = 0;
+		while (pqueue.Count > 1)
+		{
+			var item = pqueue.Dequeue();
+
+			if (item == pqueue.Peek())
+			{
+				if (item - 1 > 0)
+				{
+					pqueue.Enqueue(item - 1, -(item - 1));
+				}
+				result++;
+			}
+		}
+
+		return result;
 	}
 }
