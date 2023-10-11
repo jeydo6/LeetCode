18c19db464d447cbdfc4093914b51590639caa73
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:15 +0300

    322

---
+++ LeetCode/Algorithms/322.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _322
+	{
+		public static int CoinChange(int[] coins, int amount)
+		{
+			var dp = new int[amount + 1];
+			for (var i = 0; i < dp.Length; i++)
+			{
+				dp[i] = dp.Length;
+			}
+
+			dp[0] = 0;
+			for (var i = 1; i <= amount; i++)
+			{
+				for (var j = 0; j < coins.Length; j++)
+				{
+					if (coins[j] <= i)
+					{
+						dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+					}
+				}
+			}
+			return dp[amount] > amount ? -1 : dp[amount];
+		}
+	}
+}
