b6259e408c1698786ee1d9ec13b55732d06a7385
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:17 +0300

    277

---
+++ LeetCode/Algorithms/277.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _277
+	{
+		public static int FindCelebrity(int n)
+		{
+			var candidate = 0;
+			for (var b = 0; b < n; b++)
+			{
+				if (Knows(candidate, b))
+				{
+					candidate = b;
+				}
+			}
+
+			if (IsCelebrity(candidate, n))
+			{
+				return candidate;
+			}
+
+			return -1;
+		}
+
+		private static bool IsCelebrity(int a, int n)
+		{
+			for (var b = 0; b < n; b++)
+			{
+				if (a == b)
+				{
+					continue;
+				}
+
+				if (Knows(a, b) || !Knows(b, a))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static bool Knows(int a, int b)
+		{
+			return a == b;
+		}
+	}
+}
