e481f8ca1eb5c1ce4c6379be26636c73377c220a
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:47 +0300

    78

---
+++ LeetCode/Algorithms/78.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Algorithms
+{
+	class _78
+	{
+		public static void SetZeroes(int[][] matrix)
+		{
+			var rows = new HashSet<int>();
+			var columns = new HashSet<int>();
+
+			for (var i = 0; i < matrix.Length; i++)
+			{
+				for (var j = 0; j < matrix[0].Length; j++)
+				{
+					if (matrix[i][j] == 0)
+					{
+						rows.Add(i);
+						columns.Add(j);
+					}
+				}
+			}
+
+			for (var i = 0; i < matrix.Length; i++)
+			{
+				for (var j = 0; j < matrix[0].Length; j++)
+				{
+					if (rows.Contains(i) || columns.Contains(j))
+					{
+						matrix[i][j] = 0;
+					}
+				}
+			}
+		}
+	}
+}
