ddc648c71464f71464380c4ef3b3236e57a75df5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:14 +0300

    1721

--- LeetCode/Algorithms/1721.cs
+++ LeetCode/Algorithms/1721.cs
@@ -1,45 +1,44 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _1721
 {
-	// MEDIUM
-	internal class _1721
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
 		}
+	}
 
-		public static ListNode SwapNodes(ListNode head, int k)
+	public static ListNode SwapNodes(ListNode head, int k)
+	{
+		var frontNode = default(ListNode);
+		var endNode = default(ListNode);
+
+		var length = 0;
+		var currentNode = head;
+		while (currentNode != null)
 		{
-			ListNode frontNode = null;
-			ListNode endNode = null;
-
-			var length = 0;
-			var currentNode = head;
-			while (currentNode != null)
+			length++;
+			if (endNode != null)
 			{
-				length++;
-				if (endNode != null)
-				{
-					endNode = endNode.next;
-				}
-
-				if (length == k)
-				{
-					frontNode = currentNode;
-					endNode = head;
-				}
-				currentNode = currentNode.next;
+				endNode = endNode.next;
 			}
 
-			(frontNode.val, endNode.val) = (endNode.val, frontNode.val);
-
-			return head;
+			if (length == k)
+			{
+				frontNode = currentNode;
+				endNode = head;
+			}
+			currentNode = currentNode.next;
 		}
+
+		(frontNode.val, endNode.val) = (endNode.val, frontNode.val);
+
+		return head;
 	}
 }
