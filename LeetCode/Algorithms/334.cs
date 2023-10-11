8859761def07614aab306ebc195bd5a1c4526a67
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:24 +0300

    334

---
+++ LeetCode/Algorithms/334.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _334
+{
+	public static bool IncreasingTriplet(int[] nums)
+	{
+		var first = int.MaxValue;
+		var second = int.MaxValue;
+		foreach (var num in nums)
+		{
+			if (num <= first)
+			{
+				first = num;
+			}
+			else if (num <= second)
+			{
+				second = num;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
