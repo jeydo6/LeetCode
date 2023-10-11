e75731e765aaca97135febdb9ee6a3fe1d9e6529
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:59 +0300

    53

---
+++ LeetCode/Algorithms/53.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// EASY
+	internal class _53
+	{
+		public static int MaxSubArray(int[] nums)
+		{
+			var dp = new int[nums.Length];
+			dp[0] = nums[0];
+
+			var max = dp[0];
+			for (int i = 1; i < nums.Length; i++)
+			{
+				dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+				max = Math.Max(max, dp[i]);
+			}
+			return max;
+		}
+	}
+}
