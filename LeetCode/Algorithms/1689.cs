98bbb501cd851742cbccd1cfb714e2a6bdae6b71
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:19:48 +0300

    1689

---
+++ LeetCode/Algorithms/1689.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _1689
+	{
+		public static int MinPartitions(string n)
+		{
+			var max = 0;
+			for (int i = 0; i < n.Length; i++)
+			{
+				max = Math.Max(max, n[i] - '0');
+			}
+			return max;
+		}
+	}
+}
