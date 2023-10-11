0f0bfa10134ac2d13f245d6ca215858067fdcf15
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:12:23 +0300

    1827

---
+++ LeetCode/Algorithms/1827.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.Algorithms
+{
+	class _1827
+	{
+		public static int MinOperations(int[] numbers)
+		{
+			var result = 0;
+			for (var i = 1; i < numbers.Length; i++)
+			{
+				var temp = numbers[i];
+				numbers[i] = Math.Max(numbers[i - 1] + 1, numbers[i]);
+				result += numbers[i] - temp;
+			}
+			return result;
+		}
+	}
+}
