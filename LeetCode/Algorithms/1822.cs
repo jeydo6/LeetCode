3629dc454f49de2ef4b570fe5d5a3c8d6e968e7b
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:25:04 +0300

    1822

---
+++ LeetCode/Algorithms/1822.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1822
+{
+	public static int ArraySign(int[] nums)
+	{
+		var result = 1;
+		for (var i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] == 0)
+			{
+				return 0;
+			}
+			else if (nums[i] < 0)
+			{
+				result *= -1;
+			}
+		}
+
+		return result;
+	}
+}
