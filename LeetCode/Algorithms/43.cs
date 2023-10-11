f0bf069ae382feb2fa50901b7ee3ef06383d66e9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:14:46 +0300

    43

---
+++ LeetCode/Algorithms/43.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _43
+	{
+		public static string Multiply(string num1, string num2)
+		{
+			var positions = new int[num1.Length + num2.Length];
+			for (var i = num1.Length - 1; i >= 0; i--)
+			{
+				for (var j = num2.Length - 1; j >= 0; j--)
+				{
+					var m = (num1[i] - '0') * (num2[j] - '0');
+					var p1 = i + j;
+					var p2 = i + j + 1;
+
+					var sum = m + positions[p2];
+					positions[p1] += sum / 10;
+					positions[p2] = sum % 10;
+				}
+			}
+			var sb = new StringBuilder();
+			foreach (var p in positions)
+			{
+				if (!(sb.Length == 0 && p == 0))
+				{
+					sb.Append(p);
+				}
+			}
+			return sb.Length == 0 ? "0" : sb.ToString();
+		}
+	}
+}
