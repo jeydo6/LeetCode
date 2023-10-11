e396c10cd36823cb37d0895ccc5f129a6f2fd832
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:21:38 +0300

    523

---
+++ LeetCode/Algorithms/523.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _523
+{
+	public static bool CheckSubarraySum(int[] nums, int k)
+	{
+		var dict = new Dictionary<int, int>
+		{
+			[0] = 0
+		};
+
+		var sum = 0;
+		for (var i = 0; i < nums.Length; i++)
+		{
+			sum += nums[i];
+			if (!dict.ContainsKey(sum % k))
+			{
+				dict[sum % k] = i + 1;
+			}
+			else if (dict[sum % k] < i)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
