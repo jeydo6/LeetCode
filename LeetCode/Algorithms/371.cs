222d856cb7f999105e5b1d4b623b0d87d0761fa2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:18:45 +0300

    371

---
+++ LeetCode/Algorithms/371.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Algorithms
+{
+	// MEDIUM
+	internal class _371
+	{
+		public static int GetSum(int a, int b)
+		{
+			while (b != 0)
+			{
+				var answer = a ^ b;
+				var carry = (a & b) << 1;
+				a = answer;
+				b = carry;
+			}
+			return a;
+		}
+	}
+}
