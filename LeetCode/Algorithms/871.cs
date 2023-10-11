42fc1402640148ada068121b06a72c9b9e7aa7ec
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-11 23:20:37 +0300

    871

---
+++ LeetCode/Algorithms/871.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Algorithms
+{
+	// HARD
+	internal class _871
+	{
+		public static int MinRefuelStops(int target, int startFuel, int[][] stations)
+		{
+			var pqueue = new PriorityQueue<int, int>();
+
+			var result = 0;
+			var prev = 0;
+			for (var i = 0; i < stations.Length; i++)
+			{
+				var location = stations[i][0];
+				var capacity = stations[i][1];
+				startFuel -= location - prev;
+
+				while (pqueue.Count > 0 && startFuel < 0)
+				{
+					startFuel += pqueue.Dequeue();
+					result++;
+				}
+
+				if (startFuel < 0)
+				{
+					return -1;
+				}
+
+				pqueue.Enqueue(capacity, -capacity);
+				prev = location;
+			}
+
+			startFuel -= target - prev;
+			while (pqueue.Count > 0 && startFuel < 0)
+			{
+				startFuel += pqueue.Dequeue();
+				result++;
+			}
+
+			if (startFuel < 0)
+			{
+				return -1;
+			}
+
+			return result;
+		}
+	}
+}
