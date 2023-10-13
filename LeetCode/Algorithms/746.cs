47cece6cac81b0f3aee2bf526496e05f71c190cf
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-13 22:15:51 +0300

    746

--- LeetCode/Algorithms/746.cs
+++ LeetCode/Algorithms/746.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
-{
-	// EASY
-	internal class _746
-	{
-		public static int MinCostClimbingStairs(int[] cost)
-		{
-			var downOne = 0;
-			var downTwo = 0;
-			for (var i = 2; i < cost.Length + 1; i++)
-			{
-				(downOne, downTwo) = (Math.Min(downOne + cost[i - 1], downTwo + cost[i - 2]), downOne);
-			}
+namespace LeetCode.Algorithms;
 
-			return downOne;
+// EASY
+internal class _746
+{
+	public static int MinCostClimbingStairs(int[] cost)
+	{
+		var downOne = 0;
+		var downTwo = 0;
+		for (var i = 2; i < cost.Length + 1; i++)
+		{
+			(downOne, downTwo) = (Math.Min(downOne + cost[i - 1], downTwo + cost[i - 2]), downOne);
 		}
+
+		return downOne;
 	}
 }
