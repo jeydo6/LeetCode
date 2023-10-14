c34a97676b97ebba334b435f06dc5feacc4d004c
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-14 22:07:12 +0300

    2742

---
+++ LeetCode/Algorithms/2742.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// HARD
+internal class _2742
+{
+	public static int PaintWalls(int[] cost, int[] time)
+	{
+		var n = cost.Length;
+		var dp = new int[n + 1];
+		var prevDp = new int[n + 1];
+		Array.Fill(prevDp, (int)1e9);
+		prevDp[0] = 0;
+
+		for (var i = n - 1; i >= 0; i--)
+		{
+			dp = new int[n + 1];
+			for (var remain = 1; remain <= n; remain++)
+			{
+				var paint = cost[i] + prevDp[Math.Max(0, remain - 1 - time[i])];
+				var dontPaint = prevDp[remain];
+				dp[remain] = Math.Min(paint, dontPaint);
+			}
+
+			prevDp = dp;
+		}
+
+		return dp[n];
+	}
+}
