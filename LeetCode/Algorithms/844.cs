bb7aa7734a505ddcb61d0013bea38d04593a4bf8
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2023-10-19 22:13:52 +0300

    844

--- LeetCode/Algorithms/844.cs
+++ LeetCode/Algorithms/844.cs
@@ -1,67 +1,66 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _844
 {
-	// EASY
-	internal class _844
+	public static bool BackspaceCompare(string s, string t)
 	{
-		public static bool BackspaceCompare(string s, string t)
+		var skipS = 0;
+		var skipT = 0;
+
+		var i = s.Length - 1;
+		var j = t.Length - 1;
+		while (i >= 0 || j >= 0)
 		{
-			var skipS = 0;
-			var skipT = 0;
-
-			var i = s.Length - 1;
-			var j = t.Length - 1;
-			while (i >= 0 || j >= 0)
+			while (i >= 0)
 			{
-				while (i >= 0)
+				if (s[i] == '#')
 				{
-					if (s[i] == '#')
-					{
-						skipS++;
-						i--;
-					}
-					else if (skipS > 0)
-					{
-						skipS--;
-						i--;
-					}
-					else
-					{
-						break;
-					}
+					skipS++;
+					i--;
 				}
-
-				while (j >= 0)
+				else if (skipS > 0)
 				{
-					if (t[i] == '#')
-					{
-						skipT++;
-						j--;
-					}
-					else if (skipT > 0)
-					{
-						skipT--;
-						j--;
-					}
-					else
-					{
-						break;
-					}
+					skipS--;
+					i--;
 				}
-
-				if (i >= 0 && j >= 0 && s[i] != t[j])
+				else
 				{
-					return false;
+					break;
 				}
-
-				if ((i >= 0) != (j >= 0))
-				{
-					return false;
-				}
-
-				i--;
-				j--;
 			}
-			return true;
+
+			while (j >= 0)
+			{
+				if (t[j] == '#')
+				{
+					skipT++;
+					j--;
+				}
+				else if (skipT > 0)
+				{
+					skipT--;
+					j--;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			if (i >= 0 && j >= 0 && s[i] != t[j])
+			{
+				return false;
+			}
+
+			if ((i >= 0) != (j >= 0))
+			{
+				return false;
+			}
+
+			i--;
+			j--;
 		}
+		return true;
 	}
 }
