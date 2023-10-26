e2340aff427a3febec0bd2511f500cc424285bb8
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-10-26 21:59:43 +0300

    823

--- LeetCode/Algorithms/823.cs
+++ LeetCode/Algorithms/823.cs
@@ -1,48 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _823
 {
-	// MEDIUM
-	internal class _823
+	private const int Modulo = 1000000007;
+
+	public static int NumFactoredBinaryTrees(int[] arr)
 	{
-		private const int Modulo = 1000000007;
+		Array.Sort(arr);
 
-		public static int NumFactoredBinaryTrees(int[] arr)
+		var dict = new Dictionary<int, int>();
+		for (var i = 0; i < arr.Length; i++)
 		{
-			Array.Sort(arr);
+			dict[arr[i]] = i;
+		}
 
-			var dict = new Dictionary<int, int>();
-			for (var i = 0; i < arr.Length; i++)
+		var dp = new long[arr.Length];
+		Array.Fill(dp, 1);
+
+		for (var i = 0; i < dp.Length; i++)
+		{
+			for (var j = 0; j < i; j++)
 			{
-				dict[arr[i]] = i;
-			}
-
-			var dp = new long[arr.Length];
-			Array.Fill(dp, 1);
-
-			for (var i = 0; i < dp.Length; i++)
-			{
-				for (var j = 0; j < i; j++)
+				if (arr[i] % arr[j] == 0)
 				{
-					if (arr[i] % arr[j] == 0)
+					var right = arr[i] / arr[j];
+					if (dict.ContainsKey(right))
 					{
-						var right = arr[i] / arr[j];
-						if (dict.ContainsKey(right))
-						{
-							dp[i] = (dp[i] + dp[j] * dp[dict[right]]) % Modulo;
-						}
+						dp[i] = (dp[i] + dp[j] * dp[dict[right]]) % Modulo;
 					}
 				}
 			}
-
-			var result = 0L;
-			for (var i = 0; i < dp.Length; i++)
-			{
-				result += dp[i];
-			}
-
-			return (int)(result % Modulo);
 		}
+
+		var result = 0L;
+		for (var i = 0; i < dp.Length; i++)
+		{
+			result += dp[i];
+		}
+
+		return (int)(result % Modulo);
 	}
 }
