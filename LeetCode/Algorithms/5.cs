845673ea785c43a132105c632847df4a6e413a25
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-10-27 22:03:46 +0300

    5

--- LeetCode/Algorithms/5.cs
+++ LeetCode/Algorithms/5.cs
@@ -1,32 +1,33 @@
-﻿namespace LeetCode.Algorithms
+﻿using System;
+
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal class _5
 {
-	// MEDIUM
-	internal class _5
+	public static string LongestPalindrome(string s)
 	{
-		public static string LongestPalindrome(string s)
+		var n = s.Length;
+		var dp = new bool[n][];
+		for (var i = 0; i < n; i++)
 		{
-			var n = s.Length;
-			var dp = new bool[n][];
-			for (var i = 0; i < n; i++)
-			{
-				dp[i] = new bool[n];
-			}
+			dp[i] = new bool[n];
+		}
 
-			string result = null;
-			for (var i = n - 1; i >= 0; i--)
+		var span = s.AsSpan();
+		var result = default(ReadOnlySpan<char>);
+		for (var i = n - 1; i >= 0; i--)
+		{
+			for (var j = i; j < n; j++)
 			{
-				for (var j = i; j < n; j++)
+				dp[i][j] = span[i] == span[j] && (j - i < 3 || dp[i + 1][j - 1]);
+
+				if (dp[i][j] && (result == null || j - i + 1 > result.Length))
 				{
-					dp[i][j] = s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1]);
-
-					if (dp[i][j] && (result == null || j - i + 1 > result.Length))
-					{
-						result = s[i..(j + 1)];
-					}
+					result = span.Slice(i, j - i + 1);
 				}
 			}
-			return result;
 		}
+		return new string(result);
 	}
 }
-
