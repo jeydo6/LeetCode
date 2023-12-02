13214a747ccc1b3ef83854018e4476a5aabf7246
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-12-02 20:06:56 +0100

    1160

--- LeetCode/Algorithms/1160.cs
+++ LeetCode/Algorithms/1160.cs
@@ -1,39 +1,39 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal class _1160
 {
-	internal class _1160
+	public static int CountCharacters(string[] words, string chars)
 	{
-		public static int CountCharacters(string[] words, string chars)
+		var result = 0;
+
+		var total = new int[26];
+		foreach (var ch in chars)
 		{
-			var result = 0;
-
-			var total = new int[26];
-			foreach (var ch in chars)
-			{
-				total[ch - 'a']++;
-			}
-
-			foreach (var w in words)
-			{
-				var isValid = true;
-
-				var local = new int[26];
-				foreach (var ch in w)
-				{
-					local[ch - 'a']++;
-					if (local[ch - 'a'] > total[ch - 'a'])
-					{
-						isValid = false;
-						break;
-					}
-				}
-
-				if (isValid)
-				{
-					result += w.Length;
-				}
-			}
-
-			return result;
+			total[ch - 'a']++;
 		}
+
+		foreach (var w in words)
+		{
+			var isValid = true;
+
+			var local = new int[26];
+			foreach (var ch in w)
+			{
+				local[ch - 'a']++;
+				if (local[ch - 'a'] > total[ch - 'a'])
+				{
+					isValid = false;
+					break;
+				}
+			}
+
+			if (isValid)
+			{
+				result += w.Length;
+			}
+		}
+
+		return result;
 	}
 }
