6b5da57bde99f7e37c545be0811d098553fa1aac
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-12-15 22:28:16 +0100

    1436

--- LeetCode/Algorithms/1436.cs
+++ LeetCode/Algorithms/1436.cs
@@ -1,66 +1,26 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal sealed class _1436
 {
-	class _1436
+	public static string DestCity(IList<IList<string>> paths)
 	{
-		//public static string DestCity(IList<IList<string>> paths)
-		//{
-		//	//var dict = new Dictionary<string, IList<string>>();
-		//	//foreach (var path in paths)
-		//	//{
-		//	//	if (dict.ContainsKey(path[0]))
-		//	//	{
-		//	//		dict[path[0]].Add(path[1]);
-		//	//	}
-		//	//	else
-		//	//	{
-		//	//		dict[path[0]] = new List<string>
-		//	//		{
-		//	//			path[1]
-		//	//		};
-		//	//	}
-		//	//}
-
-		//	//foreach (var key in dict.Keys)
-		//	//{
-
-		//	//}
-
-		//	//throw new Exception();
-
-		//	var departures = new HashSet<string>();
-		//	var arrivals = new HashSet<string>();
-		//	foreach (var path in paths)
-		//	{
-		//		departures.Add(path[0]);
-		//		arrivals.Add(path[1]);
-		//	}
-
-		//	arrivals.ExceptWith(departures);
-		//	var enumerator = arrivals.GetEnumerator();
-		//	enumerator.MoveNext();
-
-		//	return enumerator.Current;
-		//}
-
-		public static string DestCity(IList<IList<string>> paths)
+		var dict = new Dictionary<string, string>();
+		foreach (var path in paths)
 		{
-			var dict = new Dictionary<string, string>();
-			foreach (var path in paths)
-			{
-				dict[path[0]] = path[1];
-			}
-
-			foreach (var value in dict.Values)
-			{
-				if (!dict.ContainsKey(value))
-				{
-					return value;
-				}
-			}
-
-			return "";
+			dict[path[0]] = path[1];
 		}
+
+		foreach (var value in dict.Values)
+		{
+			if (!dict.ContainsKey(value))
+			{
+				return value;
+			}
+		}
+
+		return string.Empty;
 	}
 }
