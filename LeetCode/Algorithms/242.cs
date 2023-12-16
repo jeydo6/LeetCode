7e732125c125f3c73e88712dd4192e19f703fb72
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-12-16 20:03:05 +0100

    242

--- LeetCode/Algorithms/242.cs
+++ LeetCode/Algorithms/242.cs
@@ -1,28 +1,27 @@
-﻿namespace LeetCode.Algorithms
-{
-	// EASY
-	internal class _242
-	{
-		public static bool IsAnagram(string s, string t)
-		{
-			var alphabet = new int[26];
-			for (var i = 0; i < s.Length; i++)
-			{
-				alphabet[s[i] - 'a']++;
-			}
-			for (var i = 0; i < t.Length; i++)
-			{
-				alphabet[t[i] - 'a']--;
-			}
-			for (var i = 0; i < alphabet.Length; i++)
-			{
-				if (alphabet[i] != 0)
-				{
-					return false;
-				}
-			}
+﻿namespace LeetCode.Algorithms;
 
-			return true;
+// EASY
+internal class _242
+{
+	public static bool IsAnagram(string s, string t)
+	{
+		var alphabet = new int[26];
+		for (var i = 0; i < s.Length; i++)
+		{
+			alphabet[s[i] - 'a']++;
 		}
+		for (var i = 0; i < t.Length; i++)
+		{
+			alphabet[t[i] - 'a']--;
+		}
+		for (var i = 0; i < alphabet.Length; i++)
+		{
+			if (alphabet[i] != 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
