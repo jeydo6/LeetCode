2cadc50be1eda6aa954da6cdf6d0e5761f58ade2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-12-18 20:09:50 +0100

    1913

--- LeetCode/Algorithms/1913.cs
+++ LeetCode/Algorithms/1913.cs
@@ -5,9 +5,9 @@ namespace LeetCode.Algorithms;
 // EASY
 internal sealed class _1913
 {
-	public static int MaxProductDifference(int[] numbers)
+	public static int MaxProductDifference(int[] nums)
 	{
-		Array.Sort(numbers);
-		return numbers[^2] * numbers[^1] - numbers[0] * numbers[1];
+		Array.Sort(nums);
+		return nums[^2] * nums[^1] - nums[0] * nums[1];
 	}
 }
