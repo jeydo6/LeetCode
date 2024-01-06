2bd74f2985f054d1b0e613575f02cb7e776e8e72
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-01-06 21:00:16 +0100

    1235

--- LeetCode/Algorithms/1235.cs
+++ LeetCode/Algorithms/1235.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// HARD
+internal sealed class _1235
 {
-	// HARD
-	internal class _1235
+	public static int JobScheduling(int[] startTime, int[] endTime, int[] profit)
 	{
-		public static int JobScheduling(int[] startTime, int[] endTime, int[] profit)
+		var keys = new List<int> { 0 };
+		var values = new List<int> { 0 };
+
+		var jobs = new int[startTime.Length][];
+		for (var i = 0; i < startTime.Length; i++)
 		{
-			var keys = new List<int> { 0 };
-			var values = new List<int> { 0 };
-
-			var jobs = new int[startTime.Length][];
-			for (var i = 0; i < startTime.Length; i++)
-			{
-				jobs[i] = new int[3] { startTime[i], endTime[i], profit[i] };
-			}
-
-			Array.Sort(jobs, (a, b) => a[1] - b[1]);
-
-			foreach (var job in jobs)
-			{
-				var prevKey = keys.BinarySearch(job[0] + 1);
-				if (prevKey < 0)
-				{
-					prevKey = ~prevKey;
-				}
-
-				prevKey--;
-
-				var currentValue = values[prevKey] + job[2];
-				if (currentValue <= values[^1])
-				{
-					continue;
-				}
-
-				keys.Add(job[1]);
-				values.Add(currentValue);
-			}
-
-			return values[^1];
+			jobs[i] = new int[3] { startTime[i], endTime[i], profit[i] };
 		}
+
+		Array.Sort(jobs, (a, b) => a[1] - b[1]);
+
+		foreach (var job in jobs)
+		{
+			var prevKey = keys.BinarySearch(job[0] + 1);
+			if (prevKey < 0)
+			{
+				prevKey = ~prevKey;
+			}
+
+			prevKey--;
+
+			var currentValue = values[prevKey] + job[2];
+			if (currentValue <= values[^1])
+			{
+				continue;
+			}
+
+			keys.Add(job[1]);
+			values.Add(currentValue);
+		}
+
+		return values[^1];
 	}
 }
