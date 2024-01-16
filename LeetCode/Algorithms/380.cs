a4bfd523f61d098940adfb40fefcd676687041a2
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-01-16 20:13:32 +0100

    380

--- LeetCode/Algorithms/380.cs
+++ LeetCode/Algorithms/380.cs
@@ -6,35 +6,12 @@ namespace LeetCode.Algorithms;
 // MEDIUM
 internal class _380
 {
-	public static void GetResult()
-	{
-		var randomizedSet = new RandomizedSet();
-		randomizedSet.Insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
-		randomizedSet.Remove(2); // Returns false as 2 does not exist in the set.
-		randomizedSet.Insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
-		randomizedSet.GetRandom(); // getRandom() should return either 1 or 2 randomly.
-		randomizedSet.Remove(1); // Removes 1 from the set, returns true. Set now contains [2].
-		randomizedSet.Insert(2); // 2 was already in the set, so return false.
-		randomizedSet.GetRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
-	}
-
 	public class RandomizedSet
 	{
-		private static readonly Random _random;
+		private static readonly Random _random = new Random();
 
-		private readonly IList<int> _list;
-		private readonly IDictionary<int, int> _dict;
-
-		static RandomizedSet()
-		{
-			_random = new Random();
-		}
-
-		public RandomizedSet()
-		{
-			_list = new List<int>();
-			_dict = new Dictionary<int, int>();
-		}
+		private readonly IList<int> _list = new List<int>();
+		private readonly IDictionary<int, int> _dict = new Dictionary<int, int>();
 
 		public bool Insert(int val)
 		{
