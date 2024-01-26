38d499c224e0ae5e4bc7b1af01c384bcd67bfdf5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-01-26 19:56:16 +0100

    576

--- LeetCode/Algorithms/576.cs
+++ LeetCode/Algorithms/576.cs
@@ -1,55 +1,54 @@
-﻿namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _576
-	{
-		private const int Modulo = 1000000000 + 7;
+﻿namespace LeetCode.Algorithms;
 
-		public static int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+// MEDIUM
+internal sealed class _576
+{
+	private const int Modulo = 1000000000 + 7;
+
+	public static int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+	{
+		var dp = new int[m][];
+		for (var i = 0; i < m; i++)
 		{
-			var dp = new int[m][];
+			dp[i] = new int[n];
+		}
+		dp[startRow][startColumn] = 1;
+
+		var count = 0;
+		for (var moves = 1; moves <= maxMove; moves++)
+		{
+			var temp = new int[m][];
 			for (var i = 0; i < m; i++)
 			{
-				dp[i] = new int[n];
-			}
-			dp[startRow][startColumn] = 1;
-
-			var count = 0;
-			for (var moves = 1; moves <= maxMove; moves++)
-			{
-				var temp = new int[m][];
-				for (var i = 0; i < m; i++)
+				temp[i] = new int[n];
+				for (var j = 0; j < n; j++)
 				{
-					temp[i] = new int[n];
-					for (var j = 0; j < n; j++)
+					if (i == m - 1)
 					{
-						if (i == m - 1)
-						{
-							count = (count + dp[i][j]) % Modulo;
-						}
-						if (j == n - 1)
-						{
-							count = (count + dp[i][j]) % Modulo;
-						}
-						if (i == 0)
-						{
-							count = (count + dp[i][j]) % Modulo;
-						}
-
-						if (j == 0)
-						{
-							count = (count + dp[i][j]) % Modulo;
-						}
-
-						temp[i][j] = (
-							((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % Modulo +
-							((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % Modulo
-						) % Modulo;
+						count = (count + dp[i][j]) % Modulo;
 					}
+					if (j == n - 1)
+					{
+						count = (count + dp[i][j]) % Modulo;
+					}
+					if (i == 0)
+					{
+						count = (count + dp[i][j]) % Modulo;
+					}
+
+					if (j == 0)
+					{
+						count = (count + dp[i][j]) % Modulo;
+					}
+
+					temp[i][j] = (
+						((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % Modulo +
+						((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % Modulo
+					) % Modulo;
 				}
-				dp = temp;
 			}
-			return count;
+			dp = temp;
 		}
+		return count;
 	}
 }
