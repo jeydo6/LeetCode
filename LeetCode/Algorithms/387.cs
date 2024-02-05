77895d0b287c235652298f4087410d6823887191
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-05 17:55:40 +0100

    387

--- LeetCode/Algorithms/387.cs
+++ LeetCode/Algorithms/387.cs
@@ -1,25 +1,24 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal sealed class _387
 {
-	// EASY
-	internal class _387
+	public static int FirstUniqChar(string s)
 	{
-		public static int FirstUniqChar(string s)
+		var counts = new int[26];
+		for (var i = 0; i < s.Length; i++)
 		{
-			var counts = new int[26];
-			for (var i = 0; i < s.Length; i++)
-			{
-				counts[s[i] - 'a']++;
-			}
-
-			for (var i = 0; i < s.Length; i++)
-			{
-				if (counts[s[i] - 'a'] == 1)
-				{
-					return i;
-				}
-			}
-
-			return -1;
+			counts[s[i] - 'a']++;
 		}
+
+		for (var i = 0; i < s.Length; i++)
+		{
+			if (counts[s[i] - 'a'] == 1)
+			{
+				return i;
+			}
+		}
+
+		return -1;
 	}
 }
