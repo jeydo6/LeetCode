746599309d8c6e8958c8088bec65d07c8f2d94cd
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-09 17:56:28 +0100

    368

--- LeetCode/Algorithms/368.cs
+++ LeetCode/Algorithms/368.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal sealed class _368
 {
-	// MEDIUM
-	internal class _368
+	public static IList<int> LargestDivisibleSubset(int[] nums)
 	{
-		public static IList<int> LargestDivisibleSubset(int[] nums)
+		Array.Sort(nums);
+
+		var lengths = new int[nums.Length];
+		var prevs = new int[nums.Length];
+
+		var maxLength = 0;
+		var index = -1;
+		for (var i = 0; i < nums.Length; i++)
 		{
-			Array.Sort(nums);
-
-			var lengths = new int[nums.Length];
-			var prevs = new int[nums.Length];
-
-			var maxLength = 0;
-			var index = -1;
-			for (var i = 0; i < nums.Length; i++)
+			lengths[i] = 1;
+			prevs[i] = -1;
+			for (var j = i - 1; j >= 0; j--)
 			{
-				lengths[i] = 1;
-				prevs[i] = -1;
-				for (var j = i - 1; j >= 0; j--)
+				if (nums[i] % nums[j] == 0 && lengths[j] + 1 > lengths[i])
 				{
-					if (nums[i] % nums[j] == 0 && lengths[j] + 1 > lengths[i])
-					{
-						lengths[i] = lengths[j] + 1;
-						prevs[i] = j;
-					}
-				}
-				if (lengths[i] > maxLength)
-				{
-					maxLength = lengths[i];
-					index = i;
+					lengths[i] = lengths[j] + 1;
+					prevs[i] = j;
 				}
 			}
-			var result = new List<int>();
-			while (index != -1)
+			if (lengths[i] > maxLength)
 			{
-				result.Add(nums[index]);
-				index = prevs[index];
+				maxLength = lengths[i];
+				index = i;
 			}
-			return result;
 		}
+		var result = new List<int>();
+		while (index != -1)
+		{
+			result.Add(nums[index]);
+			index = prevs[index];
+		}
+		return result;
 	}
 }
