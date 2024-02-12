8cb3a86fdf784162bab7f550a428a9855f620144
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-12 17:57:07 +0100

    169

--- LeetCode/Algorithms/169.cs
+++ LeetCode/Algorithms/169.cs
@@ -1,27 +1,26 @@
-﻿namespace LeetCode.Algorithms
-{
-	// EASY
-	internal class _169
-	{
-		public static int MajorityElement(int[] nums)
-		{
-			var major = nums[0];
-			var count = 1;
-			for (var i = 1; i < nums.Length; i++)
-			{
-				if (count == 0)
-				{
-					count++;
-					major = nums[i];
-				}
-				else if (major == nums[i])
-				{
-					count++;
-				}
-				else count--;
+﻿namespace LeetCode.Algorithms;
 
+// EASY
+internal sealed class _169
+{
+	public static int MajorityElement(int[] nums)
+	{
+		var major = nums[0];
+		var count = 1;
+		for (var i = 1; i < nums.Length; i++)
+		{
+			if (count == 0)
+			{
+				count++;
+				major = nums[i];
 			}
-			return major;
+			else if (major == nums[i])
+			{
+				count++;
+			}
+			else count--;
 		}
+
+		return major;
 	}
 }
