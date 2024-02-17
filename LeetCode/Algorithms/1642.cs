f0ebf50ae1f27c1675e62f539914575872baa58f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-17 19:16:24 +0100

    1642

--- LeetCode/Algorithms/1642.cs
+++ LeetCode/Algorithms/1642.cs
@@ -1,34 +1,33 @@
 ﻿using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal sealed class _1642
 {
-	// MEDIUM
-	internal class _1642
+	public static int FurthestBuilding(int[] heights, int bricks, int ladders)
 	{
-		public static int FurthestBuilding(int[] heights, int bricks, int ladders)
+		var ladderAllocations = new PriorityQueue<int, int>();
+		for (var i = 0; i < heights.Length - 1; i++)
 		{
-			var ladderAllocations = new PriorityQueue<int, int>();
-			for (var i = 0; i < heights.Length - 1; i++)
+			var climb = heights[i + 1] - heights[i];
+			if (climb <= 0)
 			{
-				var climb = heights[i + 1] - heights[i];
-				if (climb <= 0)
-				{
-					continue;
-				}
-
-				ladderAllocations.Enqueue(climb, climb);
-				if (ladderAllocations.Count <= ladders)
-				{
-					continue;
-				}
-
-				bricks -= ladderAllocations.Dequeue();
-				if (bricks < 0)
-				{
-					return i;
-				}
+				continue;
 			}
-			return heights.Length - 1;
+
+			ladderAllocations.Enqueue(climb, climb);
+			if (ladderAllocations.Count <= ladders)
+			{
+				continue;
+			}
+
+			bricks -= ladderAllocations.Dequeue();
+			if (bricks < 0)
+			{
+				return i;
+			}
 		}
+		return heights.Length - 1;
 	}
 }
