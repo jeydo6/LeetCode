ec0a81676740506fb19b536d5e513515bc122eb4
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-18 17:51:47 +0100

    1002

--- LeetCode/Algorithms/1002.cs
+++ LeetCode/Algorithms/1002.cs
@@ -1,43 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// EASY
+internal sealed class _1002
 {
-	internal class _1002
+	public static IList<string> CommonChars(string[] words)
 	{
-		public static IList<string> CommonChars(string[] words)
+		var alphabetLength = 26;
+
+		var totalCount = new int[alphabetLength];
+		for (var i = 0; i < alphabetLength; i++)
 		{
-			var alphabetLength = 26;
-
-			var totalCount = new int[alphabetLength];
-			for (var i = 0; i < alphabetLength; i++)
-			{
-				totalCount[i] = int.MaxValue;
-			}
-
-			foreach (string word in words)
-			{
-				var counts = new byte[alphabetLength];
-				foreach (char c in word)
-				{
-					counts[c - 'a']++;
-				}
-
-				for (var i = 0; i < alphabetLength; i++)
-				{
-					totalCount[i] = Math.Min(counts[i], totalCount[i]);
-				}
-			}
-
-			var result = new List<string>();
-			for (var i = 0; i < alphabetLength; i++)
-			{
-				while (totalCount[i]-- > 0)
-				{
-					result.Add(new string((char)(i + 'a'), 1));
-				}
-			}
-			return result;
+			totalCount[i] = int.MaxValue;
 		}
+
+		foreach (string word in words)
+		{
+			var counts = new byte[alphabetLength];
+			foreach (char c in word)
+			{
+				counts[c - 'a']++;
+			}
+
+			for (var i = 0; i < alphabetLength; i++)
+			{
+				totalCount[i] = Math.Min(counts[i], totalCount[i]);
+			}
+		}
+
+		var result = new List<string>();
+		for (var i = 0; i < alphabetLength; i++)
+		{
+			while (totalCount[i]-- > 0)
+			{
+				result.Add(new string((char)(i + 'a'), 1));
+			}
+		}
+		return result;
 	}
 }
