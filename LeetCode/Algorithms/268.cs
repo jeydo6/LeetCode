8abc6a3ba627491e9ccd332b4ae3b2d09d7f15d5
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-20 18:33:22 +0100

    268

--- LeetCode/Algorithms/268.cs
+++ LeetCode/Algorithms/268.cs
@@ -1,16 +1,16 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal sealed class _268
 {
-	// EASY
-	internal class _268
+	public static int MissingNumber(int[] nums)
 	{
-		public static int MissingNumber(int[] nums)
+		var result = 0;
+		for (var i = 0; i < nums.Length; i++)
 		{
-			var result = 0;
-			for (var i = 0; i < nums.Length; i++)
-			{
-				result += i + 1 - nums[i];
-			}
-			return result;
+			result += i + 1 - nums[i];
 		}
+
+		return result;
 	}
 }
