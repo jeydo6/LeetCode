54f11986e75ba33e4148e23cd2ce4a58ca04c8e9
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-02-27 19:33:17 +0100

    543

--- LeetCode/Algorithms/543.cs
+++ LeetCode/Algorithms/543.cs
@@ -1,44 +1,43 @@
 ﻿using System;
 
-namespace LeetCode.Algorithms
+namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal sealed class _543
 {
-	// MEDIUM
-	internal class _543
+	public class TreeNode
 	{
-		public class TreeNode
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 		{
-			public int val;
-			public TreeNode left;
-			public TreeNode right;
-			public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-			{
-				this.val = val;
-				this.left = left;
-				this.right = right;
-			}
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	private static int _diameter;
+
+	public static int DiameterOfBinaryTree(TreeNode root)
+	{
+		MaxHeight(root);
+
+		return _diameter;
+	}
+
+	private static int MaxHeight(TreeNode root)
+	{
+		if (root == null)
+		{
+			return 0;
 		}
 
-		private static int _diameter = 0;
+		var left = MaxHeight(root.left);
+		var right = MaxHeight(root.right);
+		_diameter = Math.Max(_diameter, left + right);
 
-		public static int DiameterOfBinaryTree(TreeNode root)
-		{
-			MaxHeight(root);
-
-			return _diameter;
-		}
-
-		private static int MaxHeight(TreeNode root)
-		{
-			if (root == null)
-			{
-				return 0;
-			}
-
-			var left = MaxHeight(root.left);
-			var right = MaxHeight(root.right);
-			_diameter = Math.Max(_diameter, left + right);
-
-			return Math.Max(left, right) + 1;
-		}
+		return Math.Max(left, right) + 1;
 	}
 }
