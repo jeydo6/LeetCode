83a559b5baf8de5d61391915d1302c44b07c7c02
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-03-15 16:00:01 +0100

    238

--- LeetCode/Algorithms/238.cs
+++ LeetCode/Algorithms/238.cs
@@ -1,24 +1,23 @@
-﻿namespace LeetCode.Algorithms
-{
-	// MEDIUM
-	internal class _238
-	{
-		public static int[] ProductExceptSelf(int[] nums)
-		{
-			var result = new int[nums.Length];
-			result[0] = 1;
-			for (var i = 1; i < nums.Length; i++)
-			{
-				result[i] = result[i - 1] * nums[i - 1];
-			}
+﻿namespace LeetCode.Algorithms;
 
-			var right = 1;
-			for (var i = nums.Length - 1; i >= 0; i--)
-			{
-				result[i] *= right;
-				right *= nums[i];
-			}
-			return result;
+// MEDIUM
+internal class _238
+{
+	public static int[] ProductExceptSelf(int[] nums)
+	{
+		var result = new int[nums.Length];
+		result[0] = 1;
+		for (var i = 1; i < nums.Length; i++)
+		{
+			result[i] = result[i - 1] * nums[i - 1];
 		}
+
+		var right = 1;
+		for (var i = nums.Length - 1; i >= 0; i--)
+		{
+			result[i] *= right;
+			right *= nums[i];
+		}
+		return result;
 	}
 }
