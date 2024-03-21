219fc8b21b037d96cff95aab502a45c16f4b98b0
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-03-21 19:35:00 +0100

    206

--- LeetCode/Algorithms/206.cs
+++ LeetCode/Algorithms/206.cs
@@ -1,30 +1,31 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// EASY
+internal sealed class _206
 {
-	// EASY
-	internal class _206
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode ReverseList(ListNode head)
+	{
+		var newHead = default(ListNode);
+		while (head != null)
+		{
+			var next = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = next;
 		}
 
-		public static ListNode ReverseList(ListNode head)
-		{
-			ListNode newHead = null;
-			while (head != null)
-			{
-				var next = head.next;
-				head.next = newHead;
-				newHead = head;
-				head = next;
-			}
-			return newHead;
-		}
+		return newHead;
 	}
 }
