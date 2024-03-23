a3fc58a52bea0541c9446672cedfb85e34ba650f
Vladimir Deryagin <Deryagin.Valdemar@yandex.ru>
2024-03-23 19:30:15 +0100

    143

--- LeetCode/Algorithms/143.cs
+++ LeetCode/Algorithms/143.cs
@@ -1,54 +1,53 @@
-﻿namespace LeetCode.Algorithms
+﻿namespace LeetCode.Algorithms;
+
+// MEDIUM
+internal sealed class _143
 {
-	// MEDIUM
-	internal class _143
+	public class ListNode
 	{
-		public class ListNode
+		public int val;
+		public ListNode next;
+		public ListNode(int val = 0, ListNode next = null)
 		{
-			public int val;
-			public ListNode next;
-			public ListNode(int val = 0, ListNode next = null)
-			{
-				this.val = val;
-				this.next = next;
-			}
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void ReorderList(ListNode head)
+	{
+		if (head?.next == null)
+		{
+			return;
 		}
 
-		public static void ReorderList(ListNode head)
+		var p1 = head;
+		var p2 = head;
+		while (p2.next != null && p2.next.next != null)
 		{
-			if (head == null || head.next == null)
-			{
-				return;
-			}
+			p1 = p1.next;
+			p2 = p2.next.next;
+		}
 
-			var p1 = head;
-			var p2 = head;
-			while (p2.next != null && p2.next.next != null)
-			{
-				p1 = p1.next;
-				p2 = p2.next.next;
-			}
+		var preMiddle = p1;
+		var preCurrent = p1.next;
+		while (preCurrent.next != null)
+		{
+			var current = preCurrent.next;
+			preCurrent.next = current.next;
+			current.next = preMiddle.next;
+			preMiddle.next = current;
+		}
 
-			var preMiddle = p1;
-			var preCurrent = p1.next;
-			while (preCurrent.next != null)
-			{
-				var current = preCurrent.next;
-				preCurrent.next = current.next;
-				current.next = preMiddle.next;
-				preMiddle.next = current;
-			}
-
-			p1 = head;
+		p1 = head;
+		p2 = preMiddle.next;
+		while (p1 != preMiddle)
+		{
+			preMiddle.next = p2.next;
+			p2.next = p1.next;
+			p1.next = p2;
+			p1 = p2.next;
 			p2 = preMiddle.next;
-			while (p1 != preMiddle)
-			{
-				preMiddle.next = p2.next;
-				p2.next = p1.next;
-				p1.next = p2;
-				p1 = p2.next;
-				p2 = preMiddle.next;
-			}
 		}
 	}
 }
